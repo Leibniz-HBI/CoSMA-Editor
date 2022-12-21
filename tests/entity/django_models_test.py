8c79bf2ceb40c2bce0ddd1bf48132d890b3b1de3
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2022-12-21 09:47:40 +0100

    More general name for existing object exception.

--- tests/entity/django_models_test.py
+++ tests/entity/django_models_test.py
@@ -3,7 +3,7 @@ import pytest
 
 import tests.entity.common as c
 from cosmae.entity.models_django import Entity
-from cosmae.exception import EntityExistsException, EntityUpdatedException
+from cosmae.exception import DbObjectExistsException, EntityUpdatedException
 
 
 @pytest.fixture
@@ -70,7 +70,7 @@ def test_no_update_on_same(entity0):
 @pytest.mark.django_db
 def test_no_update_without_version(entity0):
     entity0.save()
-    with pytest.raises(EntityExistsException):
+    with pytest.raises(DbObjectExistsException):
         Entity.change_or_create(
             id_persistent=entity0.id_persistent,
             display_txt="new_txt",

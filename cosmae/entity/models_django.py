Leibniz-HBI/DP-REX

More general name for existing object exception.

vran/entity/models_django.py
@@ -6,7 +6,7 @@ from typing import Optional, Set
 from django.db import models
 from django.db.models.aggregates import Count, Max
 
-from vran.exception import EntityExistsException, TooManyFieldsException
+from vran.exception import DbObjectExistsException, TooManyFieldsException
 from vran.util.django import change_or_create_versioned
 
 
@@ -120,8 +120,8 @@ class Entity(models.Model):
                 time_edit=time_edit,
                 **kwargs,
             )
-        except EntityExistsException as exc:
-            raise EntityExistsException(display_txt) from exc
+        except DbObjectExistsException as exc:
+            raise DbObjectExistsException(display_txt) from exc
 
     @classmethod
     def get_most_recent_chunked(cls, offset, limit):

vran/util/django.py
@@ -4,7 +4,7 @@ from typing import Iterable, Optional
 from django.db.models import Model
 from django.db.transaction import atomic
 
-from vran.exception import EntityExistsException, EntityUpdatedException
+from vran.exception import DbObjectExistsException, EntityUpdatedException
 
 
 def save_many_atomic(models: Iterable[Model]):
@@ -33,7 +33,7 @@ def change_or_create_versioned(
     else:
         by_id = cls.objects.filter(id_persistent=id_persistent)
         if by_id:
-            raise EntityExistsException("UNKNOWN")
+            raise DbObjectExistsException("UNKNOWN")
         most_recent = None
     new = cls(
         id_persistent=id_persistent,

tests/entity/django_models_test.py
@@ -3,7 +3,7 @@ import pytest
 
 import tests.entity.common as c
 from vran.entity.models_django import Entity
-from vran.exception import EntityExistsException, EntityUpdatedException
+from vran.exception import DbObjectExistsException, EntityUpdatedException
 
 
 @pytest.fixture
@@ -70,7 +70,7 @@ def test_no_update_on_same(entity0):
 @pytest.mark.django_db
 def test_no_update_without_version(entity0):
     entity0.save()
-    with pytest.raises(EntityExistsException):
+    with pytest.raises(DbObjectExistsException):
         Entity.change_or_create(
             id_persistent=entity0.id_persistent,
             display_txt="new_txt",

Leibniz-HBI/CoSMA-Editor

cosmae/entity/models_django.py
@@ -6,7 +6,7 @@ from typing import Optional, Set
 from django.db import models
 from django.db.models.aggregates import Count, Max
 
-from cosmae.exception import EntityExistsException, TooManyFieldsException
+from cosmae.exception import DbObjectExistsException, TooManyFieldsException
 from cosmae.util.django import change_or_create_versioned
 
 
@@ -120,8 +120,8 @@ class Entity(models.Model):
                 time_edit=time_edit,
                 **kwargs,
             )
-        except EntityExistsException as exc:
-            raise EntityExistsException(display_txt) from exc
+        except DbObjectExistsException as exc:
+            raise DbObjectExistsException(display_txt) from exc
 
     @classmethod
     def get_most_recent_chunked(cls, offset, limit):

cosmae/util/django.py
@@ -4,7 +4,7 @@ from typing import Iterable, Optional
 from django.db.models import Model
 from django.db.transaction import atomic
 
-from cosmae.exception import EntityExistsException, EntityUpdatedException
+from cosmae.exception import DbObjectExistsException, EntityUpdatedException
 
 
 def save_many_atomic(models: Iterable[Model]):
@@ -33,7 +33,7 @@ def change_or_create_versioned(
     else:
         by_id = cls.objects.filter(id_persistent=id_persistent)
         if by_id:
-            raise EntityExistsException("UNKNOWN")
+            raise DbObjectExistsException("UNKNOWN")
         most_recent = None
     new = cls(
         id_persistent=id_persistent,

tests/entity/django_models_test.py
@@ -3,7 +3,7 @@ import pytest
 
 import tests.entity.common as c
 from cosmae.entity.models_django import Entity
-from cosmae.exception import EntityExistsException, EntityUpdatedException
+from cosmae.exception import DbObjectExistsException, EntityUpdatedException
 
 
 @pytest.fixture
@@ -70,7 +70,7 @@ def test_no_update_on_same(entity0):
 @pytest.mark.django_db
 def test_no_update_without_version(entity0):
     entity0.save()
-    with pytest.raises(EntityExistsException):
+    with pytest.raises(DbObjectExistsException):
         Entity.change_or_create(
             id_persistent=entity0.id_persistent,
             display_txt="new_txt",