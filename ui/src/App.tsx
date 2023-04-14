4c4a7d2103efae8050665daa3df90d3de8f4b6ad
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2023-04-14 15:27:20 +0200

    Add login and registration to frontend.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,18 +2,39 @@ import './App.css'
 import { PersonTable } from './person_natural/components'
 import '@glideapps/glide-data-grid/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { LoginProvider } from './user/components/provider'
+import { CosmaeConf } from './config'
+import { HeaderProps } from './user/hooks'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+
+function CosmaeHeader({ headerProps }: { headerProps: HeaderProps }) {
+    return (
+        <Row className="cosmae-header justify-content-between ms-0 me-0 ">
+            <Col></Col>
+            <Col md="auto">CoSMA-E</Col>
+            <Col>
+                {headerProps.logoutCallback && (
+                    <Button variant="link" onClick={headerProps.logoutCallback}>
+                        Logout
+                    </Button>
+                )}
+            </Col>
+        </Row>
+    )
+}
 
 function App() {
     return (
-        <div>
-            <div className="cosmae-container">
-                <div className="cosmae-header">CoSMA-E</div>
-                <div className="cosmae-body">
-                    <PersonTable />
-                </div>
-            </div>
-            <div id="portal" />
-        </div>
+        <Container
+            fluid
+            className=" min-vh-100 d-flex flex-column ps-0 pe-0 cosmae-container"
+        >
+            <LoginProvider
+                apiPath={CosmaeConf.get().api_base}
+                header={(headerProps) => <CosmaeHeader headerProps={headerProps} />}
+                body={() => <PersonTable />}
+            />
+        </Container>
     )
 }
 
