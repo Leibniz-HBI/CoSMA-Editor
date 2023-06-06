13af28c88128550913183df195cc33e80de247aa
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2023-06-06 16:28:39 +0200

    Add Contribution step for assigning new columns to existing ones.

---
+++ ui/src/util/components/misc.tsx
@@ -0,0 +1,9 @@
+export function CosmaeLoading() {
+    return (
+        <div className="cosmae-table-container-outer">
+            <div className="cosmae-table-container-inner">
+                <div className="shimmer"></div>
+            </div>
+        </div>
+    )
+}
