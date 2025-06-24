4b3721d7c0f6f9d39994548603996ac6ec424257
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2025-06-24 15:33:38 +0200

    Adapt scores in expexted test results to changed similarity for single pair contribution.

--- tests/contribution/entity/api/integration/post_similar_test.py
+++ tests/contribution/entity/api/integration/post_similar_test.py
@@ -246,7 +246,7 @@ def test_similar_entities_assigned_duplicate_no_match(
                                 "version": entities[3].id,
                                 "disabled": False,
                             },
-                            "similarity": 0.6153846153846154,
+                            "similarity": 0.19999999999999996,
                             "id_match_column_persistent_list": [],
                         },
                         {
@@ -282,7 +282,7 @@ def test_similar_entities_assigned_duplicate_no_match(
                             "version": entities[3].id,
                             "disabled": False,
                         },
-                        "similarity": 0.6153846153846154,
+                        "similarity": 0.19999999999999996,
                         "id_match_column_persistent_list": [],
                     },
                 }
