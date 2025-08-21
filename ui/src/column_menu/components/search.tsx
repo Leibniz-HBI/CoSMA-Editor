444d1065cc3ca0d931f0345137035f9e0c64f266
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2025-08-21 11:37:04 +0200

    Add variable tail elements to column search results.

--- ui/src/column_menu/components/search.tsx
+++ ui/src/column_menu/components/search.tsx
@@ -1,13 +1,15 @@
-import { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, ReactElement, useEffect, useState } from 'react'
 import { FormField } from '../../util/form'
 import { useAppDispatch, useAppSelector } from '../../hooks'
 import { AppDispatch } from '../../store'
 import { debounce } from 'debounce'
 import { searchColumnClear } from '../slice'
 import { selectSearchResultIdPersistentList } from '../selectors'
-import { ListGroup } from 'react-bootstrap'
-import { ColumnNamePathFromId } from './misc'
+import { Col, ListGroup, ProgressBar, Row } from 'react-bootstrap'
+import { ColumnNamePath } from './misc'
 import { searchColumnThunk } from '../thunks'
+import { useColumn } from '../hooks'
+import { Column } from '../state'
 
 const debouncedSearchDispatch = debounce(
     (searchTerm: string, upUntilDate: Date | undefined, dispatch: AppDispatch) => {
@@ -51,8 +53,10 @@ export function ColumnSearchField({
 }
 
 export function ColumnSearchResults({
+    mkTailElement,
     upUntilDate = undefined
 }: {
+    mkTailElement: (def: Column) => ReactElement
     upUntilDate: Date | undefined
 }) {
     const searchResultIdPersistentList = useAppSelector(
@@ -65,12 +69,38 @@ export function ColumnSearchResults({
         <ListGroup>
             {searchResultIdPersistentList.value?.map((id) => (
                 <ListGroup.Item key={id}>
-                    <ColumnNamePathFromId
+                    <ColumnSearchResultItem
                         idColumnPersistent={id}
                         upUntilTime={upUntilDate}
+                        mkTailElement={mkTailElement}
                     />
                 </ListGroup.Item>
             ))}
         </ListGroup>
+    )
+}
+export function ColumnSearchResultItem({
+    idColumnPersistent,
+    upUntilTime,
+    mkTailElement
+}: {
+    idColumnPersistent: string
+    upUntilTime: Date | undefined
+    mkTailElement: (def: Column) => ReactElement
+}) {
+    const column = useColumn(idColumnPersistent, upUntilTime)
+    if (column.isLoading) {
+        return <ProgressBar animated={true} />
+    }
+    if (column.value === undefined) {
+        return <span></span>
+    }
+    return (
+        <Row className="justify-content-between">
+            <Col>
+                <ColumnNamePath column={column.value} />
+            </Col>
+            <Col xs="auto">{mkTailElement(column.value)}</Col>
+        </Row>
     )
 }
