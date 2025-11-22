Leibniz-HBI/DP-REX

Store and Expose to API Error Messages for Contributions.

vran/migrations/0022_contributioncandidate_error_msg_and_more.py
@@ -1,0 +1,23 @@
+# Generated by Django 4.2.2 on 2023-10-13 10:34
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0021_ownershiprequest"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="contributioncandidate",
+            name="error_msg",
+            field=models.TextField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="contributioncandidate",
+            name="error_trace",
+            field=models.TextField(blank=True, null=True),
+        ),
+    ]

tests/contribution/entity/queue_test.py
@@ -1,4 +1,6 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+from unittest.mock import MagicMock, patch
+
 import pytest
 
 import tests.contribution.entity.common as c
@@ -6,6 +8,7 @@ import tests.entity.common as ce
 import tests.tag.common as ct
 import vran.contribution.entity.queue as q
 from vran.contribution.entity.models_django import EntityDuplicate
+from vran.contribution.models_django import ContributionCandidate
 from vran.entity.models_django import Entity
 from vran.tag.models_django import TagDefinition, TagInstance
 
@@ -302,3 +305,18 @@ def test_eliminate_duplicates(contribution_candidate, tag_instances, entity_matc
             "value": "bar",
         },
     ]
+
+
+def test_sets_error(contribution_candidate):
+    mock = MagicMock()
+    mock.side_effect = Exception("error")
+    with patch("vran.contribution.entity.queue.timestamp", mock):
+        q.eliminate_duplicates(contribution_candidate.id_persistent)
+    contribution_candidate = ContributionCandidate.by_id_persistent(
+        contribution_candidate.id_persistent, contribution_candidate.created_by
+    ).get()
+    assert contribution_candidate.state == ContributionCandidate.VALUES_EXTRACTED
+    assert (
+        contribution_candidate.error_msg == "Error during Entity Duplicate Elimination."
+    )
+    assert contribution_candidate.error_trace == "Exception: error"

tests/contribution/tag_definition/queue/create_test.py
@@ -79,3 +79,19 @@ def test_extracts_with_header(contribution_other):
         )
     )
     assert contribution_candidate.state == ContributionCandidate.COLUMNS_EXTRACTED
+
+
+def test_sets_error(contribution_other):
+    conf_mock = MagicMock
+    conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/does_not_exist"
+    with patch("vran.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_other.id_persistent)
+    contribution = ContributionCandidate.by_id_persistent(
+        contribution_other.id_persistent, contribution_other.created_by
+    ).get()
+    assert contribution.state == ContributionCandidate.UPLOADED
+    assert contribution.error_msg == "Error while extracting columns."
+    assert contribution.error_trace == (
+        "FileNotFoundError: [Errno 2] No such file or directory: "
+        "'tests/files/does_not_exist/DBOeS_Parlamentarier50.csv'"
+    )

tests/contribution/tag_definition/queue/ingest_test.py
@@ -191,3 +191,17 @@ def test_ingest_string(
         .state
         == ContributionCandidate.VALUES_EXTRACTED
     )
+
+
+def test_sets_error(party_contribution, party_tag_def):
+    mock = MagicMock()
+    mock.side_effect = Exception("error")
+    contribution = party_contribution.contribution_candidate
+    with patch("vran.contribution.tag_definition.queue.util.read_csv", mock):
+        ingest_values_from_csv(contribution.id_persistent)
+    contribution = ContributionCandidate.by_id_persistent(
+        contribution.id_persistent, contribution.created_by
+    ).get()
+    assert contribution.state == ContributionCandidate.COLUMNS_EXTRACTED
+    assert contribution.error_msg == "Error during ingestion of assigned tags."
+    assert contribution.error_trace == "Exception: error"

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0022_contributioncandidate_error_msg_and_more.py
@@ -1,0 +1,23 @@
+# Generated by Django 4.2.2 on 2023-10-13 10:34
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0021_ownershiprequest"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="contributioncandidate",
+            name="error_msg",
+            field=models.TextField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="contributioncandidate",
+            name="error_trace",
+            field=models.TextField(blank=True, null=True),
+        ),
+    ]

tests/contribution/entity/queue_test.py
@@ -1,4 +1,6 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+from unittest.mock import MagicMock, patch
+
 import pytest
 
 import tests.contribution.entity.common as c
@@ -6,6 +8,7 @@ import tests.entity.common as ce
 import tests.tag.common as ct
 import cosmae.contribution.entity.queue as q
 from cosmae.contribution.entity.models_django import EntityDuplicate
+from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.entity.models_django import Entity
 from cosmae.tag.models_django import TagDefinition, TagInstance
 
@@ -302,3 +305,18 @@ def test_eliminate_duplicates(contribution_candidate, tag_instances, entity_matc
             "value": "bar",
         },
     ]
+
+
+def test_sets_error(contribution_candidate):
+    mock = MagicMock()
+    mock.side_effect = Exception("error")
+    with patch("cosmae.contribution.entity.queue.timestamp", mock):
+        q.eliminate_duplicates(contribution_candidate.id_persistent)
+    contribution_candidate = ContributionCandidate.by_id_persistent(
+        contribution_candidate.id_persistent, contribution_candidate.created_by
+    ).get()
+    assert contribution_candidate.state == ContributionCandidate.VALUES_EXTRACTED
+    assert (
+        contribution_candidate.error_msg == "Error during Entity Duplicate Elimination."
+    )
+    assert contribution_candidate.error_trace == "Exception: error"

tests/contribution/tag_definition/queue/create_test.py
@@ -79,3 +79,19 @@ def test_extracts_with_header(contribution_other):
         )
     )
     assert contribution_candidate.state == ContributionCandidate.COLUMNS_EXTRACTED
+
+
+def test_sets_error(contribution_other):
+    conf_mock = MagicMock
+    conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/does_not_exist"
+    with patch("cosmae.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_other.id_persistent)
+    contribution = ContributionCandidate.by_id_persistent(
+        contribution_other.id_persistent, contribution_other.created_by
+    ).get()
+    assert contribution.state == ContributionCandidate.UPLOADED
+    assert contribution.error_msg == "Error while extracting columns."
+    assert contribution.error_trace == (
+        "FileNotFoundError: [Errno 2] No such file or directory: "
+        "'tests/files/does_not_exist/DBOeS_Parlamentarier50.csv'"
+    )

tests/contribution/tag_definition/queue/ingest_test.py
@@ -191,3 +191,17 @@ def test_ingest_string(
         .state
         == ContributionCandidate.VALUES_EXTRACTED
     )
+
+
+def test_sets_error(party_contribution, party_tag_def):
+    mock = MagicMock()
+    mock.side_effect = Exception("error")
+    contribution = party_contribution.contribution_candidate
+    with patch("cosmae.contribution.tag_definition.queue.util.read_csv", mock):
+        ingest_values_from_csv(contribution.id_persistent)
+    contribution = ContributionCandidate.by_id_persistent(
+        contribution.id_persistent, contribution.created_by
+    ).get()
+    assert contribution.state == ContributionCandidate.COLUMNS_EXTRACTED
+    assert contribution.error_msg == "Error during ingestion of assigned tags."
+    assert contribution.error_trace == "Exception: error"