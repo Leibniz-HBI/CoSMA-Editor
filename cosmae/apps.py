Leibniz-HBI/DP-REX

Add column extraction to backend.

vran/apps.py
@@ -3,8 +3,9 @@ import logging
 from typing import List
 
 from django.apps import AppConfig, apps
+from django.core.exceptions import AppRegistryNotReady
 from django.db.backends.signals import connection_created
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 
 logger = logging.getLogger("vran.app_config")
 
@@ -92,4 +93,16 @@ class VranConfig(AppConfig):
         connection_created.connect(
             add_superuser, dispatch_uid="vran.create_initial_superuser"
         )
+        try:
+            # pylint: disable=import-outside-toplevel
+            from vran.contribution.models_django import ContributionCandidate
+            from vran.contribution.tag_definition.queue import dispatch_read_csv_head
+
+            post_save.connect(
+                dispatch_read_csv_head,
+                sender=ContributionCandidate,
+                dispatch_uid="vran.start_tag_extraction",
+            )
+        except AppRegistryNotReady:
+            pass
         super().ready()

vran/contribution/models_conversion.py
@@ -1,0 +1,34 @@
+"Convert models between database and API representation."
+from vran.contribution.models_api import ContributionCandidate
+from vran.contribution.models_django import (
+    ContributionCandidate as ContributionCandidateDb,
+)
+
+_contribution_state_mapping_db_to_api = {
+    ContributionCandidateDb.UPLOADED: "UPLOADED",
+    ContributionCandidateDb.COLUMNS_EXTRACTED: "COLUMNS_EXTRACTED",
+    ContributionCandidateDb.COLUMNS_ASSIGNED: "COLUMNS_ASSIGNED",
+    ContributionCandidateDb.ENTITIES_MATCHED: "ENTITIES_MATCHED",
+    ContributionCandidateDb.ENTITIES_ASSIGNED: "ENTITIES_ASSIGNED",
+    ContributionCandidateDb.VALUES_ASSIGNED: "VALUES_ASSIGNED",
+    ContributionCandidateDb.MERGED: "MERGED",
+}
+
+
+def contribution_db_to_api(
+    contribution_db: ContributionCandidateDb,
+) -> ContributionCandidate:
+    "Transform a contribution candidate from DB to API representation."
+    if contribution_db.anonymous:
+        author = None
+    else:
+        author = contribution_db.created_by.username
+    return ContributionCandidate(
+        id_persistent=str(contribution_db.id_persistent),
+        name=contribution_db.name,
+        description=contribution_db.description,
+        anonymous=contribution_db.anonymous,
+        has_header=contribution_db.has_header,
+        state=_contribution_state_mapping_db_to_api[contribution_db.state],
+        author=author,
+    )

vran/models.py
@@ -1,5 +1,9 @@
 """Union of all DB models."""
 # pylint: disable=unused-import
+from vran.contribution.tag_definition.models_django import (
+    TagDefinitionContribution,
+    TagInstanceContribution,
+)
 from vran.entity.models_django import Entity
 from vran.person.models_django import Person
 from vran.tag.models_django import TagDefinition, TagInstance

tests/contribution/api/integration/get_contribution_test.py
@@ -1,0 +1,45 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,disable=unused-argument,duplicate-code
+from unittest.mock import MagicMock, patch
+from uuid import uuid4
+
+import tests.contribution.api.integration.common as c
+import tests.contribution.api.integration.requests as req_contrib
+from vran.util.auth import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.contribution.api.check_user", mock):
+        rsp = req_contrib.get_contribution(server.url, "id-test", cookies=cookies)
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req_contrib.get_contribution(server.url, "id-test")
+    assert rsp.status_code == 401
+
+
+def test_404(auth_server):
+    server, cookies = auth_server
+    rsp = req_contrib.get_contribution(server.url, str(uuid4()), cookies=cookies)
+    assert rsp.status_code == 404
+
+
+def test_get(auth_server):
+    live_server, cookies = auth_server
+    rsp = req_contrib.post_contribution(
+        live_server.url, c.contribution_post0, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    id_persistent = rsp.json()["id_persistent"]
+    rsp = req_contrib.get_contribution(
+        live_server.url, id_persistent=id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    contribution = rsp.json()
+    assert contribution["id_persistent"] == id_persistent
+    contribution.pop("id_persistent")
+    assert contribution == c.contribution_test_upload0

tests/contribution/api/integration/requests.py
@@ -1,0 +1,37 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+import requests
+
+
+def post_contribution(url, contribution, cookies=None, content_type="text/csv"):
+    return requests.post(
+        url + "/vran/api/contributions",
+        data=contribution,
+        files={
+            "file": ("empty.csv", open("tests/files/empty.csv", "rb"), content_type)
+        },
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def get_contribution(url, id_persistent, cookies=None):
+    return requests.get(
+        url + f"/vran/api/contributions/{id_persistent}", cookies=cookies, timeout=900
+    )
+
+
+def get_chunk(url, offset, limit, cookies=None):
+    return requests.get(
+        url + f"/vran/api/contributions/chunk/{offset}/{limit}",
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def patch_contribution(url, id_persistent, patch_data, cookies=None):
+    return requests.patch(
+        url + f"/vran/api/contributions/{id_persistent}",
+        json=patch_data,
+        cookies=cookies,
+        timeout=900,
+    )

tests/contribution/models_django_test.py
@@ -1,0 +1,32 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,disable=unused-argument
+import tests.contribution.common as c
+from vran.contribution.models_django import ContributionCandidate
+
+
+def test_empty_chunk(user):
+    ret = ContributionCandidate.chunk_for_user(user, 2, 100)
+    assert not list(ret)
+
+
+def test_filters_user(user, contribution_user, contribution_other):
+    assert len(ContributionCandidate.chunk_for_user(user, 0, 100)) == 1
+
+
+def test_change_description(user, contribution_user):
+    ContributionCandidate.update(
+        contribution_user.id_persistent, user, **{"description": c.description_test1}
+    )
+    candidate = ContributionCandidate.by_id_persistent(
+        contribution_user.id_persistent, user
+    ).get()
+    assert candidate.description == c.description_test1
+
+
+def test_change_name(user, contribution_user):
+    ContributionCandidate.update(
+        contribution_user.id_persistent, user, **{"name": c.name_test1}
+    )
+    candidate = ContributionCandidate.by_id_persistent(
+        contribution_user.id_persistent, user
+    ).get()
+    assert candidate.name == c.name_test1

tests/contribution/tag_definition/api/integration/requests.py
@@ -1,0 +1,10 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+import requests
+
+
+def get_tag_definition(url, id_persistent, cookies=None):
+    return requests.get(
+        url + f"/vran/api/contributions/{id_persistent}/tags",
+        cookies=cookies,
+        timeout=900,
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/apps.py
@@ -3,8 +3,9 @@ import logging
 from typing import List
 
 from django.apps import AppConfig, apps
+from django.core.exceptions import AppRegistryNotReady
 from django.db.backends.signals import connection_created
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 
 logger = logging.getLogger("cosmae.app_config")
 
@@ -92,4 +93,16 @@ class CosmaeConfig(AppConfig):
         connection_created.connect(
             add_superuser, dispatch_uid="cosmae.create_initial_superuser"
         )
+        try:
+            # pylint: disable=import-outside-toplevel
+            from cosmae.contribution.models_django import ContributionCandidate
+            from cosmae.contribution.tag_definition.queue import dispatch_read_csv_head
+
+            post_save.connect(
+                dispatch_read_csv_head,
+                sender=ContributionCandidate,
+                dispatch_uid="cosmae.start_tag_extraction",
+            )
+        except AppRegistryNotReady:
+            pass
         super().ready()

cosmae/contribution/models_conversion.py
@@ -1,0 +1,34 @@
+"Convert models between database and API representation."
+from cosmae.contribution.models_api import ContributionCandidate
+from cosmae.contribution.models_django import (
+    ContributionCandidate as ContributionCandidateDb,
+)
+
+_contribution_state_mapping_db_to_api = {
+    ContributionCandidateDb.UPLOADED: "UPLOADED",
+    ContributionCandidateDb.COLUMNS_EXTRACTED: "COLUMNS_EXTRACTED",
+    ContributionCandidateDb.COLUMNS_ASSIGNED: "COLUMNS_ASSIGNED",
+    ContributionCandidateDb.ENTITIES_MATCHED: "ENTITIES_MATCHED",
+    ContributionCandidateDb.ENTITIES_ASSIGNED: "ENTITIES_ASSIGNED",
+    ContributionCandidateDb.VALUES_ASSIGNED: "VALUES_ASSIGNED",
+    ContributionCandidateDb.MERGED: "MERGED",
+}
+
+
+def contribution_db_to_api(
+    contribution_db: ContributionCandidateDb,
+) -> ContributionCandidate:
+    "Transform a contribution candidate from DB to API representation."
+    if contribution_db.anonymous:
+        author = None
+    else:
+        author = contribution_db.created_by.username
+    return ContributionCandidate(
+        id_persistent=str(contribution_db.id_persistent),
+        name=contribution_db.name,
+        description=contribution_db.description,
+        anonymous=contribution_db.anonymous,
+        has_header=contribution_db.has_header,
+        state=_contribution_state_mapping_db_to_api[contribution_db.state],
+        author=author,
+    )

cosmae/models.py
@@ -1,6 +1,9 @@
 """Union of all DB models."""
-
 # pylint: disable=unused-import
+from cosmae.contribution.tag_definition.models_django import (
+    TagDefinitionContribution,
+    TagInstanceContribution,
+)
 from cosmae.entity.models_django import Entity
 from cosmae.person.models_django import Person
 from cosmae.tag.models_django import TagDefinition, TagInstance

tests/contribution/api/integration/get_contribution_test.py
@@ -1,0 +1,45 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,disable=unused-argument,duplicate-code
+from unittest.mock import MagicMock, patch
+from uuid import uuid4
+
+import tests.contribution.api.integration.common as c
+import tests.contribution.api.integration.requests as req_contrib
+from cosmae.util.auth import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.contribution.api.check_user", mock):
+        rsp = req_contrib.get_contribution(server.url, "id-test", cookies=cookies)
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req_contrib.get_contribution(server.url, "id-test")
+    assert rsp.status_code == 401
+
+
+def test_404(auth_server):
+    server, cookies = auth_server
+    rsp = req_contrib.get_contribution(server.url, str(uuid4()), cookies=cookies)
+    assert rsp.status_code == 404
+
+
+def test_get(auth_server):
+    live_server, cookies = auth_server
+    rsp = req_contrib.post_contribution(
+        live_server.url, c.contribution_post0, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    id_persistent = rsp.json()["id_persistent"]
+    rsp = req_contrib.get_contribution(
+        live_server.url, id_persistent=id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    contribution = rsp.json()
+    assert contribution["id_persistent"] == id_persistent
+    contribution.pop("id_persistent")
+    assert contribution == c.contribution_test_upload0

tests/contribution/api/integration/requests.py
@@ -1,0 +1,37 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+import requests
+
+
+def post_contribution(url, contribution, cookies=None, content_type="text/csv"):
+    return requests.post(
+        url + "/cosmae/api/contributions",
+        data=contribution,
+        files={
+            "file": ("empty.csv", open("tests/files/empty.csv", "rb"), content_type)
+        },
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def get_contribution(url, id_persistent, cookies=None):
+    return requests.get(
+        url + f"/cosmae/api/contributions/{id_persistent}", cookies=cookies, timeout=900
+    )
+
+
+def get_chunk(url, offset, limit, cookies=None):
+    return requests.get(
+        url + f"/cosmae/api/contributions/chunk/{offset}/{limit}",
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def patch_contribution(url, id_persistent, patch_data, cookies=None):
+    return requests.patch(
+        url + f"/cosmae/api/contributions/{id_persistent}",
+        json=patch_data,
+        cookies=cookies,
+        timeout=900,
+    )

tests/contribution/models_django_test.py
@@ -1,0 +1,32 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,disable=unused-argument
+import tests.contribution.common as c
+from cosmae.contribution.models_django import ContributionCandidate
+
+
+def test_empty_chunk(user):
+    ret = ContributionCandidate.chunk_for_user(user, 2, 100)
+    assert not list(ret)
+
+
+def test_filters_user(user, contribution_user, contribution_other):
+    assert len(ContributionCandidate.chunk_for_user(user, 0, 100)) == 1
+
+
+def test_change_description(user, contribution_user):
+    ContributionCandidate.update(
+        contribution_user.id_persistent, user, **{"description": c.description_test1}
+    )
+    candidate = ContributionCandidate.by_id_persistent(
+        contribution_user.id_persistent, user
+    ).get()
+    assert candidate.description == c.description_test1
+
+
+def test_change_name(user, contribution_user):
+    ContributionCandidate.update(
+        contribution_user.id_persistent, user, **{"name": c.name_test1}
+    )
+    candidate = ContributionCandidate.by_id_persistent(
+        contribution_user.id_persistent, user
+    ).get()
+    assert candidate.name == c.name_test1

tests/contribution/tag_definition/api/integration/requests.py
@@ -1,0 +1,10 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+import requests
+
+
+def get_tag_definition(url, id_persistent, cookies=None):
+    return requests.get(
+        url + f"/cosmae/api/contributions/{id_persistent}/tags",
+        cookies=cookies,
+        timeout=900,
+    )