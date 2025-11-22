Leibniz-HBI/DP-REX

Add basic components for reviewing contributions to frontend.

ui/src/contribution/columns/components.tsx
@@ -9,7 +9,6 @@ import {
     ListGroup,
     Modal,
     Overlay,
-    Placeholder,
     Popover,
     Row
 } from 'react-bootstrap'
@@ -17,7 +16,7 @@ import { ColumnDefinitionContribution } from './state'
 import { Trash } from 'react-bootstrap-icons'
 import { ChangeEvent, useLayoutEffect, useRef } from 'react'
 import { ColumnSelector, mkListItems } from '../../column_menu/components/selection'
-import { VrAnLoading } from '../../util/components/misc'
+import { RemoteTriggerButton, VrAnLoading } from '../../util/components/misc'
 import { ColumnDefinition } from '../../column_menu/state'
 import {
     ColumnTypeCreateForm,
@@ -261,7 +260,7 @@ export function ContributionColumnAssignmentForm({
                         onClick={() => selectColumnCreationTabCallback(true)}
                         variant="outline-primary"
                     >
-                        Create new column
+                        Create new tag
                     </Button>
                 </Row>
             </Col>
@@ -272,7 +271,7 @@ export function ContributionColumnAssignmentForm({
                 data-testid="create-column-modal"
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Create a new column</Modal.Title>
+                    <Modal.Title>Create a new tag</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <NewColumnModalBody />
@@ -292,7 +291,10 @@ export function ExistingColumnForm({
     return (
         <Col>
             <Row className="ps-2">
-                <span>Please select an existing column.</span>
+                <span>
+                    Please select the tag that should receive the data of column "
+                    {columnDefinitionContribution.name}":
+                </span>
             </Row>
             <Row className="ps-1 pe-1">
                 <ColumnHierarchyContext.Consumer>
@@ -408,32 +410,12 @@ export function CompleteColumnAssignmentButton({
     remoteState: Remote<boolean>
     onClick: VoidFunction
 }) {
-    const loading = remoteState.isLoading
-    const success = remoteState.value
-    if (success) {
-        return (
-            <Button active={false} variant="outline-primary">
-                <span className="text-primary fw-bold">
-                    Column assignment successfully finalized
-                </span>
-            </Button>
-        )
-    }
-    const buttonText = 'Finalize Column Assignment'
-    if (loading) {
-        return (
-            <Placeholder.Button variant="primary" animation="wave">
-                <span>{buttonText}</span>
-            </Placeholder.Button>
-        )
-    }
     return (
-        <Button
-            variant="outline-primary"
+        <RemoteTriggerButton
+            successLabel="Column assignment successfully finalized"
+            normalLabel="Finalize Column Assignment"
+            remoteState={remoteState}
             onClick={onClick}
-            data-testid="complete-column-assignment-button"
-        >
-            <span>{buttonText}</span>
-        </Button>
+        />
     )
 }

ui/src/contribution/entity/components.tsx
@@ -1,0 +1,185 @@
+import { useLoaderData } from 'react-router-dom'
+import { PutDuplicateCallback, useContributionEntities } from './hooks'
+import { ContributionStepper } from '../components'
+import { RemoteTriggerButton, VrAnLoading } from '../../util/components/misc'
+import {
+    Button,
+    CloseButton,
+    Col,
+    Form,
+    FormCheck,
+    ListGroup,
+    Overlay,
+    Popover,
+    ProgressBar,
+    Row
+} from 'react-bootstrap'
+import { useLayoutEffect, useRef } from 'react'
+import { EntityWithDuplicates } from './state'
+
+export function EntitiesStep() {
+    const idContributionPersistent = useLoaderData() as string
+    const {
+        getEntityDuplicatesCallback,
+        contributionCandidate,
+        isLoading,
+        entities,
+        minLoadingIdx,
+        completeEntityAssignment,
+        completeEntityAssignmentCallback,
+        clearEntityAssignmentErrorCallback,
+        isDuplicates,
+        putDuplicateCallback
+    } = useContributionEntities(idContributionPersistent)
+    useLayoutEffect(() => {
+        getEntityDuplicatesCallback()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [idContributionPersistent])
+    const buttonRef = useRef(null)
+    const containerRef = useRef(null)
+    if (contributionCandidate.value === undefined || isLoading) {
+        return <VrAnLoading />
+    }
+    return (
+        <ContributionStepper
+            selectedIdx={2}
+            id_persistent={idContributionPersistent}
+            step={contributionCandidate.value.step}
+        >
+            <Col ref={containerRef} className="h-100 overflow-hidden">
+                <Row>
+                    <Col sm="auto" ref={buttonRef} key="entities-step-complete-button">
+                        <RemoteTriggerButton
+                            normalLabel="Merge Assigned Entities"
+                            successLabel="Entities Successfully Merged"
+                            remoteState={completeEntityAssignment}
+                            onClick={completeEntityAssignmentCallback}
+                        />
+                        {
+                            <Overlay
+                                show={completeEntityAssignment?.errorMsg !== undefined}
+                                target={buttonRef}
+                                container={containerRef}
+                                placement="bottom"
+                            >
+                                <Popover id="finalize-column-assignment-error-popover">
+                                    <Popover.Header className="bg-danger text-light">
+                                        <Row className="justify-content-between">
+                                            <Col>Error</Col>
+                                            <CloseButton
+                                                variant="white"
+                                                onClick={
+                                                    clearEntityAssignmentErrorCallback
+                                                }
+                                            ></CloseButton>
+                                        </Row>
+                                    </Popover.Header>
+                                    <Popover.Body>
+                                        <span>
+                                            {completeEntityAssignment?.errorMsg}
+                                        </span>
+                                    </Popover.Body>
+                                </Popover>
+                            </Overlay>
+                        }
+                    </Col>
+                    <Col key="entities-step-hint">
+                        Please check for duplicate values. Select the first Row to
+                        indicate that there is no duplicate.
+                    </Col>
+                    <Col sm="auto" key="entities-step-add-tag-button">
+                        <Button>Show Additional tag values</Button>
+                    </Col>
+                </Row>
+                <Row className="mt-4">
+                    <ProgressBar
+                        striped
+                        animated
+                        hidden={minLoadingIdx === undefined}
+                        variant="primary"
+                        now={Math.round(
+                            (100 * (minLoadingIdx ?? 100)) / entities.value.length
+                        )}
+                        label="Loading Duplicate Candidates"
+                    />
+                </Row>
+                <Row className="h-100 ms-2 mt-3 overflow-y-scroll">
+                    {isDuplicates ? (
+                        <ListGroup>
+                            {entities.value
+                                .filter((entity) => !entity.similarEntities.isLoading)
+                                .map((entity) => (
+                                    <EntitySimilarityItem
+                                        entity={entity}
+                                        putDuplicateCallback={putDuplicateCallback}
+                                    />
+                                ))}
+                        </ListGroup>
+                    ) : (
+                        <span>There are no duplicates</span>
+                    )}
+                </Row>
+            </Col>
+        </ContributionStepper>
+    )
+}
+
+export function EntitySimilarityItem({
+    entity,
+    putDuplicateCallback
+}: {
+    entity: EntityWithDuplicates
+    putDuplicateCallback: PutDuplicateCallback
+}) {
+    const { similarEntities, displayTxt, idPersistent } = entity
+    if (similarEntities.errorMsg !== undefined) {
+        return (
+            <ListGroup.Item className="bg-danger">
+                <span>{`Could not get duplicates for ${displayTxt}.\n`}</span>
+                <span>{`Reason: ${similarEntities.errorMsg}`}</span>
+            </ListGroup.Item>
+        )
+    }
+    if (similarEntities.value.length == 0) {
+        return <></>
+    }
+    return (
+        <ListGroup.Item key={idPersistent}>
+            <Form>
+                <Col>
+                    <Row className="fw-bold">{`Uploaded Entity with display txt ${displayTxt}`}</Row>
+                    <Row>
+                        <Form.Check
+                            type="radio"
+                            label="Create new entity"
+                            value=""
+                            checked={entity.assignedDuplicate.value === undefined}
+                            onChange={(_) =>
+                                putDuplicateCallback(entity.idPersistent, undefined)
+                            }
+                        />
+                    </Row>
+                    {similarEntities.value.map((candidate) => (
+                        <Row>
+                            <FormCheck
+                                type="radio"
+                                label={`Existing entity ${candidate.displayTxt} has similarity of ${candidate.similarity}.`}
+                                value={candidate.idPersistent}
+                                checked={
+                                    candidate.idPersistent ==
+                                    entity.assignedDuplicate.value?.idPersistent
+                                }
+                                onChange={(_) =>
+                                    putDuplicateCallback(
+                                        entity.idPersistent,
+                                        candidate.idPersistent
+                                    )
+                                }
+                            ></FormCheck>
+                        </Row>
+                    ))}
+                </Col>
+            </Form>
+        </ListGroup.Item>
+    )
+}

ui/src/merge_request/components.tsx
@@ -1,0 +1,50 @@
+import { useLayoutEffect } from 'react'
+import { useMergeRequests } from './hooks'
+import { VrAnLoading } from '../util/components/misc'
+import { Col, ListGroup, Row } from 'react-bootstrap'
+import { MergeRequest } from './state'
+
+export function ReviewList() {
+    const { getMergeRequestsCallback, isLoading, assigned } = useMergeRequests()
+    useLayoutEffect(() => {
+        getMergeRequestsCallback()
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    if (isLoading) {
+        return <VrAnLoading />
+    }
+    return (
+        <Col>
+            <Row className="mt-4 ms-2" key="merge-request-assigned-heading">
+                Assigned to you
+            </Row>
+            <Row className="ms-2" key="merge-request-assigned-list">
+                <ListGroup>
+                    {assigned.map((mergeRequest) => (
+                        <MergeRequestListItem mergeRequest={mergeRequest} />
+                    ))}
+                </ListGroup>
+            </Row>
+            <Row className="mt-4 ms-2" key="merge-request-created-heading">
+                Created by you
+            </Row>
+            <Row className="ms-2" key="merge-request-created-list">
+                <ListGroup as="ol">
+                    {assigned.map((mergeRequest) => (
+                        <MergeRequestListItem mergeRequest={mergeRequest} />
+                    ))}
+                </ListGroup>
+            </Row>
+        </Col>
+    )
+}
+
+export function MergeRequestListItem({ mergeRequest }: { mergeRequest: MergeRequest }) {
+    return (
+        <ListGroup.Item as="li" key={`${mergeRequest.idPersistent}`}>
+            {`${mergeRequest.createdBy.userName} wants to merge \n` +
+                `${mergeRequest.originTagDefinition.namePath[0]} \n` +
+                `into ${mergeRequest.destinationTagDefinition.namePath[0]}`}
+        </ListGroup.Item>
+    )
+}

Leibniz-HBI/CoSMA-Editor

ui/src/contribution/columns/components.tsx
@@ -9,7 +9,6 @@ import {
     ListGroup,
     Modal,
     Overlay,
-    Placeholder,
     Popover,
     Row
 } from 'react-bootstrap'
@@ -17,7 +16,7 @@ import { ColumnDefinitionContribution } from './state'
 import { Trash } from 'react-bootstrap-icons'
 import { ChangeEvent, useLayoutEffect, useRef } from 'react'
 import { ColumnSelector, mkListItems } from '../../column_menu/components/selection'
-import { CosmaeLoading } from '../../util/components/misc'
+import { RemoteTriggerButton, CosmaeLoading } from '../../util/components/misc'
 import { ColumnDefinition } from '../../column_menu/state'
 import {
     ColumnTypeCreateForm,
@@ -261,7 +260,7 @@ export function ContributionColumnAssignmentForm({
                         onClick={() => selectColumnCreationTabCallback(true)}
                         variant="outline-primary"
                     >
-                        Create new column
+                        Create new tag
                     </Button>
                 </Row>
             </Col>
@@ -272,7 +271,7 @@ export function ContributionColumnAssignmentForm({
                 data-testid="create-column-modal"
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Create a new column</Modal.Title>
+                    <Modal.Title>Create a new tag</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <NewColumnModalBody />
@@ -292,7 +291,10 @@ export function ExistingColumnForm({
     return (
         <Col>
             <Row className="ps-2">
-                <span>Please select an existing column.</span>
+                <span>
+                    Please select the tag that should receive the data of column "
+                    {columnDefinitionContribution.name}":
+                </span>
             </Row>
             <Row className="ps-1 pe-1">
                 <ColumnHierarchyContext.Consumer>
@@ -408,32 +410,12 @@ export function CompleteColumnAssignmentButton({
     remoteState: Remote<boolean>
     onClick: VoidFunction
 }) {
-    const loading = remoteState.isLoading
-    const success = remoteState.value
-    if (success) {
-        return (
-            <Button active={false} variant="outline-primary">
-                <span className="text-primary fw-bold">
-                    Column assignment successfully finalized
-                </span>
-            </Button>
-        )
-    }
-    const buttonText = 'Finalize Column Assignment'
-    if (loading) {
-        return (
-            <Placeholder.Button variant="primary" animation="wave">
-                <span>{buttonText}</span>
-            </Placeholder.Button>
-        )
-    }
     return (
-        <Button
-            variant="outline-primary"
+        <RemoteTriggerButton
+            successLabel="Column assignment successfully finalized"
+            normalLabel="Finalize Column Assignment"
+            remoteState={remoteState}
             onClick={onClick}
-            data-testid="complete-column-assignment-button"
-        >
-            <span>{buttonText}</span>
-        </Button>
+        />
     )
 }

ui/src/contribution/entity/components.tsx
@@ -1,0 +1,185 @@
+import { useLoaderData } from 'react-router-dom'
+import { PutDuplicateCallback, useContributionEntities } from './hooks'
+import { ContributionStepper } from '../components'
+import { RemoteTriggerButton, CosmaeLoading } from '../../util/components/misc'
+import {
+    Button,
+    CloseButton,
+    Col,
+    Form,
+    FormCheck,
+    ListGroup,
+    Overlay,
+    Popover,
+    ProgressBar,
+    Row
+} from 'react-bootstrap'
+import { useLayoutEffect, useRef } from 'react'
+import { EntityWithDuplicates } from './state'
+
+export function EntitiesStep() {
+    const idContributionPersistent = useLoaderData() as string
+    const {
+        getEntityDuplicatesCallback,
+        contributionCandidate,
+        isLoading,
+        entities,
+        minLoadingIdx,
+        completeEntityAssignment,
+        completeEntityAssignmentCallback,
+        clearEntityAssignmentErrorCallback,
+        isDuplicates,
+        putDuplicateCallback
+    } = useContributionEntities(idContributionPersistent)
+    useLayoutEffect(() => {
+        getEntityDuplicatesCallback()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [idContributionPersistent])
+    const buttonRef = useRef(null)
+    const containerRef = useRef(null)
+    if (contributionCandidate.value === undefined || isLoading) {
+        return <CosmaeLoading />
+    }
+    return (
+        <ContributionStepper
+            selectedIdx={2}
+            id_persistent={idContributionPersistent}
+            step={contributionCandidate.value.step}
+        >
+            <Col ref={containerRef} className="h-100 overflow-hidden">
+                <Row>
+                    <Col sm="auto" ref={buttonRef} key="entities-step-complete-button">
+                        <RemoteTriggerButton
+                            normalLabel="Merge Assigned Entities"
+                            successLabel="Entities Successfully Merged"
+                            remoteState={completeEntityAssignment}
+                            onClick={completeEntityAssignmentCallback}
+                        />
+                        {
+                            <Overlay
+                                show={completeEntityAssignment?.errorMsg !== undefined}
+                                target={buttonRef}
+                                container={containerRef}
+                                placement="bottom"
+                            >
+                                <Popover id="finalize-column-assignment-error-popover">
+                                    <Popover.Header className="bg-danger text-light">
+                                        <Row className="justify-content-between">
+                                            <Col>Error</Col>
+                                            <CloseButton
+                                                variant="white"
+                                                onClick={
+                                                    clearEntityAssignmentErrorCallback
+                                                }
+                                            ></CloseButton>
+                                        </Row>
+                                    </Popover.Header>
+                                    <Popover.Body>
+                                        <span>
+                                            {completeEntityAssignment?.errorMsg}
+                                        </span>
+                                    </Popover.Body>
+                                </Popover>
+                            </Overlay>
+                        }
+                    </Col>
+                    <Col key="entities-step-hint">
+                        Please check for duplicate values. Select the first Row to
+                        indicate that there is no duplicate.
+                    </Col>
+                    <Col sm="auto" key="entities-step-add-tag-button">
+                        <Button>Show Additional tag values</Button>
+                    </Col>
+                </Row>
+                <Row className="mt-4">
+                    <ProgressBar
+                        striped
+                        animated
+                        hidden={minLoadingIdx === undefined}
+                        variant="primary"
+                        now={Math.round(
+                            (100 * (minLoadingIdx ?? 100)) / entities.value.length
+                        )}
+                        label="Loading Duplicate Candidates"
+                    />
+                </Row>
+                <Row className="h-100 ms-2 mt-3 overflow-y-scroll">
+                    {isDuplicates ? (
+                        <ListGroup>
+                            {entities.value
+                                .filter((entity) => !entity.similarEntities.isLoading)
+                                .map((entity) => (
+                                    <EntitySimilarityItem
+                                        entity={entity}
+                                        putDuplicateCallback={putDuplicateCallback}
+                                    />
+                                ))}
+                        </ListGroup>
+                    ) : (
+                        <span>There are no duplicates</span>
+                    )}
+                </Row>
+            </Col>
+        </ContributionStepper>
+    )
+}
+
+export function EntitySimilarityItem({
+    entity,
+    putDuplicateCallback
+}: {
+    entity: EntityWithDuplicates
+    putDuplicateCallback: PutDuplicateCallback
+}) {
+    const { similarEntities, displayTxt, idPersistent } = entity
+    if (similarEntities.errorMsg !== undefined) {
+        return (
+            <ListGroup.Item className="bg-danger">
+                <span>{`Could not get duplicates for ${displayTxt}.\n`}</span>
+                <span>{`Reason: ${similarEntities.errorMsg}`}</span>
+            </ListGroup.Item>
+        )
+    }
+    if (similarEntities.value.length == 0) {
+        return <></>
+    }
+    return (
+        <ListGroup.Item key={idPersistent}>
+            <Form>
+                <Col>
+                    <Row className="fw-bold">{`Uploaded Entity with display txt ${displayTxt}`}</Row>
+                    <Row>
+                        <Form.Check
+                            type="radio"
+                            label="Create new entity"
+                            value=""
+                            checked={entity.assignedDuplicate.value === undefined}
+                            onChange={(_) =>
+                                putDuplicateCallback(entity.idPersistent, undefined)
+                            }
+                        />
+                    </Row>
+                    {similarEntities.value.map((candidate) => (
+                        <Row>
+                            <FormCheck
+                                type="radio"
+                                label={`Existing entity ${candidate.displayTxt} has similarity of ${candidate.similarity}.`}
+                                value={candidate.idPersistent}
+                                checked={
+                                    candidate.idPersistent ==
+                                    entity.assignedDuplicate.value?.idPersistent
+                                }
+                                onChange={(_) =>
+                                    putDuplicateCallback(
+                                        entity.idPersistent,
+                                        candidate.idPersistent
+                                    )
+                                }
+                            ></FormCheck>
+                        </Row>
+                    ))}
+                </Col>
+            </Form>
+        </ListGroup.Item>
+    )
+}

ui/src/merge_request/components.tsx
@@ -1,0 +1,50 @@
+import { useLayoutEffect } from 'react'
+import { useMergeRequests } from './hooks'
+import { CosmaeLoading } from '../util/components/misc'
+import { Col, ListGroup, Row } from 'react-bootstrap'
+import { MergeRequest } from './state'
+
+export function ReviewList() {
+    const { getMergeRequestsCallback, isLoading, assigned } = useMergeRequests()
+    useLayoutEffect(() => {
+        getMergeRequestsCallback()
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    if (isLoading) {
+        return <CosmaeLoading />
+    }
+    return (
+        <Col>
+            <Row className="mt-4 ms-2" key="merge-request-assigned-heading">
+                Assigned to you
+            </Row>
+            <Row className="ms-2" key="merge-request-assigned-list">
+                <ListGroup>
+                    {assigned.map((mergeRequest) => (
+                        <MergeRequestListItem mergeRequest={mergeRequest} />
+                    ))}
+                </ListGroup>
+            </Row>
+            <Row className="mt-4 ms-2" key="merge-request-created-heading">
+                Created by you
+            </Row>
+            <Row className="ms-2" key="merge-request-created-list">
+                <ListGroup as="ol">
+                    {assigned.map((mergeRequest) => (
+                        <MergeRequestListItem mergeRequest={mergeRequest} />
+                    ))}
+                </ListGroup>
+            </Row>
+        </Col>
+    )
+}
+
+export function MergeRequestListItem({ mergeRequest }: { mergeRequest: MergeRequest }) {
+    return (
+        <ListGroup.Item as="li" key={`${mergeRequest.idPersistent}`}>
+            {`${mergeRequest.createdBy.userName} wants to merge \n` +
+                `${mergeRequest.originTagDefinition.namePath[0]} \n` +
+                `into ${mergeRequest.destinationTagDefinition.namePath[0]}`}
+        </ListGroup.Item>
+    )
+}