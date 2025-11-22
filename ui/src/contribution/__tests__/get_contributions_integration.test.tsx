Leibniz-HBI/DP-REX

Refactor get contribution list to use Redux.

ui/src/contribution/__tests__/get_contributions_integration.test.tsx
@@ -1,0 +1,199 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { RenderOptions, render, waitFor, screen } from '@testing-library/react'
+import {
+    NotificationManager,
+    NotificationType,
+    notificationReducer
+} from '../../util/notification/slice'
+import { ContributionState, contributionSlice, newContributionState } from '../slice'
+import { configureStore } from '@reduxjs/toolkit'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { ContributionList } from '../components'
+import { newRemote } from '../../util/state'
+import { ContributionStep, newContribution } from '../state'
+import { useNavigate } from 'react-router-dom'
+
+jest.mock('react-router-dom', () => {
+    const mockNavigate = jest.fn()
+    return {
+        useNavigate: jest.fn().mockReturnValue(mockNavigate)
+    }
+})
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: {
+        contribution: ContributionState
+        notification: NotificationManager
+    }
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    fetchMock: jest.Mock,
+    {
+        preloadedState = {
+            contribution: newContributionState({}),
+            notification: { notificationList: [], notificationMap: {} }
+        },
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    const store = configureStore({
+        reducer: {
+            contribution: contributionSlice.reducer,
+            notification: notificationReducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({ thunk: { extraArgument: fetchMock } }),
+        preloadedState
+    })
+    function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>
+    }
+
+    // Return an object with the store and all of RTL's query functions
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+function addResponseSequence(mock: jest.Mock, responses: [number, unknown][]) {
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        mock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const nameTest0 = 'contribution test 0'
+const descriptionTest0 = 'a contribution for tests'
+const idTest0 = 'id-test-0'
+const authorTest1 = 'author test 1'
+const contributionResponse0 = {
+    name: nameTest0,
+    description: descriptionTest0,
+    id_persistent: idTest0,
+    author: authorTest1,
+    has_header: false,
+    state: 'UPLOADED'
+}
+const nameTest1 = 'contribution test 1'
+const descriptionTest1 = 'another contribution for tests'
+const idTest1 = 'id-test-1'
+const contributionResponse1 = {
+    name: nameTest1,
+    description: descriptionTest1,
+    id_persistent: idTest1,
+    has_header: true,
+    state: 'VALUES_ASSIGNED',
+    author: authorTest1
+}
+test('success and open', async () => {
+    const fetchMock = jest.fn()
+    addResponseSequence(fetchMock, [
+        [200, { contributions: [contributionResponse0, contributionResponse1] }],
+        [200, { contributions: [] }]
+    ])
+    const { store } = renderWithProviders(<ContributionList />, fetchMock)
+    await waitFor(() => {
+        expect(store.getState()).toEqual({
+            contribution: expect.objectContaining({
+                contributions: newRemote([
+                    newContribution({
+                        name: nameTest0,
+                        idPersistent: idTest0,
+                        description: descriptionTest0,
+                        step: ContributionStep.Uploaded,
+                        hasHeader: false,
+                        author: authorTest1
+                    }),
+                    newContribution({
+                        name: nameTest1,
+                        idPersistent: idTest1,
+                        description: descriptionTest1,
+                        step: ContributionStep.ValuesAssigned,
+                        hasHeader: true,
+                        author: authorTest1
+                    })
+                ])
+            }),
+            notification: { notificationList: [], notificationMap: {} }
+        })
+    })
+    expect(fetchMock.mock.calls).toEqual([
+        [
+            'http://127.0.0.1:8000/vran/api/contributions/chunk/0/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ],
+        [
+            'http://127.0.0.1:8000/vran/api/contributions/chunk/5000/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ]
+    ])
+    await waitFor(() => {
+        screen.getByText(nameTest0)
+    })
+    const label1 = screen.getByText(nameTest1)
+    label1.click()
+    await waitFor(() => {
+        const navigateMock = useNavigate() as jest.Mock
+        expect(navigateMock.mock.calls).toEqual([[`/contribute/${idTest1}`]])
+    })
+})
+test('error', async () => {
+    const fetchMock = jest.fn()
+    const errorMsg = 'error loading credentials'
+    addResponseSequence(fetchMock, [
+        [200, { contributions: [contributionResponse0, contributionResponse1] }],
+        [500, { msg: errorMsg }]
+    ])
+    const { store } = renderWithProviders(<ContributionList />, fetchMock)
+    await waitFor(() => {
+        expect(store.getState()).toEqual({
+            contribution: expect.objectContaining({
+                contributions: newRemote([])
+            }),
+            notification: {
+                notificationList: [
+                    expect.objectContaining({
+                        type: NotificationType.Error,
+                        msg: `Could not load contributions. Reason: "${errorMsg}".`
+                    })
+                ],
+                notificationMap: expect.anything()
+            }
+        })
+    })
+    expect(fetchMock.mock.calls).toEqual([
+        [
+            'http://127.0.0.1:8000/vran/api/contributions/chunk/0/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ],
+        [
+            'http://127.0.0.1:8000/vran/api/contributions/chunk/5000/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ]
+    ])
+})

ui/src/contribution/components.tsx
@@ -9,7 +9,6 @@ import {
     ModalBody,
     Row
 } from 'react-bootstrap'
-import { useContribution } from './hooks'
 import { Contribution, ContributionStep, contributionIsReady } from './state'
 import { Formik, FormikErrors, FormikTouched } from 'formik'
 import { HandleChange, SetFieldValue } from '../util/type'
@@ -17,29 +16,27 @@ import { ChangeEvent, FormEvent, ReactElement, useEffect, useRef } from 'react'
 import { FormField } from '../util/form'
 import { useNavigate } from 'react-router-dom'
 import { StepHeader } from '../util/components/stepper'
-import { useAppDispatch } from '../hooks'
-import { uploadContribution } from './thunks'
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { getContributionList, uploadContribution } from './thunks'
+import { selectContributionList, selectShowAddContribution } from './selectors'
+import { VrAnLoading } from '../util/components/misc'
+import { toggleShowAddContribution } from './slice'
 
 export function ContributionList() {
-    const {
-        contributions,
-        showAddContribution,
-        loadContributionsCallback,
-        toggleShowAddContributionCallback
-    } = useContribution()
+    const dispatch = useAppDispatch()
+    const contributions = useAppSelector(selectContributionList)
+    const showAddContribution = useAppSelector(selectShowAddContribution)
     useEffect(() => {
-        loadContributionsCallback()
+        if (!contributions.isLoading) {
+            dispatch(getContributionList())
+        }
         //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [showAddContribution])
     if (contributions.isLoading) {
-        return (
-            <div className="vran-table-container-outer">
-                <div className="vran-table-container-inner">
-                    <div className="shimmer"></div>
-                </div>
-            </div>
-        )
+        return <VrAnLoading />
     }
+    const toggleShowAddContributionCallback = () =>
+        dispatch(toggleShowAddContribution())
     return (
         <Col className="d-flex flex-column h-100 ps-1 pe-1">
             <Row className="justify-content-end ps-2 pe-4 mb-2">
@@ -59,7 +56,7 @@ export function ContributionList() {
                         />
                     ))}
                 </ListGroup>
-                <Modal show={showAddContribution.value}>
+                <Modal show={showAddContribution}>
                     <Modal.Header
                         className="bg-primary"
                         closeButton
@@ -242,7 +239,7 @@ export function UploadFormBody({
     const buttonRef = useRef(null)
 
     return (
-        <Form noValidate onSubmit={handleSubmit} ref={containerRef}>
+        <Form noValidate onSubmit={handleSubmit} ref={containerRef} role="form">
             <FormField
                 name="name"
                 handleChange={handleChange}

Leibniz-HBI/CoSMA-Editor

ui/src/contribution/__tests__/get_contributions_integration.test.tsx
@@ -1,0 +1,199 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { RenderOptions, render, waitFor, screen } from '@testing-library/react'
+import {
+    NotificationManager,
+    NotificationType,
+    notificationReducer
+} from '../../util/notification/slice'
+import { ContributionState, contributionSlice, newContributionState } from '../slice'
+import { configureStore } from '@reduxjs/toolkit'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { ContributionList } from '../components'
+import { newRemote } from '../../util/state'
+import { ContributionStep, newContribution } from '../state'
+import { useNavigate } from 'react-router-dom'
+
+jest.mock('react-router-dom', () => {
+    const mockNavigate = jest.fn()
+    return {
+        useNavigate: jest.fn().mockReturnValue(mockNavigate)
+    }
+})
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: {
+        contribution: ContributionState
+        notification: NotificationManager
+    }
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    fetchMock: jest.Mock,
+    {
+        preloadedState = {
+            contribution: newContributionState({}),
+            notification: { notificationList: [], notificationMap: {} }
+        },
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    const store = configureStore({
+        reducer: {
+            contribution: contributionSlice.reducer,
+            notification: notificationReducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({ thunk: { extraArgument: fetchMock } }),
+        preloadedState
+    })
+    function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>
+    }
+
+    // Return an object with the store and all of RTL's query functions
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+function addResponseSequence(mock: jest.Mock, responses: [number, unknown][]) {
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        mock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const nameTest0 = 'contribution test 0'
+const descriptionTest0 = 'a contribution for tests'
+const idTest0 = 'id-test-0'
+const authorTest1 = 'author test 1'
+const contributionResponse0 = {
+    name: nameTest0,
+    description: descriptionTest0,
+    id_persistent: idTest0,
+    author: authorTest1,
+    has_header: false,
+    state: 'UPLOADED'
+}
+const nameTest1 = 'contribution test 1'
+const descriptionTest1 = 'another contribution for tests'
+const idTest1 = 'id-test-1'
+const contributionResponse1 = {
+    name: nameTest1,
+    description: descriptionTest1,
+    id_persistent: idTest1,
+    has_header: true,
+    state: 'VALUES_ASSIGNED',
+    author: authorTest1
+}
+test('success and open', async () => {
+    const fetchMock = jest.fn()
+    addResponseSequence(fetchMock, [
+        [200, { contributions: [contributionResponse0, contributionResponse1] }],
+        [200, { contributions: [] }]
+    ])
+    const { store } = renderWithProviders(<ContributionList />, fetchMock)
+    await waitFor(() => {
+        expect(store.getState()).toEqual({
+            contribution: expect.objectContaining({
+                contributions: newRemote([
+                    newContribution({
+                        name: nameTest0,
+                        idPersistent: idTest0,
+                        description: descriptionTest0,
+                        step: ContributionStep.Uploaded,
+                        hasHeader: false,
+                        author: authorTest1
+                    }),
+                    newContribution({
+                        name: nameTest1,
+                        idPersistent: idTest1,
+                        description: descriptionTest1,
+                        step: ContributionStep.ValuesAssigned,
+                        hasHeader: true,
+                        author: authorTest1
+                    })
+                ])
+            }),
+            notification: { notificationList: [], notificationMap: {} }
+        })
+    })
+    expect(fetchMock.mock.calls).toEqual([
+        [
+            'http://127.0.0.1:8000/cosmae/api/contributions/chunk/0/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ],
+        [
+            'http://127.0.0.1:8000/cosmae/api/contributions/chunk/5000/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ]
+    ])
+    await waitFor(() => {
+        screen.getByText(nameTest0)
+    })
+    const label1 = screen.getByText(nameTest1)
+    label1.click()
+    await waitFor(() => {
+        const navigateMock = useNavigate() as jest.Mock
+        expect(navigateMock.mock.calls).toEqual([[`/contribute/${idTest1}`]])
+    })
+})
+test('error', async () => {
+    const fetchMock = jest.fn()
+    const errorMsg = 'error loading credentials'
+    addResponseSequence(fetchMock, [
+        [200, { contributions: [contributionResponse0, contributionResponse1] }],
+        [500, { msg: errorMsg }]
+    ])
+    const { store } = renderWithProviders(<ContributionList />, fetchMock)
+    await waitFor(() => {
+        expect(store.getState()).toEqual({
+            contribution: expect.objectContaining({
+                contributions: newRemote([])
+            }),
+            notification: {
+                notificationList: [
+                    expect.objectContaining({
+                        type: NotificationType.Error,
+                        msg: `Could not load contributions. Reason: "${errorMsg}".`
+                    })
+                ],
+                notificationMap: expect.anything()
+            }
+        })
+    })
+    expect(fetchMock.mock.calls).toEqual([
+        [
+            'http://127.0.0.1:8000/cosmae/api/contributions/chunk/0/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ],
+        [
+            'http://127.0.0.1:8000/cosmae/api/contributions/chunk/5000/5000',
+            {
+                credentials: 'include',
+                method: 'GET',
+                headers: { 'Access-Control-Allow-Credentials': 'true' }
+            }
+        ]
+    ])
+})

ui/src/contribution/components.tsx
@@ -9,7 +9,6 @@ import {
     ModalBody,
     Row
 } from 'react-bootstrap'
-import { useContribution } from './hooks'
 import { Contribution, ContributionStep, contributionIsReady } from './state'
 import { Formik, FormikErrors, FormikTouched } from 'formik'
 import { HandleChange, SetFieldValue } from '../util/type'
@@ -17,29 +16,27 @@ import { ChangeEvent, FormEvent, ReactElement, useEffect, useRef } from 'react'
 import { FormField } from '../util/form'
 import { useNavigate } from 'react-router-dom'
 import { StepHeader } from '../util/components/stepper'
-import { useAppDispatch } from '../hooks'
-import { uploadContribution } from './thunks'
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { getContributionList, uploadContribution } from './thunks'
+import { selectContributionList, selectShowAddContribution } from './selectors'
+import { CosmaeLoading } from '../util/components/misc'
+import { toggleShowAddContribution } from './slice'
 
 export function ContributionList() {
-    const {
-        contributions,
-        showAddContribution,
-        loadContributionsCallback,
-        toggleShowAddContributionCallback
-    } = useContribution()
+    const dispatch = useAppDispatch()
+    const contributions = useAppSelector(selectContributionList)
+    const showAddContribution = useAppSelector(selectShowAddContribution)
     useEffect(() => {
-        loadContributionsCallback()
+        if (!contributions.isLoading) {
+            dispatch(getContributionList())
+        }
         //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [showAddContribution])
     if (contributions.isLoading) {
-        return (
-            <div className="cosmae-table-container-outer">
-                <div className="cosmae-table-container-inner">
-                    <div className="shimmer"></div>
-                </div>
-            </div>
-        )
+        return <CosmaeLoading />
     }
+    const toggleShowAddContributionCallback = () =>
+        dispatch(toggleShowAddContribution())
     return (
         <Col className="d-flex flex-column h-100 ps-1 pe-1">
             <Row className="justify-content-end ps-2 pe-4 mb-2">
@@ -59,7 +56,7 @@ export function ContributionList() {
                         />
                     ))}
                 </ListGroup>
-                <Modal show={showAddContribution.value}>
+                <Modal show={showAddContribution}>
                     <Modal.Header
                         className="bg-primary"
                         closeButton
@@ -242,7 +239,7 @@ export function UploadFormBody({
     const buttonRef = useRef(null)
 
     return (
-        <Form noValidate onSubmit={handleSubmit} ref={containerRef}>
+        <Form noValidate onSubmit={handleSubmit} ref={containerRef} role="form">
             <FormField
                 name="name"
                 handleChange={handleChange}