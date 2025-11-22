Leibniz-HBI/DP-REX

Add possibility of editing values.

ui/src/App.tsx
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
-        <div className="vran-container">
-            <div className="vran-header">VrAN</div>
-            <div className="vran-body">
-                <PersonTable />
+        <div>
+            <div className="vran-container">
+                <div className="vran-header">VrAN</div>
+                <div className="vran-body">
+                    <PersonTable />
+                </div>
             </div>
+            <div id="portal" />
         </div>
     )
 }

Leibniz-HBI/CoSMA-Editor

ui/src/App.tsx
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
-        <div className="cosmae-container">
-            <div className="cosmae-header">CoSMA-E</div>
-            <div className="cosmae-body">
-                <PersonTable />
+        <div>
+            <div className="cosmae-container">
+                <div className="cosmae-header">CoSMA-E</div>
+                <div className="cosmae-body">
+                    <PersonTable />
+                </div>
             </div>
+            <div id="portal" />
         </div>
     )
 }