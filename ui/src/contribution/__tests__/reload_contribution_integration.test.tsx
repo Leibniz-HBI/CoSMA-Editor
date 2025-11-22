Leibniz-HBI/DP-REX

Auto reload in contribution steps when waiting for data.

ui/src/contribution/__tests__/reload_contribution_integration.test.tsx
@@ -1,0 +1,113 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { RenderOptions, render, waitFor, screen } from '@testing-library/react'
+import { ContributionState, contributionSlice, newContributionState } from '../slice'
+import { NotificationManager, notificationReducer } from '../../util/notification/slice'
+import { configureStore } from '@reduxjs/toolkit'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { ContributionStepper } from '../components'
+
+jest.mock('react-router-dom', () => {
+    const mockNavigate = jest.fn()
+    return {
+        useNavigate: jest.fn().mockReturnValue(mockNavigate),
+        useLoaderData: jest.fn().mockReturnValue('id-test-1')
+    }
+})
+jest.mock('../../config', () => {
+    return { ...jest.requireActual('../../config'), secondDelay: 100 }
+})
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: {
+        contribution: ContributionState
+        notification: NotificationManager
+    }
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    fetchMock: jest.Mock,
+    {
+        preloadedState = {
+            contribution: newContributionState({}),
+            notification: { notificationList: [], notificationMap: {} }
+        },
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    const store = configureStore({
+        reducer: {
+            contribution: contributionSlice.reducer,
+            notification: notificationReducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({ thunk: { extraArgument: fetchMock } }),
+        preloadedState
+    })
+    function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>
+    }
+
+    // Return an object with the store and all of RTL's query functions
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+function addResponseSequence(mock: jest.Mock, responses: [number, unknown][]) {
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        mock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const nameTest1 = 'contribution test 1'
+const descriptionTest1 = 'another contribution for tests'
+const idTest1 = 'id-test-1'
+const authorTest1 = 'author test 1'
+const contributionResponse1 = {
+    name: nameTest1,
+    description: descriptionTest1,
+    id_persistent: idTest1,
+    has_header: true,
+    state: 'COLUMNS_ASSIGNED',
+    author: authorTest1
+}
+test('reloads automatically', async () => {
+    const fetchMock = jest.fn()
+    addResponseSequence(fetchMock, [
+        [200, { ...contributionResponse1 }],
+        [200, { ...contributionResponse1 }],
+        [200, { ...contributionResponse1, state: 'VALUES_EXTRACTED' }]
+    ])
+    const testBodyText = 'body of contribution step for tests'
+    renderWithProviders(
+        <ContributionStepper selectedIdx={2}>
+            <div>{testBodyText}</div>
+        </ContributionStepper>,
+        fetchMock
+    )
+    await waitFor(() => {
+        screen.getByText(/not yet available/i)
+    })
+    await waitFor(
+        () => {
+            expect(fetchMock.mock.calls.length).toEqual(3)
+            for (let idx = 0; idx < 3; ++idx) {
+                expect(fetchMock.mock.calls[idx]).toEqual([
+                    'http://127.0.0.1:8000/vran/api/contributions/id-test-1',
+                    { credentials: 'include' }
+                ])
+            }
+        },
+        { timeout: 2000 }
+    )
+    screen.getByText(testBodyText)
+})

ui/src/contribution/complete/components.tsx
@@ -8,22 +8,23 @@ import { VrAnLoading } from '../../util/components/misc'
 export function CompleteStep() {
     const contribution = useAppSelector(selectContribution)
     const navigate = useNavigate()
+    let body
     if (contribution.value === undefined || contribution.isLoading) {
-        return <VrAnLoading />
-    }
-    return (
-        <ContributionStepper selectedIdx={3}>
+        body = <VrAnLoading />
+    } else {
+        body = (
             <>
-                <Row>
+                <Row className="justify-content-center">
                     All entities were assigned. Please review the merge requests created
                     for this contribution
                 </Row>
-                <Row>
+                <Row className="justify-content-center">
                     <Button onClick={() => navigate('/review')}>
                         Review Merge Requests
                     </Button>
                 </Row>
             </>
-        </ContributionStepper>
-    )
+        )
+    }
+    return <ContributionStepper selectedIdx={3}>{body}</ContributionStepper>
 }

Leibniz-HBI/CoSMA-Editor

ui/src/contribution/__tests__/reload_contribution_integration.test.tsx
@@ -1,0 +1,113 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { RenderOptions, render, waitFor, screen } from '@testing-library/react'
+import { ContributionState, contributionSlice, newContributionState } from '../slice'
+import { NotificationManager, notificationReducer } from '../../util/notification/slice'
+import { configureStore } from '@reduxjs/toolkit'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { ContributionStepper } from '../components'
+
+jest.mock('react-router-dom', () => {
+    const mockNavigate = jest.fn()
+    return {
+        useNavigate: jest.fn().mockReturnValue(mockNavigate),
+        useLoaderData: jest.fn().mockReturnValue('id-test-1')
+    }
+})
+jest.mock('../../config', () => {
+    return { ...jest.requireActual('../../config'), secondDelay: 100 }
+})
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: {
+        contribution: ContributionState
+        notification: NotificationManager
+    }
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    fetchMock: jest.Mock,
+    {
+        preloadedState = {
+            contribution: newContributionState({}),
+            notification: { notificationList: [], notificationMap: {} }
+        },
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    const store = configureStore({
+        reducer: {
+            contribution: contributionSlice.reducer,
+            notification: notificationReducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({ thunk: { extraArgument: fetchMock } }),
+        preloadedState
+    })
+    function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>
+    }
+
+    // Return an object with the store and all of RTL's query functions
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+function addResponseSequence(mock: jest.Mock, responses: [number, unknown][]) {
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        mock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const nameTest1 = 'contribution test 1'
+const descriptionTest1 = 'another contribution for tests'
+const idTest1 = 'id-test-1'
+const authorTest1 = 'author test 1'
+const contributionResponse1 = {
+    name: nameTest1,
+    description: descriptionTest1,
+    id_persistent: idTest1,
+    has_header: true,
+    state: 'COLUMNS_ASSIGNED',
+    author: authorTest1
+}
+test('reloads automatically', async () => {
+    const fetchMock = jest.fn()
+    addResponseSequence(fetchMock, [
+        [200, { ...contributionResponse1 }],
+        [200, { ...contributionResponse1 }],
+        [200, { ...contributionResponse1, state: 'VALUES_EXTRACTED' }]
+    ])
+    const testBodyText = 'body of contribution step for tests'
+    renderWithProviders(
+        <ContributionStepper selectedIdx={2}>
+            <div>{testBodyText}</div>
+        </ContributionStepper>,
+        fetchMock
+    )
+    await waitFor(() => {
+        screen.getByText(/not yet available/i)
+    })
+    await waitFor(
+        () => {
+            expect(fetchMock.mock.calls.length).toEqual(3)
+            for (let idx = 0; idx < 3; ++idx) {
+                expect(fetchMock.mock.calls[idx]).toEqual([
+                    'http://127.0.0.1:8000/cosmae/api/contributions/id-test-1',
+                    { credentials: 'include' }
+                ])
+            }
+        },
+        { timeout: 2000 }
+    )
+    screen.getByText(testBodyText)
+})

ui/src/contribution/complete/components.tsx
@@ -8,22 +8,23 @@ import { CosmaeLoading } from '../../util/components/misc'
 export function CompleteStep() {
     const contribution = useAppSelector(selectContribution)
     const navigate = useNavigate()
+    let body
     if (contribution.value === undefined || contribution.isLoading) {
-        return <CosmaeLoading />
-    }
-    return (
-        <ContributionStepper selectedIdx={3}>
+        body = <CosmaeLoading />
+    } else {
+        body = (
             <>
-                <Row>
+                <Row className="justify-content-center">
                     All entities were assigned. Please review the merge requests created
                     for this contribution
                 </Row>
-                <Row>
+                <Row className="justify-content-center">
                     <Button onClick={() => navigate('/review')}>
                         Review Merge Requests
                     </Button>
                 </Row>
             </>
-        </ContributionStepper>
-    )
+        )
+    }
+    return <ContributionStepper selectedIdx={3}>{body}</ContributionStepper>
 }