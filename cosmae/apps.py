Leibniz-HBI/DP-REX

Add Backend functionality for setting user permissions.

vran/apps.py
@@ -3,6 +3,7 @@ import logging
 from typing import List
 
 from django.apps import AppConfig, apps
+from django.conf import settings
 from django.core.exceptions import AppRegistryNotReady
 from django.db.backends.signals import connection_created
 from django.db.models.signals import post_migrate, post_save
@@ -83,42 +84,47 @@ class VranConfig(AppConfig):
             add_superuser, dispatch_uid="vran.create_initial_superuser"
         )
         try:
-            # pylint: disable=import-outside-toplevel
-            from vran.contribution.models_django import ContributionCandidate
-            from vran.contribution.tag_definition.queue import dispatch_read_csv_head
+            if not settings.IS_UNITTEST:
+                # pylint: disable=import-outside-toplevel
+                from vran.contribution.models_django import ContributionCandidate
+                from vran.contribution.tag_definition.queue import (
+                    dispatch_read_csv_head,
+                )
 
-            post_save.connect(
-                dispatch_read_csv_head,
-                sender=ContributionCandidate,
-                dispatch_uid="vran.start_tag_extraction",
-            )
-            from vran.merge_request.models_django import MergeRequest
-            from vran.merge_request.queue import dispatch_merge_request_queue_process
+                post_save.connect(
+                    dispatch_read_csv_head,
+                    sender=ContributionCandidate,
+                    dispatch_uid="vran.start_tag_extraction",
+                )
+                from vran.merge_request.models_django import MergeRequest
+                from vran.merge_request.queue import (
+                    dispatch_merge_request_queue_process,
+                )
 
-            post_save.connect(
-                dispatch_merge_request_queue_process,
-                sender=MergeRequest,
-                dispatch_uid="vran_merge_request_queue",
-            )
-            from django_rq import enqueue
+                post_save.connect(
+                    dispatch_merge_request_queue_process,
+                    sender=MergeRequest,
+                    dispatch_uid="vran_merge_request_queue",
+                )
+                from django_rq import enqueue
 
-            from vran.tag.models_django import TagDefinition
-            from vran.tag.queue import (
-                dispatch_tag_definition_queue_process,
-                update_tag_definition_name_path,
-            )
+                from vran.tag.models_django import TagDefinition
+                from vran.tag.queue import (
+                    dispatch_tag_definition_queue_process,
+                    update_tag_definition_name_path,
+                )
 
-            try:
-                roots = TagDefinition.most_recent_children(None)
-                for root in roots:
-                    enqueue(update_tag_definition_name_path, root.id_persistent, [])
-            except OperationalError:
-                pass  #
-            post_save.connect(
-                dispatch_tag_definition_queue_process,
-                sender=TagDefinition,
-                dispatch_uid="vran_tag_definition_queue",
-            )
+                try:
+                    roots = TagDefinition.most_recent_children(None)
+                    for root in roots:
+                        enqueue(update_tag_definition_name_path, root.id_persistent, [])
+                except OperationalError:
+                    pass  #
+                post_save.connect(
+                    dispatch_tag_definition_queue_process,
+                    sender=TagDefinition,
+                    dispatch_uid="vran_tag_definition_queue",
+                )
         except AppRegistryNotReady:
             pass
         super().ready()

tests/conftest.py
@@ -95,6 +95,33 @@ def auth_server1(auth_server):
 
 
 @pytest.fixture
+def auth_server_commissioner(live_server):
+    user_id_persistent = UUID(cu.test_uuid_commissioner)
+    uuidMock = MagicMock(return_value=user_id_persistent)
+    with patch("vran.user.api.uuid4", uuidMock):
+        rsp = post_register(
+            live_server.url,
+            {
+                "user_name": cu.test_username_commissioner,
+                "password": cu.test_password_commissioner,
+                "email": cu.test_email_commissioner,
+                "names_personal": cu.test_names_personal_commissioner,
+            },
+        )
+    user = VranUser.objects.filter(id_persistent=user_id_persistent).get()
+    user.permission_group = VranUser.COMMISSIONER
+    user.save()
+    rsp = post_login(
+        live_server.url,
+        {
+            "name": cu.test_username_commissioner,
+            "password": cu.test_password_commissioner,
+        },
+    )
+    return live_server, rsp.cookies
+
+
+@pytest.fixture
 def user(db):  # pylint: disable=unused-argument
     try:
         user = VranUser.objects.create_user(
@@ -122,3 +149,14 @@ def user1(db):  # pylint: disable=unused-argument
         return user
     except IntegrityError:
         return VranUser.objects.get(email=cu.test_email1)  # pylint: disable=no-member
+
+
+@pytest.fixture
+def super_user(db):  # pylint: disable=unused-argument
+    super_user = VranUser.objects.create_superuser(
+        email=cu.test_email_super,
+        username=cu.test_username_super,
+        password=cu.test_password,
+        id_persistent=cu.test_uuid_super,
+    )
+    return super_user

tests/user/api/integration/get_user_chunk_test.py
@@ -1,0 +1,69 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.user.api.integration.requests as req
+from vran.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server):
+    "Test the correct response when user can not be authenticated."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.user.api.check_user", mock):
+        rsp = req.get_user_chunk(server.url, 0, 1, cookies=cookies)
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Test the correct response for missing user data in request"
+    server, _ = auth_server
+    rsp = req.get_user_chunk(server.url, 0, 1)
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server):
+    """Test if a change permission group request is denied,
+    when the requesting user has insufficient permissions."""
+    server, cookies = auth_server
+    rsp = req.get_user_chunk(server.url, 0, 1, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_exceed_max_chunk_size(auth_server_commissioner):
+    """Test whether a large chunk results in a bad request response."""
+    server, cookies = auth_server_commissioner
+    rsp = req.get_user_chunk(server.url, 0, 5001, cookies=cookies)
+    assert rsp.status_code == 400
+
+
+def test_can_chunk(auth_server_commissioner, user, user1):
+    """Test if the get user call works correctly"""
+    server, cookies = auth_server_commissioner
+    rsp = req.get_user_chunk(server.url, 0, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 2
+    next_offset = json["next_offset"]
+    rsp = req.get_user_chunk(server.url, next_offset, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 1
+    next_offset = json["next_offset"]
+    rsp = req.get_user_chunk(server.url, next_offset, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 0
+
+
+def test_excludes_super_user(auth_server_commissioner, super_user):
+    """Test whether super user accounts are excluded."""
+    server, cookies = auth_server_commissioner
+    rsp = req.get_user_chunk(server.url, 0, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 1

tests/user/api/integration/put_permission_group_test.py
@@ -1,0 +1,105 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.user.api.integration.requests as req
+import tests.user.common as c
+from vran.exception import NotAuthenticatedException
+from vran.util import VranUser
+
+
+def test_unknown_user(auth_server):
+    "Test the correct response when user can not be authenticated."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.user.api.check_user", mock):
+        rsp = req.put_permission_group(
+            server.url,
+            "7ed0d5d0-99e3-4445-846c-27afa6324116",
+            "CONTRIBUTOR",
+            cookies=cookies,
+        )
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Test the correct response for missing user data in request"
+    server, _ = auth_server
+    rsp = req.put_permission_group(
+        server.url,
+        "7ed0d5d0-99e3-4445-846c-27afa6324116",
+        "CONTRIBUTOR",
+    )
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server):
+    """Test if a change permission group request is denied,
+    when the requesting user has insufficient permissions."""
+    server, cookies = auth_server
+    rsp = req.put_permission_group(
+        server.url,
+        "7ed0d5d0-99e3-4445-846c-27afa6324116",
+        "CONTRIBUTOR",
+        cookies=cookies,
+    )
+    assert rsp.status_code == 403
+
+
+def test_can_not_change_for_self(auth_server_commissioner):
+    """Test that you can not change your own permissions.
+    This is intended to ensure that there is always a commissioner."""
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, c.test_uuid_commissioner, "APPLICANT", cookies=cookies
+    )
+    assert rsp.status_code == 400
+    db_user = VranUser.objects.filter(  # pylint: disable=no-member
+        id_persistent=c.test_uuid_commissioner
+    ).get()
+    assert db_user.permission_group == VranUser.COMMISSIONER
+
+
+def test_can_change_for_other(auth_server_commissioner, user):
+    """Test if you can change the permission group of a user."""
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, user.id_persistent, "EDITOR", cookies=cookies
+    )
+    assert rsp.status_code == 200
+    db_user = VranUser.objects.filter(  # pylint: disable=no-member
+        id_persistent=user.id_persistent
+    ).get()
+    assert db_user.permission_group == VranUser.EDITOR
+
+
+def test_non_existent_user(auth_server_commissioner):
+    """Test for the correct status code when requesting
+    to change the permission_group of a user who does not exist."""
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, "7ed0d5d0-99e3-4445-846c-27afa6324116", "EDITOR", cookies=cookies
+    )
+    assert rsp.status_code == 404
+
+
+def test_unknown_permission_group(auth_server_commissioner, user):
+    "Test the correct error code when attempting to set an unknown permission group."
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, user.id_persistent, "unknown", cookies=cookies
+    )
+    assert rsp.status_code == 400
+
+
+def test_no_superuser_permission(auth_server_commissioner, super_user):
+    "Test that the permission_group of a super_user can not be changed."
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, super_user.id_persistent, "EDITOR", cookies=cookies
+    )
+    assert rsp.status_code == 400
+    db_user = VranUser.objects.filter(  # pylint: disable=no-member
+        id_persistent=super_user.id_persistent
+    ).get()
+    assert db_user.permission_group == VranUser.APPLICANT

tests/user/api/integration/requests.py
@@ -47,3 +47,20 @@ def post_change_tag_definitions(url, start_idx, end_idx, cookies=None):
         cookies=cookies,
         timeout=900,
     )
+
+
+def get_user_chunk(url, offset, limit, cookies=None):
+    return requests.get(
+        url + f"/vran/api/user/chunks/{offset}/{limit}",
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def put_permission_group(url, id_user_persistent, permission_group, cookies=None):
+    return requests.put(
+        url + f"/vran/api/user/{id_user_persistent}/permission_group",
+        json={"permission_group": permission_group},
+        cookies=cookies,
+        timeout=900,
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/apps.py
@@ -1,8 +1,10 @@
 """Django app configuration for CoSMA-Editor"""
+
 import logging
 from typing import List
 
 from django.apps import AppConfig, apps
+from django.conf import settings
 from django.core.exceptions import AppRegistryNotReady
 from django.db.backends.signals import connection_created
 from django.db.models.signals import post_migrate, post_save
@@ -83,42 +85,47 @@ class CosmaeConfig(AppConfig):
             add_superuser, dispatch_uid="cosmae.create_initial_superuser"
         )
         try:
-            # pylint: disable=import-outside-toplevel
-            from cosmae.contribution.models_django import ContributionCandidate
-            from cosmae.contribution.tag_definition.queue import dispatch_read_csv_head
+            if not settings.IS_UNITTEST:
+                # pylint: disable=import-outside-toplevel
+                from cosmae.contribution.models_django import ContributionCandidate
+                from cosmae.contribution.tag_definition.queue import (
+                    dispatch_read_csv_head,
+                )
 
-            post_save.connect(
-                dispatch_read_csv_head,
-                sender=ContributionCandidate,
-                dispatch_uid="cosmae.start_tag_extraction",
-            )
-            from cosmae.merge_request.models_django import MergeRequest
-            from cosmae.merge_request.queue import dispatch_merge_request_queue_process
+                post_save.connect(
+                    dispatch_read_csv_head,
+                    sender=ContributionCandidate,
+                    dispatch_uid="cosmae.start_tag_extraction",
+                )
+                from cosmae.merge_request.models_django import MergeRequest
+                from cosmae.merge_request.queue import (
+                    dispatch_merge_request_queue_process,
+                )
 
-            post_save.connect(
-                dispatch_merge_request_queue_process,
-                sender=MergeRequest,
-                dispatch_uid="cosmae_merge_request_queue",
-            )
-            from django_rq import enqueue
+                post_save.connect(
+                    dispatch_merge_request_queue_process,
+                    sender=MergeRequest,
+                    dispatch_uid="cosmae_merge_request_queue",
+                )
+                from django_rq import enqueue
 
-            from cosmae.tag.models_django import TagDefinition
-            from cosmae.tag.queue import (
-                dispatch_tag_definition_queue_process,
-                update_tag_definition_name_path,
-            )
+                from cosmae.tag.models_django import TagDefinition
+                from cosmae.tag.queue import (
+                    dispatch_tag_definition_queue_process,
+                    update_tag_definition_name_path,
+                )
 
-            try:
-                roots = TagDefinition.most_recent_children(None)
-                for root in roots:
-                    enqueue(update_tag_definition_name_path, root.id_persistent, [])
-            except OperationalError:
-                pass  #
-            post_save.connect(
-                dispatch_tag_definition_queue_process,
-                sender=TagDefinition,
-                dispatch_uid="cosmae_tag_definition_queue",
-            )
+                try:
+                    roots = TagDefinition.most_recent_children(None)
+                    for root in roots:
+                        enqueue(update_tag_definition_name_path, root.id_persistent, [])
+                except OperationalError:
+                    pass  #
+                post_save.connect(
+                    dispatch_tag_definition_queue_process,
+                    sender=TagDefinition,
+                    dispatch_uid="cosmae_tag_definition_queue",
+                )
         except AppRegistryNotReady:
             pass
         super().ready()

tests/conftest.py
@@ -95,6 +95,33 @@ def auth_server1(auth_server):
 
 
 @pytest.fixture
+def auth_server_commissioner(live_server):
+    user_id_persistent = UUID(cu.test_uuid_commissioner)
+    uuidMock = MagicMock(return_value=user_id_persistent)
+    with patch("cosmae.user.api.uuid4", uuidMock):
+        rsp = post_register(
+            live_server.url,
+            {
+                "user_name": cu.test_username_commissioner,
+                "password": cu.test_password_commissioner,
+                "email": cu.test_email_commissioner,
+                "names_personal": cu.test_names_personal_commissioner,
+            },
+        )
+    user = CosmaeUser.objects.filter(id_persistent=user_id_persistent).get()
+    user.permission_group = CosmaeUser.COMMISSIONER
+    user.save()
+    rsp = post_login(
+        live_server.url,
+        {
+            "name": cu.test_username_commissioner,
+            "password": cu.test_password_commissioner,
+        },
+    )
+    return live_server, rsp.cookies
+
+
+@pytest.fixture
 def user(db):  # pylint: disable=unused-argument
     try:
         user = CosmaeUser.objects.create_user(
@@ -122,3 +149,14 @@ def user1(db):  # pylint: disable=unused-argument
         return user
     except IntegrityError:
         return CosmaeUser.objects.get(email=cu.test_email1)  # pylint: disable=no-member
+
+
+@pytest.fixture
+def super_user(db):  # pylint: disable=unused-argument
+    super_user = CosmaeUser.objects.create_superuser(
+        email=cu.test_email_super,
+        username=cu.test_username_super,
+        password=cu.test_password,
+        id_persistent=cu.test_uuid_super,
+    )
+    return super_user

tests/user/api/integration/get_user_chunk_test.py
@@ -1,0 +1,69 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.user.api.integration.requests as req
+from cosmae.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server):
+    "Test the correct response when user can not be authenticated."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.user.api.check_user", mock):
+        rsp = req.get_user_chunk(server.url, 0, 1, cookies=cookies)
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Test the correct response for missing user data in request"
+    server, _ = auth_server
+    rsp = req.get_user_chunk(server.url, 0, 1)
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server):
+    """Test if a change permission group request is denied,
+    when the requesting user has insufficient permissions."""
+    server, cookies = auth_server
+    rsp = req.get_user_chunk(server.url, 0, 1, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_exceed_max_chunk_size(auth_server_commissioner):
+    """Test whether a large chunk results in a bad request response."""
+    server, cookies = auth_server_commissioner
+    rsp = req.get_user_chunk(server.url, 0, 5001, cookies=cookies)
+    assert rsp.status_code == 400
+
+
+def test_can_chunk(auth_server_commissioner, user, user1):
+    """Test if the get user call works correctly"""
+    server, cookies = auth_server_commissioner
+    rsp = req.get_user_chunk(server.url, 0, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 2
+    next_offset = json["next_offset"]
+    rsp = req.get_user_chunk(server.url, next_offset, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 1
+    next_offset = json["next_offset"]
+    rsp = req.get_user_chunk(server.url, next_offset, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 0
+
+
+def test_excludes_super_user(auth_server_commissioner, super_user):
+    """Test whether super user accounts are excluded."""
+    server, cookies = auth_server_commissioner
+    rsp = req.get_user_chunk(server.url, 0, 2, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    user_list = json["user_list"]
+    assert len(user_list) == 1

tests/user/api/integration/put_permission_group_test.py
@@ -1,0 +1,105 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.user.api.integration.requests as req
+import tests.user.common as c
+from cosmae.exception import NotAuthenticatedException
+from cosmae.util import CosmaeUser
+
+
+def test_unknown_user(auth_server):
+    "Test the correct response when user can not be authenticated."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.user.api.check_user", mock):
+        rsp = req.put_permission_group(
+            server.url,
+            "7ed0d5d0-99e3-4445-846c-27afa6324116",
+            "CONTRIBUTOR",
+            cookies=cookies,
+        )
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Test the correct response for missing user data in request"
+    server, _ = auth_server
+    rsp = req.put_permission_group(
+        server.url,
+        "7ed0d5d0-99e3-4445-846c-27afa6324116",
+        "CONTRIBUTOR",
+    )
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server):
+    """Test if a change permission group request is denied,
+    when the requesting user has insufficient permissions."""
+    server, cookies = auth_server
+    rsp = req.put_permission_group(
+        server.url,
+        "7ed0d5d0-99e3-4445-846c-27afa6324116",
+        "CONTRIBUTOR",
+        cookies=cookies,
+    )
+    assert rsp.status_code == 403
+
+
+def test_can_not_change_for_self(auth_server_commissioner):
+    """Test that you can not change your own permissions.
+    This is intended to ensure that there is always a commissioner."""
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, c.test_uuid_commissioner, "APPLICANT", cookies=cookies
+    )
+    assert rsp.status_code == 400
+    db_user = CosmaeUser.objects.filter(  # pylint: disable=no-member
+        id_persistent=c.test_uuid_commissioner
+    ).get()
+    assert db_user.permission_group == CosmaeUser.COMMISSIONER
+
+
+def test_can_change_for_other(auth_server_commissioner, user):
+    """Test if you can change the permission group of a user."""
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, user.id_persistent, "EDITOR", cookies=cookies
+    )
+    assert rsp.status_code == 200
+    db_user = CosmaeUser.objects.filter(  # pylint: disable=no-member
+        id_persistent=user.id_persistent
+    ).get()
+    assert db_user.permission_group == CosmaeUser.EDITOR
+
+
+def test_non_existent_user(auth_server_commissioner):
+    """Test for the correct status code when requesting
+    to change the permission_group of a user who does not exist."""
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, "7ed0d5d0-99e3-4445-846c-27afa6324116", "EDITOR", cookies=cookies
+    )
+    assert rsp.status_code == 404
+
+
+def test_unknown_permission_group(auth_server_commissioner, user):
+    "Test the correct error code when attempting to set an unknown permission group."
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, user.id_persistent, "unknown", cookies=cookies
+    )
+    assert rsp.status_code == 400
+
+
+def test_no_superuser_permission(auth_server_commissioner, super_user):
+    "Test that the permission_group of a super_user can not be changed."
+    server, cookies = auth_server_commissioner
+    rsp = req.put_permission_group(
+        server.url, super_user.id_persistent, "EDITOR", cookies=cookies
+    )
+    assert rsp.status_code == 400
+    db_user = CosmaeUser.objects.filter(  # pylint: disable=no-member
+        id_persistent=super_user.id_persistent
+    ).get()
+    assert db_user.permission_group == CosmaeUser.APPLICANT

tests/user/api/integration/requests.py
@@ -47,3 +47,20 @@ def post_change_tag_definitions(url, start_idx, end_idx, cookies=None):
         cookies=cookies,
         timeout=900,
     )
+
+
+def get_user_chunk(url, offset, limit, cookies=None):
+    return requests.get(
+        url + f"/cosmae/api/user/chunks/{offset}/{limit}",
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def put_permission_group(url, id_user_persistent, permission_group, cookies=None):
+    return requests.put(
+        url + f"/cosmae/api/user/{id_user_persistent}/permission_group",
+        json={"permission_group": permission_group},
+        cookies=cookies,
+        timeout=900,
+    )