Leibniz-HBI/DP-REX

Switch user model and squash migrations

vran/admin.py
@@ -2,3 +2,30 @@
 from django.contrib import admin  # pylint: disable=unused-import
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+from vran.util import VranUser
+
+
+class VranUserChangeForm(UserChangeForm):
+    # pylint: disable=too-few-public-methods
+    "Admin form for changing users"
+
+    class Meta(UserChangeForm.Meta):
+        # pylint: disable=too-few-public-methods
+        "Meta class for admin user form."
+        model = VranUser
+
+
+class VranUserAdmin(UserAdmin):
+    # pylint: disable=too-few-public-methods
+    "Configure admin."
+    form = VranUserChangeForm
+
+    # fieldsets = UserAdmin.fieldsets + (
+    #         (None, {'fields': ('some_extra_data',)}),
+    # )
+
+
+admin.site.register(VranUser, VranUserAdmin)

vran/apps.py
@@ -1,5 +1,62 @@
 """Django app configuration for VrAN"""
-from django.apps import AppConfig
+import logging
+from typing import List
+
+from django.apps import AppConfig, apps
+from django.db.models.signals import post_migrate
+
+logger = logging.getLogger("vran.app_config")
+
+READ_PERMISSIONS = [
+    "view_person",
+    "view_entity",
+    "view_taginstance",
+    "view_tagdefinition",
+]
+
+WRITE_PERMISSIONS = [
+    "add_person",
+    "add_entity",
+    "add_taginstance",
+    "add_tagdefinition",
+]
+
+added_permissions = {
+    "VranGroup.APPLICANT": [],
+    "VranGroup.READER": [READ_PERMISSIONS],
+    "VranGroup.CONTRIBUTOR": [READ_PERMISSIONS],
+    "VranGroup.EDITOR": [READ_PERMISSIONS, WRITE_PERMISSIONS],
+    "VranGroup.COMMISSIONER": [READ_PERMISSIONS, WRITE_PERMISSIONS],
+}
+
+
+def add_permission_for_group(group: str, permission_list_list: List[List[str]]):
+    "Method for granting permissions to groups."
+    group_model = apps.get_model("auth", "Group")
+    permission_model = apps.get_model("auth", "Permission")
+    group_name = str(group)
+
+    group, _created_group = group_model.objects.get_or_create(name=group_name)
+    for permission_list in permission_list_list:
+        for permission_name in permission_list:
+            permission = permission_model.objects.get(codename=permission_name)
+            group.permissions.add(permission)
+    group.save()
+
+
+def add_permissions(
+    app_config: AppConfig, verbosity=2, **kwargs
+):  # pylint: disable=unused-argument
+    "Create all new groups and permissions."
+    logger.setLevel(10 * (4 - verbosity))
+    logger.info("Create VrAN groups.")
+    permission_model = apps.get_model("auth", "Permission")
+    for group, permissions in added_permissions.items():
+        try:
+            add_permission_for_group(group, permissions)
+        except permission_model.DoesNotExist:
+            logger.warning("Permissions do not exist yet.")
+            return
 
 
 class VranConfig(AppConfig):
@@ -7,3 +64,7 @@ class VranConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "vran"
+
+    def ready(self) -> None:
+        post_migrate.connect(add_permissions, dispatch_uid="vran.create_groups")
+        super().ready()

vran/migrations/0001_initial.py
@@ -1,6 +1,9 @@
-# Generated by Django 4.1.3 on 2022-11-28 12:19
+# Generated by Django 4.1.3 on 2023-04-11 07:54
 
+import django.contrib.auth.models
+import django.contrib.auth.validators
 import django.db.models.deletion
+import django.utils.timezone
 from django.db import migrations, models
 
 
@@ -8,9 +11,120 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = []
+    dependencies = [
+        ("auth", "0012_alter_user_first_name_max_length"),
+    ]
 
     operations = [
+        migrations.CreateModel(
+            name="VranUser",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "is_superuser",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates that this user has all permissions without explicitly assigning them.",
+                        verbose_name="superuser status",
+                    ),
+                ),
+                (
+                    "username",
+                    models.CharField(
+                        error_messages={
+                            "unique": "A user with that username already exists."
+                        },
+                        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+                        max_length=150,
+                        unique=True,
+                        validators=[
+                            django.contrib.auth.validators.UnicodeUsernameValidator()
+                        ],
+                        verbose_name="username",
+                    ),
+                ),
+                (
+                    "first_name",
+                    models.CharField(
+                        blank=True, max_length=150, verbose_name="first name"
+                    ),
+                ),
+                (
+                    "last_name",
+                    models.CharField(
+                        blank=True, max_length=150, verbose_name="last name"
+                    ),
+                ),
+                (
+                    "is_staff",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates whether the user can log into this admin site.",
+                        verbose_name="staff status",
+                    ),
+                ),
+                (
+                    "is_active",
+                    models.BooleanField(
+                        default=True,
+                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        verbose_name="active",
+                    ),
+                ),
+                (
+                    "date_joined",
+                    models.DateTimeField(
+                        default=django.utils.timezone.now, verbose_name="date joined"
+                    ),
+                ),
+                ("email", models.EmailField(max_length=254, unique=True)),
+                (
+                    "groups",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.group",
+                        verbose_name="groups",
+                    ),
+                ),
+                (
+                    "user_permissions",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="Specific permissions for this user.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.permission",
+                        verbose_name="user permissions",
+                    ),
+                ),
+            ],
+            options={
+                "verbose_name": "user",
+                "verbose_name_plural": "users",
+                "abstract": False,
+            },
+            managers=[
+                ("objects", django.contrib.auth.models.UserManager()),
+            ],
+        ),
         migrations.CreateModel(
             name="Entity",
             fields=[
@@ -50,5 +164,70 @@ class Migration(migrations.Migration):
                 "constraints": [],
             },
             bases=("vran.entity",),
+        ),
+        migrations.CreateModel(
+            name="TagInstance",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("id_persistent", models.TextField()),
+                ("id_entity_persistent", models.TextField()),
+                ("id_tag_definition_persistent", models.TextField()),
+                ("value", models.TextField(blank=True, null=True)),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.taginstance",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="TagDefinition",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.TextField()),
+                ("id_persistent", models.TextField()),
+                ("id_parent_persistent", models.TextField(blank=True, null=True)),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[("INR", "inner"), ("FLT", "float"), ("STR", "string")],
+                        default="INR",
+                        max_length=3,
+                    ),
+                ),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.tagdefinition",
+                        unique=True,
+                    ),
+                ),
+            ],
         ),
     ]

Leibniz-HBI/CoSMA-Editor

cosmae/admin.py
@@ -2,3 +2,30 @@
 from django.contrib import admin  # pylint: disable=unused-import
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+from cosmae.util import CosmaeUser
+
+
+class CosmaeUserChangeForm(UserChangeForm):
+    # pylint: disable=too-few-public-methods
+    "Admin form for changing users"
+
+    class Meta(UserChangeForm.Meta):
+        # pylint: disable=too-few-public-methods
+        "Meta class for admin user form."
+        model = CosmaeUser
+
+
+class CosmaeUserAdmin(UserAdmin):
+    # pylint: disable=too-few-public-methods
+    "Configure admin."
+    form = CosmaeUserChangeForm
+
+    # fieldsets = UserAdmin.fieldsets + (
+    #         (None, {'fields': ('some_extra_data',)}),
+    # )
+
+
+admin.site.register(CosmaeUser, CosmaeUserAdmin)

cosmae/apps.py
@@ -1,9 +1,70 @@
 """Django app configuration for CoSMA-Editor"""
-from django.apps import AppConfig
+import logging
+from typing import List
+
+from django.apps import AppConfig, apps
+from django.db.models.signals import post_migrate
+
+logger = logging.getLogger("cosmae.app_config")
+
+READ_PERMISSIONS = [
+    "view_person",
+    "view_entity",
+    "view_taginstance",
+    "view_tagdefinition",
+]
+
+WRITE_PERMISSIONS = [
+    "add_person",
+    "add_entity",
+    "add_taginstance",
+    "add_tagdefinition",
+]
+
+added_permissions = {
+    "CosmaeGroup.APPLICANT": [],
+    "CosmaeGroup.READER": [READ_PERMISSIONS],
+    "CosmaeGroup.CONTRIBUTOR": [READ_PERMISSIONS],
+    "CosmaeGroup.EDITOR": [READ_PERMISSIONS, WRITE_PERMISSIONS],
+    "CosmaeGroup.COMMISSIONER": [READ_PERMISSIONS, WRITE_PERMISSIONS],
+}
+
+
+def add_permission_for_group(group: str, permission_list_list: List[List[str]]):
+    "Method for granting permissions to groups."
+    group_model = apps.get_model("auth", "Group")
+    permission_model = apps.get_model("auth", "Permission")
+    group_name = str(group)
+
+    group, _created_group = group_model.objects.get_or_create(name=group_name)
+    for permission_list in permission_list_list:
+        for permission_name in permission_list:
+            permission = permission_model.objects.get(codename=permission_name)
+            group.permissions.add(permission)
+    group.save()
+
+
+def add_permissions(
+    app_config: AppConfig, verbosity=2, **kwargs
+):  # pylint: disable=unused-argument
+    "Create all new groups and permissions."
+    logger.setLevel(10 * (4 - verbosity))
+    logger.info("Create CoSMA-E groups.")
+    permission_model = apps.get_model("auth", "Permission")
+    for group, permissions in added_permissions.items():
+        try:
+            add_permission_for_group(group, permissions)
+        except permission_model.DoesNotExist:
+            logger.warning("Permissions do not exist yet.")
+            return
 
 
 class CosmaeConfig(AppConfig):
-    """Configuration for the CoSMA-Editor Django app"""
+    """Configuration for the CoSMA-E Django app"""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "cosmae"
+
+    def ready(self) -> None:
+        post_migrate.connect(add_permissions, dispatch_uid="cosmae.create_groups")
+        super().ready()

cosmae/migrations/0001_initial.py
@@ -1,6 +1,9 @@
-# Generated by Django 4.1.3 on 2022-11-28 12:19
+# Generated by Django 4.1.3 on 2023-04-11 07:54
 
+import django.contrib.auth.models
+import django.contrib.auth.validators
 import django.db.models.deletion
+import django.utils.timezone
 from django.db import migrations, models
 
 
@@ -8,9 +11,120 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = []
+    dependencies = [
+        ("auth", "0012_alter_user_first_name_max_length"),
+    ]
 
     operations = [
+        migrations.CreateModel(
+            name="CosmaeUser",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "is_superuser",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates that this user has all permissions without explicitly assigning them.",
+                        verbose_name="superuser status",
+                    ),
+                ),
+                (
+                    "username",
+                    models.CharField(
+                        error_messages={
+                            "unique": "A user with that username already exists."
+                        },
+                        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+                        max_length=150,
+                        unique=True,
+                        validators=[
+                            django.contrib.auth.validators.UnicodeUsernameValidator()
+                        ],
+                        verbose_name="username",
+                    ),
+                ),
+                (
+                    "first_name",
+                    models.CharField(
+                        blank=True, max_length=150, verbose_name="first name"
+                    ),
+                ),
+                (
+                    "last_name",
+                    models.CharField(
+                        blank=True, max_length=150, verbose_name="last name"
+                    ),
+                ),
+                (
+                    "is_staff",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates whether the user can log into this admin site.",
+                        verbose_name="staff status",
+                    ),
+                ),
+                (
+                    "is_active",
+                    models.BooleanField(
+                        default=True,
+                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        verbose_name="active",
+                    ),
+                ),
+                (
+                    "date_joined",
+                    models.DateTimeField(
+                        default=django.utils.timezone.now, verbose_name="date joined"
+                    ),
+                ),
+                ("email", models.EmailField(max_length=254, unique=True)),
+                (
+                    "groups",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.group",
+                        verbose_name="groups",
+                    ),
+                ),
+                (
+                    "user_permissions",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="Specific permissions for this user.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.permission",
+                        verbose_name="user permissions",
+                    ),
+                ),
+            ],
+            options={
+                "verbose_name": "user",
+                "verbose_name_plural": "users",
+                "abstract": False,
+            },
+            managers=[
+                ("objects", django.contrib.auth.models.UserManager()),
+            ],
+        ),
         migrations.CreateModel(
             name="Entity",
             fields=[
@@ -50,5 +164,70 @@ class Migration(migrations.Migration):
                 "constraints": [],
             },
             bases=("cosmae.entity",),
+        ),
+        migrations.CreateModel(
+            name="TagInstance",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("id_persistent", models.TextField()),
+                ("id_entity_persistent", models.TextField()),
+                ("id_tag_definition_persistent", models.TextField()),
+                ("value", models.TextField(blank=True, null=True)),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.taginstance",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="TagDefinition",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.TextField()),
+                ("id_persistent", models.TextField()),
+                ("id_parent_persistent", models.TextField(blank=True, null=True)),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[("INR", "inner"), ("FLT", "float"), ("STR", "string")],
+                        default="INR",
+                        max_length=3,
+                    ),
+                ),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.tagdefinition",
+                        unique=True,
+                    ),
+                ),
+            ],
         ),
     ]