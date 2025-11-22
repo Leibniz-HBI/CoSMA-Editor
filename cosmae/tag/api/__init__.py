Leibniz-HBI/DP-REX

Add API for posting tag definitions and instances.

vran/tag/api/__init__.py
@@ -1,0 +1,5 @@
+"Combination of tag defintions and instance API."
+from vran.tag.api.definitions import router as defintions_router
+from vran.tag.api.instances import router
+
+router.add_router("definitions", defintions_router)

vran/tag/api/instances.py
@@ -1,0 +1,120 @@
+"API methods for tag instances."
+from datetime import datetime
+from typing import List, Optional, Union
+from uuid import uuid4
+
+from django.db import IntegrityError
+from ninja import Router, Schema
+
+from vran.exception import (
+    ApiError,
+    EntityMissingException,
+    EntityUpdatedException,
+    InvalidTagValueException,
+    TagDefinitionMissingException,
+    TagInstanceExistsException,
+    ValidationException,
+)
+from vran.tag.models_django import TagInstance as TagInstanceDb
+from vran.util.django import save_many_atomic
+
+router = Router()
+
+
+class TagInstancePost(Schema):
+    # pylint: disable=too-few-public-methods
+    "A single API tag instance for post requests."
+    id_entity_persistent: str
+    id_tag_definition_persistent: str
+    value: Union[float, int, None]
+    id_persistent: Optional[str]
+    version: Optional[int]
+
+
+class TagInstancePostList(Schema):
+    # pylint: disable=too-few-public-methods
+    "Multiple tag instances for post requests."
+    tag_instances: List[TagInstancePost]
+
+
+@router.post("", response={200: TagInstancePostList, 400: ApiError, 500: ApiError})
+def post_tag_instance(_, tag_list: TagInstancePostList):
+    "Create or change a tag instance."
+    # pylint: disable=too-many-return-statements
+    tag_apis = tag_list.tag_instances
+    now = datetime.utcnow()
+    try:
+        tag_dbs = [tag_instance_api_to_db(tag, now) for tag in tag_apis]
+    except ValidationException as exc:
+        return 400, ApiError(msg=str(exc))
+    except TagInstanceExistsException as exc:
+        return 500, ApiError(
+            msg="Could not generate id_persistent for tag instance with "
+            f"id_entity_persistent {exc.id_entity_persistent}, "
+            f"id_tag_definition_persistent {exc.id_tag_definition_persistent} and "
+            f"value {exc.value}."
+        )
+    except EntityMissingException as exc:
+        return 400, ApiError(
+            msg=f"There is no entity with id_persistent {exc.id_persistent}."
+        )
+    except TagDefinitionMissingException as exc:
+        return 400, ApiError(
+            msg=f"There is no tag definition with id_persistent {exc.id_persistent}."
+        )
+    except InvalidTagValueException as exc:
+        return 400, ApiError(
+            msg=f"Value {exc.value} should be of type {exc.type_name} "
+            f"for tag with id_persistent {exc.tag_id_persistent}."
+        )
+    except EntityUpdatedException as exc:
+        return 500, ApiError(
+            msg="There has been a concurrent modification "
+            f"to the tag instance with id_persistent {exc.id_affected}."
+        )
+    tag_db_saves = [tag for tag, do_write in tag_dbs if do_write]
+    try:
+        save_many_atomic(tag_db_saves)
+    except IntegrityError:
+        return 500, ApiError(msg="Provided data not consistent with database.")
+    return 200, TagInstancePostList(
+        tag_instances=[tag_instance_db_to_api(tag) for tag, _ in tag_dbs]
+    )
+
+
+def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
+    "Convert a tag instance from API to databse representation."
+    if tag_api.id_persistent:
+        persistent_id = tag_api.id_persistent
+        if tag_api.version is None:
+            raise ValidationException(
+                f"Tag instance with id_persistent {tag_api.id_persistent} "
+                "has no previous version."
+            )
+    else:
+        if tag_api.version:
+            raise ValidationException(
+                f"Tag instance with id_entity_persistent {tag_api.id_entity_persistent}, "
+                f"id_tag_definition_persistent {tag_api.id_tag_definition_persistent} and "
+                f"value {tag_api.value} has version but no id_persistent."
+            )
+        persistent_id = str(uuid4())
+    return TagInstanceDb.change_or_create(
+        id_persistent=persistent_id,
+        id_entity_persistent=tag_api.id_entity_persistent,
+        id_tag_definition_persistent=tag_api.id_tag_definition_persistent,
+        value=tag_api.value,
+        time_edit=time,
+        version=tag_api.version,
+    )
+
+
+def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+    "Convert tag istances from databse to API representation."
+    return TagInstancePost(
+        id_persistent=tag_db.id_persistent,
+        id_entity_persistent=tag_db.id_entity_persistent,
+        id_tag_definition_persistent=tag_db.id_tag_definition_persistent,
+        value=tag_db.value,
+        version=tag_db.id,
+    )

vran/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 from ninja import NinjaAPI
 
 from vran.person.api import router as person_router
+from vran.tag.api import router as tag_router
 
 ninja_api = NinjaAPI()
 ninja_api.add_router("persons", person_router)
+ninja_api.add_router("tags", tag_router)
 
 urlpatterns = [path("manage/", admin.site.urls), path("api/", ninja_api.urls)]

tests/tag/api/integration/post_tag_defs_test.py
@@ -1,0 +1,124 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, patch
+
+from django.db import IntegrityError
+
+import tests.tag.common as c
+from tests.tag.api.integration import requests as r
+
+
+def test_id_no_version(live_server, root_tag_def):
+    root_tag_def["id_persistent"] = c.id_tag_def_parent_persistent_test
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"] == "Tag definition with id_persistent "
+        f"{c.id_tag_def_parent_persistent_test} has no previous version."
+    )
+
+
+def test_no_id_version(live_server, root_tag_def):
+    root_tag_def["version"] = 5
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"]
+        == f"Tag definition with name {c.name_tag_def_test} has version but no id_persistent."
+    )
+
+
+def test_concurrent_modification(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    created = req.json()["tag_definitions"][0]
+    created["type"] = "FLOAT"
+    req = r.post_tag_def(live_server.url, created)
+    assert req.status_code == 200
+    created["type"] = "INTEGER"
+    req = r.post_tag_def(live_server.url, created)
+    assert req.status_code == 500
+    assert req.json()["msg"] == (
+        "There has been a concurrent modification "
+        "to the tag definition with id_persistent "
+        f'{created["id_persistent"]}.'
+    )
+
+
+def test_no_modification_is_returned(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    created = req.json()["tag_definitions"][0]
+    req = r.post_tag_def(live_server.url, created)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 1
+    assert tag_definitions[0] == created
+
+
+def test_exists(live_server, root_tag_def):
+    mock = MagicMock()
+    mock.return_value = "same_id"
+    with patch("vran.tag.api.definitions.uuid4"):
+        req = r.post_tag_def(live_server.url, root_tag_def)
+        assert req.status_code == 200
+        req = r.post_tag_def(live_server.url, root_tag_def)
+        assert req.status_code == 500
+        assert req.json()["msg"] == (
+            "Could not generate id_persistent for tag definition with "
+            f"name {c.name_tag_def_test}."
+        )
+
+
+def test_name_exists(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert req.json()["msg"] == (
+        "There is an existing tag definition with name "
+        f"{c.name_tag_def_test} and id_parent_persistent {None}."
+    )
+
+
+def test_no_parent(live_server, child_tag_def):
+    child_tag_def["id_parent_persistent"] = "unknown_id_persistent_test"
+    req = r.post_tag_def(live_server.url, child_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"]
+        == "There is no tag definition with id_persistent unknown_id_persistent_test."
+    )
+
+
+def test_unknown_type(live_server, root_tag_def):
+    root_tag_def["type"] = "UNKNOWN"
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert req.json()["msg"] == "Type UNKNOWN is not known."
+
+
+def test_no_child_tag_allowed(live_server, child_tag_def):
+    req = r.post_tag_def(live_server.url, child_tag_def)
+    assert req.status_code == 200
+    id_parent_persistent = req.json()["tag_definitions"][0]["id_persistent"]
+    invalid_child_tag_def = {
+        "name": "some child",
+        "type": "FLOAT",
+        "id_parent_persistent": id_parent_persistent,
+    }
+    req = r.post_tag_def(live_server.url, invalid_child_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"]
+        == f"Tag definition with id_persistent {id_parent_persistent} "
+        "is not allowed to have child tags."
+    )
+
+
+def test_bad_db(live_server, root_tag_def):
+    mock = MagicMock()
+    mock.side_effect = IntegrityError()
+    with patch("vran.tag.models_django.TagDefinition.save", mock):
+        req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 500
+    assert req.json()["msg"] == "Provided data not consistent with database."

tests/tag/api/integration/requests.py
@@ -1,0 +1,41 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from urllib.parse import urljoin
+
+import requests
+
+
+def post_tag_def(url, tag_def):
+    return post_tag_defs(url, [tag_def])
+
+
+def post_tag_defs(url, tag_defs):
+    return requests.post(
+        urljoin(url, "/vran/api/tags/definitions"),
+        json={"tag_definitions": tag_defs},
+        timeout=900,
+    )
+
+
+def get_tagdefs(url):
+    return requests.get(
+        urljoin(url, "/vran/api/tags/definitions"),
+        timeout=900,
+    )
+
+
+def post_tag_instance(url, tag):
+    return post_tag_instances(url, [tag])
+
+
+def post_tag_instances(url, tags):
+    return requests.post(
+        urljoin(url, "/vran/api/tags"), json={"tag_instances": tags}, timeout=900
+    )
+
+
+def get_tag_chunked(url, tag_def_id, offset, limit):
+    return requests.post(
+        urljoin(url, "/vran/api/tags/definitions"),
+        json={"tag_definition_id": tag_def_id, "offset": offset, "limit": limit},
+        timeout=900,
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/tag/api/__init__.py
@@ -1,0 +1,5 @@
+"Combination of tag defintions and instance API."
+from cosmae.tag.api.definitions import router as defintions_router
+from cosmae.tag.api.instances import router
+
+router.add_router("definitions", defintions_router)

cosmae/tag/api/instances.py
@@ -1,0 +1,120 @@
+"API methods for tag instances."
+from datetime import datetime
+from typing import List, Optional, Union
+from uuid import uuid4
+
+from django.db import IntegrityError
+from ninja import Router, Schema
+
+from cosmae.exception import (
+    ApiError,
+    EntityMissingException,
+    EntityUpdatedException,
+    InvalidTagValueException,
+    TagDefinitionMissingException,
+    TagInstanceExistsException,
+    ValidationException,
+)
+from cosmae.tag.models_django import TagInstance as TagInstanceDb
+from cosmae.util.django import save_many_atomic
+
+router = Router()
+
+
+class TagInstancePost(Schema):
+    # pylint: disable=too-few-public-methods
+    "A single API tag instance for post requests."
+    id_entity_persistent: str
+    id_tag_definition_persistent: str
+    value: Union[float, int, None]
+    id_persistent: Optional[str]
+    version: Optional[int]
+
+
+class TagInstancePostList(Schema):
+    # pylint: disable=too-few-public-methods
+    "Multiple tag instances for post requests."
+    tag_instances: List[TagInstancePost]
+
+
+@router.post("", response={200: TagInstancePostList, 400: ApiError, 500: ApiError})
+def post_tag_instance(_, tag_list: TagInstancePostList):
+    "Create or change a tag instance."
+    # pylint: disable=too-many-return-statements
+    tag_apis = tag_list.tag_instances
+    now = datetime.utcnow()
+    try:
+        tag_dbs = [tag_instance_api_to_db(tag, now) for tag in tag_apis]
+    except ValidationException as exc:
+        return 400, ApiError(msg=str(exc))
+    except TagInstanceExistsException as exc:
+        return 500, ApiError(
+            msg="Could not generate id_persistent for tag instance with "
+            f"id_entity_persistent {exc.id_entity_persistent}, "
+            f"id_tag_definition_persistent {exc.id_tag_definition_persistent} and "
+            f"value {exc.value}."
+        )
+    except EntityMissingException as exc:
+        return 400, ApiError(
+            msg=f"There is no entity with id_persistent {exc.id_persistent}."
+        )
+    except TagDefinitionMissingException as exc:
+        return 400, ApiError(
+            msg=f"There is no tag definition with id_persistent {exc.id_persistent}."
+        )
+    except InvalidTagValueException as exc:
+        return 400, ApiError(
+            msg=f"Value {exc.value} should be of type {exc.type_name} "
+            f"for tag with id_persistent {exc.tag_id_persistent}."
+        )
+    except EntityUpdatedException as exc:
+        return 500, ApiError(
+            msg="There has been a concurrent modification "
+            f"to the tag instance with id_persistent {exc.id_affected}."
+        )
+    tag_db_saves = [tag for tag, do_write in tag_dbs if do_write]
+    try:
+        save_many_atomic(tag_db_saves)
+    except IntegrityError:
+        return 500, ApiError(msg="Provided data not consistent with database.")
+    return 200, TagInstancePostList(
+        tag_instances=[tag_instance_db_to_api(tag) for tag, _ in tag_dbs]
+    )
+
+
+def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
+    "Convert a tag instance from API to databse representation."
+    if tag_api.id_persistent:
+        persistent_id = tag_api.id_persistent
+        if tag_api.version is None:
+            raise ValidationException(
+                f"Tag instance with id_persistent {tag_api.id_persistent} "
+                "has no previous version."
+            )
+    else:
+        if tag_api.version:
+            raise ValidationException(
+                f"Tag instance with id_entity_persistent {tag_api.id_entity_persistent}, "
+                f"id_tag_definition_persistent {tag_api.id_tag_definition_persistent} and "
+                f"value {tag_api.value} has version but no id_persistent."
+            )
+        persistent_id = str(uuid4())
+    return TagInstanceDb.change_or_create(
+        id_persistent=persistent_id,
+        id_entity_persistent=tag_api.id_entity_persistent,
+        id_tag_definition_persistent=tag_api.id_tag_definition_persistent,
+        value=tag_api.value,
+        time_edit=time,
+        version=tag_api.version,
+    )
+
+
+def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+    "Convert tag istances from databse to API representation."
+    return TagInstancePost(
+        id_persistent=tag_db.id_persistent,
+        id_entity_persistent=tag_db.id_entity_persistent,
+        id_tag_definition_persistent=tag_db.id_tag_definition_persistent,
+        value=tag_db.value,
+        version=tag_db.id,
+    )

cosmae/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 from ninja import NinjaAPI
 
 from cosmae.person.api import router as person_router
+from cosmae.tag.api import router as tag_router
 
 ninja_api = NinjaAPI()
 ninja_api.add_router("persons", person_router)
+ninja_api.add_router("tags", tag_router)
 
 urlpatterns = [path("manage/", admin.site.urls), path("api/", ninja_api.urls)]

tests/tag/api/integration/post_tag_defs_test.py
@@ -1,0 +1,124 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, patch
+
+from django.db import IntegrityError
+
+import tests.tag.common as c
+from tests.tag.api.integration import requests as r
+
+
+def test_id_no_version(live_server, root_tag_def):
+    root_tag_def["id_persistent"] = c.id_tag_def_parent_persistent_test
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"] == "Tag definition with id_persistent "
+        f"{c.id_tag_def_parent_persistent_test} has no previous version."
+    )
+
+
+def test_no_id_version(live_server, root_tag_def):
+    root_tag_def["version"] = 5
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"]
+        == f"Tag definition with name {c.name_tag_def_test} has version but no id_persistent."
+    )
+
+
+def test_concurrent_modification(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    created = req.json()["tag_definitions"][0]
+    created["type"] = "FLOAT"
+    req = r.post_tag_def(live_server.url, created)
+    assert req.status_code == 200
+    created["type"] = "INTEGER"
+    req = r.post_tag_def(live_server.url, created)
+    assert req.status_code == 500
+    assert req.json()["msg"] == (
+        "There has been a concurrent modification "
+        "to the tag definition with id_persistent "
+        f'{created["id_persistent"]}.'
+    )
+
+
+def test_no_modification_is_returned(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    created = req.json()["tag_definitions"][0]
+    req = r.post_tag_def(live_server.url, created)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 1
+    assert tag_definitions[0] == created
+
+
+def test_exists(live_server, root_tag_def):
+    mock = MagicMock()
+    mock.return_value = "same_id"
+    with patch("cosmae.tag.api.definitions.uuid4"):
+        req = r.post_tag_def(live_server.url, root_tag_def)
+        assert req.status_code == 200
+        req = r.post_tag_def(live_server.url, root_tag_def)
+        assert req.status_code == 500
+        assert req.json()["msg"] == (
+            "Could not generate id_persistent for tag definition with "
+            f"name {c.name_tag_def_test}."
+        )
+
+
+def test_name_exists(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert req.json()["msg"] == (
+        "There is an existing tag definition with name "
+        f"{c.name_tag_def_test} and id_parent_persistent {None}."
+    )
+
+
+def test_no_parent(live_server, child_tag_def):
+    child_tag_def["id_parent_persistent"] = "unknown_id_persistent_test"
+    req = r.post_tag_def(live_server.url, child_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"]
+        == "There is no tag definition with id_persistent unknown_id_persistent_test."
+    )
+
+
+def test_unknown_type(live_server, root_tag_def):
+    root_tag_def["type"] = "UNKNOWN"
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 400
+    assert req.json()["msg"] == "Type UNKNOWN is not known."
+
+
+def test_no_child_tag_allowed(live_server, child_tag_def):
+    req = r.post_tag_def(live_server.url, child_tag_def)
+    assert req.status_code == 200
+    id_parent_persistent = req.json()["tag_definitions"][0]["id_persistent"]
+    invalid_child_tag_def = {
+        "name": "some child",
+        "type": "FLOAT",
+        "id_parent_persistent": id_parent_persistent,
+    }
+    req = r.post_tag_def(live_server.url, invalid_child_tag_def)
+    assert req.status_code == 400
+    assert (
+        req.json()["msg"]
+        == f"Tag definition with id_persistent {id_parent_persistent} "
+        "is not allowed to have child tags."
+    )
+
+
+def test_bad_db(live_server, root_tag_def):
+    mock = MagicMock()
+    mock.side_effect = IntegrityError()
+    with patch("cosmae.tag.models_django.TagDefinition.save", mock):
+        req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 500
+    assert req.json()["msg"] == "Provided data not consistent with database."

tests/tag/api/integration/requests.py
@@ -1,0 +1,41 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from urllib.parse import urljoin
+
+import requests
+
+
+def post_tag_def(url, tag_def):
+    return post_tag_defs(url, [tag_def])
+
+
+def post_tag_defs(url, tag_defs):
+    return requests.post(
+        urljoin(url, "/cosmae/api/tags/definitions"),
+        json={"tag_definitions": tag_defs},
+        timeout=900,
+    )
+
+
+def get_tagdefs(url):
+    return requests.get(
+        urljoin(url, "/cosmae/api/tags/definitions"),
+        timeout=900,
+    )
+
+
+def post_tag_instance(url, tag):
+    return post_tag_instances(url, [tag])
+
+
+def post_tag_instances(url, tags):
+    return requests.post(
+        urljoin(url, "/cosmae/api/tags"), json={"tag_instances": tags}, timeout=900
+    )
+
+
+def get_tag_chunked(url, tag_def_id, offset, limit):
+    return requests.post(
+        urljoin(url, "/cosmae/api/tags/definitions"),
+        json={"tag_definition_id": tag_def_id, "offset": offset, "limit": limit},
+        timeout=900,
+    )