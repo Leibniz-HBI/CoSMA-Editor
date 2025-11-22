Leibniz-HBI/DP-REX

Move attribute check to Entity superclass.

tests/person/django_models_test.py
@@ -4,6 +4,7 @@ import pytest
 
 from tests.person import common as c
 from vran.entity.models_django import Entity
+from vran.exception import TooManyFieldsException
 from vran.person.models_django import Person
 
 
@@ -19,6 +20,18 @@ def test_no_key_mix():
     assert person_keys == person_keys_after
 
 
+def test_can_not_set_other_attribute():
+    with pytest.raises(TooManyFieldsException):
+        Person(
+            id_persistent=c.id_persistent_test,
+            names_family=c.names_family_test,
+            names_personal=c.names_personal_test,
+            display_txt=c.display_txt_test,
+            time_edit=c.time_edit_test,
+            verified_social=False,
+        )
+
+
 @pytest.fixture
 def person():
     return Person(
@@ -28,6 +41,26 @@ def person():
         display_txt=c.display_txt_test,
         time_edit=c.time_edit_test,
     )
+
+
+@pytest.mark.django_db
+def test_different_names_family(person):
+    person1 = Person(
+        id_persistent=person.id_persistent,
+        time_edit=person.time_edit,
+        names_family="changed test name family",
+    )
+    assert person.check_different_before_save(person1)
+
+
+@pytest.mark.django_db
+def test_different_names_personal(person):
+    person1 = Person(
+        id_persistent=person.id_persistent,
+        time_edit=person.time_edit,
+        names_personal="changed test name personal",
+    )
+    assert person.check_different_before_save(person1)
 
 
 def test_create_person(person):

Leibniz-HBI/CoSMA-Editor

tests/person/django_models_test.py
@@ -4,6 +4,7 @@ import pytest
 
 from tests.person import common as c
 from cosmae.entity.models_django import Entity
+from cosmae.exception import TooManyFieldsException
 from cosmae.person.models_django import Person
 
 
@@ -19,6 +20,18 @@ def test_no_key_mix():
     assert person_keys == person_keys_after
 
 
+def test_can_not_set_other_attribute():
+    with pytest.raises(TooManyFieldsException):
+        Person(
+            id_persistent=c.id_persistent_test,
+            names_family=c.names_family_test,
+            names_personal=c.names_personal_test,
+            display_txt=c.display_txt_test,
+            time_edit=c.time_edit_test,
+            verified_social=False,
+        )
+
+
 @pytest.fixture
 def person():
     return Person(
@@ -28,6 +41,26 @@ def person():
         display_txt=c.display_txt_test,
         time_edit=c.time_edit_test,
     )
+
+
+@pytest.mark.django_db
+def test_different_names_family(person):
+    person1 = Person(
+        id_persistent=person.id_persistent,
+        time_edit=person.time_edit,
+        names_family="changed test name family",
+    )
+    assert person.check_different_before_save(person1)
+
+
+@pytest.mark.django_db
+def test_different_names_personal(person):
+    person1 = Person(
+        id_persistent=person.id_persistent,
+        time_edit=person.time_edit,
+        names_personal="changed test name personal",
+    )
+    assert person.check_different_before_save(person1)
 
 
 def test_create_person(person):