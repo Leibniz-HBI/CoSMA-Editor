Leibniz-HBI/DP-REX

Add view for most recent tag instance.

vran/contribution/tag_definition/queue/ingest.py
@@ -11,7 +11,7 @@ from vran.contribution.tag_definition.models_django import TagDefinitionContribu
 from vran.contribution.tag_definition.queue.util import read_csv_of_candidate
 from vran.entity.models_django import Entity
 from vran.merge_request.models_django import TagMergeRequest
-from vran.tag.models_django import TagDefinition, TagInstance
+from vran.tag.models_django import TagDefinition, TagInstanceHistory
 
 
 def ingest_values_from_csv(id_contribution_persistent):
@@ -83,7 +83,7 @@ def ingest_values_from_csv(id_contribution_persistent):
                     value = str(row_tpl[int(idx_in_file)])
                     if value == "nan":
                         continue
-                    tag_instance, _ = TagInstance.change_or_create(
+                    tag_instance, _ = TagInstanceHistory.change_or_create(
                         id_persistent=id_tag_instance_persistent,
                         id_entity_persistent=id_entity_persistent,
                         id_tag_definition_persistent=tag_definition.id_persistent,

vran/migrations/0030_taginstancehistory.py
@@ -1,0 +1,13 @@
+# Generated by Django 4.2.8 on 2024-01-03 16:28
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("vran", "0029_alter_taginstance_id_entity_persistent"),
+    ]
+
+    operations = [
+        migrations.RenameModel(old_name="TagInstance", new_name="TagInstanceHistory")
+    ]

vran/tag/api/instances.py
@@ -23,6 +23,8 @@ from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.api.definitions import tag_definition_db_to_api
 from vran.tag.api.models_api import TagDefinitionResponse
 from vran.tag.models_django import TagInstance as TagInstanceDb
+from vran.tag.models_django import TagInstanceAbstract as TagInstanceAbstractDb
+from vran.tag.models_django import TagInstanceHistory as TagInstanceHistoryDb
 from vran.util import VranUser
 from vran.util.auth import check_user
 from vran.util.django import save_many_atomic
@@ -314,7 +316,7 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, user: VranUser, time: datet
                 f"value {tag_api.value} has version but no id_persistent."
             )
         persistent_id = str(uuid4())
-    return TagInstanceDb.change_or_create(
+    return TagInstanceHistoryDb.change_or_create(
         id_persistent=persistent_id,
         id_entity_persistent=tag_api.id_entity_persistent,
         id_tag_definition_persistent=tag_api.id_tag_definition_persistent,
@@ -325,7 +327,7 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, user: VranUser, time: datet
     )
 
 
-def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+def tag_instance_db_to_api(tag_db: TagInstanceAbstractDb) -> TagInstancePost:
     "Convert tag instances from database to API representation."
     return TagInstancePost(
         id_persistent=tag_db.id_persistent,
@@ -336,7 +338,9 @@ def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
     )
 
 
-def tag_instance_with_existing_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+def tag_instance_with_existing_db_to_api(
+    tag_db: TagInstanceAbstractDb,
+) -> TagInstancePost:
     "Convert tag instances from database to API representation."
     return TagInstanceValueWithExistingFlagResponse(
         id_persistent=tag_db.id_persistent,

tests/merge_request/api/integration/post_merge_test.py
@@ -5,7 +5,7 @@ import tests.merge_request.api.integration.requests as req
 import tests.merge_request.common as c
 from vran.exception import NotAuthenticatedException
 from vran.merge_request.models_django import TagMergeRequest
-from vran.tag.models_django import TagInstance
+from vran.tag.models_django import TagInstanceHistory
 from vran.util import timestamp
 
 
@@ -85,7 +85,7 @@ def test_updated_data(
 ):
     server, cookies = auth_server
     old_instance = conflict_resolution_replace.tag_instance_destination
-    instance, _ = TagInstance.change_or_create(
+    instance, _ = TagInstanceHistory.change_or_create(
         id_persistent=old_instance.id_persistent,
         id_entity_persistent=old_instance.id_entity_persistent,
         id_tag_definition_persistent=old_instance.id_tag_definition_persistent,

tests/merge_request/entity/api/integration/get_entity_conflicts_test.py
@@ -9,7 +9,7 @@ from tests.tag import common as ct
 from tests.user import common as cu
 from tests.utils import assert_versioned
 from vran.exception import NotAuthenticatedException
-from vran.tag.models_django import TagInstance
+from vran.tag.models_django import TagInstanceHistory
 
 
 def test_unknown_user(auth_server):
@@ -146,7 +146,7 @@ def test_conflicts_same_value(
     instances_merge_request_origin_user,
 ):
     for instance_origin in instances_merge_request_origin_user:
-        instance_destination = TagInstance(
+        instance_destination = TagInstanceHistory(
             id_entity_persistent=merge_request_user.id_destination_persistent,
             id_tag_definition_persistent=instance_origin.id_tag_definition_persistent,
             id_persistent=str(uuid4()),

tests/tag/models_django_instance_test.py
@@ -10,12 +10,12 @@ from vran.exception import (
     TagDefinitionMissingException,
     TagDefinitionPermissionException,
 )
-from vran.tag.models_django import TagDefinition, TagInstance
+from vran.tag.models_django import TagDefinition, TagInstance, TagInstanceHistory
 
 
 @pytest.fixture
 def tag():
-    return TagInstance(
+    return TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -25,7 +25,7 @@ def tag():
 
 
 def test_different_entity(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent="id_entity_test_1",
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -36,7 +36,7 @@ def test_different_entity(tag):
 
 
 def test_different_value(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent="id_tag_def_persistent1",
@@ -47,7 +47,7 @@ def test_different_value(tag):
 
 
 def test_different_tag_def(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -58,7 +58,7 @@ def test_different_tag_def(tag):
 
 
 def test_same(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -71,7 +71,7 @@ def test_same(tag):
 @pytest.mark.django_db
 def test_get_most_recent(tag):
     tag.save()
-    new = TagInstance(
+    new = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -80,14 +80,14 @@ def test_get_most_recent(tag):
         previous_version=tag,
     )
     new.save()
-    by_id = TagInstance.most_recent_by_id(c.id_tag_persistent_test)
+    by_id = TagInstance.get_by_id(c.id_tag_persistent_test)
     assert by_id.value == "1.0"
 
 
 @pytest.mark.django_db
 def test_get_most_recent_by_ids(tag):
     tag.save()
-    new = TagInstance(
+    new = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -105,7 +105,7 @@ def test_get_most_recent_by_ids(tag):
 @pytest.mark.django_db
 def test_entity_missing(user):
     with pytest.raises(EntityMissingException) as exc:
-        TagInstance.change_or_create(
+        TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
             user=user,
@@ -119,7 +119,7 @@ def test_entity_missing(user):
 def test_tag_def_missing(entity0, user):
     entity0.save()
     with pytest.raises(TagDefinitionMissingException) as exc:
-        TagInstance.change_or_create(
+        TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
             user=user,
@@ -134,7 +134,7 @@ def test_tag_def_no_permission(entity0, tag_def_user, user1):
     entity0.save()
     tag_def_user.save()
     with pytest.raises(TagDefinitionPermissionException) as exc:
-        TagInstance.change_or_create(
+        TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
             user=user1,
@@ -149,7 +149,7 @@ def test_tag_def_no_permission(entity0, tag_def_user, user1):
 def test_add_tag_root(entity0, tag_def_user):
     entity0.save()
     tag_def_user.save()
-    ret, _ = TagInstance.change_or_create(
+    ret, _ = TagInstanceHistory.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
         user=tag_def_user.owner,
@@ -178,7 +178,7 @@ def test_add_tag_child(entity0, tag_def_user):
     entity0.save()
     tag_def_user.id_parent_persistent = c.id_tag_def_parent_persistent_test
     tag_def_user.save()
-    ret, _ = TagInstance.change_or_create(
+    ret, _ = TagInstanceHistory.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
         user=tag_def_user.owner,
@@ -205,11 +205,12 @@ def test_empty_chunk(tag_def):
 def test_chunk_versions(tag_def):
     tag_def.save()
     last_values = []
+    last_ids = []
     for i in range(10):
         tag = None
         previous_version = None
         for j in range(i % 3 + 1):
-            tag = TagInstance(
+            tag = TagInstanceHistory(
                 id_persistent=f"id_tag_test{i}",
                 id_entity_persistent=cp.id_persistent_test,
                 id_tag_definition_persistent=tag_def.id_persistent,
@@ -219,8 +220,9 @@ def test_chunk_versions(tag_def):
             )
             tag.save()
             previous_version = tag  # pylint: disable=no-member
+        last_ids.append(tag.id)  # pylint: disable=no-member
         last_values.append(tag.value)
-    ret = TagInstance.by_tag_chunked(c.id_tag_def_persistent_test, 2, 3)
+    ret = TagInstance.by_tag_chunked(c.id_tag_def_persistent_test, last_ids[2], 3)
     ret_values = [tag.value for tag in ret]
     assert ret_values == last_values[2 : 2 + 3]
 
@@ -229,7 +231,7 @@ def test_chunk_versions(tag_def):
 def test_chunk_filter_tag_instance(tag_def):
     tag_def.save()
     for i in range(10):
-        tag = TagInstance(
+        tag = TagInstanceHistory(
             id_persistent=f"id_tag_test{i}",
             id_entity_persistent=cp.id_persistent_test,
             id_tag_definition_persistent=tag_def.id_persistent + i * "0",

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/tag_definition/queue/ingest.py
@@ -11,7 +11,7 @@ from cosmae.contribution.tag_definition.models_django import TagDefinitionContri
 from cosmae.contribution.tag_definition.queue.util import read_csv_of_candidate
 from cosmae.entity.models_django import Entity
 from cosmae.merge_request.models_django import TagMergeRequest
-from cosmae.tag.models_django import TagDefinition, TagInstance
+from cosmae.tag.models_django import TagDefinition, TagInstanceHistory
 
 
 def ingest_values_from_csv(id_contribution_persistent):
@@ -83,7 +83,7 @@ def ingest_values_from_csv(id_contribution_persistent):
                     value = str(row_tpl[int(idx_in_file)])
                     if value == "nan":
                         continue
-                    tag_instance, _ = TagInstance.change_or_create(
+                    tag_instance, _ = TagInstanceHistory.change_or_create(
                         id_persistent=id_tag_instance_persistent,
                         id_entity_persistent=id_entity_persistent,
                         id_tag_definition_persistent=tag_definition.id_persistent,

cosmae/migrations/0030_taginstancehistory.py
@@ -1,0 +1,13 @@
+# Generated by Django 4.2.8 on 2024-01-03 16:28
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("cosmae", "0029_alter_taginstance_id_entity_persistent"),
+    ]
+
+    operations = [
+        migrations.RenameModel(old_name="TagInstance", new_name="TagInstanceHistory")
+    ]

cosmae/tag/api/instances.py
@@ -23,6 +23,8 @@ from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.api.definitions import tag_definition_db_to_api
 from cosmae.tag.api.models_api import TagDefinitionResponse
 from cosmae.tag.models_django import TagInstance as TagInstanceDb
+from cosmae.tag.models_django import TagInstanceAbstract as TagInstanceAbstractDb
+from cosmae.tag.models_django import TagInstanceHistory as TagInstanceHistoryDb
 from cosmae.util import CosmaeUser
 from cosmae.util.auth import check_user
 from cosmae.util.django import save_many_atomic
@@ -314,7 +316,7 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, user: CosmaeUser, time: dat
                 f"value {tag_api.value} has version but no id_persistent."
             )
         persistent_id = str(uuid4())
-    return TagInstanceDb.change_or_create(
+    return TagInstanceHistoryDb.change_or_create(
         id_persistent=persistent_id,
         id_entity_persistent=tag_api.id_entity_persistent,
         id_tag_definition_persistent=tag_api.id_tag_definition_persistent,
@@ -325,7 +327,7 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, user: CosmaeUser, time: dat
     )
 
 
-def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+def tag_instance_db_to_api(tag_db: TagInstanceAbstractDb) -> TagInstancePost:
     "Convert tag instances from database to API representation."
     return TagInstancePost(
         id_persistent=tag_db.id_persistent,
@@ -336,7 +338,9 @@ def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
     )
 
 
-def tag_instance_with_existing_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+def tag_instance_with_existing_db_to_api(
+    tag_db: TagInstanceAbstractDb,
+) -> TagInstancePost:
     "Convert tag instances from database to API representation."
     return TagInstanceValueWithExistingFlagResponse(
         id_persistent=tag_db.id_persistent,

tests/merge_request/api/integration/post_merge_test.py
@@ -5,7 +5,7 @@ import tests.merge_request.api.integration.requests as req
 import tests.merge_request.common as c
 from cosmae.exception import NotAuthenticatedException
 from cosmae.merge_request.models_django import TagMergeRequest
-from cosmae.tag.models_django import TagInstance
+from cosmae.tag.models_django import TagInstanceHistory
 from cosmae.util import timestamp
 
 
@@ -85,7 +85,7 @@ def test_updated_data(
 ):
     server, cookies = auth_server
     old_instance = conflict_resolution_replace.tag_instance_destination
-    instance, _ = TagInstance.change_or_create(
+    instance, _ = TagInstanceHistory.change_or_create(
         id_persistent=old_instance.id_persistent,
         id_entity_persistent=old_instance.id_entity_persistent,
         id_tag_definition_persistent=old_instance.id_tag_definition_persistent,

tests/merge_request/entity/api/integration/get_entity_conflicts_test.py
@@ -9,7 +9,7 @@ from tests.tag import common as ct
 from tests.user import common as cu
 from tests.utils import assert_versioned
 from cosmae.exception import NotAuthenticatedException
-from cosmae.tag.models_django import TagInstance
+from cosmae.tag.models_django import TagInstanceHistory
 
 
 def test_unknown_user(auth_server):
@@ -146,7 +146,7 @@ def test_conflicts_same_value(
     instances_merge_request_origin_user,
 ):
     for instance_origin in instances_merge_request_origin_user:
-        instance_destination = TagInstance(
+        instance_destination = TagInstanceHistory(
             id_entity_persistent=merge_request_user.id_destination_persistent,
             id_tag_definition_persistent=instance_origin.id_tag_definition_persistent,
             id_persistent=str(uuid4()),

tests/tag/models_django_instance_test.py
@@ -10,12 +10,12 @@ from cosmae.exception import (
     TagDefinitionMissingException,
     TagDefinitionPermissionException,
 )
-from cosmae.tag.models_django import TagDefinition, TagInstance
+from cosmae.tag.models_django import TagDefinition, TagInstance, TagInstanceHistory
 
 
 @pytest.fixture
 def tag():
-    return TagInstance(
+    return TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -25,7 +25,7 @@ def tag():
 
 
 def test_different_entity(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent="id_entity_test_1",
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -36,7 +36,7 @@ def test_different_entity(tag):
 
 
 def test_different_value(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent="id_tag_def_persistent1",
@@ -47,7 +47,7 @@ def test_different_value(tag):
 
 
 def test_different_tag_def(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -58,7 +58,7 @@ def test_different_tag_def(tag):
 
 
 def test_same(tag):
-    other = TagInstance(
+    other = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -71,7 +71,7 @@ def test_same(tag):
 @pytest.mark.django_db
 def test_get_most_recent(tag):
     tag.save()
-    new = TagInstance(
+    new = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -80,14 +80,14 @@ def test_get_most_recent(tag):
         previous_version=tag,
     )
     new.save()
-    by_id = TagInstance.most_recent_by_id(c.id_tag_persistent_test)
+    by_id = TagInstance.get_by_id(c.id_tag_persistent_test)
     assert by_id.value == "1.0"
 
 
 @pytest.mark.django_db
 def test_get_most_recent_by_ids(tag):
     tag.save()
-    new = TagInstance(
+    new = TagInstanceHistory(
         id_persistent=c.id_tag_persistent_test,
         id_entity_persistent=cp.id_persistent_test,
         id_tag_definition_persistent=c.id_tag_def_persistent_test,
@@ -105,7 +105,7 @@ def test_get_most_recent_by_ids(tag):
 @pytest.mark.django_db
 def test_entity_missing(user):
     with pytest.raises(EntityMissingException) as exc:
-        TagInstance.change_or_create(
+        TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
             user=user,
@@ -119,7 +119,7 @@ def test_entity_missing(user):
 def test_tag_def_missing(entity0, user):
     entity0.save()
     with pytest.raises(TagDefinitionMissingException) as exc:
-        TagInstance.change_or_create(
+        TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
             user=user,
@@ -134,7 +134,7 @@ def test_tag_def_no_permission(entity0, tag_def_user, user1):
     entity0.save()
     tag_def_user.save()
     with pytest.raises(TagDefinitionPermissionException) as exc:
-        TagInstance.change_or_create(
+        TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
             user=user1,
@@ -149,7 +149,7 @@ def test_tag_def_no_permission(entity0, tag_def_user, user1):
 def test_add_tag_root(entity0, tag_def_user):
     entity0.save()
     tag_def_user.save()
-    ret, _ = TagInstance.change_or_create(
+    ret, _ = TagInstanceHistory.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
         user=tag_def_user.owner,
@@ -178,7 +178,7 @@ def test_add_tag_child(entity0, tag_def_user):
     entity0.save()
     tag_def_user.id_parent_persistent = c.id_tag_def_parent_persistent_test
     tag_def_user.save()
-    ret, _ = TagInstance.change_or_create(
+    ret, _ = TagInstanceHistory.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
         user=tag_def_user.owner,
@@ -205,11 +205,12 @@ def test_empty_chunk(tag_def):
 def test_chunk_versions(tag_def):
     tag_def.save()
     last_values = []
+    last_ids = []
     for i in range(10):
         tag = None
         previous_version = None
         for j in range(i % 3 + 1):
-            tag = TagInstance(
+            tag = TagInstanceHistory(
                 id_persistent=f"id_tag_test{i}",
                 id_entity_persistent=cp.id_persistent_test,
                 id_tag_definition_persistent=tag_def.id_persistent,
@@ -219,8 +220,9 @@ def test_chunk_versions(tag_def):
             )
             tag.save()
             previous_version = tag  # pylint: disable=no-member
+        last_ids.append(tag.id)  # pylint: disable=no-member
         last_values.append(tag.value)
-    ret = TagInstance.by_tag_chunked(c.id_tag_def_persistent_test, 2, 3)
+    ret = TagInstance.by_tag_chunked(c.id_tag_def_persistent_test, last_ids[2], 3)
     ret_values = [tag.value for tag in ret]
     assert ret_values == last_values[2 : 2 + 3]
 
@@ -229,7 +231,7 @@ def test_chunk_versions(tag_def):
 def test_chunk_filter_tag_instance(tag_def):
     tag_def.save()
     for i in range(10):
-        tag = TagInstance(
+        tag = TagInstanceHistory(
             id_persistent=f"id_tag_test{i}",
             id_entity_persistent=cp.id_persistent_test,
             id_tag_definition_persistent=tag_def.id_persistent + i * "0",