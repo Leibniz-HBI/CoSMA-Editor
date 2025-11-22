Leibniz-HBI/DP-REX

Add API endpoint for getting child tag definitions.

vran/tag/api/definitions.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import List, Optional
 from uuid import uuid4
 
-from django.db import IntegrityError
+from django.db import DatabaseError, IntegrityError
 from ninja import Router, Schema
 
 from vran.exception import (
@@ -15,22 +15,22 @@ from vran.exception import (
     TagDefinitionExistsException,
     ValidationException,
 )
-from vran.tag.models_django import TagDefinition as TagDefintionDb
+from vran.tag.models_django import TagDefinition as TagDefinitionDb
 from vran.util.django import save_many_atomic
 
 router = Router()
 
 
 _tag_type_mapping_api_to_db = {
-    "INNER": TagDefintionDb.INNER,
-    "FLOAT": TagDefintionDb.FLOAT,
-    "STRING": TagDefintionDb.STRING,
+    "INNER": TagDefinitionDb.INNER,
+    "FLOAT": TagDefinitionDb.FLOAT,
+    "STRING": TagDefinitionDb.STRING,
 }
 
 _tag_type_mapping_db_to_api = {
-    TagDefintionDb.INNER: "INNER",
-    TagDefintionDb.FLOAT: "FLOAT",
-    TagDefintionDb.STRING: "STRING",
+    TagDefinitionDb.INNER: "INNER",
+    TagDefinitionDb.FLOAT: "FLOAT",
+    TagDefinitionDb.STRING: "STRING",
 }
 
 
@@ -48,6 +48,12 @@ class TagDefinitionList(Schema):
     "API model for a list of tag definitions."
     # pylint: disable=too-few-public-methods
     tag_definitions: List[TagDefinition]
+
+
+class PostGetChildrenRequest(Schema):
+    "API model for getting tag definitions by parent_id_persistent"
+    # pylint: disable=too-few-public-methods
+    id_parent_persistent: Optional[str]
 
 
 @router.post("", response={200: TagDefinitionList, 400: ApiError, 500: ApiError})
@@ -97,14 +103,32 @@ def post_tag_definitions(_, tag_definition_list: TagDefinitionList):
 
     return 200, TagDefinitionList(
         tag_definitions=[
-            tag_definition_db_to_api(tag_def[0]) for tag_def in tag_def_dbs
+            tag_definition_db_to_api(tag_def) for tag_def, _ in tag_def_dbs
         ]
     )
 
 
+@router.post("/children", response={200: TagDefinitionList, 500: ApiError})
+def post_get_tag_definition_children(_, post_children_request: PostGetChildrenRequest):
+    "Get tag definitions by id_parent_persistent."
+    try:
+        child_definitions_db = list(
+            TagDefinitionDb.most_recent_children(
+                post_children_request.id_parent_persistent
+            )
+        )
+        return 200, TagDefinitionList(
+            tag_definitions=[
+                tag_definition_db_to_api(tag_def) for tag_def in child_definitions_db
+            ]
+        )
+    except DatabaseError:
+        return 500, ApiError(msg="Database Error.")
+
+
 def tag_definition_api_to_db(
     tag_definition: TagDefinition, time_edit: datetime
-) -> TagDefintionDb:
+) -> TagDefinitionDb:
     "Convert a tag definition from API to database model."
     if tag_definition.id_persistent:
         persistent_id = tag_definition.id_persistent
@@ -120,7 +144,7 @@ def tag_definition_api_to_db(
                 "has version but no id_persistent."
             )
         persistent_id = str(uuid4())
-    return TagDefintionDb.change_or_create(
+    return TagDefinitionDb.change_or_create(
         id_persistent=persistent_id,
         id_parent_persistent=tag_definition.id_parent_persistent,
         version=tag_definition.version,
@@ -130,7 +154,7 @@ def tag_definition_api_to_db(
     )
 
 
-def tag_definition_db_to_api(tag_definition: TagDefintionDb) -> TagDefinition:
+def tag_definition_db_to_api(tag_definition: TagDefinitionDb) -> TagDefinition:
     "Convert a tag defintion from database to API model."
     return TagDefinition(
         id_persistent=tag_definition.id_persistent,

tests/tag/api/integration/post_tag_def_children_test.py
@@ -1,0 +1,78 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, patch
+
+from django.db import DatabaseError
+
+from tests.tag.api.integration import requests as r
+
+
+def test_empty_db(live_server):
+    req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 200
+    assert req.json()["tag_definitions"] == []
+
+
+def test_single_root_none(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    root_tag_def_rsp = req.json()["tag_definitions"][0]
+    req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 1
+    assert tag_definitions[0] == root_tag_def_rsp
+
+
+def test_multi_root(live_server, root_tag_def):
+    root_tag_def1 = root_tag_def.copy()
+    req = r.post_tag_defs(live_server.url, [root_tag_def, root_tag_def1])
+    assert req.status_code == 200
+    root_tag_def_rsps = req.json()["tag_definitions"]
+    req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 2
+    assert tag_definitions[0] == root_tag_def_rsps[0]
+    assert tag_definitions[1] == root_tag_def_rsps[1]
+
+
+def test_single_child(live_server, root_tag_def, child_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    root_tag_def_rsp = req.json()["tag_definitions"][0]
+    root_tag_def_rsp_id_persistent = root_tag_def_rsp["id_persistent"]
+    child_tag_def["id_parent_persistent"] = root_tag_def_rsp_id_persistent
+    req = r.post_tag_def(live_server.url, child_tag_def)
+    assert req.status_code == 200
+    child_tag_def_rsp = req.json()["tag_definitions"][0]
+    req = r.post_tag_def_children(live_server.url, root_tag_def_rsp_id_persistent)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 1
+    assert tag_definitions[0] == child_tag_def_rsp
+
+
+def test_multi_child(live_server, root_tag_def, child_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    root_tag_def_rsp_id_persistent = req.json()["tag_definitions"][0]["id_persistent"]
+    child_tag_def["id_parent_persistent"] = root_tag_def_rsp_id_persistent
+    child_tag_def1 = child_tag_def.copy()
+    req = r.post_tag_defs(live_server.url, [child_tag_def, child_tag_def1])
+    assert req.status_code == 200
+    child_tag_def_rsps = req.json()["tag_definitions"]
+    req = r.post_tag_def_children(live_server.url, root_tag_def_rsp_id_persistent)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 2
+    assert tag_definitions[0] == child_tag_def_rsps[0]
+    assert tag_definitions[1] == child_tag_def_rsps[1]
+
+
+def test_bad_db(live_server):
+    mock = MagicMock()
+    mock.side_effect = DatabaseError()
+    with patch("vran.tag.models_django.TagDefinition.most_recent_children", mock):
+        req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 500
+    assert req.json()["msg"] == "Database Error."

tests/tag/api/integration/requests.py
@@ -43,3 +43,11 @@ def post_tag_instance_chunks(url, tag_def_id, offset, limit):
         },
         timeout=900,
     )
+
+
+def post_tag_def_children(url, id_persistent):
+    return requests.post(
+        urljoin(url, "vran/api/tags/definitions/children"),
+        json={"id_parent_persistent": id_persistent},
+        timeout=900,
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/tag/api/definitions.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import List, Optional
 from uuid import uuid4
 
-from django.db import IntegrityError
+from django.db import DatabaseError, IntegrityError
 from ninja import Router, Schema
 
 from cosmae.exception import (
@@ -15,22 +15,22 @@ from cosmae.exception import (
     TagDefinitionExistsException,
     ValidationException,
 )
-from cosmae.tag.models_django import TagDefinition as TagDefintionDb
+from cosmae.tag.models_django import TagDefinition as TagDefinitionDb
 from cosmae.util.django import save_many_atomic
 
 router = Router()
 
 
 _tag_type_mapping_api_to_db = {
-    "INNER": TagDefintionDb.INNER,
-    "FLOAT": TagDefintionDb.FLOAT,
-    "STRING": TagDefintionDb.STRING,
+    "INNER": TagDefinitionDb.INNER,
+    "FLOAT": TagDefinitionDb.FLOAT,
+    "STRING": TagDefinitionDb.STRING,
 }
 
 _tag_type_mapping_db_to_api = {
-    TagDefintionDb.INNER: "INNER",
-    TagDefintionDb.FLOAT: "FLOAT",
-    TagDefintionDb.STRING: "STRING",
+    TagDefinitionDb.INNER: "INNER",
+    TagDefinitionDb.FLOAT: "FLOAT",
+    TagDefinitionDb.STRING: "STRING",
 }
 
 
@@ -48,6 +48,12 @@ class TagDefinitionList(Schema):
     "API model for a list of tag definitions."
     # pylint: disable=too-few-public-methods
     tag_definitions: List[TagDefinition]
+
+
+class PostGetChildrenRequest(Schema):
+    "API model for getting tag definitions by parent_id_persistent"
+    # pylint: disable=too-few-public-methods
+    id_parent_persistent: Optional[str]
 
 
 @router.post("", response={200: TagDefinitionList, 400: ApiError, 500: ApiError})
@@ -97,14 +103,32 @@ def post_tag_definitions(_, tag_definition_list: TagDefinitionList):
 
     return 200, TagDefinitionList(
         tag_definitions=[
-            tag_definition_db_to_api(tag_def[0]) for tag_def in tag_def_dbs
+            tag_definition_db_to_api(tag_def) for tag_def, _ in tag_def_dbs
         ]
     )
 
 
+@router.post("/children", response={200: TagDefinitionList, 500: ApiError})
+def post_get_tag_definition_children(_, post_children_request: PostGetChildrenRequest):
+    "Get tag definitions by id_parent_persistent."
+    try:
+        child_definitions_db = list(
+            TagDefinitionDb.most_recent_children(
+                post_children_request.id_parent_persistent
+            )
+        )
+        return 200, TagDefinitionList(
+            tag_definitions=[
+                tag_definition_db_to_api(tag_def) for tag_def in child_definitions_db
+            ]
+        )
+    except DatabaseError:
+        return 500, ApiError(msg="Database Error.")
+
+
 def tag_definition_api_to_db(
     tag_definition: TagDefinition, time_edit: datetime
-) -> TagDefintionDb:
+) -> TagDefinitionDb:
     "Convert a tag definition from API to database model."
     if tag_definition.id_persistent:
         persistent_id = tag_definition.id_persistent
@@ -120,7 +144,7 @@ def tag_definition_api_to_db(
                 "has version but no id_persistent."
             )
         persistent_id = str(uuid4())
-    return TagDefintionDb.change_or_create(
+    return TagDefinitionDb.change_or_create(
         id_persistent=persistent_id,
         id_parent_persistent=tag_definition.id_parent_persistent,
         version=tag_definition.version,
@@ -130,7 +154,7 @@ def tag_definition_api_to_db(
     )
 
 
-def tag_definition_db_to_api(tag_definition: TagDefintionDb) -> TagDefinition:
+def tag_definition_db_to_api(tag_definition: TagDefinitionDb) -> TagDefinition:
     "Convert a tag defintion from database to API model."
     return TagDefinition(
         id_persistent=tag_definition.id_persistent,

tests/tag/api/integration/post_tag_def_children_test.py
@@ -1,0 +1,78 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, patch
+
+from django.db import DatabaseError
+
+from tests.tag.api.integration import requests as r
+
+
+def test_empty_db(live_server):
+    req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 200
+    assert req.json()["tag_definitions"] == []
+
+
+def test_single_root_none(live_server, root_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    root_tag_def_rsp = req.json()["tag_definitions"][0]
+    req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 1
+    assert tag_definitions[0] == root_tag_def_rsp
+
+
+def test_multi_root(live_server, root_tag_def):
+    root_tag_def1 = root_tag_def.copy()
+    req = r.post_tag_defs(live_server.url, [root_tag_def, root_tag_def1])
+    assert req.status_code == 200
+    root_tag_def_rsps = req.json()["tag_definitions"]
+    req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 2
+    assert tag_definitions[0] == root_tag_def_rsps[0]
+    assert tag_definitions[1] == root_tag_def_rsps[1]
+
+
+def test_single_child(live_server, root_tag_def, child_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    root_tag_def_rsp = req.json()["tag_definitions"][0]
+    root_tag_def_rsp_id_persistent = root_tag_def_rsp["id_persistent"]
+    child_tag_def["id_parent_persistent"] = root_tag_def_rsp_id_persistent
+    req = r.post_tag_def(live_server.url, child_tag_def)
+    assert req.status_code == 200
+    child_tag_def_rsp = req.json()["tag_definitions"][0]
+    req = r.post_tag_def_children(live_server.url, root_tag_def_rsp_id_persistent)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 1
+    assert tag_definitions[0] == child_tag_def_rsp
+
+
+def test_multi_child(live_server, root_tag_def, child_tag_def):
+    req = r.post_tag_def(live_server.url, root_tag_def)
+    assert req.status_code == 200
+    root_tag_def_rsp_id_persistent = req.json()["tag_definitions"][0]["id_persistent"]
+    child_tag_def["id_parent_persistent"] = root_tag_def_rsp_id_persistent
+    child_tag_def1 = child_tag_def.copy()
+    req = r.post_tag_defs(live_server.url, [child_tag_def, child_tag_def1])
+    assert req.status_code == 200
+    child_tag_def_rsps = req.json()["tag_definitions"]
+    req = r.post_tag_def_children(live_server.url, root_tag_def_rsp_id_persistent)
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert len(tag_definitions) == 2
+    assert tag_definitions[0] == child_tag_def_rsps[0]
+    assert tag_definitions[1] == child_tag_def_rsps[1]
+
+
+def test_bad_db(live_server):
+    mock = MagicMock()
+    mock.side_effect = DatabaseError()
+    with patch("cosmae.tag.models_django.TagDefinition.most_recent_children", mock):
+        req = r.post_tag_def_children(live_server.url, None)
+    assert req.status_code == 500
+    assert req.json()["msg"] == "Database Error."

tests/tag/api/integration/requests.py
@@ -43,3 +43,11 @@ def post_tag_instance_chunks(url, tag_def_id, offset, limit):
         },
         timeout=900,
     )
+
+
+def post_tag_def_children(url, id_persistent):
+    return requests.post(
+        urljoin(url, "cosmae/api/tags/definitions/children"),
+        json={"id_parent_persistent": id_persistent},
+        timeout=900,
+    )