Leibniz-HBI/DP-REX

Get correct tag values for display text of contribution entities.

vran/management/display_txt/util.py
@@ -1,24 +1,47 @@
 "Utils for managing display text alternatives"
-from django.db.models import Case, IntegerField, When
+from django.db.models import Case, IntegerField, OuterRef, Subquery, When
 
 from vran.management.models_django import ConfigValue
+from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.models_django import TagDefinition
 
 DISPLAY_TXT_ORDER_CONFIG_KEY = "display_txt_order"
 
 
-def get_display_txt_order_tag_definitions():
+def get_display_txt_order_tag_definitions(id_contribution_persistent=None):
     "Retrieve the order of tag definitions used for display text alternatives."
     id_tag_def_persistent_list = ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY, [])
-    order = [
-        When(id_persistent=val, then=idx)
-        for idx, val in enumerate(id_tag_def_persistent_list)
-    ]
-    tag_def_query = (
-        TagDefinition.most_recent_query_set()
-        .filter(id_persistent__in=id_tag_def_persistent_list)
-        .annotate(sort_key=Case(*order, output_field=IntegerField()))
-        .order_by("sort_key")
-    )
+    if id_contribution_persistent is None:
+        order = [
+            When(id_persistent=val, then=idx)
+            for idx, val in enumerate(id_tag_def_persistent_list)
+        ]
+        tag_def_query = TagDefinition.most_recent_query_set().filter(
+            id_persistent__in=id_tag_def_persistent_list
+        )
 
-    return tag_def_query
+    else:
+        merge_requests = TagMergeRequest.get_for_contribution_query_set(
+            str(id_contribution_persistent)
+        )
+        relevant_for_display_txt_order = merge_requests.filter(
+            id_destination_persistent__in=id_tag_def_persistent_list
+        )
+        tag_def_query = (
+            TagDefinition.most_recent_query_set()
+            .annotate(
+                id_destination_persistent=Subquery(
+                    relevant_for_display_txt_order.filter(
+                        id_origin_persistent=OuterRef("id_persistent")
+                    ).values("id_destination_persistent")
+                )
+            )
+            .filter(id_destination_persistent__isnull=False)
+        )
+        order = [
+            When(id_destination_persistent=val, then=idx)
+            for idx, val in enumerate(id_tag_def_persistent_list)
+        ]
+    return tag_def_query.annotate(
+        sort_key=Case(*order, output_field=IntegerField())
+    ).order_by("sort_key")

tests/entity/queue_test.py
@@ -5,18 +5,24 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 import tests.tag.common as ct
+from vran.contribution.models_django import ContributionCandidate
 from vran.entity.models_django import Entity
 from vran.entity.queue import (
     entity_display_txt_information_cache,
     update_display_txt_cache,
 )
+from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.models_django import TagInstanceHistory
 
 id_persistent_entity_no_display_txt = "7d5c19e6-f47d-4c4f-a92f-0c858c18885f"
 time_edit_entity_no_display_txt = datetime(2002, 3, 7, tzinfo=timezone.utc)
+time_edit_entity_contribution_no_display_txt = datetime(2002, 4, 7, tzinfo=timezone.utc)
 id_persistent_instance_tag_def_1 = "4603a4ed-b1b2-4a25-9556-dd82011e3d06"
 time_edit_instance_tag_def_1 = datetime(2002, 5, 6, tzinfo=timezone.utc)
 value_instance_tag_def_1 = "Some value used as display txt"
+id_contribution_persistent = "2c206f03-8926-4b22-85ff-56131da543f8"
+id_tag_merge_request_persistent = "c412772a-b894-495a-9fe9-fd4b5c31894f"
+time_tag_def_created_at = datetime(2002, 5, 8, tzinfo=timezone.utc)
 
 
 @pytest.mark.django_db
@@ -100,3 +106,62 @@ def test_exception(entity_without_display_txt):
         entity_without_display_txt.id_persistent
     )
     assert result == (entity_without_display_txt.id_persistent, "id_persistent")
+
+
+@pytest.fixture
+def contribution_instance_without_display_txt(
+    entity_without_display_txt, tag_def, tag_def1, user, instance_tag_def_1
+):
+    (
+        contribution,
+        _,
+    ) = ContributionCandidate.objects.get_or_create(  # pylint: disable=no-member
+        id_persistent=id_contribution_persistent,
+        name="a test contribution",
+        description="This is a contribution used in tests",
+        anonymous=False,
+        has_header=True,
+        created_by=user,
+        file_name="file.csv",
+        state=ContributionCandidate.VALUES_EXTRACTED,
+    )
+    entity, _ = Entity.change_or_create(
+        entity_without_display_txt.id_persistent,
+        time_edit_entity_contribution_no_display_txt,
+        version=entity_without_display_txt.id,
+        contribution_candidate_id=contribution.id_persistent,
+    )
+    entity.save()
+    TagMergeRequest.objects.create(  # pylint: disable=no-member
+        id_persistent=id_tag_merge_request_persistent,
+        assigned_to=None,
+        contribution_candidate=contribution,
+        id_origin_persistent=tag_def1.id_persistent,
+        id_destination_persistent=tag_def.id_persistent,
+        created_at=time_tag_def_created_at,
+        created_by=user,
+    )
+    return instance_tag_def_1
+
+
+def test_contribution(contribution_instance_without_display_txt, display_txt_order_0):
+    update_display_txt_cache(
+        contribution_instance_without_display_txt.id_entity_persistent
+    )
+    result = entity_display_txt_information_cache.get(
+        contribution_instance_without_display_txt.id_entity_persistent
+    )
+    assert result[0] == value_instance_tag_def_1
+    tag_def = result[1]
+    assert len(tag_def) == 8
+    assert "id" in tag_def
+    tag_def.pop("id")
+    assert tag_def == {
+        "id_persistent": ct.id_tag_def_persistent_test_user1,
+        "id_parent_persistent": None,
+        "name": ct.name_tag_def_test1,
+        "type": "STR",
+        "owner": {"username": "test-user1"},
+        "curated": False,
+        "hidden": False,
+    }

Leibniz-HBI/CoSMA-Editor

cosmae/management/display_txt/util.py
@@ -1,24 +1,48 @@
 "Utils for managing display text alternatives"
-from django.db.models import Case, IntegerField, When
+
+from django.db.models import Case, IntegerField, OuterRef, Subquery, When
 
 from cosmae.management.models_django import ConfigValue
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.models_django import TagDefinition
 
 DISPLAY_TXT_ORDER_CONFIG_KEY = "display_txt_order"
 
 
-def get_display_txt_order_tag_definitions():
+def get_display_txt_order_tag_definitions(id_contribution_persistent=None):
     "Retrieve the order of tag definitions used for display text alternatives."
     id_tag_def_persistent_list = ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY, [])
-    order = [
-        When(id_persistent=val, then=idx)
-        for idx, val in enumerate(id_tag_def_persistent_list)
-    ]
-    tag_def_query = (
-        TagDefinition.most_recent_query_set()
-        .filter(id_persistent__in=id_tag_def_persistent_list)
-        .annotate(sort_key=Case(*order, output_field=IntegerField()))
-        .order_by("sort_key")
-    )
+    if id_contribution_persistent is None:
+        order = [
+            When(id_persistent=val, then=idx)
+            for idx, val in enumerate(id_tag_def_persistent_list)
+        ]
+        tag_def_query = TagDefinition.most_recent_query_set().filter(
+            id_persistent__in=id_tag_def_persistent_list
+        )
 
-    return tag_def_query
+    else:
+        merge_requests = TagMergeRequest.get_for_contribution_query_set(
+            str(id_contribution_persistent)
+        )
+        relevant_for_display_txt_order = merge_requests.filter(
+            id_destination_persistent__in=id_tag_def_persistent_list
+        )
+        tag_def_query = (
+            TagDefinition.most_recent_query_set()
+            .annotate(
+                id_destination_persistent=Subquery(
+                    relevant_for_display_txt_order.filter(
+                        id_origin_persistent=OuterRef("id_persistent")
+                    ).values("id_destination_persistent")
+                )
+            )
+            .filter(id_destination_persistent__isnull=False)
+        )
+        order = [
+            When(id_destination_persistent=val, then=idx)
+            for idx, val in enumerate(id_tag_def_persistent_list)
+        ]
+    return tag_def_query.annotate(
+        sort_key=Case(*order, output_field=IntegerField())
+    ).order_by("sort_key")

tests/entity/queue_test.py
@@ -5,18 +5,24 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 import tests.tag.common as ct
+from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.entity.models_django import Entity
 from cosmae.entity.queue import (
     entity_display_txt_information_cache,
     update_display_txt_cache,
 )
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.models_django import TagInstanceHistory
 
 id_persistent_entity_no_display_txt = "7d5c19e6-f47d-4c4f-a92f-0c858c18885f"
 time_edit_entity_no_display_txt = datetime(2002, 3, 7, tzinfo=timezone.utc)
+time_edit_entity_contribution_no_display_txt = datetime(2002, 4, 7, tzinfo=timezone.utc)
 id_persistent_instance_tag_def_1 = "4603a4ed-b1b2-4a25-9556-dd82011e3d06"
 time_edit_instance_tag_def_1 = datetime(2002, 5, 6, tzinfo=timezone.utc)
 value_instance_tag_def_1 = "Some value used as display txt"
+id_contribution_persistent = "2c206f03-8926-4b22-85ff-56131da543f8"
+id_tag_merge_request_persistent = "c412772a-b894-495a-9fe9-fd4b5c31894f"
+time_tag_def_created_at = datetime(2002, 5, 8, tzinfo=timezone.utc)
 
 
 @pytest.mark.django_db
@@ -100,3 +106,62 @@ def test_exception(entity_without_display_txt):
         entity_without_display_txt.id_persistent
     )
     assert result == (entity_without_display_txt.id_persistent, "id_persistent")
+
+
+@pytest.fixture
+def contribution_instance_without_display_txt(
+    entity_without_display_txt, tag_def, tag_def1, user, instance_tag_def_1
+):
+    (
+        contribution,
+        _,
+    ) = ContributionCandidate.objects.get_or_create(  # pylint: disable=no-member
+        id_persistent=id_contribution_persistent,
+        name="a test contribution",
+        description="This is a contribution used in tests",
+        anonymous=False,
+        has_header=True,
+        created_by=user,
+        file_name="file.csv",
+        state=ContributionCandidate.VALUES_EXTRACTED,
+    )
+    entity, _ = Entity.change_or_create(
+        entity_without_display_txt.id_persistent,
+        time_edit_entity_contribution_no_display_txt,
+        version=entity_without_display_txt.id,
+        contribution_candidate_id=contribution.id_persistent,
+    )
+    entity.save()
+    TagMergeRequest.objects.create(  # pylint: disable=no-member
+        id_persistent=id_tag_merge_request_persistent,
+        assigned_to=None,
+        contribution_candidate=contribution,
+        id_origin_persistent=tag_def1.id_persistent,
+        id_destination_persistent=tag_def.id_persistent,
+        created_at=time_tag_def_created_at,
+        created_by=user,
+    )
+    return instance_tag_def_1
+
+
+def test_contribution(contribution_instance_without_display_txt, display_txt_order_0):
+    update_display_txt_cache(
+        contribution_instance_without_display_txt.id_entity_persistent
+    )
+    result = entity_display_txt_information_cache.get(
+        contribution_instance_without_display_txt.id_entity_persistent
+    )
+    assert result[0] == value_instance_tag_def_1
+    tag_def = result[1]
+    assert len(tag_def) == 8
+    assert "id" in tag_def
+    tag_def.pop("id")
+    assert tag_def == {
+        "id_persistent": ct.id_tag_def_persistent_test_user1,
+        "id_parent_persistent": None,
+        "name": ct.name_tag_def_test1,
+        "type": "STR",
+        "owner": {"username": "test-user1"},
+        "curated": False,
+        "hidden": False,
+    }