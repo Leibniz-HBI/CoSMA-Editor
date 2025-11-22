Leibniz-HBI/DP-REX

Remove anonymous contribution field from DB and API models.

vran/migrations/0034_remove_contributioncandidate_anonymous.py
@@ -1,0 +1,17 @@
+# Generated by Django 4.2.8 on 2024-01-23 10:49
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0033_alter_entity_display_txt"),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name="contributioncandidate",
+            name="anonymous",
+        ),
+    ]

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0034_remove_contributioncandidate_anonymous.py
@@ -1,0 +1,17 @@
+# Generated by Django 4.2.8 on 2024-01-23 10:49
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0033_alter_entity_display_txt"),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name="contributioncandidate",
+            name="anonymous",
+        ),
+    ]