Leibniz-HBI/DP-REX

Add frontend for managing display text order.

ui/src/management/display_txt/__tests__/display_txt_order_integration.test.tsx
@@ -1,0 +1,228 @@
+/**
+ * @jest-environment jsdom
+ */
+import { configureStore } from '@reduxjs/toolkit'
+import { TagSelectionState, newTagSelectionState } from '../../../column_menu/state'
+import { ErrorManager, errorSlice } from '../../../util/error/slice'
+import { newRemote } from '../../../util/state'
+import { DisplayTxtManagementState } from '../state'
+import { RenderOptions, render, screen, waitFor } from '@testing-library/react'
+import { displayTxtManagementReducer } from '../slice'
+import { tagSelectionSlice } from '../../../column_menu/slice'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { DisplayTxtManagementComponent } from '../components'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: {
+        displayTxtManagement: DisplayTxtManagementState
+        tagSelection: TagSelectionState
+        error: ErrorManager
+    }
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    fetchMock: jest.Mock,
+    {
+        preloadedState = {
+            displayTxtManagement: { tagDefinitions: newRemote([]) },
+            tagSelection: newTagSelectionState({}),
+            error: { errorList: [], errorMap: {} }
+        },
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    const store = configureStore({
+        reducer: {
+            displayTxtManagement: displayTxtManagementReducer,
+            tagSelection: tagSelectionSlice.reducer,
+            error: errorSlice.reducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({ thunk: { extraArgument: fetchMock } }),
+        preloadedState
+    })
+    function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>
+    }
+
+    // Return an object with the store and all of RTL's query functions
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+
+function addResponseSequence(mock: jest.Mock, responses: [number, unknown][]) {
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        mock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const idTagDef0 = 'id-tag-test-0'
+const nameTagDef0 = 'tag def 0'
+const idTagDef1 = 'id-tag-test-1'
+const nameTagDef1 = 'tag def 1'
+const idTagDef2 = 'id-tag-test-2'
+const nameTagDef2 = 'tag def 2'
+
+function initialResponseSequence(mock: jest.Mock) {
+    addResponseSequence(mock, [
+        [
+            200,
+            {
+                tag_definitions: [
+                    {
+                        id_persistent: idTagDef2,
+                        name_path: [nameTagDef2],
+                        name: nameTagDef2,
+                        curated: true,
+                        version: 2,
+                        type: 'STRING'
+                    }
+                ]
+            }
+        ],
+        [
+            200,
+            {
+                tag_definitions: [
+                    {
+                        id_persistent: idTagDef0,
+                        name_path: [nameTagDef0],
+                        name: nameTagDef0,
+                        curated: true,
+                        version: 0,
+                        type: 'STRING'
+                    },
+                    {
+                        id_persistent: idTagDef1,
+                        name_path: [nameTagDef1],
+                        name: nameTagDef1,
+                        curated: false,
+                        version: 1,
+                        type: 'STRING'
+                    },
+                    {
+                        id_persistent: idTagDef2,
+                        name_path: [nameTagDef2],
+                        name: nameTagDef2,
+                        curated: true,
+                        version: 2,
+                        type: 'STRING'
+                    }
+                ]
+            }
+        ],
+        [200, { tag_definitions: [] }],
+        [200, { tag_definitions: [] }],
+        [200, { tag_definitions: [] }]
+    ])
+}
+
+const expectedGetRequests = [
+    [
+        'http://127.0.0.1:8000/vran/api/manage/display_txt/order',
+        { credentials: 'include' }
+    ],
+    [
+        'http://127.0.0.1:8000/vran/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: undefined })
+        }
+    ],
+    [
+        'http://127.0.0.1:8000/vran/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: idTagDef0 })
+        }
+    ],
+    [
+        'http://127.0.0.1:8000/vran/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: idTagDef1 })
+        }
+    ],
+    [
+        'http://127.0.0.1:8000/vran/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: idTagDef2 })
+        }
+    ]
+]
+test('get', async () => {
+    const fetchMock = jest.fn()
+    initialResponseSequence(fetchMock)
+    renderWithProviders(<DisplayTxtManagementComponent />, fetchMock)
+    await waitFor(() => {
+        screen.getByText(nameTagDef0)
+        screen.getByText(nameTagDef1)
+        const tagDef2Texts = screen.getAllByText(nameTagDef2)
+        expect(tagDef2Texts.length).toEqual(2)
+    })
+    expect(fetchMock.mock.calls).toEqual(expectedGetRequests)
+})
+test('append and remove', async () => {
+    const fetchMock = jest.fn()
+    initialResponseSequence(fetchMock)
+    addResponseSequence(fetchMock, [[200, {}]])
+    addResponseSequence(fetchMock, [[200, {}]])
+    renderWithProviders(<DisplayTxtManagementComponent />, fetchMock)
+    await waitFor(() => {
+        const tagDef0Text = screen.getByText(nameTagDef0)
+        const listEntry =
+            tagDef0Text.parentElement?.parentElement?.parentElement?.parentElement
+        expect(listEntry?.className).toEqual(
+            'list-group-item d-flex flex-row justify-content-between'
+        )
+        ;(listEntry?.children[listEntry.children.length - 1] as HTMLElement)?.click()
+    })
+
+    await waitFor(() => {
+        const tagDef0Texts = screen.getAllByText(nameTagDef2)
+        expect(tagDef0Texts.length).toEqual(2)
+        const listElement = tagDef0Texts[0]?.parentElement?.parentElement
+        expect(listElement?.className).toEqual('justify-content-between row'),
+            (
+                listElement?.children[listElement.children.length - 1] as HTMLElement
+            )?.click()
+    })
+    await waitFor(() => {
+        screen.getByText(nameTagDef2)
+    })
+    expect(fetchMock.mock.calls).toEqual([
+        ...expectedGetRequests,
+        [
+            'http://127.0.0.1:8000/vran/api/manage/display_txt/order/append',
+            {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify({ id_tag_definition_persistent: idTagDef0 })
+            }
+        ],
+        [
+            `http://127.0.0.1:8000/vran/api/manage/display_txt/order/${idTagDef2}`,
+            {
+                method: 'DELETE',
+                credentials: 'include'
+            }
+        ]
+    ])
+})

ui/src/management/display_txt/components.tsx
@@ -1,0 +1,142 @@
+import { useDispatch, useSelector } from 'react-redux'
+import {
+    selectDisplayTxtTagDefinitions,
+    selectDisplayTxtTagIdPersistentSet
+} from './selectors'
+import { Col, ListGroup, Row } from 'react-bootstrap'
+import { AppDispatch } from '../../store'
+import { VrAnLoading } from '../../util/components/misc'
+import { TagDefinition } from '../../column_menu/state'
+import {
+    ColumnSelector,
+    constructColumnTitleSpans,
+    mkListItems
+} from '../../column_menu/components/selection'
+import { PlusLg, XLg } from 'react-bootstrap-icons'
+import { useEffect } from 'react'
+import {
+    appendTagDefinitionThunk,
+    getDisplayTxtTagDefinitions,
+    removeTagDefinitionThunk
+} from './thunks'
+import { selectNavigationEntries } from '../../column_menu/selectors'
+import { toggleExpansion } from '../../column_menu/slice'
+import { loadTagDefinitionHierarchy } from '../../column_menu/thunks'
+
+export function DisplayTxtManagementComponent() {
+    const dispatch: AppDispatch = useDispatch()
+    useEffect(
+        () => {
+            dispatch(getDisplayTxtTagDefinitions())
+            dispatch(loadTagDefinitionHierarchy({ expand: true }))
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )
+    return (
+        <Row className="h-100 overflow-hidden d-flex flex-row">
+            <Col className="pt-2" xs={6}>
+                <DisplayTxtOrder />
+            </Col>
+            <Col xs={6}>
+                <DisplayTxtAddMenu />
+            </Col>
+        </Row>
+    )
+}
+
+function DisplayTxtOrder() {
+    const tagDefinitions = useSelector(selectDisplayTxtTagDefinitions)
+    const dispatch: AppDispatch = useDispatch()
+    const removeTagDefinitionCallback = (tagDef: TagDefinition) =>
+        dispatch(removeTagDefinitionThunk(tagDef))
+    if (tagDefinitions.isLoading) {
+        return <VrAnLoading />
+    }
+    return (
+        <ListGroup>
+            {tagDefinitions.value.map((tagDefinition, idx) => (
+                <DisplayTxtOrderItem
+                    key={idx}
+                    tagDefinition={tagDefinition}
+                    removeTagDefinitionCallback={removeTagDefinitionCallback}
+                />
+            ))}
+        </ListGroup>
+    )
+}
+
+function DisplayTxtOrderItem({
+    tagDefinition,
+    removeTagDefinitionCallback
+}: {
+    tagDefinition: TagDefinition
+    removeTagDefinitionCallback: (tagDef: TagDefinition) => void
+}) {
+    return (
+        <ListGroup.Item>
+            <Row className="justify-content-between">
+                <Col>{constructColumnTitleSpans(tagDefinition.namePath)}</Col>
+                <Col
+                    xs="auto"
+                    className="ms-1 me-1 align-top"
+                    onClick={() => removeTagDefinitionCallback(tagDefinition)}
+                >
+                    <XLg />
+                </Col>
+            </Row>
+        </ListGroup.Item>
+    )
+}
+
+function DisplayTxtAddMenu() {
+    const dispatch: AppDispatch = useDispatch()
+    const selectionEntries = useSelector(selectNavigationEntries)
+    const alreadyPresentTagDefIdPersistentList = useSelector(
+        selectDisplayTxtTagIdPersistentSet
+    )
+    const toggleExpansionCallback = (path: number[]) => dispatch(toggleExpansion(path))
+    const appendTagDefinitionCallback = (tagDef: TagDefinition) =>
+        dispatch(appendTagDefinitionThunk(tagDef))
+    return (
+        <ColumnSelector
+            listEntries={mkListItems({
+                columnSelectionEntries: selectionEntries,
+                toggleExpansionCallback,
+                path: [],
+                level: 0,
+                mkTailElement: (tagDef) => (
+                    <DisplayTxtAddTailElement
+                        tagDefinition={tagDef}
+                        alreadyPresent={
+                            alreadyPresentTagDefIdPersistentList[tagDef.idPersistent]
+                        }
+                        appendTagDefinitionCallback={appendTagDefinitionCallback}
+                    />
+                )
+            })}
+        />
+    )
+}
+
+function DisplayTxtAddTailElement({
+    tagDefinition,
+    alreadyPresent,
+    appendTagDefinitionCallback
+}: {
+    tagDefinition: TagDefinition
+    alreadyPresent: boolean
+    appendTagDefinitionCallback: (tagDef: TagDefinition) => void
+}) {
+    if (!tagDefinition.curated || alreadyPresent) {
+        return <></>
+    }
+    return (
+        <div
+            className="ms-1 me-1 align-text-top"
+            onClick={() => appendTagDefinitionCallback(tagDefinition)}
+        >
+            <PlusLg />
+        </div>
+    )
+}

Leibniz-HBI/CoSMA-Editor

ui/src/management/display_txt/__tests__/display_txt_order_integration.test.tsx
@@ -1,0 +1,228 @@
+/**
+ * @jest-environment jsdom
+ */
+import { configureStore } from '@reduxjs/toolkit'
+import { TagSelectionState, newTagSelectionState } from '../../../column_menu/state'
+import { ErrorManager, errorSlice } from '../../../util/error/slice'
+import { newRemote } from '../../../util/state'
+import { DisplayTxtManagementState } from '../state'
+import { RenderOptions, render, screen, waitFor } from '@testing-library/react'
+import { displayTxtManagementReducer } from '../slice'
+import { tagSelectionSlice } from '../../../column_menu/slice'
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { DisplayTxtManagementComponent } from '../components'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: {
+        displayTxtManagement: DisplayTxtManagementState
+        tagSelection: TagSelectionState
+        error: ErrorManager
+    }
+}
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    fetchMock: jest.Mock,
+    {
+        preloadedState = {
+            displayTxtManagement: { tagDefinitions: newRemote([]) },
+            tagSelection: newTagSelectionState({}),
+            error: { errorList: [], errorMap: {} }
+        },
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    const store = configureStore({
+        reducer: {
+            displayTxtManagement: displayTxtManagementReducer,
+            tagSelection: tagSelectionSlice.reducer,
+            error: errorSlice.reducer
+        },
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({ thunk: { extraArgument: fetchMock } }),
+        preloadedState
+    })
+    function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>
+    }
+
+    // Return an object with the store and all of RTL's query functions
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+
+function addResponseSequence(mock: jest.Mock, responses: [number, unknown][]) {
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        mock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const idTagDef0 = 'id-tag-test-0'
+const nameTagDef0 = 'tag def 0'
+const idTagDef1 = 'id-tag-test-1'
+const nameTagDef1 = 'tag def 1'
+const idTagDef2 = 'id-tag-test-2'
+const nameTagDef2 = 'tag def 2'
+
+function initialResponseSequence(mock: jest.Mock) {
+    addResponseSequence(mock, [
+        [
+            200,
+            {
+                tag_definitions: [
+                    {
+                        id_persistent: idTagDef2,
+                        name_path: [nameTagDef2],
+                        name: nameTagDef2,
+                        curated: true,
+                        version: 2,
+                        type: 'STRING'
+                    }
+                ]
+            }
+        ],
+        [
+            200,
+            {
+                tag_definitions: [
+                    {
+                        id_persistent: idTagDef0,
+                        name_path: [nameTagDef0],
+                        name: nameTagDef0,
+                        curated: true,
+                        version: 0,
+                        type: 'STRING'
+                    },
+                    {
+                        id_persistent: idTagDef1,
+                        name_path: [nameTagDef1],
+                        name: nameTagDef1,
+                        curated: false,
+                        version: 1,
+                        type: 'STRING'
+                    },
+                    {
+                        id_persistent: idTagDef2,
+                        name_path: [nameTagDef2],
+                        name: nameTagDef2,
+                        curated: true,
+                        version: 2,
+                        type: 'STRING'
+                    }
+                ]
+            }
+        ],
+        [200, { tag_definitions: [] }],
+        [200, { tag_definitions: [] }],
+        [200, { tag_definitions: [] }]
+    ])
+}
+
+const expectedGetRequests = [
+    [
+        'http://127.0.0.1:8000/cosmae/api/manage/display_txt/order',
+        { credentials: 'include' }
+    ],
+    [
+        'http://127.0.0.1:8000/cosmae/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: undefined })
+        }
+    ],
+    [
+        'http://127.0.0.1:8000/cosmae/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: idTagDef0 })
+        }
+    ],
+    [
+        'http://127.0.0.1:8000/cosmae/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: idTagDef1 })
+        }
+    ],
+    [
+        'http://127.0.0.1:8000/cosmae/api/tags/definitions/children',
+        {
+            credentials: 'include',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id_parent_persistent: idTagDef2 })
+        }
+    ]
+]
+test('get', async () => {
+    const fetchMock = jest.fn()
+    initialResponseSequence(fetchMock)
+    renderWithProviders(<DisplayTxtManagementComponent />, fetchMock)
+    await waitFor(() => {
+        screen.getByText(nameTagDef0)
+        screen.getByText(nameTagDef1)
+        const tagDef2Texts = screen.getAllByText(nameTagDef2)
+        expect(tagDef2Texts.length).toEqual(2)
+    })
+    expect(fetchMock.mock.calls).toEqual(expectedGetRequests)
+})
+test('append and remove', async () => {
+    const fetchMock = jest.fn()
+    initialResponseSequence(fetchMock)
+    addResponseSequence(fetchMock, [[200, {}]])
+    addResponseSequence(fetchMock, [[200, {}]])
+    renderWithProviders(<DisplayTxtManagementComponent />, fetchMock)
+    await waitFor(() => {
+        const tagDef0Text = screen.getByText(nameTagDef0)
+        const listEntry =
+            tagDef0Text.parentElement?.parentElement?.parentElement?.parentElement
+        expect(listEntry?.className).toEqual(
+            'list-group-item d-flex flex-row justify-content-between'
+        )
+        ;(listEntry?.children[listEntry.children.length - 1] as HTMLElement)?.click()
+    })
+
+    await waitFor(() => {
+        const tagDef0Texts = screen.getAllByText(nameTagDef2)
+        expect(tagDef0Texts.length).toEqual(2)
+        const listElement = tagDef0Texts[0]?.parentElement?.parentElement
+        expect(listElement?.className).toEqual('justify-content-between row'),
+            (
+                listElement?.children[listElement.children.length - 1] as HTMLElement
+            )?.click()
+    })
+    await waitFor(() => {
+        screen.getByText(nameTagDef2)
+    })
+    expect(fetchMock.mock.calls).toEqual([
+        ...expectedGetRequests,
+        [
+            'http://127.0.0.1:8000/cosmae/api/manage/display_txt/order/append',
+            {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify({ id_tag_definition_persistent: idTagDef0 })
+            }
+        ],
+        [
+            `http://127.0.0.1:8000/cosmae/api/manage/display_txt/order/${idTagDef2}`,
+            {
+                method: 'DELETE',
+                credentials: 'include'
+            }
+        ]
+    ])
+})

ui/src/management/display_txt/components.tsx
@@ -1,0 +1,142 @@
+import { useDispatch, useSelector } from 'react-redux'
+import {
+    selectDisplayTxtTagDefinitions,
+    selectDisplayTxtTagIdPersistentSet
+} from './selectors'
+import { Col, ListGroup, Row } from 'react-bootstrap'
+import { AppDispatch } from '../../store'
+import { CosmaeLoading } from '../../util/components/misc'
+import { TagDefinition } from '../../column_menu/state'
+import {
+    ColumnSelector,
+    constructColumnTitleSpans,
+    mkListItems
+} from '../../column_menu/components/selection'
+import { PlusLg, XLg } from 'react-bootstrap-icons'
+import { useEffect } from 'react'
+import {
+    appendTagDefinitionThunk,
+    getDisplayTxtTagDefinitions,
+    removeTagDefinitionThunk
+} from './thunks'
+import { selectNavigationEntries } from '../../column_menu/selectors'
+import { toggleExpansion } from '../../column_menu/slice'
+import { loadTagDefinitionHierarchy } from '../../column_menu/thunks'
+
+export function DisplayTxtManagementComponent() {
+    const dispatch: AppDispatch = useDispatch()
+    useEffect(
+        () => {
+            dispatch(getDisplayTxtTagDefinitions())
+            dispatch(loadTagDefinitionHierarchy({ expand: true }))
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )
+    return (
+        <Row className="h-100 overflow-hidden d-flex flex-row">
+            <Col className="pt-2" xs={6}>
+                <DisplayTxtOrder />
+            </Col>
+            <Col xs={6}>
+                <DisplayTxtAddMenu />
+            </Col>
+        </Row>
+    )
+}
+
+function DisplayTxtOrder() {
+    const tagDefinitions = useSelector(selectDisplayTxtTagDefinitions)
+    const dispatch: AppDispatch = useDispatch()
+    const removeTagDefinitionCallback = (tagDef: TagDefinition) =>
+        dispatch(removeTagDefinitionThunk(tagDef))
+    if (tagDefinitions.isLoading) {
+        return <CosmaeLoading />
+    }
+    return (
+        <ListGroup>
+            {tagDefinitions.value.map((tagDefinition, idx) => (
+                <DisplayTxtOrderItem
+                    key={idx}
+                    tagDefinition={tagDefinition}
+                    removeTagDefinitionCallback={removeTagDefinitionCallback}
+                />
+            ))}
+        </ListGroup>
+    )
+}
+
+function DisplayTxtOrderItem({
+    tagDefinition,
+    removeTagDefinitionCallback
+}: {
+    tagDefinition: TagDefinition
+    removeTagDefinitionCallback: (tagDef: TagDefinition) => void
+}) {
+    return (
+        <ListGroup.Item>
+            <Row className="justify-content-between">
+                <Col>{constructColumnTitleSpans(tagDefinition.namePath)}</Col>
+                <Col
+                    xs="auto"
+                    className="ms-1 me-1 align-top"
+                    onClick={() => removeTagDefinitionCallback(tagDefinition)}
+                >
+                    <XLg />
+                </Col>
+            </Row>
+        </ListGroup.Item>
+    )
+}
+
+function DisplayTxtAddMenu() {
+    const dispatch: AppDispatch = useDispatch()
+    const selectionEntries = useSelector(selectNavigationEntries)
+    const alreadyPresentTagDefIdPersistentList = useSelector(
+        selectDisplayTxtTagIdPersistentSet
+    )
+    const toggleExpansionCallback = (path: number[]) => dispatch(toggleExpansion(path))
+    const appendTagDefinitionCallback = (tagDef: TagDefinition) =>
+        dispatch(appendTagDefinitionThunk(tagDef))
+    return (
+        <ColumnSelector
+            listEntries={mkListItems({
+                columnSelectionEntries: selectionEntries,
+                toggleExpansionCallback,
+                path: [],
+                level: 0,
+                mkTailElement: (tagDef) => (
+                    <DisplayTxtAddTailElement
+                        tagDefinition={tagDef}
+                        alreadyPresent={
+                            alreadyPresentTagDefIdPersistentList[tagDef.idPersistent]
+                        }
+                        appendTagDefinitionCallback={appendTagDefinitionCallback}
+                    />
+                )
+            })}
+        />
+    )
+}
+
+function DisplayTxtAddTailElement({
+    tagDefinition,
+    alreadyPresent,
+    appendTagDefinitionCallback
+}: {
+    tagDefinition: TagDefinition
+    alreadyPresent: boolean
+    appendTagDefinitionCallback: (tagDef: TagDefinition) => void
+}) {
+    if (!tagDefinition.curated || alreadyPresent) {
+        return <></>
+    }
+    return (
+        <div
+            className="ms-1 me-1 align-text-top"
+            onClick={() => appendTagDefinitionCallback(tagDefinition)}
+        >
+            <PlusLg />
+        </div>
+    )
+}