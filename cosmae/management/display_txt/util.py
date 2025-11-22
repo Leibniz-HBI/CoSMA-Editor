Leibniz-HBI/DP-REX

Compute and expose display text alternatives in backend.

vran/management/display_txt/util.py
@@ -1,0 +1,24 @@
+"Utils for managing display text alternatives"
+from django.db.models import Case, IntegerField, When
+
+from vran.management.models_django import ConfigValue
+from vran.tag.models_django import TagDefinition
+
+DISPLAY_TXT_ORDER_CONFIG_KEY = "display_txt_order"
+
+
+def get_display_txt_order_tag_definitions():
+    "Retrieve the order of tag definitions used for display text alternatives."
+    id_tag_def_persistent_list = ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY, [])
+    order = [
+        When(id_persistent=val, then=idx)
+        for idx, val in enumerate(id_tag_def_persistent_list)
+    ]
+    tag_def_query = (
+        TagDefinition.most_recent_query_set()
+        .filter(id_persistent__in=id_tag_def_persistent_list)
+        .annotate(sort_key=Case(*order, output_field=IntegerField()))
+        .order_by("sort_key")
+    )
+
+    return tag_def_query

vran/migrations/0033_alter_entity_display_txt.py
@@ -1,0 +1,18 @@
+# Generated by Django 4.2.8 on 2024-01-15 13:13
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0032_configvalue"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="entity",
+            name="display_txt",
+            field=models.TextField(blank=True, null=True),
+        ),
+    ]

vran/signals.py
@@ -1,0 +1,101 @@
+"Collection of methods for attaching signals to models."
+from uuid import uuid4
+
+from django.apps import apps
+from django.db.backends.signals import connection_created
+from django.db.models.signals import post_save
+
+
+def connect_read_csv_signal():
+    "Connect the signal for reading csv files on uploads."
+    # pylint: disable=import-outside-toplevel
+    from vran.contribution.models_django import ContributionCandidate
+    from vran.contribution.tag_definition.queue import dispatch_read_csv_head
+
+    post_save.connect(
+        dispatch_read_csv_head,
+        sender=ContributionCandidate,
+        dispatch_uid="vran.start_tag_extraction",
+    )
+
+
+def connect_merge_request_queue_process():
+    "Connect the signal for processing merge requests."
+    # pylint: disable=import-outside-toplevel
+    from vran.merge_request.models_django import TagMergeRequest
+    from vran.merge_request.queue import dispatch_merge_request_queue_process
+
+    post_save.connect(
+        dispatch_merge_request_queue_process,
+        sender=TagMergeRequest,
+        dispatch_uid="vran_merge_request_queue",
+    )
+
+
+def connect_tag_definition_queue_process():
+    "Connect the signal for computing tag definition name paths."
+    # pylint: disable=import-outside-toplevel
+    from vran.tag.queue import TagDefinition, dispatch_tag_definition_queue_process
+
+    post_save.connect(
+        dispatch_tag_definition_queue_process,
+        sender=TagDefinition,
+        dispatch_uid="vran_tag_definition_queue",
+    )
+
+
+def add_superuser(
+    sender, connection, verbosity=2, **kwargs
+):  # pylint: disable=unused-argument
+    "Add superuser if no users exist"
+    try:
+        user_model = apps.get_model("vran", "vranuser")
+        if user_model.objects.count() == 0:
+            username = "admin"
+            email = "mail@test.url"
+            password = "changeme"
+            print(f"Creating account for {username} ({email})")
+            admin = user_model.objects.create_superuser(
+                email=email,
+                username=username,
+                password=password,
+                id_persistent=str(uuid4()),
+            )
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+    except Exception:  # pylint: disable=broad-except
+        pass
+
+
+def connect_add_superuser():
+    "Connect signal for adding a superuser."
+    connection_created.connect(
+        add_superuser, dispatch_uid="vran.create_initial_superuser"
+    )
+
+
+def connect_entity_display_txt():
+    "Connect the signal for updating display txt on entity change."
+    # pylint: disable=import-outside-toplevel
+    from vran.entity.models_django import Entity
+    from vran.entity.queue import dispatch_display_txt_queue_process
+
+    post_save.connect(
+        dispatch_display_txt_queue_process,
+        sender=Entity,
+        dispatch_uid="vran.entity_display_txt",
+    )
+
+
+def connect_tag_instance_display_txt():
+    "Connect signal for updating display txt on tag instance change."
+    # pylint: disable=import-outside-toplevel
+    from vran.tag.models_django import TagInstanceHistory
+    from vran.tag.queue import dispatch_display_txt_queue_process
+
+    post_save.connect(
+        dispatch_display_txt_queue_process,
+        sender=TagInstanceHistory,
+        dispatch_uid="vran.taginstancehistory_display_txt_queue_process",
+    )

tests/conftest.py
@@ -12,6 +12,8 @@ from tests.tag import common as ct
 from tests.user import common as cu
 from tests.user.api.integration.requests import post_login, post_register
 from vran.entity.models_django import Entity
+from vran.management.display_txt.util import DISPLAY_TXT_ORDER_CONFIG_KEY
+from vran.management.models_django import ConfigValue
 from vran.tag.models_django import TagDefinition
 from vran.util import VranUser
 
@@ -244,3 +246,17 @@ redis_fixture = factories.redis_proc(port=redis_port)
 @pytest.fixture(autouse=True, scope="session")
 def redis(redis_fixture):
     return redis_fixture
+
+
+@pytest.fixture
+def display_txt_order_0(tag_def):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+
+
+@pytest.fixture
+def display_txt_order_0_1_curated(tag_def, tag_def1, tag_def_curated):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def1.id_persistent)
+    ConfigValue.append_to_list(
+        DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def_curated.id_persistent
+    )

tests/entity/queue_test.py
@@ -1,0 +1,102 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+from datetime import datetime, timezone
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+import tests.tag.common as ct
+from vran.entity.models_django import Entity
+from vran.entity.queue import (
+    entity_display_txt_information_cache,
+    update_display_txt_cache,
+)
+from vran.tag.models_django import TagInstanceHistory
+
+id_persistent_entity_no_display_txt = "7d5c19e6-f47d-4c4f-a92f-0c858c18885f"
+time_edit_entity_no_display_txt = datetime(2002, 3, 7, tzinfo=timezone.utc)
+id_persistent_instance_tag_def_1 = "4603a4ed-b1b2-4a25-9556-dd82011e3d06"
+time_edit_instance_tag_def_1 = datetime(2002, 5, 6, tzinfo=timezone.utc)
+value_instance_tag_def_1 = "Some value used as display txt"
+
+
+@pytest.mark.django_db
+def test_with_display_txt(entity0):
+    update_display_txt_cache(entity0.id_persistent)
+    result = entity_display_txt_information_cache.get(entity0.id_persistent)
+    assert result == (entity0.display_txt, "display_txt")
+
+
+@pytest.fixture
+def entity_without_display_txt(db):
+    entity, _ = Entity.change_or_create(
+        id_persistent=id_persistent_entity_no_display_txt,
+        time_edit=time_edit_entity_no_display_txt,
+    )
+    entity.save()
+    return entity
+
+
+def test_without_display_txt_and_no_tag_def_order(entity_without_display_txt):
+    update_display_txt_cache(entity_without_display_txt.id_persistent)
+    result = entity_display_txt_information_cache.get(
+        entity_without_display_txt.id_persistent
+    )
+    assert result == (entity_without_display_txt.id_persistent, "id_persistent")
+
+
+@pytest.fixture
+def instance_tag_def_1(entity_without_display_txt, tag_def1):
+    instance, _ = TagInstanceHistory.change_or_create(
+        id_persistent=id_persistent_instance_tag_def_1,
+        time_edit=time_edit_instance_tag_def_1,
+        id_entity_persistent=entity_without_display_txt.id_persistent,
+        id_tag_definition_persistent=tag_def1.id_persistent,
+        value=value_instance_tag_def_1,
+        user=tag_def1.owner,
+    )
+    instance.save()
+    return instance
+
+
+def test_without_display_txt_but_relevant_tag_instance(
+    display_txt_order_0_1_curated, instance_tag_def_1
+):
+    update_display_txt_cache(instance_tag_def_1.id_entity_persistent)
+    result = entity_display_txt_information_cache.get(
+        instance_tag_def_1.id_entity_persistent
+    )
+    assert result[0] == value_instance_tag_def_1
+    tag_def = result[1]
+    assert len(tag_def) == 8
+    assert "id" in tag_def
+    tag_def.pop("id")
+    assert tag_def == {
+        "id_persistent": ct.id_tag_def_persistent_test_user1,
+        "id_parent_persistent": None,
+        "name": ct.name_tag_def_test1,
+        "type": "STR",
+        "owner": {"username": "test-user1"},
+        "curated": False,
+        "hidden": False,
+    }
+
+
+def test_without_display_txt_and_no_relevant_tag_instance(
+    display_txt_order_0, instance_tag_def_1
+):
+    update_display_txt_cache(instance_tag_def_1.id_entity_persistent)
+    result = entity_display_txt_information_cache.get(
+        instance_tag_def_1.id_entity_persistent
+    )
+    assert result == (id_persistent_entity_no_display_txt, "id_persistent")
+
+
+def test_exception(entity_without_display_txt):
+    mock = MagicMock()
+    mock.side_effect = Exception()
+    with patch("vran.entity.queue.get_display_txt_order_tag_definitions", mock):
+        update_display_txt_cache(entity_without_display_txt.id_persistent)
+    result = entity_display_txt_information_cache.get(
+        entity_without_display_txt.id_persistent
+    )
+    assert result == (entity_without_display_txt.id_persistent, "id_persistent")

tests/management/display_txt/api/integration/append_display_txt_test.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 import tests.management.display_txt.api.integration.requests as req
 import tests.tag.common as ct
 from vran.exception import NotAuthenticatedException
-from vran.management.display_txt.api import DISPLAY_TXT_ORDER_CONFIG_KEY
+from vran.management.display_txt.util import DISPLAY_TXT_ORDER_CONFIG_KEY
 from vran.management.models_django import ConfigValue
 
 

Leibniz-HBI/CoSMA-Editor

cosmae/management/display_txt/util.py
@@ -1,0 +1,24 @@
+"Utils for managing display text alternatives"
+from django.db.models import Case, IntegerField, When
+
+from cosmae.management.models_django import ConfigValue
+from cosmae.tag.models_django import TagDefinition
+
+DISPLAY_TXT_ORDER_CONFIG_KEY = "display_txt_order"
+
+
+def get_display_txt_order_tag_definitions():
+    "Retrieve the order of tag definitions used for display text alternatives."
+    id_tag_def_persistent_list = ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY, [])
+    order = [
+        When(id_persistent=val, then=idx)
+        for idx, val in enumerate(id_tag_def_persistent_list)
+    ]
+    tag_def_query = (
+        TagDefinition.most_recent_query_set()
+        .filter(id_persistent__in=id_tag_def_persistent_list)
+        .annotate(sort_key=Case(*order, output_field=IntegerField()))
+        .order_by("sort_key")
+    )
+
+    return tag_def_query

cosmae/migrations/0033_alter_entity_display_txt.py
@@ -1,0 +1,18 @@
+# Generated by Django 4.2.8 on 2024-01-15 13:13
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0032_configvalue"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="entity",
+            name="display_txt",
+            field=models.TextField(blank=True, null=True),
+        ),
+    ]

cosmae/signals.py
@@ -1,0 +1,101 @@
+"Collection of methods for attaching signals to models."
+from uuid import uuid4
+
+from django.apps import apps
+from django.db.backends.signals import connection_created
+from django.db.models.signals import post_save
+
+
+def connect_read_csv_signal():
+    "Connect the signal for reading csv files on uploads."
+    # pylint: disable=import-outside-toplevel
+    from cosmae.contribution.models_django import ContributionCandidate
+    from cosmae.contribution.tag_definition.queue import dispatch_read_csv_head
+
+    post_save.connect(
+        dispatch_read_csv_head,
+        sender=ContributionCandidate,
+        dispatch_uid="cosmae.start_tag_extraction",
+    )
+
+
+def connect_merge_request_queue_process():
+    "Connect the signal for processing merge requests."
+    # pylint: disable=import-outside-toplevel
+    from cosmae.merge_request.models_django import TagMergeRequest
+    from cosmae.merge_request.queue import dispatch_merge_request_queue_process
+
+    post_save.connect(
+        dispatch_merge_request_queue_process,
+        sender=TagMergeRequest,
+        dispatch_uid="cosmae_merge_request_queue",
+    )
+
+
+def connect_tag_definition_queue_process():
+    "Connect the signal for computing tag definition name paths."
+    # pylint: disable=import-outside-toplevel
+    from cosmae.tag.queue import TagDefinition, dispatch_tag_definition_queue_process
+
+    post_save.connect(
+        dispatch_tag_definition_queue_process,
+        sender=TagDefinition,
+        dispatch_uid="cosmae_tag_definition_queue",
+    )
+
+
+def add_superuser(
+    sender, connection, verbosity=2, **kwargs
+):  # pylint: disable=unused-argument
+    "Add superuser if no users exist"
+    try:
+        user_model = apps.get_model("cosmae", "cosmaeuser")
+        if user_model.objects.count() == 0:
+            username = "admin"
+            email = "mail@test.url"
+            password = "changeme"
+            print(f"Creating account for {username} ({email})")
+            admin = user_model.objects.create_superuser(
+                email=email,
+                username=username,
+                password=password,
+                id_persistent=str(uuid4()),
+            )
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+    except Exception:  # pylint: disable=broad-except
+        pass
+
+
+def connect_add_superuser():
+    "Connect signal for adding a superuser."
+    connection_created.connect(
+        add_superuser, dispatch_uid="cosmae.create_initial_superuser"
+    )
+
+
+def connect_entity_display_txt():
+    "Connect the signal for updating display txt on entity change."
+    # pylint: disable=import-outside-toplevel
+    from cosmae.entity.models_django import Entity
+    from cosmae.entity.queue import dispatch_display_txt_queue_process
+
+    post_save.connect(
+        dispatch_display_txt_queue_process,
+        sender=Entity,
+        dispatch_uid="cosmae.entity_display_txt",
+    )
+
+
+def connect_tag_instance_display_txt():
+    "Connect signal for updating display txt on tag instance change."
+    # pylint: disable=import-outside-toplevel
+    from cosmae.tag.models_django import TagInstanceHistory
+    from cosmae.tag.queue import dispatch_display_txt_queue_process
+
+    post_save.connect(
+        dispatch_display_txt_queue_process,
+        sender=TagInstanceHistory,
+        dispatch_uid="cosmae.taginstancehistory_display_txt_queue_process",
+    )

tests/conftest.py
@@ -12,6 +12,8 @@ from tests.tag import common as ct
 from tests.user import common as cu
 from tests.user.api.integration.requests import post_login, post_register
 from cosmae.entity.models_django import Entity
+from cosmae.management.display_txt.util import DISPLAY_TXT_ORDER_CONFIG_KEY
+from cosmae.management.models_django import ConfigValue
 from cosmae.tag.models_django import TagDefinition
 from cosmae.util import CosmaeUser
 
@@ -244,3 +246,17 @@ redis_fixture = factories.redis_proc(port=redis_port)
 @pytest.fixture(autouse=True, scope="session")
 def redis(redis_fixture):
     return redis_fixture
+
+
+@pytest.fixture
+def display_txt_order_0(tag_def):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+
+
+@pytest.fixture
+def display_txt_order_0_1_curated(tag_def, tag_def1, tag_def_curated):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def1.id_persistent)
+    ConfigValue.append_to_list(
+        DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def_curated.id_persistent
+    )

tests/entity/queue_test.py
@@ -1,0 +1,102 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+from datetime import datetime, timezone
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+import tests.tag.common as ct
+from cosmae.entity.models_django import Entity
+from cosmae.entity.queue import (
+    entity_display_txt_information_cache,
+    update_display_txt_cache,
+)
+from cosmae.tag.models_django import TagInstanceHistory
+
+id_persistent_entity_no_display_txt = "7d5c19e6-f47d-4c4f-a92f-0c858c18885f"
+time_edit_entity_no_display_txt = datetime(2002, 3, 7, tzinfo=timezone.utc)
+id_persistent_instance_tag_def_1 = "4603a4ed-b1b2-4a25-9556-dd82011e3d06"
+time_edit_instance_tag_def_1 = datetime(2002, 5, 6, tzinfo=timezone.utc)
+value_instance_tag_def_1 = "Some value used as display txt"
+
+
+@pytest.mark.django_db
+def test_with_display_txt(entity0):
+    update_display_txt_cache(entity0.id_persistent)
+    result = entity_display_txt_information_cache.get(entity0.id_persistent)
+    assert result == (entity0.display_txt, "display_txt")
+
+
+@pytest.fixture
+def entity_without_display_txt(db):
+    entity, _ = Entity.change_or_create(
+        id_persistent=id_persistent_entity_no_display_txt,
+        time_edit=time_edit_entity_no_display_txt,
+    )
+    entity.save()
+    return entity
+
+
+def test_without_display_txt_and_no_tag_def_order(entity_without_display_txt):
+    update_display_txt_cache(entity_without_display_txt.id_persistent)
+    result = entity_display_txt_information_cache.get(
+        entity_without_display_txt.id_persistent
+    )
+    assert result == (entity_without_display_txt.id_persistent, "id_persistent")
+
+
+@pytest.fixture
+def instance_tag_def_1(entity_without_display_txt, tag_def1):
+    instance, _ = TagInstanceHistory.change_or_create(
+        id_persistent=id_persistent_instance_tag_def_1,
+        time_edit=time_edit_instance_tag_def_1,
+        id_entity_persistent=entity_without_display_txt.id_persistent,
+        id_tag_definition_persistent=tag_def1.id_persistent,
+        value=value_instance_tag_def_1,
+        user=tag_def1.owner,
+    )
+    instance.save()
+    return instance
+
+
+def test_without_display_txt_but_relevant_tag_instance(
+    display_txt_order_0_1_curated, instance_tag_def_1
+):
+    update_display_txt_cache(instance_tag_def_1.id_entity_persistent)
+    result = entity_display_txt_information_cache.get(
+        instance_tag_def_1.id_entity_persistent
+    )
+    assert result[0] == value_instance_tag_def_1
+    tag_def = result[1]
+    assert len(tag_def) == 8
+    assert "id" in tag_def
+    tag_def.pop("id")
+    assert tag_def == {
+        "id_persistent": ct.id_tag_def_persistent_test_user1,
+        "id_parent_persistent": None,
+        "name": ct.name_tag_def_test1,
+        "type": "STR",
+        "owner": {"username": "test-user1"},
+        "curated": False,
+        "hidden": False,
+    }
+
+
+def test_without_display_txt_and_no_relevant_tag_instance(
+    display_txt_order_0, instance_tag_def_1
+):
+    update_display_txt_cache(instance_tag_def_1.id_entity_persistent)
+    result = entity_display_txt_information_cache.get(
+        instance_tag_def_1.id_entity_persistent
+    )
+    assert result == (id_persistent_entity_no_display_txt, "id_persistent")
+
+
+def test_exception(entity_without_display_txt):
+    mock = MagicMock()
+    mock.side_effect = Exception()
+    with patch("cosmae.entity.queue.get_display_txt_order_tag_definitions", mock):
+        update_display_txt_cache(entity_without_display_txt.id_persistent)
+    result = entity_display_txt_information_cache.get(
+        entity_without_display_txt.id_persistent
+    )
+    assert result == (entity_without_display_txt.id_persistent, "id_persistent")

tests/management/display_txt/api/integration/append_display_txt_test.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 import tests.management.display_txt.api.integration.requests as req
 import tests.tag.common as ct
 from cosmae.exception import NotAuthenticatedException
-from cosmae.management.display_txt.api import DISPLAY_TXT_ORDER_CONFIG_KEY
+from cosmae.management.display_txt.util import DISPLAY_TXT_ORDER_CONFIG_KEY
 from cosmae.management.models_django import ConfigValue
 
 