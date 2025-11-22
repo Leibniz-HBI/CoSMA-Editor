Leibniz-HBI/DP-REX

Add columns from list, remove columns, resize columns.

ui/src/column_menu/components.test.tsx
@@ -1,0 +1,79 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe } from '@jest/globals'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ColumnAddButton, ColumnMenu, constructColumnTitle } from './components'
+import { useColumnMenu } from './hooks'
+import { ColumnDefinition, ColumnType } from './state'
+jest.mock('./hooks', () => {
+    return {
+        useColumnMenu: jest.fn().mockImplementation()
+    }
+})
+
+describe('create column name', () => {
+    const levels = ['first', 'second', 'third', 'fourth']
+    test('empty', () => {
+        expect(constructColumnTitle([])).toBe('UNKNOWN')
+    })
+    test('single level', () => {
+        expect(constructColumnTitle(levels.slice(0, 1))).toBe('first')
+    })
+    test('two levels', () => {
+        expect(constructColumnTitle(levels.slice(0, 2))).toBe('first -> second')
+    })
+    test('three levels', () => {
+        expect(constructColumnTitle(levels.slice(0, 3))).toBe(
+            'first -> second -> third'
+        )
+    })
+    test('four levels', () => {
+        expect(constructColumnTitle(levels.slice(0, 4))).toBe(
+            'first -> ... -> third -> fourth'
+        )
+    })
+})
+
+describe('ColumnAddMenu', () => {
+    const idTest = 'id_column_test'
+    const nameTest = 'Name Test'
+
+    const columnDefinitionTest = new ColumnDefinition({
+        namePath: [nameTest],
+        idPersistent: idTest,
+        idParentPersistent: undefined,
+        columnType: ColumnType.String,
+        version: 0
+    })
+    test('click items', async () => {
+        let triggered = undefined
+        function trigger(id: ColumnDefinition) {
+            triggered = id
+        }
+        ;(useColumnMenu as jest.Mock).mockReturnValue([columnDefinitionTest])
+        const user = userEvent.setup()
+        const { container } = render(<ColumnMenu addColumnCallback={trigger} />)
+        const button = container.getElementsByClassName('vran-column-menu-item')[0]
+        await user.click(button)
+        expect(triggered).toBe(columnDefinitionTest)
+    })
+})
+
+describe('ColumnAddButton', () => {
+    test('triggers', async () => {
+        let triggered = false
+        function trigger() {
+            triggered = true
+        }
+        const user = userEvent.setup()
+        const { container } = render(
+            <ColumnAddButton onClick={trigger}>click</ColumnAddButton>
+        )
+        const button = container.getElementsByClassName('vran-column-add-button')
+        await user.click(button[0])
+        expect(triggered).toBeTruthy()
+    })
+})

ui/src/column_menu/components.tsx
@@ -1,0 +1,45 @@
+import { useColumnMenu } from './hooks'
+import { ColumnDefinition } from './state'
+type ColumnMenuProps = {
+    addColumnCallback: (columnDefintion: ColumnDefinition) => void
+}
+
+export function constructColumnTitle(namePath: string[]): string {
+    if (namePath === undefined || namePath.length == 0) {
+        return 'UNKNOWN'
+    }
+    if (namePath.length > 3) {
+        return `${namePath[0]} -> ... -> ${namePath[namePath.length - 2]} -> ${
+            namePath[namePath.length - 1]
+        }`
+    }
+    return namePath.join(' -> ')
+}
+
+export function ColumnMenu(props: ColumnMenuProps) {
+    const columnDefs = useColumnMenu()
+    return (
+        <div className="vran-column-menu-container">
+            {columnDefs.map((colDef: ColumnDefinition) => (
+                <div
+                    className="vran-column-menu-item"
+                    onClick={() => {
+                        props.addColumnCallback(colDef)
+                    }}
+                    key={colDef.idPersistent}
+                >
+                    {constructColumnTitle(colDef.namePath)}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function ColumnAddButton(props: any) {
+    return (
+        <div className="vran-column-add-button" onClick={props.onClick}>
+            {props.children}
+        </div>
+    )
+}

ui/src/header_menu.tsx
@@ -1,0 +1,22 @@
+type HeaderMenuProps = {
+    removeColumnCallback: () => void
+    closeHeaderMenuCallback: () => void
+}
+export function HeaderMenu(props: HeaderMenuProps) {
+    return (
+        <div className="vran-header-menu-container">
+            <div className="vran-header-menu-close-row">
+                <div onClick={props.closeHeaderMenuCallback}>✖</div>
+            </div>
+            <div
+                className="vran-header-menu-item"
+                onClick={() => {
+                    props.removeColumnCallback()
+                    props.closeHeaderMenuCallback()
+                }}
+            >
+                <div className="danger">Hide Column</div>
+            </div>
+        </div>
+    )
+}

Leibniz-HBI/CoSMA-Editor

ui/src/column_menu/components.test.tsx
@@ -1,0 +1,79 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe } from '@jest/globals'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ColumnAddButton, ColumnMenu, constructColumnTitle } from './components'
+import { useColumnMenu } from './hooks'
+import { ColumnDefinition, ColumnType } from './state'
+jest.mock('./hooks', () => {
+    return {
+        useColumnMenu: jest.fn().mockImplementation()
+    }
+})
+
+describe('create column name', () => {
+    const levels = ['first', 'second', 'third', 'fourth']
+    test('empty', () => {
+        expect(constructColumnTitle([])).toBe('UNKNOWN')
+    })
+    test('single level', () => {
+        expect(constructColumnTitle(levels.slice(0, 1))).toBe('first')
+    })
+    test('two levels', () => {
+        expect(constructColumnTitle(levels.slice(0, 2))).toBe('first -> second')
+    })
+    test('three levels', () => {
+        expect(constructColumnTitle(levels.slice(0, 3))).toBe(
+            'first -> second -> third'
+        )
+    })
+    test('four levels', () => {
+        expect(constructColumnTitle(levels.slice(0, 4))).toBe(
+            'first -> ... -> third -> fourth'
+        )
+    })
+})
+
+describe('ColumnAddMenu', () => {
+    const idTest = 'id_column_test'
+    const nameTest = 'Name Test'
+
+    const columnDefinitionTest = new ColumnDefinition({
+        namePath: [nameTest],
+        idPersistent: idTest,
+        idParentPersistent: undefined,
+        columnType: ColumnType.String,
+        version: 0
+    })
+    test('click items', async () => {
+        let triggered = undefined
+        function trigger(id: ColumnDefinition) {
+            triggered = id
+        }
+        ;(useColumnMenu as jest.Mock).mockReturnValue([columnDefinitionTest])
+        const user = userEvent.setup()
+        const { container } = render(<ColumnMenu addColumnCallback={trigger} />)
+        const button = container.getElementsByClassName('cosmae-column-menu-item')[0]
+        await user.click(button)
+        expect(triggered).toBe(columnDefinitionTest)
+    })
+})
+
+describe('ColumnAddButton', () => {
+    test('triggers', async () => {
+        let triggered = false
+        function trigger() {
+            triggered = true
+        }
+        const user = userEvent.setup()
+        const { container } = render(
+            <ColumnAddButton onClick={trigger}>click</ColumnAddButton>
+        )
+        const button = container.getElementsByClassName('cosmae-column-add-button')
+        await user.click(button[0])
+        expect(triggered).toBeTruthy()
+    })
+})

ui/src/column_menu/components.tsx
@@ -1,0 +1,45 @@
+import { useColumnMenu } from './hooks'
+import { ColumnDefinition } from './state'
+type ColumnMenuProps = {
+    addColumnCallback: (columnDefintion: ColumnDefinition) => void
+}
+
+export function constructColumnTitle(namePath: string[]): string {
+    if (namePath === undefined || namePath.length == 0) {
+        return 'UNKNOWN'
+    }
+    if (namePath.length > 3) {
+        return `${namePath[0]} -> ... -> ${namePath[namePath.length - 2]} -> ${
+            namePath[namePath.length - 1]
+        }`
+    }
+    return namePath.join(' -> ')
+}
+
+export function ColumnMenu(props: ColumnMenuProps) {
+    const columnDefs = useColumnMenu()
+    return (
+        <div className="cosmae-column-menu-container">
+            {columnDefs.map((colDef: ColumnDefinition) => (
+                <div
+                    className="cosmae-column-menu-item"
+                    onClick={() => {
+                        props.addColumnCallback(colDef)
+                    }}
+                    key={colDef.idPersistent}
+                >
+                    {constructColumnTitle(colDef.namePath)}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function ColumnAddButton(props: any) {
+    return (
+        <div className="cosmae-column-add-button" onClick={props.onClick}>
+            {props.children}
+        </div>
+    )
+}

ui/src/header_menu.tsx
@@ -1,0 +1,22 @@
+type HeaderMenuProps = {
+    removeColumnCallback: () => void
+    closeHeaderMenuCallback: () => void
+}
+export function HeaderMenu(props: HeaderMenuProps) {
+    return (
+        <div className="cosmae-header-menu-container">
+            <div className="cosmae-header-menu-close-row">
+                <div onClick={props.closeHeaderMenuCallback}>✖</div>
+            </div>
+            <div
+                className="cosmae-header-menu-item"
+                onClick={() => {
+                    props.removeColumnCallback()
+                    props.closeHeaderMenuCallback()
+                }}
+            >
+                <div className="danger">Hide Column</div>
+            </div>
+        </div>
+    )
+}