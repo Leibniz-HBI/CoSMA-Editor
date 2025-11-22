Leibniz-HBI/DP-REX

Remove integer tag value type.

vran/migrations/0004_alter_tagdefinition_type.py
@@ -1,0 +1,22 @@
+# Generated by Django 4.1.3 on 2023-01-04 10:29
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0003_remove_taginstance_valid_from_and_more"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tagdefinition",
+            name="type",
+            field=models.CharField(
+                choices=[("INR", "inner"), ("FLT", "float")],
+                default="INR",
+                max_length=3,
+            ),
+        ),
+    ]

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0004_alter_tagdefinition_type.py
@@ -1,0 +1,22 @@
+# Generated by Django 4.1.3 on 2023-01-04 10:29
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0003_remove_taginstance_valid_from_and_more"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tagdefinition",
+            name="type",
+            field=models.CharField(
+                choices=[("INR", "inner"), ("FLT", "float")],
+                default="INR",
+                max_length=3,
+            ),
+        ),
+    ]