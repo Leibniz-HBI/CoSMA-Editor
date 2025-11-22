Leibniz-HBI/DP-REX

Add backend functionality for completing column to tag assignment.

vran/contribution/tag_definition/queue/__init__.py
@@ -1,0 +1,28 @@
+"Dispatch of queue functions for contribution candidates."
+import django_rq
+
+from vran.contribution.models_django import ContributionCandidate
+from vran.contribution.tag_definition.queue.create import read_csv_head
+from vran.contribution.tag_definition.queue.ingest import ingest_values_from_csv
+
+
+def dispatch_read_csv_head(
+    sender,
+    instance,
+    created,
+    update_fields,
+    **kwargs  # pylint: disable=unused-argument
+):
+    "Queues the task for extracting tags from columns."
+    if created or (
+        update_fields
+        and "has_header" in update_fields
+        and instance.state == ContributionCandidate.UPLOADED
+    ):
+        django_rq.enqueue(read_csv_head, str(instance.id_persistent))
+    elif (
+        update_fields
+        and "state" in update_fields
+        and instance.state == ContributionCandidate.COLUMNS_ASSIGNED
+    ):
+        django_rq.enqueue(ingest_values_from_csv, str(instance.id_persistent))

vran/contribution/tag_definition/queue/create.py
@@ -1,23 +1,15 @@
-"Queue jobs for processing contribution candidates."
-from os.path import join
+"Queue jobs for creating contribution candidates."
 from uuid import uuid4
 
-import django_rq
-from django.conf import settings
 from django.db import transaction
 from django.db.utils import OperationalError
-from pandas import read_csv
 
 from vran.contribution.models_django import ContributionCandidate
 from vran.contribution.tag_definition.models_django import (
     TagDefinitionContribution,
     TagInstanceContribution,
 )
-
-
-def mk_extract_tags_group_name(contribution: ContributionCandidate):
-    "Create a group name for extracting csvs."
-    return "extract_tags_" + str(contribution.id_persistent)
+from vran.contribution.tag_definition.queue.util import read_csv_of_candidate
 
 
 def read_csv_head(id_contribution_persistent):
@@ -36,15 +28,9 @@ def read_csv_head(id_contribution_persistent):
         ).delete()
         # set state correctly
         if contribution.state != ContributionCandidate.UPLOADED:
-            contribution.state = ContributionCandidate.UPLOADED
-            contribution.save()
+            return
         # start extracting tags
-        pth = join(settings.CONTRIBUTION_DIRECTORY, contribution.file_name)
-        if contribution.has_header:
-            header_param = 0
-        else:
-            header_param = None
-        data_frame = read_csv(pth, header=header_param, nrows=10)
+        data_frame = read_csv_of_candidate(contribution, nrows=10)
         definitions = []
         for idx, series_name in enumerate(data_frame):
             uuid = str(uuid4())
@@ -63,15 +49,3 @@ def read_csv_head(id_contribution_persistent):
                 )
         contribution.state = ContributionCandidate.COLUMNS_EXTRACTED
         contribution.save()
-
-
-def dispatch_read_csv_head(
-    sender,
-    instance,
-    created,
-    update_fields,
-    **kwargs  # pylint: disable=unused-argument
-):
-    "Queues the task for extracting tags from columns."
-    if created or (update_fields and "has_header" in update_fields):
-        django_rq.enqueue(read_csv_head, str(instance.id_persistent))

vran/contribution/tag_definition/queue/ingest.py
@@ -1,0 +1,83 @@
+"Queue job for ingesting data after columns have been assigned."
+from datetime import datetime
+from uuid import uuid4
+
+from django.db import transaction
+from django.db.utils import OperationalError
+
+from vran.contribution.models_django import ContributionCandidate
+from vran.contribution.tag_definition.models_django import TagDefinitionContribution
+from vran.contribution.tag_definition.queue.util import read_csv_of_candidate
+from vran.person.models_django import Person
+from vran.tag.models_django import TagDefinition, TagInstance
+
+
+def ingest_values_from_csv(id_contribution_persistent):
+    # pylint: disable=too-many-locals
+    "Reads values from a csv files according to column assignments of a contribution candidate"
+
+    contribution_query = ContributionCandidate.objects.select_for_update().filter(  # pylint: disable=no-member
+        id_persistent=id_contribution_persistent
+    )
+    try:
+        with transaction.atomic():
+            try:
+                contribution = contribution_query.get()
+            except OperationalError:
+                return
+            active_columns = (
+                TagDefinitionContribution.objects.filter(  # pylint: disable=no-member
+                    contribution_candidate=contribution, discard=False
+                )
+            )
+            display_txt_idx = None
+            column_assignments = []
+            for column_assignment in active_columns:
+                if column_assignment.id_existing_persistent == "display_txt":
+                    display_txt_idx = column_assignment.index_in_file
+                else:
+                    tag_definition = TagDefinition.most_recent_by_id(
+                        column_assignment.id_existing_persistent
+                    )
+                    column_assignments.append(
+                        (column_assignment.index_in_file, tag_definition)
+                    )
+            if display_txt_idx is None:
+                raise ContributionCandidate.MissingRequiredAssignmentsException(
+                    ["display_txt"]
+                )
+            data_frame = read_csv_of_candidate(contribution)
+            time_add = datetime.now()
+            for row_tpl in data_frame.itertuples(index=False):
+                display_txt = row_tpl[display_txt_idx]
+                id_entity_persistent = str(uuid4())
+                entity, _ = Person.change_or_create(
+                    id_persistent=id_entity_persistent,
+                    time_edit=time_add,
+                    display_txt=display_txt,
+                    names_personal=display_txt,
+                    version=None,
+                )
+                entity.save()
+                for idx_in_file, tag_definition in column_assignments:
+                    id_tag_instance_persistent = str(uuid4())
+                    value = str(row_tpl[int(idx_in_file)])
+                    if value == "nan":
+                        continue
+                    tag_instance, _ = TagInstance.change_or_create(
+                        id_persistent=id_tag_instance_persistent,
+                        id_entity_persistent=id_entity_persistent,
+                        id_tag_definition_persistent=tag_definition.id_persistent,
+                        time_edit=time_add,
+                        value=value,
+                    )
+                    tag_instance.save()
+            contribution.state = ContributionCandidate.MERGED
+            contribution.save()
+    except (  # pylint: disable=broad-except
+        ContributionCandidate.MissingRequiredAssignmentsException,
+        Exception,
+    ):
+        contribution_candidate = contribution_query.get()
+        contribution_candidate.state = ContributionCandidate.COLUMNS_EXTRACTED
+        contribution_candidate.save()

tests/contribution/api/integration/requests.py
@@ -35,3 +35,11 @@ def patch_contribution(url, id_persistent, patch_data, cookies=None):
         cookies=cookies,
         timeout=900,
     )
+
+
+def post_column_assignment_complete(url, id_persistent, cookies=None):
+    return requests.post(
+        url + f"/vran/api/contributions/{id_persistent}/column_assignment_complete",
+        cookies=cookies,
+        timeout=900,
+    )

tests/contribution/tag_definition/queue/create_test.py
@@ -4,20 +4,15 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from vran.contribution.models_django import ContributionCandidate
-from vran.contribution.tag_definition import queue as q
 from vran.contribution.tag_definition.models_django import TagDefinitionContribution
-
-
-def test_mk_extract_tags_group_name(contribution_user):
-    group_name = q.mk_extract_tags_group_name(contribution_user)
-    assert group_name == "extract_tags_a02649c9-094f-40fd-8403-06c2b9017553"
+from vran.contribution.tag_definition.queue.create import read_csv_head
 
 
 def test_deletes_existing_tag_definition(contribution_tag_def):
     conf_mock = MagicMock
     conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
-    with patch("vran.contribution.tag_definition.queue.settings", conf_mock):
-        q.read_csv_head(contribution_tag_def.contribution_candidate.id_persistent)
+    with patch("vran.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_tag_def.contribution_candidate.id_persistent)
     with pytest.raises(
         TagDefinitionContribution.DoesNotExist  # pylint: disable=no-member
     ):
@@ -29,27 +24,11 @@ def test_deletes_existing_tag_definition(contribution_tag_def):
 def test_extracts_without_header(contribution_user):
     conf_mock = MagicMock
     conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
-    with patch("vran.contribution.tag_definition.queue.settings", conf_mock):
-        q.read_csv_head(contribution_user.id_persistent)
+    with patch("vran.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_user.id_persistent)
     tag_defs = TagDefinitionContribution.objects.all()  # pylint: disable=no-member
     for idx, tag_def in enumerate(tag_defs):
         assert tag_def.name == str(idx)
-
-
-def test_extracts_with_header(contribution_other):
-    conf_mock = MagicMock
-    conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
-    with patch("vran.contribution.tag_definition.queue.settings", conf_mock):
-        q.read_csv_head(contribution_other.id_persistent)
-    tag_defs = TagDefinitionContribution.objects.all()  # pylint: disable=no-member
-    for idx, tag_def in enumerate(tag_defs):
-        assert tag_def.name == _expected_tag_defs[idx]
-    contribution_candidate = (
-        ContributionCandidate.objects.get(  # pylint: disable=no-member
-            id_persistent=contribution_other.id_persistent
-        )
-    )
-    assert contribution_candidate.state == ContributionCandidate.COLUMNS_EXTRACTED
 
 
 _expected_tag_defs = [
@@ -84,3 +63,19 @@ _expected_tag_defs = [
     "modified_at",
     "modified_by",
 ]
+
+
+def test_extracts_with_header(contribution_other):
+    conf_mock = MagicMock
+    conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
+    with patch("vran.contribution.tag_definition.queue.create.settings", conf_mock):
+        read_csv_head(contribution_other.id_persistent)
+    tag_defs = TagDefinitionContribution.objects.all()  # pylint: disable=no-member
+    for idx, tag_def in enumerate(tag_defs):
+        assert tag_def.name == _expected_tag_defs[idx]
+    contribution_candidate = (
+        ContributionCandidate.objects.get(  # pylint: disable=no-member
+            id_persistent=contribution_other.id_persistent
+        )
+    )
+    assert contribution_candidate.state == ContributionCandidate.COLUMNS_EXTRACTED

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/tag_definition/queue/__init__.py
@@ -1,0 +1,28 @@
+"Dispatch of queue functions for contribution candidates."
+import django_rq
+
+from cosmae.contribution.models_django import ContributionCandidate
+from cosmae.contribution.tag_definition.queue.create import read_csv_head
+from cosmae.contribution.tag_definition.queue.ingest import ingest_values_from_csv
+
+
+def dispatch_read_csv_head(
+    sender,
+    instance,
+    created,
+    update_fields,
+    **kwargs  # pylint: disable=unused-argument
+):
+    "Queues the task for extracting tags from columns."
+    if created or (
+        update_fields
+        and "has_header" in update_fields
+        and instance.state == ContributionCandidate.UPLOADED
+    ):
+        django_rq.enqueue(read_csv_head, str(instance.id_persistent))
+    elif (
+        update_fields
+        and "state" in update_fields
+        and instance.state == ContributionCandidate.COLUMNS_ASSIGNED
+    ):
+        django_rq.enqueue(ingest_values_from_csv, str(instance.id_persistent))

cosmae/contribution/tag_definition/queue/create.py
@@ -1,23 +1,15 @@
-"Queue jobs for processing contribution candidates."
-from os.path import join
+"Queue jobs for creating contribution candidates."
 from uuid import uuid4
 
-import django_rq
-from django.conf import settings
 from django.db import transaction
 from django.db.utils import OperationalError
-from pandas import read_csv
 
 from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.contribution.tag_definition.models_django import (
     TagDefinitionContribution,
     TagInstanceContribution,
 )
-
-
-def mk_extract_tags_group_name(contribution: ContributionCandidate):
-    "Create a group name for extracting csvs."
-    return "extract_tags_" + str(contribution.id_persistent)
+from cosmae.contribution.tag_definition.queue.util import read_csv_of_candidate
 
 
 def read_csv_head(id_contribution_persistent):
@@ -36,15 +28,9 @@ def read_csv_head(id_contribution_persistent):
         ).delete()
         # set state correctly
         if contribution.state != ContributionCandidate.UPLOADED:
-            contribution.state = ContributionCandidate.UPLOADED
-            contribution.save()
+            return
         # start extracting tags
-        pth = join(settings.CONTRIBUTION_DIRECTORY, contribution.file_name)
-        if contribution.has_header:
-            header_param = 0
-        else:
-            header_param = None
-        data_frame = read_csv(pth, header=header_param, nrows=10)
+        data_frame = read_csv_of_candidate(contribution, nrows=10)
         definitions = []
         for idx, series_name in enumerate(data_frame):
             uuid = str(uuid4())
@@ -63,15 +49,3 @@ def read_csv_head(id_contribution_persistent):
                 )
         contribution.state = ContributionCandidate.COLUMNS_EXTRACTED
         contribution.save()
-
-
-def dispatch_read_csv_head(
-    sender,
-    instance,
-    created,
-    update_fields,
-    **kwargs  # pylint: disable=unused-argument
-):
-    "Queues the task for extracting tags from columns."
-    if created or (update_fields and "has_header" in update_fields):
-        django_rq.enqueue(read_csv_head, str(instance.id_persistent))

cosmae/contribution/tag_definition/queue/ingest.py
@@ -1,0 +1,83 @@
+"Queue job for ingesting data after columns have been assigned."
+from datetime import datetime
+from uuid import uuid4
+
+from django.db import transaction
+from django.db.utils import OperationalError
+
+from cosmae.contribution.models_django import ContributionCandidate
+from cosmae.contribution.tag_definition.models_django import TagDefinitionContribution
+from cosmae.contribution.tag_definition.queue.util import read_csv_of_candidate
+from cosmae.person.models_django import Person
+from cosmae.tag.models_django import TagDefinition, TagInstance
+
+
+def ingest_values_from_csv(id_contribution_persistent):
+    # pylint: disable=too-many-locals
+    "Reads values from a csv files according to column assignments of a contribution candidate"
+
+    contribution_query = ContributionCandidate.objects.select_for_update().filter(  # pylint: disable=no-member
+        id_persistent=id_contribution_persistent
+    )
+    try:
+        with transaction.atomic():
+            try:
+                contribution = contribution_query.get()
+            except OperationalError:
+                return
+            active_columns = (
+                TagDefinitionContribution.objects.filter(  # pylint: disable=no-member
+                    contribution_candidate=contribution, discard=False
+                )
+            )
+            display_txt_idx = None
+            column_assignments = []
+            for column_assignment in active_columns:
+                if column_assignment.id_existing_persistent == "display_txt":
+                    display_txt_idx = column_assignment.index_in_file
+                else:
+                    tag_definition = TagDefinition.most_recent_by_id(
+                        column_assignment.id_existing_persistent
+                    )
+                    column_assignments.append(
+                        (column_assignment.index_in_file, tag_definition)
+                    )
+            if display_txt_idx is None:
+                raise ContributionCandidate.MissingRequiredAssignmentsException(
+                    ["display_txt"]
+                )
+            data_frame = read_csv_of_candidate(contribution)
+            time_add = datetime.now()
+            for row_tpl in data_frame.itertuples(index=False):
+                display_txt = row_tpl[display_txt_idx]
+                id_entity_persistent = str(uuid4())
+                entity, _ = Person.change_or_create(
+                    id_persistent=id_entity_persistent,
+                    time_edit=time_add,
+                    display_txt=display_txt,
+                    names_personal=display_txt,
+                    version=None,
+                )
+                entity.save()
+                for idx_in_file, tag_definition in column_assignments:
+                    id_tag_instance_persistent = str(uuid4())
+                    value = str(row_tpl[int(idx_in_file)])
+                    if value == "nan":
+                        continue
+                    tag_instance, _ = TagInstance.change_or_create(
+                        id_persistent=id_tag_instance_persistent,
+                        id_entity_persistent=id_entity_persistent,
+                        id_tag_definition_persistent=tag_definition.id_persistent,
+                        time_edit=time_add,
+                        value=value,
+                    )
+                    tag_instance.save()
+            contribution.state = ContributionCandidate.MERGED
+            contribution.save()
+    except (  # pylint: disable=broad-except
+        ContributionCandidate.MissingRequiredAssignmentsException,
+        Exception,
+    ):
+        contribution_candidate = contribution_query.get()
+        contribution_candidate.state = ContributionCandidate.COLUMNS_EXTRACTED
+        contribution_candidate.save()

tests/contribution/api/integration/requests.py
@@ -35,3 +35,11 @@ def patch_contribution(url, id_persistent, patch_data, cookies=None):
         cookies=cookies,
         timeout=900,
     )
+
+
+def post_column_assignment_complete(url, id_persistent, cookies=None):
+    return requests.post(
+        url + f"/cosmae/api/contributions/{id_persistent}/column_assignment_complete",
+        cookies=cookies,
+        timeout=900,
+    )

tests/contribution/tag_definition/queue/create_test.py
@@ -4,20 +4,15 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from cosmae.contribution.models_django import ContributionCandidate
-from cosmae.contribution.tag_definition import queue as q
 from cosmae.contribution.tag_definition.models_django import TagDefinitionContribution
-
-
-def test_mk_extract_tags_group_name(contribution_user):
-    group_name = q.mk_extract_tags_group_name(contribution_user)
-    assert group_name == "extract_tags_a02649c9-094f-40fd-8403-06c2b9017553"
+from cosmae.contribution.tag_definition.queue.create import read_csv_head
 
 
 def test_deletes_existing_tag_definition(contribution_tag_def):
     conf_mock = MagicMock
     conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
-    with patch("cosmae.contribution.tag_definition.queue.settings", conf_mock):
-        q.read_csv_head(contribution_tag_def.contribution_candidate.id_persistent)
+    with patch("cosmae.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_tag_def.contribution_candidate.id_persistent)
     with pytest.raises(
         TagDefinitionContribution.DoesNotExist  # pylint: disable=no-member
     ):
@@ -29,27 +24,11 @@ def test_deletes_existing_tag_definition(contribution_tag_def):
 def test_extracts_without_header(contribution_user):
     conf_mock = MagicMock
     conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
-    with patch("cosmae.contribution.tag_definition.queue.settings", conf_mock):
-        q.read_csv_head(contribution_user.id_persistent)
+    with patch("cosmae.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_user.id_persistent)
     tag_defs = TagDefinitionContribution.objects.all()  # pylint: disable=no-member
     for idx, tag_def in enumerate(tag_defs):
         assert tag_def.name == str(idx)
-
-
-def test_extracts_with_header(contribution_other):
-    conf_mock = MagicMock
-    conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
-    with patch("cosmae.contribution.tag_definition.queue.settings", conf_mock):
-        q.read_csv_head(contribution_other.id_persistent)
-    tag_defs = TagDefinitionContribution.objects.all()  # pylint: disable=no-member
-    for idx, tag_def in enumerate(tag_defs):
-        assert tag_def.name == _expected_tag_defs[idx]
-    contribution_candidate = (
-        ContributionCandidate.objects.get(  # pylint: disable=no-member
-            id_persistent=contribution_other.id_persistent
-        )
-    )
-    assert contribution_candidate.state == ContributionCandidate.COLUMNS_EXTRACTED
 
 
 _expected_tag_defs = [
@@ -84,3 +63,19 @@ _expected_tag_defs = [
     "modified_at",
     "modified_by",
 ]
+
+
+def test_extracts_with_header(contribution_other):
+    conf_mock = MagicMock
+    conf_mock.CONTRIBUTION_DIRECTORY = "tests/files/"
+    with patch("cosmae.contribution.tag_definition.queue.util.settings", conf_mock):
+        read_csv_head(contribution_other.id_persistent)
+    tag_defs = TagDefinitionContribution.objects.all()  # pylint: disable=no-member
+    for idx, tag_def in enumerate(tag_defs):
+        assert tag_def.name == _expected_tag_defs[idx]
+    contribution_candidate = (
+        ContributionCandidate.objects.get(  # pylint: disable=no-member
+            id_persistent=contribution_other.id_persistent
+        )
+    )
+    assert contribution_candidate.state == ContributionCandidate.COLUMNS_EXTRACTED