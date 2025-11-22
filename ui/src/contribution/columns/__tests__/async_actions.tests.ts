Leibniz-HBI/DP-REX

Add UI functionality for completing contribution assignments.

ui/src/contribution/columns/__tests__/async_actions.tests.ts
@@ -1,5 +1,8 @@
 import { Contribution, ContributionStep } from '../../state'
 import {
+    FinalizeColumnAssignmentErrorAction,
+    FinalizeColumnAssignmentStartAction,
+    FinalizeColumnAssignmentSuccessAction,
     LoadColumnDefinitionsContributionErrorAction,
     LoadColumnDefinitionsContributionStartAction,
     LoadColumnDefinitionsContributionSuccessAction,
@@ -8,6 +11,7 @@ import {
     PatchColumnDefinitionContributionSuccessAction
 } from '../actions'
 import {
+    FinalizeColumnAssignmentAction,
     LoadColumnDefinitionsContributionAction,
     PatchColumnDefinitionContributionAction
 } from '../async_actions'
@@ -176,6 +180,47 @@ describe('patch column definition contribution', () => {
         expect(dispatch.mock.calls).toEqual([
             [new PatchColumnDefinitionContributionStartAction()],
             [new PatchColumnDefinitionContributionErrorAction('test error')]
+        ])
+    })
+})
+
+describe('finalize column assignment', () => {
+    test('success', async () => {
+        responseSequence([
+            [
+                200,
+                () => {
+                    return {}
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new FinalizeColumnAssignmentAction('contribution-id-test').run(dispatch)
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/vran/api/contributions/contribution-id-test/column_assignment_complete',
+                { credentials: 'include', method: 'POST' }
+            ]
+        ])
+        expect(dispatch.mock.calls).toEqual([
+            [new FinalizeColumnAssignmentStartAction()],
+            [new FinalizeColumnAssignmentSuccessAction()]
+        ])
+    })
+    test('error', async () => {
+        responseSequence([
+            [
+                400,
+                () => {
+                    return { msg: 'test error' }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new FinalizeColumnAssignmentAction('id-test').run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new FinalizeColumnAssignmentStartAction()],
+            [new FinalizeColumnAssignmentErrorAction('test error')]
         ])
     })
 })

Leibniz-HBI/CoSMA-Editor

ui/src/contribution/columns/__tests__/async_actions.tests.ts
@@ -1,5 +1,8 @@
 import { Contribution, ContributionStep } from '../../state'
 import {
+    FinalizeColumnAssignmentErrorAction,
+    FinalizeColumnAssignmentStartAction,
+    FinalizeColumnAssignmentSuccessAction,
     LoadColumnDefinitionsContributionErrorAction,
     LoadColumnDefinitionsContributionStartAction,
     LoadColumnDefinitionsContributionSuccessAction,
@@ -8,6 +11,7 @@ import {
     PatchColumnDefinitionContributionSuccessAction
 } from '../actions'
 import {
+    FinalizeColumnAssignmentAction,
     LoadColumnDefinitionsContributionAction,
     PatchColumnDefinitionContributionAction
 } from '../async_actions'
@@ -176,6 +180,47 @@ describe('patch column definition contribution', () => {
         expect(dispatch.mock.calls).toEqual([
             [new PatchColumnDefinitionContributionStartAction()],
             [new PatchColumnDefinitionContributionErrorAction('test error')]
+        ])
+    })
+})
+
+describe('finalize column assignment', () => {
+    test('success', async () => {
+        responseSequence([
+            [
+                200,
+                () => {
+                    return {}
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new FinalizeColumnAssignmentAction('contribution-id-test').run(dispatch)
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/cosmae/api/contributions/contribution-id-test/column_assignment_complete',
+                { credentials: 'include', method: 'POST' }
+            ]
+        ])
+        expect(dispatch.mock.calls).toEqual([
+            [new FinalizeColumnAssignmentStartAction()],
+            [new FinalizeColumnAssignmentSuccessAction()]
+        ])
+    })
+    test('error', async () => {
+        responseSequence([
+            [
+                400,
+                () => {
+                    return { msg: 'test error' }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new FinalizeColumnAssignmentAction('id-test').run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new FinalizeColumnAssignmentStartAction()],
+            [new FinalizeColumnAssignmentErrorAction('test error')]
         ])
     })
 })