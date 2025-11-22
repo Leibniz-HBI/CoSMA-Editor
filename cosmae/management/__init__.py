Leibniz-HBI/DP-REX

Add backend for managing display txt order.

vran/management/__init__.py
@@ -1,0 +1,7 @@
+"Combine routers for management"
+from ninja import Router
+
+from vran.management.display_txt.api import router as display_txt_router
+
+router = Router()
+router.add_router("display_txt", display_txt_router)

vran/management/display_txt/api.py
@@ -1,0 +1,140 @@
+"API methods for managing display_txt order."
+from django.db.models import Case, IntegerField, When
+from django.http import HttpRequest
+from ninja import Router, Schema
+
+from vran.exception import ApiError, NotAuthenticatedException
+from vran.management.models_django import AlreadyInListException, ConfigValue
+from vran.tag.api.definitions import (
+    TagDefinitionResponse,
+    TagDefinitionResponseList,
+    tag_definition_db_to_api,
+)
+from vran.tag.models_django import TagDefinition
+from vran.util import VranUser
+from vran.util.auth import check_user
+
+router = Router()
+
+DISPLAY_TXT_ORDER_CONFIG_KEY = "display_txt_order"
+
+
+class DisplayTxtOrderAppend(Schema):
+    "API model for appending a tag definition to the display text order"
+    # pylint: disable=too-few-public-methods
+    id_tag_definition_persistent: str
+
+
+@router.get(
+    "order",
+    response={
+        200: TagDefinitionResponseList,
+        401: ApiError,
+        403: ApiError,
+        500: ApiError,
+    },
+)
+def get(request: HttpRequest):
+    "API method to get the tag definition order for determining the display txt"
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    if user.permission_group != VranUser.COMMISSIONER:
+        return 403, ApiError(msg="Insufficient permissions.")
+    try:
+        id_tag_def_persistent_list = ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY, [])
+        order = [
+            When(id_persistent=val, then=idx)
+            for idx, val in enumerate(id_tag_def_persistent_list)
+        ]
+        tag_def_query = (
+            TagDefinition.most_recent_query_set()
+            .filter(id_persistent__in=id_tag_def_persistent_list)
+            .annotate(sort_key=Case(*order, output_field=IntegerField()))
+            .order_by("sort_key")
+        )
+        return 200, TagDefinitionResponseList(
+            tag_definitions=[
+                tag_definition_db_to_api(tag_def) for tag_def in tag_def_query
+            ]
+        )
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not get tag definitions for display text order."
+        )
+
+
+@router.post(
+    "order/append",
+    response={
+        200: TagDefinitionResponse,
+        400: ApiError,
+        401: ApiError,
+        403: ApiError,
+        404: ApiError,
+        500: ApiError,
+    },
+)
+def append(request: HttpRequest, request_data: DisplayTxtOrderAppend):
+    "API method for appending a tag definition to the display text order by its persistent id."
+    # pylint: disable=too-many-return-statements
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    if user.permission_group != VranUser.COMMISSIONER:
+        return 403, ApiError(msg="Insufficient permissions.")
+    try:
+        tag_definition = TagDefinition.most_recent_by_id(
+            request_data.id_tag_definition_persistent
+        )
+        if not tag_definition.curated:
+            return 400, ApiError(
+                msg="Can only use curated tag definitions in display text order."
+            )
+        ConfigValue.append_to_list(
+            DISPLAY_TXT_ORDER_CONFIG_KEY, request_data.id_tag_definition_persistent
+        )
+        return 200, tag_definition_db_to_api(tag_definition)
+    except AlreadyInListException:
+        return 400, ApiError(msg="Can not add tag definition that is already in list.")
+    except TagDefinition.DoesNotExist:  # pylint: disable=no-member
+        return 404, ApiError(msg="Tag definition does not exist.")
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not append tag definition to display text order."
+        )
+
+
+@router.delete(
+    "order/{id_tag_definition_persistent}",
+    response={
+        200: None,
+        401: ApiError,
+        403: ApiError,
+        404: ApiError,
+        500: ApiError,
+    },
+)
+def remove(request: HttpRequest, id_tag_definition_persistent: str):
+    "API method for deleting a tag definition from the display text order by its persistent id."
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    if user.permission_group != VranUser.COMMISSIONER:
+        return 403, ApiError(msg="Insufficient permissions.")
+    try:
+        ConfigValue.remove_from_list(
+            DISPLAY_TXT_ORDER_CONFIG_KEY, id_tag_definition_persistent
+        )
+        return 200, None
+    except TagDefinition.DoesNotExist:  # pylint: disable=no-member
+        return 404, ApiError(msg="Tag definition does not exist.")
+    except ValueError:
+        return 200, None
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not remove tag definition from display text order."
+        )

vran/management/models_django.py
@@ -1,0 +1,54 @@
+"Database model for configuration values"
+from django.db import models
+
+
+class AlreadyInListException(Exception):
+    "Indicate that an element is already in a list."
+
+
+class ConfigValue(models.Model):
+    "Django ORM model for config values used by vran."
+    key = models.TextField(unique=True)
+    value = models.JSONField()
+
+    @classmethod
+    def get(cls, key, default=None):
+        "Get a config value"
+        try:
+            return (
+                cls.objects.filter(key=key)  # pylint: disable= no-member
+                .values_list("value", flat=True)
+                .get()
+            )
+        except ConfigValue.DoesNotExist:  # pylint: disable= no-member
+            return default
+
+    @classmethod
+    def set(cls, key, value):
+        "Set a config value."
+        return cls.objects.update_or_create(  # pylint: disable=no-member
+            key=key, defaults={"value": value}
+        )
+
+    @classmethod
+    def append_to_list(cls, key, element, unique=True):
+        "Append element to a list in a config value"
+        existing = cls.get(key, default=[])
+        assert isinstance(existing, list)
+        if unique:
+            # make sure element does not already exist
+            try:
+                existing.index(element)
+                raise AlreadyInListException
+            except ValueError:
+                pass
+        existing.append(element)
+        cls.set(key, existing)
+
+    @classmethod
+    def remove_from_list(cls, key, element):
+        "Remove an element from a list"
+        existing = cls.get(key, default=[])
+        assert isinstance(existing, list)
+        existing.remove(element)
+        cls.set(key, existing)

vran/migrations/0032_configvalue.py
@@ -1,0 +1,29 @@
+# Generated by Django 4.2.8 on 2024-01-05 10:49
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0031_tag_instance_most_recent_view"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="ConfigValue",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("key", models.TextField(unique=True)),
+                ("value", models.JSONField()),
+            ],
+        ),
+    ]

tests/management/display_txt/api/integration/append_display_txt_test.py
@@ -1,0 +1,77 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+from unittest.mock import MagicMock, patch
+
+import tests.management.display_txt.api.integration.requests as req
+import tests.tag.common as ct
+from vran.exception import NotAuthenticatedException
+from vran.management.display_txt.api import DISPLAY_TXT_ORDER_CONFIG_KEY
+from vran.management.models_django import ConfigValue
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req.post_append_tag_definition(server.url, "some-id")
+    assert rsp.status_code == 401
+
+
+def test_not_authenticated(auth_server):
+    server, cookies = auth_server
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    with patch("vran.management.display_txt.api.check_user", mock):
+        rsp = req.post_append_tag_definition(server.url, "some-id", cookies=cookies)
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server):
+    server, cookies = auth_server
+    rsp = req.post_append_tag_definition(server.url, "some-id", cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_unknown_tag_def(auth_server_commissioner):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(server.url, "some-id", cookies=cookies)
+    assert rsp.status_code == 404
+
+
+def test_append_not_curated_config(auth_server_commissioner, tag_def):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 400
+
+
+def test_append_already_present(auth_server_commissioner, tag_def_curated):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 400
+
+
+def test_append_to_nonexisting_config(auth_server_commissioner, tag_def_curated):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+
+
+def test_append_to_existing(
+    auth_server_commissioner, tag_def_curated, display_txt_order_0
+):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    assert ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY) == [
+        ct.id_tag_persistent_test,
+        ct.id_tag_def_curated_test,
+    ]

tests/management/display_txt/api/integration/requests.py
@@ -1,0 +1,31 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from urllib.parse import urljoin
+
+import requests
+
+
+def get_tag_definition(url, cookies=None):
+    return requests.get(
+        urljoin(url, "/vran/api/manage/display_txt/order"),
+        cookies=cookies,
+        timeout=9,
+    )
+
+
+def post_append_tag_definition(url, id_tag_definition_persistent, cookies=None):
+    return requests.post(
+        urljoin(url, "/vran/api/manage/display_txt/order/append"),
+        json={"id_tag_definition_persistent": id_tag_definition_persistent},
+        cookies=cookies,
+        timeout=9,
+    )
+
+
+def delete_tag_definition(url, id_tag_definition_persistent, cookies=None):
+    return requests.delete(
+        urljoin(
+            url, f"/vran/api/manage/display_txt/order/{id_tag_definition_persistent}"
+        ),
+        cookies=cookies,
+        timeout=9,
+    )

tests/management/display_txt/conftest.py
@@ -1,0 +1,19 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+import pytest
+
+from vran.management.display_txt.api import DISPLAY_TXT_ORDER_CONFIG_KEY
+from vran.management.models_django import ConfigValue
+
+
+@pytest.fixture
+def display_txt_order_0(tag_def):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+
+
+@pytest.fixture
+def display_txt_order_0_1_curated(tag_def, tag_def1, tag_def_curated):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def1.id_persistent)
+    ConfigValue.append_to_list(
+        DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def_curated.id_persistent
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/management/__init__.py
@@ -1,0 +1,7 @@
+"Combine routers for management"
+from ninja import Router
+
+from cosmae.management.display_txt.api import router as display_txt_router
+
+router = Router()
+router.add_router("display_txt", display_txt_router)

cosmae/management/display_txt/api.py
@@ -1,0 +1,140 @@
+"API methods for managing display_txt order."
+from django.db.models import Case, IntegerField, When
+from django.http import HttpRequest
+from ninja import Router, Schema
+
+from cosmae.exception import ApiError, NotAuthenticatedException
+from cosmae.management.models_django import AlreadyInListException, ConfigValue
+from cosmae.tag.api.definitions import (
+    TagDefinitionResponse,
+    TagDefinitionResponseList,
+    tag_definition_db_to_api,
+)
+from cosmae.tag.models_django import TagDefinition
+from cosmae.util import CosmaeUser
+from cosmae.util.auth import check_user
+
+router = Router()
+
+DISPLAY_TXT_ORDER_CONFIG_KEY = "display_txt_order"
+
+
+class DisplayTxtOrderAppend(Schema):
+    "API model for appending a tag definition to the display text order"
+    # pylint: disable=too-few-public-methods
+    id_tag_definition_persistent: str
+
+
+@router.get(
+    "order",
+    response={
+        200: TagDefinitionResponseList,
+        401: ApiError,
+        403: ApiError,
+        500: ApiError,
+    },
+)
+def get(request: HttpRequest):
+    "API method to get the tag definition order for determining the display txt"
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    if user.permission_group != CosmaeUser.COMMISSIONER:
+        return 403, ApiError(msg="Insufficient permissions.")
+    try:
+        id_tag_def_persistent_list = ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY, [])
+        order = [
+            When(id_persistent=val, then=idx)
+            for idx, val in enumerate(id_tag_def_persistent_list)
+        ]
+        tag_def_query = (
+            TagDefinition.most_recent_query_set()
+            .filter(id_persistent__in=id_tag_def_persistent_list)
+            .annotate(sort_key=Case(*order, output_field=IntegerField()))
+            .order_by("sort_key")
+        )
+        return 200, TagDefinitionResponseList(
+            tag_definitions=[
+                tag_definition_db_to_api(tag_def) for tag_def in tag_def_query
+            ]
+        )
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not get tag definitions for display text order."
+        )
+
+
+@router.post(
+    "order/append",
+    response={
+        200: TagDefinitionResponse,
+        400: ApiError,
+        401: ApiError,
+        403: ApiError,
+        404: ApiError,
+        500: ApiError,
+    },
+)
+def append(request: HttpRequest, request_data: DisplayTxtOrderAppend):
+    "API method for appending a tag definition to the display text order by its persistent id."
+    # pylint: disable=too-many-return-statements
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    if user.permission_group != CosmaeUser.COMMISSIONER:
+        return 403, ApiError(msg="Insufficient permissions.")
+    try:
+        tag_definition = TagDefinition.most_recent_by_id(
+            request_data.id_tag_definition_persistent
+        )
+        if not tag_definition.curated:
+            return 400, ApiError(
+                msg="Can only use curated tag definitions in display text order."
+            )
+        ConfigValue.append_to_list(
+            DISPLAY_TXT_ORDER_CONFIG_KEY, request_data.id_tag_definition_persistent
+        )
+        return 200, tag_definition_db_to_api(tag_definition)
+    except AlreadyInListException:
+        return 400, ApiError(msg="Can not add tag definition that is already in list.")
+    except TagDefinition.DoesNotExist:  # pylint: disable=no-member
+        return 404, ApiError(msg="Tag definition does not exist.")
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not append tag definition to display text order."
+        )
+
+
+@router.delete(
+    "order/{id_tag_definition_persistent}",
+    response={
+        200: None,
+        401: ApiError,
+        403: ApiError,
+        404: ApiError,
+        500: ApiError,
+    },
+)
+def remove(request: HttpRequest, id_tag_definition_persistent: str):
+    "API method for deleting a tag definition from the display text order by its persistent id."
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    if user.permission_group != CosmaeUser.COMMISSIONER:
+        return 403, ApiError(msg="Insufficient permissions.")
+    try:
+        ConfigValue.remove_from_list(
+            DISPLAY_TXT_ORDER_CONFIG_KEY, id_tag_definition_persistent
+        )
+        return 200, None
+    except TagDefinition.DoesNotExist:  # pylint: disable=no-member
+        return 404, ApiError(msg="Tag definition does not exist.")
+    except ValueError:
+        return 200, None
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not remove tag definition from display text order."
+        )

cosmae/management/models_django.py
@@ -1,0 +1,54 @@
+"Database model for configuration values"
+from django.db import models
+
+
+class AlreadyInListException(Exception):
+    "Indicate that an element is already in a list."
+
+
+class ConfigValue(models.Model):
+    "Django ORM model for config values used by cosmae."
+    key = models.TextField(unique=True)
+    value = models.JSONField()
+
+    @classmethod
+    def get(cls, key, default=None):
+        "Get a config value"
+        try:
+            return (
+                cls.objects.filter(key=key)  # pylint: disable= no-member
+                .values_list("value", flat=True)
+                .get()
+            )
+        except ConfigValue.DoesNotExist:  # pylint: disable= no-member
+            return default
+
+    @classmethod
+    def set(cls, key, value):
+        "Set a config value."
+        return cls.objects.update_or_create(  # pylint: disable=no-member
+            key=key, defaults={"value": value}
+        )
+
+    @classmethod
+    def append_to_list(cls, key, element, unique=True):
+        "Append element to a list in a config value"
+        existing = cls.get(key, default=[])
+        assert isinstance(existing, list)
+        if unique:
+            # make sure element does not already exist
+            try:
+                existing.index(element)
+                raise AlreadyInListException
+            except ValueError:
+                pass
+        existing.append(element)
+        cls.set(key, existing)
+
+    @classmethod
+    def remove_from_list(cls, key, element):
+        "Remove an element from a list"
+        existing = cls.get(key, default=[])
+        assert isinstance(existing, list)
+        existing.remove(element)
+        cls.set(key, existing)

cosmae/migrations/0032_configvalue.py
@@ -1,0 +1,29 @@
+# Generated by Django 4.2.8 on 2024-01-05 10:49
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0031_tag_instance_most_recent_view"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="ConfigValue",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("key", models.TextField(unique=True)),
+                ("value", models.JSONField()),
+            ],
+        ),
+    ]

tests/management/display_txt/api/integration/append_display_txt_test.py
@@ -1,0 +1,77 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+from unittest.mock import MagicMock, patch
+
+import tests.management.display_txt.api.integration.requests as req
+import tests.tag.common as ct
+from cosmae.exception import NotAuthenticatedException
+from cosmae.management.display_txt.api import DISPLAY_TXT_ORDER_CONFIG_KEY
+from cosmae.management.models_django import ConfigValue
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req.post_append_tag_definition(server.url, "some-id")
+    assert rsp.status_code == 401
+
+
+def test_not_authenticated(auth_server):
+    server, cookies = auth_server
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    with patch("cosmae.management.display_txt.api.check_user", mock):
+        rsp = req.post_append_tag_definition(server.url, "some-id", cookies=cookies)
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server):
+    server, cookies = auth_server
+    rsp = req.post_append_tag_definition(server.url, "some-id", cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_unknown_tag_def(auth_server_commissioner):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(server.url, "some-id", cookies=cookies)
+    assert rsp.status_code == 404
+
+
+def test_append_not_curated_config(auth_server_commissioner, tag_def):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 400
+
+
+def test_append_already_present(auth_server_commissioner, tag_def_curated):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 400
+
+
+def test_append_to_nonexisting_config(auth_server_commissioner, tag_def_curated):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+
+
+def test_append_to_existing(
+    auth_server_commissioner, tag_def_curated, display_txt_order_0
+):
+    server, cookies = auth_server_commissioner
+    rsp = req.post_append_tag_definition(
+        server.url, tag_def_curated.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    assert ConfigValue.get(DISPLAY_TXT_ORDER_CONFIG_KEY) == [
+        ct.id_tag_persistent_test,
+        ct.id_tag_def_curated_test,
+    ]

tests/management/display_txt/api/integration/requests.py
@@ -1,0 +1,31 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from urllib.parse import urljoin
+
+import requests
+
+
+def get_tag_definition(url, cookies=None):
+    return requests.get(
+        urljoin(url, "/cosmae/api/manage/display_txt/order"),
+        cookies=cookies,
+        timeout=9,
+    )
+
+
+def post_append_tag_definition(url, id_tag_definition_persistent, cookies=None):
+    return requests.post(
+        urljoin(url, "/cosmae/api/manage/display_txt/order/append"),
+        json={"id_tag_definition_persistent": id_tag_definition_persistent},
+        cookies=cookies,
+        timeout=9,
+    )
+
+
+def delete_tag_definition(url, id_tag_definition_persistent, cookies=None):
+    return requests.delete(
+        urljoin(
+            url, f"/cosmae/api/manage/display_txt/order/{id_tag_definition_persistent}"
+        ),
+        cookies=cookies,
+        timeout=9,
+    )

tests/management/display_txt/conftest.py
@@ -1,0 +1,19 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+import pytest
+
+from cosmae.management.display_txt.api import DISPLAY_TXT_ORDER_CONFIG_KEY
+from cosmae.management.models_django import ConfigValue
+
+
+@pytest.fixture
+def display_txt_order_0(tag_def):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+
+
+@pytest.fixture
+def display_txt_order_0_1_curated(tag_def, tag_def1, tag_def_curated):
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def.id_persistent)
+    ConfigValue.append_to_list(DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def1.id_persistent)
+    ConfigValue.append_to_list(
+        DISPLAY_TXT_ORDER_CONFIG_KEY, tag_def_curated.id_persistent
+    )