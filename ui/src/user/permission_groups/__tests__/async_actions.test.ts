Leibniz-HBI/DP-REX

Add UI components for setting user permission group.

ui/src/user/permission_groups/__tests__/async_actions.test.ts
@@ -1,0 +1,149 @@
+import { UserInfo, UserPermissionGroup } from '../../state'
+import {
+    GetUserInfoListErrorAction,
+    GetUserInfoListStartAction,
+    GetUserInfoListSuccessAction,
+    SetUserPermissionErrorAction,
+    SetUserPermissionStartAction,
+    SetUserPermissionSuccessAction
+} from '../actions'
+import { GetUserInfoListAction, SetUserPermissionAction } from '../async_actions'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function responseSequence(responses: [number, any][]) {
+    const fetchMock = jest.spyOn(global, 'fetch')
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        fetchMock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const userNameTest = 'userTest'
+const emailTest = 'me@test.url'
+const namesPersonalTest = 'names personal test'
+const idPersistentTest = 'id-user=test'
+const permissionGroupTest = UserPermissionGroup.EDITOR
+const userNameTest1 = 'userTest1'
+const emailTest1 = 'me1@test.url'
+const namesPersonalTest1 = 'names personal test1'
+const idPersistentTest1 = 'id-user=test1'
+const permissionGroupTest1 = UserPermissionGroup.CONTRIBUTOR
+const userInfoTest = new UserInfo({
+    userName: userNameTest,
+    email: emailTest,
+    namesPersonal: namesPersonalTest,
+    idPersistent: idPersistentTest,
+    permissionGroup: permissionGroupTest
+})
+const userInfoTest1 = new UserInfo({
+    userName: userNameTest1,
+    email: emailTest1,
+    namesPersonal: namesPersonalTest1,
+    idPersistent: idPersistentTest1,
+    permissionGroup: permissionGroupTest1
+})
+
+const userInfoJsonTest = {
+    user_name: userNameTest,
+    email: emailTest,
+    names_personal: namesPersonalTest,
+    id_persistent: idPersistentTest,
+    permission_group: 'EDITOR',
+    tag_definition_list: []
+}
+
+const userInfoJsonTest1 = {
+    user_name: userNameTest1,
+    email: emailTest1,
+    names_personal: namesPersonalTest1,
+    id_persistent: idPersistentTest1,
+    permission_group: 'CONTRIBUTOR',
+    tag_definition_list: []
+}
+
+describe('get users', () => {
+    test('success', async () => {
+        responseSequence([
+            [200, { user_list: [userInfoJsonTest], next_offset: 5 }],
+            [200, { user_list: [userInfoJsonTest1], next_offset: 10 }],
+            [200, { user_list: [], next_offset: -1 }]
+        ])
+        const dispatch = jest.fn()
+        await new GetUserInfoListAction().run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new GetUserInfoListStartAction()],
+            [new GetUserInfoListSuccessAction([userInfoTest, userInfoTest1])]
+        ])
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/vran/api/user/chunks/0/5000',
+                { credentials: 'include', method: 'GET' }
+            ],
+            [
+                'http://127.0.0.1:8000/vran/api/user/chunks/5/5000',
+                { credentials: 'include', method: 'GET' }
+            ],
+            [
+                'http://127.0.0.1:8000/vran/api/user/chunks/10/5000',
+                { credentials: 'include', method: 'GET' }
+            ]
+        ])
+    })
+    test('error', async () => {
+        responseSequence([
+            [200, { user_list: [userInfoJsonTest], next_offset: 5 }],
+            [400, { msg: 'error' }]
+        ])
+        const dispatch = jest.fn()
+        await new GetUserInfoListAction().run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new GetUserInfoListStartAction()],
+            [new GetUserInfoListErrorAction('error')]
+        ])
+    })
+})
+describe('set user permissions', () => {
+    test('success', async () => {
+        responseSequence([[200, {}]])
+        const dispatch = jest.fn()
+        await new SetUserPermissionAction(
+            idPersistentTest,
+            UserPermissionGroup.APPLICANT
+        ).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new SetUserPermissionStartAction()],
+            [
+                new SetUserPermissionSuccessAction(
+                    idPersistentTest,
+                    UserPermissionGroup.APPLICANT
+                )
+            ]
+        ])
+        expect((fetch as jest.Mock).mock.calls).toContainEqual([
+            `http://127.0.0.1:8000/vran/api/user/${idPersistentTest}/permission_group`,
+            {
+                method: 'PUT',
+                credentials: 'include',
+                body: JSON.stringify({ permission_group: 'APPLICANT' })
+            }
+        ])
+    })
+    test('error', async () => {
+        responseSequence([[400, { msg: 'error' }]])
+        const dispatch = jest.fn()
+        await new SetUserPermissionAction(
+            idPersistentTest,
+            UserPermissionGroup.APPLICANT
+        ).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new SetUserPermissionStartAction()],
+            [new SetUserPermissionErrorAction('error')]
+        ])
+    })
+})

ui/src/user/permission_groups/components.tsx
@@ -1,0 +1,129 @@
+import { useLayoutEffect } from 'react'
+import { useUserPermissionGroup } from './hooks'
+import { VrAnLoading } from '../../util/components/misc'
+import { Alert, Col, FormCheck, ListGroup, Row } from 'react-bootstrap'
+import { UserInfo, UserPermissionGroup } from '../state'
+import { Remote } from '../../util/state'
+export function UserPermissionGroupComponent() {
+    const {
+        userInfoList,
+        selectedUser,
+        getUserInfoListCallback,
+        selectUserCallback,
+        setUserPermissionCallback,
+        setUserPermissionClearErrorCallback
+    } = useUserPermissionGroup()
+    useLayoutEffect(
+        () => {
+            getUserInfoListCallback()
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )
+    if (userInfoList.isLoading) {
+        return <VrAnLoading />
+    }
+    return (
+        <Row className="h-100">
+            <Col className="h-100" xs={6}>
+                <ListGroup>
+                    {userInfoList.value.map((userInfo) => (
+                        <UserInfoListItem
+                            userInfo={userInfo}
+                            active={
+                                userInfo.idPersistent ==
+                                selectedUser.value?.idPersistent
+                            }
+                            selectUserCallback={selectUserCallback}
+                        />
+                    ))}
+                </ListGroup>
+            </Col>
+            <UserPermissionGroupForm
+                userInfo={selectedUser}
+                setUserPermissionCallback={setUserPermissionCallback}
+                clearErrorCallback={setUserPermissionClearErrorCallback}
+            />
+        </Row>
+    )
+}
+export function UserInfoListItem({
+    userInfo,
+    active,
+    selectUserCallback
+}: {
+    userInfo: UserInfo
+    active: boolean
+    selectUserCallback: (userInfo: UserInfo) => void
+}): JSX.Element {
+    return (
+        <ListGroup.Item active={active} onClick={() => selectUserCallback(userInfo)}>
+            {userInfo.userName}
+        </ListGroup.Item>
+    )
+}
+
+export function UserPermissionGroupForm({
+    userInfo,
+    setUserPermissionCallback,
+    clearErrorCallback
+}: {
+    userInfo: Remote<UserInfo | undefined>
+    setUserPermissionCallback: (
+        idUserPersistent: string,
+        permission: UserPermissionGroup
+    ) => void
+    clearErrorCallback: VoidFunction
+}) {
+    if (userInfo.value === undefined) {
+        return <span>Please select a user.</span>
+    }
+    return (
+        <Col>
+            <Row className="mb-3">
+                <Col>
+                    <span>Change permissions for user: </span>
+                    <span className="fw-bold">{userInfo.value.userName}</span>
+                </Col>
+            </Row>
+            <Row className="ms-3">
+                {[
+                    UserPermissionGroup.APPLICANT,
+                    UserPermissionGroup.READER,
+                    UserPermissionGroup.CONTRIBUTOR,
+                    UserPermissionGroup.EDITOR,
+                    UserPermissionGroup.COMMISSIONER
+                ].map((permission) => (
+                    <FormCheck
+                        checked={userInfo.value?.permissionGroup == permission}
+                        value={permission}
+                        onChange={(_event) => {
+                            if (userInfo.value == undefined) {
+                                return
+                            }
+                            setUserPermissionCallback(
+                                userInfo.value.idPersistent,
+                                permission
+                            )
+                        }}
+                        label={permission.toString()}
+                        type="radio"
+                        name="user-permission"
+                        disabled={userInfo.isLoading}
+                    ></FormCheck>
+                ))}
+            </Row>
+            {userInfo.errorMsg !== undefined && (
+                <Alert
+                    variant="danger"
+                    dismissible
+                    onClose={clearErrorCallback}
+                    closeVariant="white"
+                >
+                    <Alert.Heading>Error</Alert.Heading>
+                    {userInfo.errorMsg}
+                </Alert>
+            )}
+        </Col>
+    )
+}

Leibniz-HBI/CoSMA-Editor

ui/src/user/permission_groups/__tests__/async_actions.test.ts
@@ -1,0 +1,149 @@
+import { UserInfo, UserPermissionGroup } from '../../state'
+import {
+    GetUserInfoListErrorAction,
+    GetUserInfoListStartAction,
+    GetUserInfoListSuccessAction,
+    SetUserPermissionErrorAction,
+    SetUserPermissionStartAction,
+    SetUserPermissionSuccessAction
+} from '../actions'
+import { GetUserInfoListAction, SetUserPermissionAction } from '../async_actions'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function responseSequence(responses: [number, any][]) {
+    const fetchMock = jest.spyOn(global, 'fetch')
+    for (const tpl of responses) {
+        const [status_code, rsp] = tpl
+        fetchMock.mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp)
+                })
+            ) as jest.Mock
+        )
+    }
+}
+const userNameTest = 'userTest'
+const emailTest = 'me@test.url'
+const namesPersonalTest = 'names personal test'
+const idPersistentTest = 'id-user=test'
+const permissionGroupTest = UserPermissionGroup.EDITOR
+const userNameTest1 = 'userTest1'
+const emailTest1 = 'me1@test.url'
+const namesPersonalTest1 = 'names personal test1'
+const idPersistentTest1 = 'id-user=test1'
+const permissionGroupTest1 = UserPermissionGroup.CONTRIBUTOR
+const userInfoTest = new UserInfo({
+    userName: userNameTest,
+    email: emailTest,
+    namesPersonal: namesPersonalTest,
+    idPersistent: idPersistentTest,
+    permissionGroup: permissionGroupTest
+})
+const userInfoTest1 = new UserInfo({
+    userName: userNameTest1,
+    email: emailTest1,
+    namesPersonal: namesPersonalTest1,
+    idPersistent: idPersistentTest1,
+    permissionGroup: permissionGroupTest1
+})
+
+const userInfoJsonTest = {
+    user_name: userNameTest,
+    email: emailTest,
+    names_personal: namesPersonalTest,
+    id_persistent: idPersistentTest,
+    permission_group: 'EDITOR',
+    tag_definition_list: []
+}
+
+const userInfoJsonTest1 = {
+    user_name: userNameTest1,
+    email: emailTest1,
+    names_personal: namesPersonalTest1,
+    id_persistent: idPersistentTest1,
+    permission_group: 'CONTRIBUTOR',
+    tag_definition_list: []
+}
+
+describe('get users', () => {
+    test('success', async () => {
+        responseSequence([
+            [200, { user_list: [userInfoJsonTest], next_offset: 5 }],
+            [200, { user_list: [userInfoJsonTest1], next_offset: 10 }],
+            [200, { user_list: [], next_offset: -1 }]
+        ])
+        const dispatch = jest.fn()
+        await new GetUserInfoListAction().run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new GetUserInfoListStartAction()],
+            [new GetUserInfoListSuccessAction([userInfoTest, userInfoTest1])]
+        ])
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/cosmae/api/user/chunks/0/5000',
+                { credentials: 'include', method: 'GET' }
+            ],
+            [
+                'http://127.0.0.1:8000/cosmae/api/user/chunks/5/5000',
+                { credentials: 'include', method: 'GET' }
+            ],
+            [
+                'http://127.0.0.1:8000/cosmae/api/user/chunks/10/5000',
+                { credentials: 'include', method: 'GET' }
+            ]
+        ])
+    })
+    test('error', async () => {
+        responseSequence([
+            [200, { user_list: [userInfoJsonTest], next_offset: 5 }],
+            [400, { msg: 'error' }]
+        ])
+        const dispatch = jest.fn()
+        await new GetUserInfoListAction().run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new GetUserInfoListStartAction()],
+            [new GetUserInfoListErrorAction('error')]
+        ])
+    })
+})
+describe('set user permissions', () => {
+    test('success', async () => {
+        responseSequence([[200, {}]])
+        const dispatch = jest.fn()
+        await new SetUserPermissionAction(
+            idPersistentTest,
+            UserPermissionGroup.APPLICANT
+        ).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new SetUserPermissionStartAction()],
+            [
+                new SetUserPermissionSuccessAction(
+                    idPersistentTest,
+                    UserPermissionGroup.APPLICANT
+                )
+            ]
+        ])
+        expect((fetch as jest.Mock).mock.calls).toContainEqual([
+            `http://127.0.0.1:8000/cosmae/api/user/${idPersistentTest}/permission_group`,
+            {
+                method: 'PUT',
+                credentials: 'include',
+                body: JSON.stringify({ permission_group: 'APPLICANT' })
+            }
+        ])
+    })
+    test('error', async () => {
+        responseSequence([[400, { msg: 'error' }]])
+        const dispatch = jest.fn()
+        await new SetUserPermissionAction(
+            idPersistentTest,
+            UserPermissionGroup.APPLICANT
+        ).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new SetUserPermissionStartAction()],
+            [new SetUserPermissionErrorAction('error')]
+        ])
+    })
+})

ui/src/user/permission_groups/components.tsx
@@ -1,0 +1,129 @@
+import { useLayoutEffect } from 'react'
+import { useUserPermissionGroup } from './hooks'
+import { CosmaeLoading } from '../../util/components/misc'
+import { Alert, Col, FormCheck, ListGroup, Row } from 'react-bootstrap'
+import { UserInfo, UserPermissionGroup } from '../state'
+import { Remote } from '../../util/state'
+export function UserPermissionGroupComponent() {
+    const {
+        userInfoList,
+        selectedUser,
+        getUserInfoListCallback,
+        selectUserCallback,
+        setUserPermissionCallback,
+        setUserPermissionClearErrorCallback
+    } = useUserPermissionGroup()
+    useLayoutEffect(
+        () => {
+            getUserInfoListCallback()
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )
+    if (userInfoList.isLoading) {
+        return <CosmaeLoading />
+    }
+    return (
+        <Row className="h-100">
+            <Col className="h-100" xs={6}>
+                <ListGroup>
+                    {userInfoList.value.map((userInfo) => (
+                        <UserInfoListItem
+                            userInfo={userInfo}
+                            active={
+                                userInfo.idPersistent ==
+                                selectedUser.value?.idPersistent
+                            }
+                            selectUserCallback={selectUserCallback}
+                        />
+                    ))}
+                </ListGroup>
+            </Col>
+            <UserPermissionGroupForm
+                userInfo={selectedUser}
+                setUserPermissionCallback={setUserPermissionCallback}
+                clearErrorCallback={setUserPermissionClearErrorCallback}
+            />
+        </Row>
+    )
+}
+export function UserInfoListItem({
+    userInfo,
+    active,
+    selectUserCallback
+}: {
+    userInfo: UserInfo
+    active: boolean
+    selectUserCallback: (userInfo: UserInfo) => void
+}): JSX.Element {
+    return (
+        <ListGroup.Item active={active} onClick={() => selectUserCallback(userInfo)}>
+            {userInfo.userName}
+        </ListGroup.Item>
+    )
+}
+
+export function UserPermissionGroupForm({
+    userInfo,
+    setUserPermissionCallback,
+    clearErrorCallback
+}: {
+    userInfo: Remote<UserInfo | undefined>
+    setUserPermissionCallback: (
+        idUserPersistent: string,
+        permission: UserPermissionGroup
+    ) => void
+    clearErrorCallback: VoidFunction
+}) {
+    if (userInfo.value === undefined) {
+        return <span>Please select a user.</span>
+    }
+    return (
+        <Col>
+            <Row className="mb-3">
+                <Col>
+                    <span>Change permissions for user: </span>
+                    <span className="fw-bold">{userInfo.value.userName}</span>
+                </Col>
+            </Row>
+            <Row className="ms-3">
+                {[
+                    UserPermissionGroup.APPLICANT,
+                    UserPermissionGroup.READER,
+                    UserPermissionGroup.CONTRIBUTOR,
+                    UserPermissionGroup.EDITOR,
+                    UserPermissionGroup.COMMISSIONER
+                ].map((permission) => (
+                    <FormCheck
+                        checked={userInfo.value?.permissionGroup == permission}
+                        value={permission}
+                        onChange={(_event) => {
+                            if (userInfo.value == undefined) {
+                                return
+                            }
+                            setUserPermissionCallback(
+                                userInfo.value.idPersistent,
+                                permission
+                            )
+                        }}
+                        label={permission.toString()}
+                        type="radio"
+                        name="user-permission"
+                        disabled={userInfo.isLoading}
+                    ></FormCheck>
+                ))}
+            </Row>
+            {userInfo.errorMsg !== undefined && (
+                <Alert
+                    variant="danger"
+                    dismissible
+                    onClose={clearErrorCallback}
+                    closeVariant="white"
+                >
+                    <Alert.Heading>Error</Alert.Heading>
+                    {userInfo.errorMsg}
+                </Alert>
+            )}
+        </Col>
+    )
+}