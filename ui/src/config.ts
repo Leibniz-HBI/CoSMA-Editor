Leibniz-HBI/DP-REX

Change mechanism for switching between development and production config for the UI

ui/src/config.ts
@@ -1,0 +1,1 @@
+export const config = { api_path: 'http://127.0.0.1:8000/vran/api' }

Leibniz-HBI/CoSMA-Editor

ui/src/config.ts
@@ -1,0 +1,1 @@
+export const config = { api_path: 'http://127.0.0.1:8000/cosmae/api' }