Leibniz-HBI/DP-REX

Add authentication to backend.

vran/urls.py
@@ -1,13 +1,38 @@
 """Registry for VrAN urls."""
+
 from django.contrib import admin
+from django.contrib.auth import authenticate, login
 from django.urls import path
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from ninja.constants import NOT_SET
 
 from vran.person.api import router as person_router
 from vran.tag.api import router as tag_router
+from vran.user.api import router as user_router
+from vran.util.auth import vran_auth
 
-ninja_api = NinjaAPI()
-ninja_api.add_router("persons", person_router)
-ninja_api.add_router("tags", tag_router)
+ninja_api = NinjaAPI(csrf=False)
+ninja_api.add_router("user", user_router, auth=NOT_SET)
+ninja_api.add_router("persons", person_router, auth=vran_auth)
+ninja_api.add_router("tags", tag_router, auth=vran_auth)
+
+
+class LoginRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "API model for login requests"
+    name: str
+    password: str
+
+
+@ninja_api.post("login", auth=NOT_SET)
+def login_post(request, credentials: LoginRequest):
+    "API endpoint for login"
+    user = authenticate(
+        request, username=credentials.name, password=credentials.password
+    )
+    if user is None:
+        return
+    login(request, user)
+
 
 urlpatterns = [path("manage/", admin.site.urls), path("api/", ninja_api.urls)]

tests/tag/api/integration/requests.py
@@ -4,36 +4,41 @@ from urllib.parse import urljoin
 import requests
 
 
-def post_tag_def(url, tag_def):
-    return post_tag_defs(url, [tag_def])
+def post_tag_def(url, tag_def, cookies=None):
+    return post_tag_defs(url, [tag_def], cookies)
 
 
-def post_tag_defs(url, tag_defs):
+def post_tag_defs(url, tag_defs, cookies=None):
     return requests.post(
         urljoin(url, "/vran/api/tags/definitions"),
         json={"tag_definitions": tag_defs},
+        cookies=cookies,
         timeout=900,
     )
 
 
-def get_tagdefs(url):
+def get_tagdefs(url, cookies=None):
     return requests.get(
         urljoin(url, "/vran/api/tags/definitions"),
+        cookies=cookies,
         timeout=900,
     )
 
 
-def post_tag_instance(url, tag):
-    return post_tag_instances(url, [tag])
+def post_tag_instance(url, tag, **kwargs):
+    return post_tag_instances(url, [tag], **kwargs)
 
 
-def post_tag_instances(url, tags):
+def post_tag_instances(url, tags, cookies=None):
     return requests.post(
-        urljoin(url, "/vran/api/tags"), json={"tag_instances": tags}, timeout=900
+        urljoin(url, "/vran/api/tags"),
+        json={"tag_instances": tags},
+        cookies=cookies,
+        timeout=900,
     )
 
 
-def post_tag_instance_chunks(url, tag_def_id, offset, limit):
+def post_tag_instance_chunks(url, tag_def_id, offset, limit, cookies=None):
     return requests.post(
         urljoin(url, "/vran/api/tags/chunk"),
         json={
@@ -41,25 +46,29 @@ def post_tag_instance_chunks(url, tag_def_id, offset, limit):
             "offset": offset,
             "limit": limit,
         },
+        cookies=cookies,
         timeout=900,
     )
 
 
-def post_tag_def_children(url, id_persistent):
+def post_tag_def_children(url, id_persistent, cookies=None):
     return requests.post(
         urljoin(url, "vran/api/tags/definitions/children"),
         json={"id_parent_persistent": id_persistent},
         timeout=900,
+        cookies=cookies,
     )
 
 
-def post_tag_instance_values(url, id_entity_persistent, id_tag_definition_persistent):
+def post_tag_instance_values(
+    url, id_entity_persistent, id_tag_definition_persistent, cookies=None
+):
     return post_tag_instances_values(
-        url, [(id_entity_persistent, id_tag_definition_persistent)]
+        url, [(id_entity_persistent, id_tag_definition_persistent)], cookies
     )
 
 
-def post_tag_instances_values(url, id_persistent_pairs):
+def post_tag_instances_values(url, id_persistent_pairs, cookies):
     return requests.post(
         urljoin(url, "vran/api/tags/values"),
         json={
@@ -71,5 +80,6 @@ def post_tag_instances_values(url, id_persistent_pairs):
                 for id_entity_persistent, id_tag_definition_persistent in id_persistent_pairs
             ]
         },
+        cookies=cookies,
         timeout=900,
     )

tests/user/api/integration/to_string_test.py
@@ -1,0 +1,24 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from tests.user import common as c
+from vran.user.api import LoginRequest, RegisterRequest
+
+
+def test_login_no_password_print():
+    assert 0 > (
+        str(LoginRequest(name=c.test_username, password=c.test_password)).find(
+            c.test_password
+        )
+    )
+
+
+def test_register_no_password_print():
+    assert 0 > (
+        str(
+            RegisterRequest(
+                user_name=c.test_username,
+                password=c.test_password,
+                names_personal=c.test_names_personal,
+                email=c.test_email,
+            )
+        ).find(c.test_password)
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/urls.py
@@ -1,13 +1,38 @@
 """Registry for CoSMA-Editor urls."""
+
 from django.contrib import admin
+from django.contrib.auth import authenticate, login
 from django.urls import path
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from ninja.constants import NOT_SET
 
 from cosmae.person.api import router as person_router
 from cosmae.tag.api import router as tag_router
+from cosmae.user.api import router as user_router
+from cosmae.util.auth import cosmae_auth
 
-ninja_api = NinjaAPI()
-ninja_api.add_router("persons", person_router)
-ninja_api.add_router("tags", tag_router)
+ninja_api = NinjaAPI(csrf=False)
+ninja_api.add_router("user", user_router, auth=NOT_SET)
+ninja_api.add_router("persons", person_router, auth=cosmae_auth)
+ninja_api.add_router("tags", tag_router, auth=cosmae_auth)
+
+
+class LoginRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "API model for login requests"
+    name: str
+    password: str
+
+
+@ninja_api.post("login", auth=NOT_SET)
+def login_post(request, credentials: LoginRequest):
+    "API endpoint for login"
+    user = authenticate(
+        request, username=credentials.name, password=credentials.password
+    )
+    if user is None:
+        return
+    login(request, user)
+
 
 urlpatterns = [path("manage/", admin.site.urls), path("api/", ninja_api.urls)]

tests/tag/api/integration/requests.py
@@ -4,36 +4,41 @@ from urllib.parse import urljoin
 import requests
 
 
-def post_tag_def(url, tag_def):
-    return post_tag_defs(url, [tag_def])
+def post_tag_def(url, tag_def, cookies=None):
+    return post_tag_defs(url, [tag_def], cookies)
 
 
-def post_tag_defs(url, tag_defs):
+def post_tag_defs(url, tag_defs, cookies=None):
     return requests.post(
         urljoin(url, "/cosmae/api/tags/definitions"),
         json={"tag_definitions": tag_defs},
+        cookies=cookies,
         timeout=900,
     )
 
 
-def get_tagdefs(url):
+def get_tagdefs(url, cookies=None):
     return requests.get(
         urljoin(url, "/cosmae/api/tags/definitions"),
+        cookies=cookies,
         timeout=900,
     )
 
 
-def post_tag_instance(url, tag):
-    return post_tag_instances(url, [tag])
+def post_tag_instance(url, tag, **kwargs):
+    return post_tag_instances(url, [tag], **kwargs)
 
 
-def post_tag_instances(url, tags):
+def post_tag_instances(url, tags, cookies=None):
     return requests.post(
-        urljoin(url, "/cosmae/api/tags"), json={"tag_instances": tags}, timeout=900
+        urljoin(url, "/cosmae/api/tags"),
+        json={"tag_instances": tags},
+        cookies=cookies,
+        timeout=900,
     )
 
 
-def post_tag_instance_chunks(url, tag_def_id, offset, limit):
+def post_tag_instance_chunks(url, tag_def_id, offset, limit, cookies=None):
     return requests.post(
         urljoin(url, "/cosmae/api/tags/chunk"),
         json={
@@ -41,25 +46,29 @@ def post_tag_instance_chunks(url, tag_def_id, offset, limit):
             "offset": offset,
             "limit": limit,
         },
+        cookies=cookies,
         timeout=900,
     )
 
 
-def post_tag_def_children(url, id_persistent):
+def post_tag_def_children(url, id_persistent, cookies=None):
     return requests.post(
         urljoin(url, "cosmae/api/tags/definitions/children"),
         json={"id_parent_persistent": id_persistent},
         timeout=900,
+        cookies=cookies,
     )
 
 
-def post_tag_instance_values(url, id_entity_persistent, id_tag_definition_persistent):
+def post_tag_instance_values(
+    url, id_entity_persistent, id_tag_definition_persistent, cookies=None
+):
     return post_tag_instances_values(
-        url, [(id_entity_persistent, id_tag_definition_persistent)]
+        url, [(id_entity_persistent, id_tag_definition_persistent)], cookies
     )
 
 
-def post_tag_instances_values(url, id_persistent_pairs):
+def post_tag_instances_values(url, id_persistent_pairs, cookies):
     return requests.post(
         urljoin(url, "cosmae/api/tags/values"),
         json={
@@ -71,5 +80,6 @@ def post_tag_instances_values(url, id_persistent_pairs):
                 for id_entity_persistent, id_tag_definition_persistent in id_persistent_pairs
             ]
         },
+        cookies=cookies,
         timeout=900,
     )

tests/user/api/integration/to_string_test.py
@@ -1,0 +1,24 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from tests.user import common as c
+from cosmae.user.api import LoginRequest, RegisterRequest
+
+
+def test_login_no_password_print():
+    assert 0 > (
+        str(LoginRequest(name=c.test_username, password=c.test_password)).find(
+            c.test_password
+        )
+    )
+
+
+def test_register_no_password_print():
+    assert 0 > (
+        str(
+            RegisterRequest(
+                user_name=c.test_username,
+                password=c.test_password,
+                names_personal=c.test_names_personal,
+                email=c.test_email,
+            )
+        ).find(c.test_password)
+    )
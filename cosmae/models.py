88906f5a896e1376e8a03c54bbc4aca2e0c71e4e
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2023-04-12 13:51:29 +0200

    Add authentication to backend.

--- cosmae/models.py
+++ cosmae/models.py
@@ -1,5 +1,7 @@
 """Union of all DB models."""
+
 # pylint: disable=unused-import
 from cosmae.entity.models_django import Entity
 from cosmae.person.models_django import Person
 from cosmae.tag.models_django import TagDefinition, TagInstance
+from cosmae.util import CosmaeUser
--- cosmae/tag/api/__init__.py
+++ cosmae/tag/api/__init__.py
@@ -1,5 +1,7 @@
 "Combination of tag defintions and instance API."
 from cosmae.tag.api.definitions import router as defintions_router
 from cosmae.tag.api.instances import router
+from cosmae.util.auth import cosmae_auth
 
-router.add_router("definitions", defintions_router)
+router.add_router("definitions", defintions_router, auth=cosmae_auth)
+
