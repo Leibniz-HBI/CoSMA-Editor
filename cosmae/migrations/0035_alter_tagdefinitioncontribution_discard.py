Leibniz-HBI/DP-REX

Do not import columns by default.

vran/migrations/0035_alter_tagdefinitioncontribution_discard.py
@@ -1,0 +1,18 @@
+# Generated by Django 4.2.8 on 2024-01-25 13:04
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0034_remove_contributioncandidate_anonymous"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tagdefinitioncontribution",
+            name="discard",
+            field=models.BooleanField(default=True),
+        ),
+    ]

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0035_alter_tagdefinitioncontribution_discard.py
@@ -1,0 +1,18 @@
+# Generated by Django 4.2.8 on 2024-01-25 13:04
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0034_remove_contributioncandidate_anonymous"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tagdefinitioncontribution",
+            name="discard",
+            field=models.BooleanField(default=True),
+        ),
+    ]