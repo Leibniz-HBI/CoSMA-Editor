Leibniz-HBI/DP-REX

Add backend function for finalizing merge request.

tests/merge_request/api/integration/post_merge_test.py
@@ -1,0 +1,103 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.merge_request.api.integration.requests as req
+import tests.merge_request.common as c
+from vran.exception import NotAuthenticatedException
+from vran.merge_request.models_django import MergeRequest
+from vran.tag.models_django import TagInstance
+from vran.util import timestamp
+
+
+def test_unknown_user(auth_server):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.merge_request.api.check_user", mock):
+        rsp = req.post_start_merge(
+            server.url, c.id_persistent_merge_request, cookies=cookies
+        )
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req.post_start_merge(server.url, c.id_persistent_merge_request)
+    assert rsp.status_code == 401
+
+
+def test_no_mr(auth_server):
+    server, cookies = auth_server
+    rsp = req.post_start_merge(
+        server.url, "4e679630-241e-40f8-b175-c4b7916be379", cookies=cookies
+    )
+    assert rsp.status_code == 404
+
+
+def test_wrong_user(auth_server1, merge_request_user):
+    server, _, cookies = auth_server1
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 403
+    assert rsp.json() == {
+        "msg": "You do not have write permissions for the destination tag."
+    }
+
+
+def test_resolved_conflict(
+    auth_server,
+    merge_request_user,
+    conflict_resolution_replace,
+    conflict_resolution_keep,
+):
+    server, cookies = auth_server
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 200
+    merge_request = (
+        MergeRequest.by_id_persistent_query_set(  # pylint: disable=no-member
+            merge_request_user.id_persistent
+        )
+    ).get()
+    assert merge_request.state == MergeRequest.RESOLVED
+
+
+def test_open_conflicts(
+    auth_server, merge_request_user, instances_merge_request_origin_user
+):
+    server, cookies = auth_server
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 400
+    assert rsp.json() == {
+        "msg": "There are unresolved conflicts for the merge request."
+    }
+
+
+def test_updated_data(
+    auth_server,
+    merge_request_user,
+    conflict_resolution_replace,
+    conflict_resolution_keep,
+):
+    server, cookies = auth_server
+    old_instance = conflict_resolution_replace.tag_instance_destination
+    instance, _ = TagInstance.change_or_create(
+        id_persistent=old_instance.id_persistent,
+        id_entity_persistent=old_instance.id_entity_persistent,
+        id_tag_definition_persistent=old_instance.id_tag_definition_persistent,
+        version=old_instance.id,
+        time_edit=timestamp(),
+        value="updated value test",
+    )
+    instance.save()
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 400
+    assert rsp.json() == {
+        "msg": "There are conflicts for the merge request, where the underlying data has changed."
+    }

tests/merge_request/api/integration/requests.py
@@ -12,3 +12,47 @@ def get_conflicts(url, id_merge_request_persistent, cookies=None):
         cookies=cookies,
         timeout=900,
     )
+
+
+def post_resolution(  # pylint: disable=too-many-arguments
+    url,
+    id_merge_request_persistent,
+    id_entity_persistent,
+    id_entity_version,
+    id_tag_definition_origin_persistent,
+    id_tag_definition_origin_version,
+    id_tag_definition_destination_persistent,
+    id_tag_definition_destination_version,
+    id_tag_instance_origin_persistent,
+    id_tag_instance_origin_version,
+    id_tag_instance_destination_persistent,
+    id_tag_instance_destination_version,
+    replace,
+    cookies=None,
+):
+    return requests.post(
+        url + f"/vran/api/merge_requests/{id_merge_request_persistent}/resolve",
+        json={
+            "id_entity_version": id_entity_version,
+            "id_tag_definition_origin_version": id_tag_definition_origin_version,
+            "id_tag_definition_destination_version": id_tag_definition_destination_version,
+            "id_tag_instance_origin_version": id_tag_instance_origin_version,
+            "id_tag_instance_destination_version": id_tag_instance_destination_version,
+            "id_entity_persistent": id_entity_persistent,
+            "id_tag_definition_origin_persistent": id_tag_definition_origin_persistent,
+            "id_tag_definition_destination_persistent": id_tag_definition_destination_persistent,
+            "id_tag_instance_origin_persistent": id_tag_instance_origin_persistent,
+            "id_tag_instance_destination_persistent": id_tag_instance_destination_persistent,
+            "replace": replace,
+        },
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def post_start_merge(url, id_merge_request_persistent, cookies=None):
+    return requests.post(
+        url + f"/vran/api/merge_requests/{id_merge_request_persistent}/merge",
+        cookies=cookies,
+        timeout=900,
+    )

tests/merge_request/queue_test.py
@@ -1,6 +1,8 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+
 import vran.merge_request.queue as q
 from vran.merge_request.models_django import MergeRequest
+from vran.tag.models_django import TagInstance
 
 
 def test_fast_forward_destination_empty(
@@ -54,7 +56,7 @@ def test_fast_forward_conflict(
 def test_fast_forward_no_conflict_same_value(
     merge_request_user_fast_forward,
     instances_merge_request_origin_user,
-    instance_merge_request_destination_user_same_value,
+    instance_merge_request_destination_user_same_value1,
 ):
     q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
     merge_request_after = MergeRequest.by_id_persistent(
@@ -62,3 +64,142 @@ def test_fast_forward_no_conflict_same_value(
         merge_request_user_fast_forward.created_by,
     )
     assert merge_request_after.state == MergeRequest.MERGED
+
+
+def test_applies_resolutions(
+    merge_request_user, conflict_resolution_keep, conflict_resolution_replace
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value origin 1"
+
+
+def test_incomplete_resolution_stays_open_keep(
+    merge_request_user, conflict_resolution_keep
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.OPEN
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value destination"
+
+
+def test_incomplete_resolution_stays_open_replace(
+    merge_request_user, conflict_resolution_replace
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.OPEN
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value destination"
+
+
+def test_merges_for_equal_value_replace(
+    merge_request_user, conflict_resolution_replace, instance_destination_same_value
+):
+
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 2
+    instance = instances[0]
+    assert instance.value == "value origin"
+    instance = instances[1]
+    assert instance.value == "value origin 1"
+
+
+def test_merges_for_equal_value_keep(
+    merge_request_user,
+    conflict_resolution_keep,
+    instance_merge_request_destination_user_same_value1,
+):
+
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == instance_merge_request_destination_user_same_value1.value
+
+
+def test_merges_for_equal_value_updated(
+    merge_request_user, instance_destination_updated_same_value1
+):
+
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value origin 1"
+
+
+def test_instance_changed(
+    merge_request_user,
+    conflict_resolution_replace,
+    instance_merge_request_origin_user_changed,
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.OPEN

Leibniz-HBI/CoSMA-Editor

tests/merge_request/api/integration/post_merge_test.py
@@ -1,0 +1,103 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.merge_request.api.integration.requests as req
+import tests.merge_request.common as c
+from cosmae.exception import NotAuthenticatedException
+from cosmae.merge_request.models_django import MergeRequest
+from cosmae.tag.models_django import TagInstance
+from cosmae.util import timestamp
+
+
+def test_unknown_user(auth_server):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.merge_request.api.check_user", mock):
+        rsp = req.post_start_merge(
+            server.url, c.id_persistent_merge_request, cookies=cookies
+        )
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req.post_start_merge(server.url, c.id_persistent_merge_request)
+    assert rsp.status_code == 401
+
+
+def test_no_mr(auth_server):
+    server, cookies = auth_server
+    rsp = req.post_start_merge(
+        server.url, "4e679630-241e-40f8-b175-c4b7916be379", cookies=cookies
+    )
+    assert rsp.status_code == 404
+
+
+def test_wrong_user(auth_server1, merge_request_user):
+    server, _, cookies = auth_server1
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 403
+    assert rsp.json() == {
+        "msg": "You do not have write permissions for the destination tag."
+    }
+
+
+def test_resolved_conflict(
+    auth_server,
+    merge_request_user,
+    conflict_resolution_replace,
+    conflict_resolution_keep,
+):
+    server, cookies = auth_server
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 200
+    merge_request = (
+        MergeRequest.by_id_persistent_query_set(  # pylint: disable=no-member
+            merge_request_user.id_persistent
+        )
+    ).get()
+    assert merge_request.state == MergeRequest.RESOLVED
+
+
+def test_open_conflicts(
+    auth_server, merge_request_user, instances_merge_request_origin_user
+):
+    server, cookies = auth_server
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 400
+    assert rsp.json() == {
+        "msg": "There are unresolved conflicts for the merge request."
+    }
+
+
+def test_updated_data(
+    auth_server,
+    merge_request_user,
+    conflict_resolution_replace,
+    conflict_resolution_keep,
+):
+    server, cookies = auth_server
+    old_instance = conflict_resolution_replace.tag_instance_destination
+    instance, _ = TagInstance.change_or_create(
+        id_persistent=old_instance.id_persistent,
+        id_entity_persistent=old_instance.id_entity_persistent,
+        id_tag_definition_persistent=old_instance.id_tag_definition_persistent,
+        version=old_instance.id,
+        time_edit=timestamp(),
+        value="updated value test",
+    )
+    instance.save()
+    rsp = req.post_start_merge(
+        server.url, str(merge_request_user.id_persistent), cookies=cookies
+    )
+    assert rsp.status_code == 400
+    assert rsp.json() == {
+        "msg": "There are conflicts for the merge request, where the underlying data has changed."
+    }

tests/merge_request/api/integration/requests.py
@@ -12,3 +12,47 @@ def get_conflicts(url, id_merge_request_persistent, cookies=None):
         cookies=cookies,
         timeout=900,
     )
+
+
+def post_resolution(  # pylint: disable=too-many-arguments
+    url,
+    id_merge_request_persistent,
+    id_entity_persistent,
+    id_entity_version,
+    id_tag_definition_origin_persistent,
+    id_tag_definition_origin_version,
+    id_tag_definition_destination_persistent,
+    id_tag_definition_destination_version,
+    id_tag_instance_origin_persistent,
+    id_tag_instance_origin_version,
+    id_tag_instance_destination_persistent,
+    id_tag_instance_destination_version,
+    replace,
+    cookies=None,
+):
+    return requests.post(
+        url + f"/cosmae/api/merge_requests/{id_merge_request_persistent}/resolve",
+        json={
+            "id_entity_version": id_entity_version,
+            "id_tag_definition_origin_version": id_tag_definition_origin_version,
+            "id_tag_definition_destination_version": id_tag_definition_destination_version,
+            "id_tag_instance_origin_version": id_tag_instance_origin_version,
+            "id_tag_instance_destination_version": id_tag_instance_destination_version,
+            "id_entity_persistent": id_entity_persistent,
+            "id_tag_definition_origin_persistent": id_tag_definition_origin_persistent,
+            "id_tag_definition_destination_persistent": id_tag_definition_destination_persistent,
+            "id_tag_instance_origin_persistent": id_tag_instance_origin_persistent,
+            "id_tag_instance_destination_persistent": id_tag_instance_destination_persistent,
+            "replace": replace,
+        },
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def post_start_merge(url, id_merge_request_persistent, cookies=None):
+    return requests.post(
+        url + f"/cosmae/api/merge_requests/{id_merge_request_persistent}/merge",
+        cookies=cookies,
+        timeout=900,
+    )

tests/merge_request/queue_test.py
@@ -1,6 +1,8 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+
 import cosmae.merge_request.queue as q
 from cosmae.merge_request.models_django import MergeRequest
+from cosmae.tag.models_django import TagInstance
 
 
 def test_fast_forward_destination_empty(
@@ -54,7 +56,7 @@ def test_fast_forward_conflict(
 def test_fast_forward_no_conflict_same_value(
     merge_request_user_fast_forward,
     instances_merge_request_origin_user,
-    instance_merge_request_destination_user_same_value,
+    instance_merge_request_destination_user_same_value1,
 ):
     q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
     merge_request_after = MergeRequest.by_id_persistent(
@@ -62,3 +64,142 @@ def test_fast_forward_no_conflict_same_value(
         merge_request_user_fast_forward.created_by,
     )
     assert merge_request_after.state == MergeRequest.MERGED
+
+
+def test_applies_resolutions(
+    merge_request_user, conflict_resolution_keep, conflict_resolution_replace
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value origin 1"
+
+
+def test_incomplete_resolution_stays_open_keep(
+    merge_request_user, conflict_resolution_keep
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.OPEN
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value destination"
+
+
+def test_incomplete_resolution_stays_open_replace(
+    merge_request_user, conflict_resolution_replace
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.OPEN
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value destination"
+
+
+def test_merges_for_equal_value_replace(
+    merge_request_user, conflict_resolution_replace, instance_destination_same_value
+):
+
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 2
+    instance = instances[0]
+    assert instance.value == "value origin"
+    instance = instances[1]
+    assert instance.value == "value origin 1"
+
+
+def test_merges_for_equal_value_keep(
+    merge_request_user,
+    conflict_resolution_keep,
+    instance_merge_request_destination_user_same_value1,
+):
+
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == instance_merge_request_destination_user_same_value1.value
+
+
+def test_merges_for_equal_value_updated(
+    merge_request_user, instance_destination_updated_same_value1
+):
+
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.MERGED
+    instances = list(
+        TagInstance.most_recent_queryset(
+            TagInstance.objects.filter(  # pylint: disable=no-member
+                id_tag_definition_persistent=merge_request_user.id_destination_persistent
+            )
+        )
+    )
+    assert len(instances) == 1
+    instance = instances[0]
+    assert instance.value == "value origin 1"
+
+
+def test_instance_changed(
+    merge_request_user,
+    conflict_resolution_replace,
+    instance_merge_request_origin_user_changed,
+):
+    q.merge_request_resolve_conflicts(merge_request_user.id_persistent)
+    merge_request = MergeRequest.by_id_persistent(
+        merge_request_user.id_persistent, merge_request_user.assigned_to
+    )
+    assert merge_request.state == MergeRequest.OPEN