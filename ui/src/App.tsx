Leibniz-HBI/DP-REX

Add UI for uploading and editing contributions.

ui/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
 import '@glideapps/glide-data-grid/dist/index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
 import { LoginProvider } from './user/components/provider'
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { RemoteDataTable } from './table/components'
 import { UserContext } from './user/hooks'
 import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ContributionList } from './contribution/components'
+import { ContributionDetailsStep } from './contribution/details/components'
 
 export function VranRoot() {
     return (
-        <Row className="flex-grow-1 m-0">
-            <Col className="ps-0 pe-0">
+        <Row className="flex-grow-1 m-0 h-100">
+            <Col className="ps-0 pe-0 h-100">
                 <div className="vran-page-container">
-                    <Navbar expand="lg" className="vran-nav-bar">
-                        <Container>
+                    <Navbar expand="lg" className="bg-primary flex-shrink-0 mb-3">
+                        <Container className="text-secondary">
                             <Navbar.Brand href="#home">VrAN</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
@@ -21,14 +23,14 @@ export function VranRoot() {
                                     <Nav.Link as={NavLink} to="/">
                                         View
                                     </Nav.Link>
-                                    <Nav.Link as={NavLink} to="/upload">
-                                        Upload
+                                    <Nav.Link as={NavLink} to="/contribute">
+                                        Contribute
                                     </Nav.Link>
                                 </Nav>
                             </Navbar.Collapse>
-                        </Container>{' '}
+                        </Container>
                     </Navbar>
-                    <div className="vran-page-body">
+                    <div className="vran-page-body flex-grow-1 flex-basis-0 flex-fill">
                         <Outlet />
                     </div>
                 </div>
@@ -43,7 +45,17 @@ const router = createBrowserRouter([
         element: <VranRoot />,
         children: [
             { path: '', element: <TableConnector />, index: true },
-            { path: 'upload', element: <p>UPLOAD</p> }
+            {
+                path: 'contribute',
+                element: <ContributionList />
+            },
+            {
+                path: 'contribute/:idpersistent',
+                element: <ContributionDetailsStep />,
+                loader: ({ params }) => {
+                    return params.idpersistent
+                }
+            }
         ]
     }
 ])
@@ -52,7 +64,7 @@ function App() {
     return (
         <Container
             fluid
-            className=" min-vh-100 d-flex flex-column ps-0 pe-0 ms-0 me-0 vran-container"
+            className="vh-100 d-flex flex-column ps-0 pe-0 ms-0 me-0 vran-container"
         >
             <LoginProvider body={<RouterProvider router={router} />} />
         </Container>

ui/src/contribution/details/__tests__/async_actions.test.ts
@@ -1,0 +1,159 @@
+import { Contribution, ContributionStep } from '../../state'
+import {
+    LoadContributionDetailsErrorAction,
+    LoadContributionDetailsStartAction,
+    LoadContributionDetailsSuccessAction,
+    PatchContributionDetailsErrorAction,
+    PatchContributionDetailsStartAction,
+    PatchContributionDetailsSuccessAction
+} from '../action'
+import { LoadContributionDetailsAction, PatchContributionAction } from '../async_action'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function responseSequence(respones: [number, () => any][]) {
+    global.fetch = jest.fn()
+    for (const tpl of respones) {
+        const [status_code, rsp] = tpl
+        ;(global.fetch as jest.Mock).mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp())
+                })
+            ) as jest.Mock
+        )
+    }
+}
+
+const nameTest0 = 'contribution test 0'
+const descriptionTest0 = 'a contribution for tests'
+const idTest0 = 'id-test-0'
+describe('get details', () => {
+    test('correct response', async () => {
+        responseSequence([
+            [
+                200,
+                () => {
+                    return {
+                        name: nameTest0,
+                        description: descriptionTest0,
+                        id_persistent: idTest0,
+                        anonymous: true,
+                        has_header: false,
+                        state: 'COLUMNS_EXTRACTED'
+                    }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new LoadContributionDetailsAction(idTest0).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new LoadContributionDetailsStartAction()],
+            [
+                new LoadContributionDetailsSuccessAction(
+                    new Contribution({
+                        name: nameTest0,
+                        description: descriptionTest0,
+                        idPersistent: idTest0,
+                        anonymous: true,
+                        hasHeader: false,
+                        step: ContributionStep.ColumnsExtracted
+                    })
+                )
+            ]
+        ])
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/vran/api/contributions/id-test-0',
+                { credentials: 'include' }
+            ]
+        ])
+    })
+    test('error response', async () => {
+        responseSequence([
+            [
+                500,
+                () => {
+                    return { msg: 'test error' }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new LoadContributionDetailsAction(idTest0).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new LoadContributionDetailsStartAction()],
+            [
+                new LoadContributionDetailsErrorAction(
+                    'Could not load contribution details. Reason: "test error".'
+                )
+            ]
+        ])
+    })
+})
+
+describe('patch contribution', () => {
+    test('success', async () => {
+        responseSequence([
+            [
+                200,
+                () => {
+                    return {
+                        name: nameTest0,
+                        description: descriptionTest0,
+                        id_persistent: idTest0,
+                        anonymous: true,
+                        has_header: true,
+                        state: 'COLUMNS_ASSIGNED'
+                    }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new PatchContributionAction({
+            idPersistent: idTest0,
+            hasHeader: true
+        }).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new PatchContributionDetailsStartAction()],
+            [
+                new PatchContributionDetailsSuccessAction(
+                    new Contribution({
+                        name: nameTest0,
+                        idPersistent: idTest0,
+                        description: descriptionTest0,
+                        anonymous: true,
+                        hasHeader: true,
+                        step: ContributionStep.ColumnsAssigned
+                    })
+                )
+            ]
+        ])
+        const body: { [key: string]: string | boolean } = { has_header: true }
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/vran/api/contributions/id-test-0',
+                { method: 'PATCH', credentials: 'include', body: JSON.stringify(body) }
+            ]
+        ])
+    })
+    test('server error', async () => {
+        responseSequence([
+            [
+                500,
+                () => {
+                    return { msg: 'test error' }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new PatchContributionAction({ idPersistent: idTest0 }).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new PatchContributionDetailsStartAction()],
+            [
+                new PatchContributionDetailsErrorAction(
+                    'Could not update contribution. Reason: "test error".'
+                )
+            ]
+        ])
+    })
+})

Leibniz-HBI/CoSMA-Editor

ui/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
 import '@glideapps/glide-data-grid/dist/index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
 import { LoginProvider } from './user/components/provider'
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { RemoteDataTable } from './table/components'
 import { UserContext } from './user/hooks'
 import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ContributionList } from './contribution/components'
+import { ContributionDetailsStep } from './contribution/details/components'
 
 export function CosmaeRoot() {
     return (
-        <Row className="flex-grow-1 m-0">
-            <Col className="ps-0 pe-0">
+        <Row className="flex-grow-1 m-0 h-100">
+            <Col className="ps-0 pe-0 h-100">
                 <div className="cosmae-page-container">
-                    <Navbar expand="lg" className="cosmae-nav-bar">
-                        <Container>
+                    <Navbar expand="lg" className="bg-primary flex-shrink-0 mb-3">
+                        <Container className="text-secondary">
                             <Navbar.Brand href="#home">CoSMA-E</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
@@ -21,14 +23,14 @@ export function CosmaeRoot() {
                                     <Nav.Link as={NavLink} to="/">
                                         View
                                     </Nav.Link>
-                                    <Nav.Link as={NavLink} to="/upload">
-                                        Upload
+                                    <Nav.Link as={NavLink} to="/contribute">
+                                        Contribute
                                     </Nav.Link>
                                 </Nav>
                             </Navbar.Collapse>
-                        </Container>{' '}
+                        </Container>
                     </Navbar>
-                    <div className="cosmae-page-body">
+                    <div className="cosmae-page-body flex-grow-1 flex-basis-0 flex-fill">
                         <Outlet />
                     </div>
                 </div>
@@ -43,7 +45,17 @@ const router = createBrowserRouter([
         element: <CosmaeRoot />,
         children: [
             { path: '', element: <TableConnector />, index: true },
-            { path: 'upload', element: <p>UPLOAD</p> }
+            {
+                path: 'contribute',
+                element: <ContributionList />
+            },
+            {
+                path: 'contribute/:idpersistent',
+                element: <ContributionDetailsStep />,
+                loader: ({ params }) => {
+                    return params.idpersistent
+                }
+            }
         ]
     }
 ])
@@ -52,7 +64,7 @@ function App() {
     return (
         <Container
             fluid
-            className=" min-vh-100 d-flex flex-column ps-0 pe-0 ms-0 me-0 cosmae-container"
+            className="vh-100 d-flex flex-column ps-0 pe-0 ms-0 me-0 cosmae-container"
         >
             <LoginProvider body={<RouterProvider router={router} />} />
         </Container>

ui/src/contribution/details/__tests__/async_actions.test.ts
@@ -1,0 +1,159 @@
+import { Contribution, ContributionStep } from '../../state'
+import {
+    LoadContributionDetailsErrorAction,
+    LoadContributionDetailsStartAction,
+    LoadContributionDetailsSuccessAction,
+    PatchContributionDetailsErrorAction,
+    PatchContributionDetailsStartAction,
+    PatchContributionDetailsSuccessAction
+} from '../action'
+import { LoadContributionDetailsAction, PatchContributionAction } from '../async_action'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function responseSequence(respones: [number, () => any][]) {
+    global.fetch = jest.fn()
+    for (const tpl of respones) {
+        const [status_code, rsp] = tpl
+        ;(global.fetch as jest.Mock).mockImplementationOnce(
+            jest.fn(() =>
+                Promise.resolve({
+                    status: status_code,
+                    json: () => Promise.resolve(rsp())
+                })
+            ) as jest.Mock
+        )
+    }
+}
+
+const nameTest0 = 'contribution test 0'
+const descriptionTest0 = 'a contribution for tests'
+const idTest0 = 'id-test-0'
+describe('get details', () => {
+    test('correct response', async () => {
+        responseSequence([
+            [
+                200,
+                () => {
+                    return {
+                        name: nameTest0,
+                        description: descriptionTest0,
+                        id_persistent: idTest0,
+                        anonymous: true,
+                        has_header: false,
+                        state: 'COLUMNS_EXTRACTED'
+                    }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new LoadContributionDetailsAction(idTest0).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new LoadContributionDetailsStartAction()],
+            [
+                new LoadContributionDetailsSuccessAction(
+                    new Contribution({
+                        name: nameTest0,
+                        description: descriptionTest0,
+                        idPersistent: idTest0,
+                        anonymous: true,
+                        hasHeader: false,
+                        step: ContributionStep.ColumnsExtracted
+                    })
+                )
+            ]
+        ])
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/cosmae/api/contributions/id-test-0',
+                { credentials: 'include' }
+            ]
+        ])
+    })
+    test('error response', async () => {
+        responseSequence([
+            [
+                500,
+                () => {
+                    return { msg: 'test error' }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new LoadContributionDetailsAction(idTest0).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new LoadContributionDetailsStartAction()],
+            [
+                new LoadContributionDetailsErrorAction(
+                    'Could not load contribution details. Reason: "test error".'
+                )
+            ]
+        ])
+    })
+})
+
+describe('patch contribution', () => {
+    test('success', async () => {
+        responseSequence([
+            [
+                200,
+                () => {
+                    return {
+                        name: nameTest0,
+                        description: descriptionTest0,
+                        id_persistent: idTest0,
+                        anonymous: true,
+                        has_header: true,
+                        state: 'COLUMNS_ASSIGNED'
+                    }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new PatchContributionAction({
+            idPersistent: idTest0,
+            hasHeader: true
+        }).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new PatchContributionDetailsStartAction()],
+            [
+                new PatchContributionDetailsSuccessAction(
+                    new Contribution({
+                        name: nameTest0,
+                        idPersistent: idTest0,
+                        description: descriptionTest0,
+                        anonymous: true,
+                        hasHeader: true,
+                        step: ContributionStep.ColumnsAssigned
+                    })
+                )
+            ]
+        ])
+        const body: { [key: string]: string | boolean } = { has_header: true }
+        expect((fetch as jest.Mock).mock.calls).toEqual([
+            [
+                'http://127.0.0.1:8000/cosmae/api/contributions/id-test-0',
+                { method: 'PATCH', credentials: 'include', body: JSON.stringify(body) }
+            ]
+        ])
+    })
+    test('server error', async () => {
+        responseSequence([
+            [
+                500,
+                () => {
+                    return { msg: 'test error' }
+                }
+            ]
+        ])
+        const dispatch = jest.fn()
+        await new PatchContributionAction({ idPersistent: idTest0 }).run(dispatch)
+        expect(dispatch.mock.calls).toEqual([
+            [new PatchContributionDetailsStartAction()],
+            [
+                new PatchContributionDetailsErrorAction(
+                    'Could not update contribution. Reason: "test error".'
+                )
+            ]
+        ])
+    })
+})