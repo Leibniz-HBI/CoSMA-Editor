Leibniz-HBI/DP-REX

Add router navigation to UI.

ui/src/App.tsx
@@ -2,46 +2,73 @@ import './App.css'
 import '@glideapps/glide-data-grid/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { LoginProvider } from './user/components/provider'
-import { config } from './config'
-import { HeaderProps } from './user/hooks'
-import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { RemoteDataTable } from './table/components'
-import { UserInfo } from './user/state'
+import { UserContext } from './user/hooks'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-function VranHeader({ headerProps }: { headerProps: HeaderProps }) {
+export function VranRoot() {
     return (
-        <Row className="vran-header justify-content-between ms-0 me-0 ">
-            <Col></Col>
-            <Col md="auto">VrAN</Col>
-            <Col>
-                {headerProps.logoutCallback && (
-                    <Button variant="link" onClick={headerProps.logoutCallback}>
-                        Logout
-                    </Button>
-                )}
+        <Row className="flex-grow-1 m-0">
+            <Col className="ps-0 pe-0">
+                <div className="vran-page-container">
+                    <Navbar expand="lg" className="vran-nav-bar">
+                        <Container>
+                            <Navbar.Brand href="#home">VrAN</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link as={NavLink} to="/">
+                                        View
+                                    </Nav.Link>
+                                    <Nav.Link as={NavLink} to="/upload">
+                                        Upload
+                                    </Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>{' '}
+                    </Navbar>
+                    <div className="vran-page-body">
+                        <Outlet />
+                    </div>
+                </div>
             </Col>
         </Row>
     )
 }
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <VranRoot />,
+        children: [
+            { path: '', element: <TableConnector />, index: true },
+            { path: 'upload', element: <p>UPLOAD</p> }
+        ]
+    }
+])
+
 function App() {
     return (
         <Container
             fluid
-            className=" min-vh-100 d-flex flex-column ps-0 pe-0 vran-container"
+            className=" min-vh-100 d-flex flex-column ps-0 pe-0 ms-0 me-0 vran-container"
         >
-            <LoginProvider
-                apiPath={config.api_path}
-                header={(headerProps) => <VranHeader headerProps={headerProps} />}
-                body={(userInfo: UserInfo) => (
-                    <RemoteDataTable
-                        base_url={config.api_path}
-                        column_defs={userInfo.columns}
-                    />
-                )}
-            />
+            <LoginProvider body={<RouterProvider router={router} />} />
         </Container>
     )
 }
-
 export default App
+function TableConnector() {
+    return (
+        <UserContext.Consumer>
+            {(userInfoWithLogout) =>
+                userInfoWithLogout && (
+                    <RemoteDataTable
+                        column_defs={userInfoWithLogout.userInfo.columns}
+                    />
+                )
+            }
+        </UserContext.Consumer>
+    )
+}

ui/src/column_menu/__tests__/async_actions.test.ts
@@ -26,7 +26,6 @@ function responseSequence(respones: [number, () => any][]) {
     }
 }
 
-const apiPathTest = 'http://test.org'
 describe('getHierarchyAction', () => {
     beforeEach(() => {
         jest.restoreAllMocks()
@@ -44,7 +43,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest })
+        const action = new GetHierarchyAction({})
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(2)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -59,7 +58,7 @@ describe('getHierarchyAction', () => {
         jest.spyOn(global, 'fetch').mockImplementationOnce(() => {
             throw Error('fetch error')
         })
-        const action = new GetHierarchyAction({ apiPath: apiPathTest })
+        const action = new GetHierarchyAction({})
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(2)
         const actionFromCall = dispatch.mock.calls[1][0]
@@ -79,7 +78,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest })
+        const action = new GetHierarchyAction({})
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(2)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -135,7 +134,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest, expand: true })
+        const action = new GetHierarchyAction({ expand: true })
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(4)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -223,7 +222,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest, expand: true })
+        const action = new GetHierarchyAction({ expand: true })
         await action.run(dispatch)
         // expect(dispatch.mock.calls.length).toBe(4)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -291,7 +290,6 @@ describe('SubmitColumnDefinition', () => {
         ])
         const dispatch = jest.fn()
         await new SubmitColumnDefinitionAction({
-            apiPath: apiPathTest,
             columnTypeIdx: 2,
             idParentPersistent: idParentPersistentTest,
             name: nameTest
@@ -302,7 +300,7 @@ describe('SubmitColumnDefinition', () => {
         ])
         expect((fetch as jest.Mock).mock.calls).toEqual([
             [
-                'http://test.org/tags/definitions',
+                'http://127.0.0.1:8000/vran/api/tags/definitions',
                 {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
@@ -331,7 +329,6 @@ describe('SubmitColumnDefinition', () => {
         ])
         const dispatch = jest.fn()
         await new SubmitColumnDefinitionAction({
-            apiPath: apiPathTest,
             columnTypeIdx: 2,
             idParentPersistent: idParentPersistentTest,
             name: nameTest
@@ -356,7 +353,6 @@ describe('SubmitColumnDefinition', () => {
         ])
         const dispatch = jest.fn()
         await new SubmitColumnDefinitionAction({
-            apiPath: apiPathTest,
             columnTypeIdx: 2,
             idParentPersistent: idParentPersistentTest,
             name: nameTest

Leibniz-HBI/CoSMA-Editor

ui/src/App.tsx
@@ -2,46 +2,73 @@ import './App.css'
 import '@glideapps/glide-data-grid/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { LoginProvider } from './user/components/provider'
-import { config } from './config'
-import { HeaderProps } from './user/hooks'
-import { Button, Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { RemoteDataTable } from './table/components'
-import { UserInfo } from './user/state'
+import { UserContext } from './user/hooks'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-function CosmaeHeader({ headerProps }: { headerProps: HeaderProps }) {
+export function CosmaeRoot() {
     return (
-        <Row className="cosmae-header justify-content-between ms-0 me-0 ">
-            <Col></Col>
-            <Col md="auto">CoSMA-E</Col>
-            <Col>
-                {headerProps.logoutCallback && (
-                    <Button variant="link" onClick={headerProps.logoutCallback}>
-                        Logout
-                    </Button>
-                )}
+        <Row className="flex-grow-1 m-0">
+            <Col className="ps-0 pe-0">
+                <div className="cosmae-page-container">
+                    <Navbar expand="lg" className="cosmae-nav-bar">
+                        <Container>
+                            <Navbar.Brand href="#home">CoSMA-E</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link as={NavLink} to="/">
+                                        View
+                                    </Nav.Link>
+                                    <Nav.Link as={NavLink} to="/upload">
+                                        Upload
+                                    </Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>{' '}
+                    </Navbar>
+                    <div className="cosmae-page-body">
+                        <Outlet />
+                    </div>
+                </div>
             </Col>
         </Row>
     )
 }
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <CosmaeRoot />,
+        children: [
+            { path: '', element: <TableConnector />, index: true },
+            { path: 'upload', element: <p>UPLOAD</p> }
+        ]
+    }
+])
+
 function App() {
     return (
         <Container
             fluid
-            className=" min-vh-100 d-flex flex-column ps-0 pe-0 cosmae-container"
+            className=" min-vh-100 d-flex flex-column ps-0 pe-0 ms-0 me-0 cosmae-container"
         >
-            <LoginProvider
-                apiPath={config.api_path}
-                header={(headerProps) => <CosmaeHeader headerProps={headerProps} />}
-                body={(userInfo: UserInfo) => (
-                    <RemoteDataTable
-                        base_url={config.api_path}
-                        column_defs={userInfo.columns}
-                    />
-                )}
-            />
+            <LoginProvider body={<RouterProvider router={router} />} />
         </Container>
     )
 }
-
 export default App
+function TableConnector() {
+    return (
+        <UserContext.Consumer>
+            {(userInfoWithLogout) =>
+                userInfoWithLogout && (
+                    <RemoteDataTable
+                        column_defs={userInfoWithLogout.userInfo.columns}
+                    />
+                )
+            }
+        </UserContext.Consumer>
+    )
+}

ui/src/column_menu/__tests__/async_actions.test.ts
@@ -26,7 +26,6 @@ function responseSequence(respones: [number, () => any][]) {
     }
 }
 
-const apiPathTest = 'http://test.org'
 describe('getHierarchyAction', () => {
     beforeEach(() => {
         jest.restoreAllMocks()
@@ -44,7 +43,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest })
+        const action = new GetHierarchyAction({})
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(2)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -59,7 +58,7 @@ describe('getHierarchyAction', () => {
         jest.spyOn(global, 'fetch').mockImplementationOnce(() => {
             throw Error('fetch error')
         })
-        const action = new GetHierarchyAction({ apiPath: apiPathTest })
+        const action = new GetHierarchyAction({})
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(2)
         const actionFromCall = dispatch.mock.calls[1][0]
@@ -79,7 +78,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest })
+        const action = new GetHierarchyAction({})
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(2)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -135,7 +134,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest, expand: true })
+        const action = new GetHierarchyAction({ expand: true })
         await action.run(dispatch)
         expect(dispatch.mock.calls.length).toBe(4)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -223,7 +222,7 @@ describe('getHierarchyAction', () => {
                 }
             ]
         ])
-        const action = new GetHierarchyAction({ apiPath: apiPathTest, expand: true })
+        const action = new GetHierarchyAction({ expand: true })
         await action.run(dispatch)
         // expect(dispatch.mock.calls.length).toBe(4)
         expect(dispatch.mock.calls[0][0]).toEqual(new StartLoadingAction([]))
@@ -291,7 +290,6 @@ describe('SubmitColumnDefinition', () => {
         ])
         const dispatch = jest.fn()
         await new SubmitColumnDefinitionAction({
-            apiPath: apiPathTest,
             columnTypeIdx: 2,
             idParentPersistent: idParentPersistentTest,
             name: nameTest
@@ -302,7 +300,7 @@ describe('SubmitColumnDefinition', () => {
         ])
         expect((fetch as jest.Mock).mock.calls).toEqual([
             [
-                'http://test.org/tags/definitions',
+                'http://127.0.0.1:8000/cosmae/api/tags/definitions',
                 {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
@@ -331,7 +329,6 @@ describe('SubmitColumnDefinition', () => {
         ])
         const dispatch = jest.fn()
         await new SubmitColumnDefinitionAction({
-            apiPath: apiPathTest,
             columnTypeIdx: 2,
             idParentPersistent: idParentPersistentTest,
             name: nameTest
@@ -356,7 +353,6 @@ describe('SubmitColumnDefinition', () => {
         ])
         const dispatch = jest.fn()
         await new SubmitColumnDefinitionAction({
-            apiPath: apiPathTest,
             columnTypeIdx: 2,
             idParentPersistent: idParentPersistentTest,
             name: nameTest