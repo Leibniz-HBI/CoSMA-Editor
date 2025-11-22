Leibniz-HBI/DP-REX

Add eslint to pre-commit.

ui/src/App.tsx
@@ -1,14 +1,16 @@
-import './App.css';
-import { PersonTable } from './person_natural/components';
-import "@glideapps/glide-data-grid/dist/index.css";
+import './App.css'
+import { PersonTable } from './person_natural/components'
+import '@glideapps/glide-data-grid/dist/index.css'
 
 function App() {
-  return <div className="vran-container">
-    <div className="vran-header">VrAN</div>
-    <div className="vran-body">
-        <PersonTable />
-    </div>
-  </div>
+    return (
+        <div className="vran-container">
+            <div className="vran-header">VrAN</div>
+            <div className="vran-body">
+                <PersonTable />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App

ui/src/config.tsx
@@ -1,4 +1,3 @@
-
 export class VranConf {
     private static instance?: VranConf
     api_base: string
@@ -10,11 +9,10 @@ export class VranConf {
         if (VranConf.instance === undefined || VranConf.instance === null) {
             let base_url = process.env.VRAN_HOST
             if (base_url === undefined || base_url == null) {
-                base_url = "http://127.0.0.1:8000"
+                base_url = 'http://127.0.0.1:8000'
             }
-            VranConf.instance = new VranConf({ api_base: base_url + "/vran/api" })
+            VranConf.instance = new VranConf({ api_base: base_url + '/vran/api' })
         }
         return VranConf.instance
-
     }
 }

ui/src/layout/state.tsx
@@ -1,15 +1,18 @@
-import { FunctionComponent, ComponentClass, createElement} from "react";
+import { FunctionComponent, ComponentClass, createElement } from 'react'
 
-type ComponentHook = FunctionComponent | ComponentClass | string;
+type ComponentHook = FunctionComponent | ComponentClass | string
 const emptyObject = {}
+//eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function mkEmptyDiv(props: any) {
     return <div></div>
 }
 
 export class ComponentBuilder {
     hook: ComponentHook
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
     props: any
 
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(hook: ComponentHook, props: any) {
         this.hook = hook
         this.props = props
@@ -19,25 +22,29 @@ export class ComponentBuilder {
 export const emptyBuilder = new ComponentBuilder(mkEmptyDiv, emptyObject)
 
 export class OverlayBuilder extends ComponentBuilder {
-
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(hook: ComponentHook, props: any) {
-        super((props_inner) =>
-            <div className="vran-overlay">{createElement(hook, props_inner)}</div>, props)
+        super(
+            (props_inner) => (
+                <div className="vran-overlay">{createElement(hook, props_inner)}</div>
+            ),
+            props
+        )
     }
 }
-
-
 
 export class LayoutState {
     mainBuilder: ComponentBuilder
     overlayBuilder: ComponentBuilder
 
-    public constructor(mainBuilder: ComponentBuilder, overlayBuilder?: ComponentBuilder) {
+    public constructor(
+        mainBuilder: ComponentBuilder,
+        overlayBuilder?: ComponentBuilder
+    ) {
         this.mainBuilder = mainBuilder
         if (overlayBuilder) {
             this.overlayBuilder = overlayBuilder
-        }
-        else {
+        } else {
             this.overlayBuilder = emptyBuilder
         }
     }

ui/src/person_natural/components.tsx
@@ -1,9 +1,16 @@
-import { VranConf } from "../config";
-import { RemoteDataTable } from "../table/components";
+import { VranConf } from '../config'
+import { RemoteDataTable } from '../table/components'
+//eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export function PersonTable(props: any) {
     const columns = [
-        { id: "names_personal", title: "Personal Names" },
-        { id: "names_family", title: "Family Names" },
-        { id: "display_txt", title: "Display Text" }]
-    return <RemoteDataTable base_url={VranConf.get().api_base + "/persons"} initialColumns={columns} />
+        { id: 'names_personal', title: 'Personal Names' },
+        { id: 'names_family', title: 'Family Names' },
+        { id: 'display_txt', title: 'Display Text' }
+    ]
+    return (
+        <RemoteDataTable
+            base_url={VranConf.get().api_base + '/persons'}
+            initialColumns={columns}
+        />
+    )
 }

ui/src/table/components.test.tsx
@@ -1,79 +1,81 @@
 /**
  * @jest-environment jsdom
  */
-jest.mock("@glideapps/glide-data-grid", () => ({
-  __esmodule: true,
-  DataEditor: jest.fn().mockImplementation((props: any) => <MockTable />)
+jest.mock('@glideapps/glide-data-grid', () => ({
+    __esmodule: true,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    DataEditor: jest.fn().mockImplementation((props: any) => <MockTable />)
 }))
 import { describe } from '@jest/globals'
 import { render, screen } from '@testing-library/react'
 import { DataTable } from './components'
 import { TableState } from './state'
-import { DataEditor, Item, GridCellKind } from '@glideapps/glide-data-grid';
-
-
-function indexCell(cell: Item) {
-  const [col_idx, row_idx] = cell
-  const cell_content = `${row_idx}, ${col_idx}`
-  return {
-    kind: GridCellKind.Text,
-    allowOverlay: false,
-    displayData: cell_content,
-    data: cell_content,
-  }
-}
+import { DataEditor } from '@glideapps/glide-data-grid'
 
 const testColumns = [
-  {
-    id: "test_column_0",
-    title: "test title 0"
-  },
-  {
-    id: "test_column_1",
-    title: "test title 1"
-  }
+    {
+        id: 'test_column_0',
+        title: 'test title 0'
+    },
+    {
+        id: 'test_column_1',
+        title: 'test title 1'
+    }
 ]
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function MockTable(props: any) {
-  return <div className='mock'></div>
+    return <div className="mock"></div>
 }
 
-describe("table from state", () => {
-
-
-  test("should show error", () => {
-    const state = new TableState({ columns: [], errorMsg: "test error", isLoading: false })
-    render(<DataTable state={state} />)
-    screen.getByText("test error", { exact: false })
-  })
-  test("should show loading with empty args", () => {
-    const state = new TableState({ columns: [] })
-    const { container } = render(<DataTable state={state} />)
-    const shimmers = container.getElementsByClassName("shimmer")
-    expect(shimmers.length).toBe(1)
-  })
-  test("should show loading with loading set", () => {
-    const state = new TableState({ columns: [], isLoading: true })
-    const { container } = render(<DataTable state={state} />)
-    const shimmers = container.getElementsByClassName("shimmer")
-    expect(shimmers.length).toBe(1)
-  })
-  test("renders table", () => {
-    const state = new TableState({ columns: testColumns, row_objects: [{}, {}, {}], isLoading: false })
-    const mock_fn = () => { }
-    const { container } = render(<DataTable state={state} cellContent={mock_fn} />)
-    const outer = container.getElementsByClassName("vran-table-container-outer")
-    expect(outer.length).toBe(1)
-    const inner = outer[0].getElementsByClassName("vran-table-container-inner")
-    expect(inner.length).toBe(1)
-    const mock = inner[0].getElementsByClassName("mock")
-    expect(mock.length).toBe(1)
-    expect((DataEditor as any).mock.calls).toEqual([[{
-      rows: 3,
-      width: "100%",
-      height: "100%",
-      getCellContent: mock_fn,
-      columns: testColumns
-    }, {}]])
-  })
+describe('table from state', () => {
+    test('should show error', () => {
+        const state = new TableState({
+            columns: [],
+            errorMsg: 'test error',
+            isLoading: false
+        })
+        render(<DataTable state={state} />)
+        screen.getByText('test error', { exact: false })
+    })
+    test('should show loading with empty args', () => {
+        const state = new TableState({ columns: [] })
+        const { container } = render(<DataTable state={state} />)
+        const shimmers = container.getElementsByClassName('shimmer')
+        expect(shimmers.length).toBe(1)
+    })
+    test('should show loading with loading set', () => {
+        const state = new TableState({ columns: [], isLoading: true })
+        const { container } = render(<DataTable state={state} />)
+        const shimmers = container.getElementsByClassName('shimmer')
+        expect(shimmers.length).toBe(1)
+    })
+    test('renders table', () => {
+        const state = new TableState({
+            columns: testColumns,
+            row_objects: [{}, {}, {}],
+            isLoading: false
+        })
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const mock_fn = () => {}
+        const { container } = render(<DataTable state={state} cellContent={mock_fn} />)
+        const outer = container.getElementsByClassName('vran-table-container-outer')
+        expect(outer.length).toBe(1)
+        const inner = outer[0].getElementsByClassName('vran-table-container-inner')
+        expect(inner.length).toBe(1)
+        const mock = inner[0].getElementsByClassName('mock')
+        expect(mock.length).toBe(1)
+        expect((DataEditor as unknown as jest.Mock).mock.calls).toEqual([
+            [
+                {
+                    rows: 3,
+                    width: '100%',
+                    height: '100%',
+                    getCellContent: mock_fn,
+                    columns: testColumns
+                },
+                {}
+            ]
+        ])
+    })
 })

ui/src/table/components.tsx
@@ -1,58 +1,75 @@
-import { DataEditor, GridCell, GridCellKind, Item } from "@glideapps/glide-data-grid";
-import { useEffect, useCallback } from "react";
-import { tableReducer } from "./reducer";
-import { TableState } from "./state";
-import { GetTableAsyncAction } from "./async_actions"
-import { useThunkReducer } from "../util/state"
+import { DataEditor, GridCell, GridCellKind, Item } from '@glideapps/glide-data-grid'
+import { useEffect, useCallback } from 'react'
+import { tableReducer } from './reducer'
+import { TableState } from './state'
+import { GetTableAsyncAction } from './async_actions'
+import { useThunkReducer } from '../util/state'
 
-
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function RemoteDataTable(props: any) {
     const [state, dispatch] = useThunkReducer(
-        tableReducer, new TableState({ columns: props.initialColumns }))
+        tableReducer,
+        new TableState({ columns: props.initialColumns })
+    )
     useEffect(() => {
         dispatch(new GetTableAsyncAction(props.base_url))
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    const cellContent = useCallback((cell: Item): GridCell => {
-        const [col_idx, row_idx] = cell
-        const row = state.row_objects![row_idx]
-        let cell_content = row[state.columns[col_idx].id as string]
-        if (cell_content === undefined || cell_content === null) {
-            cell_content = ""
-        }
-        return {
-            kind: GridCellKind.Text,
-            allowOverlay: false,
-            displayData: cell_content,
-            data: cell_content,
-        }
-    }, [state.row_objects])
+    const cellContent = useCallback(
+        (cell: Item): GridCell => {
+            const [col_idx, row_idx] = cell
+            const row = state.row_objects?.[row_idx]
+            let cell_content = row[state.columns[col_idx].id as string]
+            if (cell_content === undefined || cell_content === null) {
+                cell_content = ''
+            }
+            return {
+                kind: GridCellKind.Text,
+                allowOverlay: false,
+                displayData: cell_content,
+                data: cell_content
+            }
+        },
+        [state.row_objects, state.columns]
+    )
     return <DataTable state={state} cellContent={cellContent} />
-
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function DataTable(props: any) {
     const state = props.state
     if (state.isLoading === undefined || state.isLoading === null || state.isLoading) {
-        return <div className="vran-table-container-outer">
-            <div className="vran-table-container-inner">
-                <div className="shimmer"></div>
+        return (
+            <div className="vran-table-container-outer">
+                <div className="vran-table-container-inner">
+                    <div className="shimmer"></div>
+                </div>
             </div>
-        </div>
-    } else if (!(state.errorMsg === undefined || state.errorMsg === null || state.errorMsg === "")) {
-        return <div><p>Error: {state.errorMsg}</p></div>
-
+        )
+    } else if (
+        !(
+            state.errorMsg === undefined ||
+            state.errorMsg === null ||
+            state.errorMsg === ''
+        )
+    ) {
+        return (
+            <div>
+                <p>Error: {state.errorMsg}</p>
+            </div>
+        )
     } else {
-        return <div className="vran-table-container-outer">
-            <div className="vran-table-container-inner">
-                <DataEditor
-                    rows={state.row_objects!.length}
-                    columns={state.columns}
-                    getCellContent={props.cellContent}
-                    width="100%"
-                    height="100%"
-                />
-            </div >
-        </div >
+        return (
+            <div className="vran-table-container-outer">
+                <div className="vran-table-container-inner">
+                    <DataEditor
+                        rows={state.row_objects?.length ?? 0}
+                        columns={state.columns}
+                        getCellContent={props.cellContent}
+                        width="100%"
+                        height="100%"
+                    />
+                </div>
+            </div>
+        )
     }
 }

Leibniz-HBI/CoSMA-Editor

ui/src/App.tsx
@@ -1,14 +1,16 @@
-import './App.css';
-import { PersonTable } from './person_natural/components';
-import "@glideapps/glide-data-grid/dist/index.css";
+import './App.css'
+import { PersonTable } from './person_natural/components'
+import '@glideapps/glide-data-grid/dist/index.css'
 
 function App() {
-  return <div className="cosmae-container">
-    <div className="cosmae-header">CoSMA-E</div>
-    <div className="cosmae-body">
-        <PersonTable />
-    </div>
-  </div>
+    return (
+        <div className="cosmae-container">
+            <div className="cosmae-header">CoSMA-E</div>
+            <div className="cosmae-body">
+                <PersonTable />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App

ui/src/config.tsx
@@ -1,4 +1,3 @@
-
 export class CosmaeConf {
     private static instance?: CosmaeConf
     api_base: string
@@ -10,11 +9,10 @@ export class CosmaeConf {
         if (CosmaeConf.instance === undefined || CosmaeConf.instance === null) {
             let base_url = process.env.COSMAE_HOST
             if (base_url === undefined || base_url == null) {
-                base_url = "http://127.0.0.1:8000"
+                base_url = 'http://127.0.0.1:8000'
             }
-            CosmaeConf.instance = new CosmaeConf({ api_base: base_url + "/cosmae/api" })
+            CosmaeConf.instance = new CosmaeConf({ api_base: base_url + '/cosmae/api' })
         }
         return CosmaeConf.instance
-
     }
 }

ui/src/layout/state.tsx
@@ -1,15 +1,18 @@
-import { FunctionComponent, ComponentClass, createElement} from "react";
+import { FunctionComponent, ComponentClass, createElement } from 'react'
 
-type ComponentHook = FunctionComponent | ComponentClass | string;
+type ComponentHook = FunctionComponent | ComponentClass | string
 const emptyObject = {}
+//eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function mkEmptyDiv(props: any) {
     return <div></div>
 }
 
 export class ComponentBuilder {
     hook: ComponentHook
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
     props: any
 
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(hook: ComponentHook, props: any) {
         this.hook = hook
         this.props = props
@@ -19,25 +22,29 @@ export class ComponentBuilder {
 export const emptyBuilder = new ComponentBuilder(mkEmptyDiv, emptyObject)
 
 export class OverlayBuilder extends ComponentBuilder {
-
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(hook: ComponentHook, props: any) {
-        super((props_inner) =>
-            <div className="cosmae-overlay">{createElement(hook, props_inner)}</div>, props)
+        super(
+            (props_inner) => (
+                <div className="cosmae-overlay">{createElement(hook, props_inner)}</div>
+            ),
+            props
+        )
     }
 }
-
-
 
 export class LayoutState {
     mainBuilder: ComponentBuilder
     overlayBuilder: ComponentBuilder
 
-    public constructor(mainBuilder: ComponentBuilder, overlayBuilder?: ComponentBuilder) {
+    public constructor(
+        mainBuilder: ComponentBuilder,
+        overlayBuilder?: ComponentBuilder
+    ) {
         this.mainBuilder = mainBuilder
         if (overlayBuilder) {
             this.overlayBuilder = overlayBuilder
-        }
-        else {
+        } else {
             this.overlayBuilder = emptyBuilder
         }
     }

ui/src/person_natural/components.tsx
@@ -1,9 +1,16 @@
-import { CosmaeConf } from "../config";
-import { RemoteDataTable } from "../table/components";
+import { CosmaeConf } from '../config'
+import { RemoteDataTable } from '../table/components'
+//eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export function PersonTable(props: any) {
     const columns = [
-        { id: "names_personal", title: "Personal Names" },
-        { id: "names_family", title: "Family Names" },
-        { id: "display_txt", title: "Display Text" }]
-    return <RemoteDataTable base_url={CosmaeConf.get().api_base + "/persons"} initialColumns={columns} />
+        { id: 'names_personal', title: 'Personal Names' },
+        { id: 'names_family', title: 'Family Names' },
+        { id: 'display_txt', title: 'Display Text' }
+    ]
+    return (
+        <RemoteDataTable
+            base_url={CosmaeConf.get().api_base + '/persons'}
+            initialColumns={columns}
+        />
+    )
 }

ui/src/table/components.test.tsx
@@ -1,79 +1,81 @@
 /**
  * @jest-environment jsdom
  */
-jest.mock("@glideapps/glide-data-grid", () => ({
-  __esmodule: true,
-  DataEditor: jest.fn().mockImplementation((props: any) => <MockTable />)
+jest.mock('@glideapps/glide-data-grid', () => ({
+    __esmodule: true,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    DataEditor: jest.fn().mockImplementation((props: any) => <MockTable />)
 }))
 import { describe } from '@jest/globals'
 import { render, screen } from '@testing-library/react'
 import { DataTable } from './components'
 import { TableState } from './state'
-import { DataEditor, Item, GridCellKind } from '@glideapps/glide-data-grid';
-
-
-function indexCell(cell: Item) {
-  const [col_idx, row_idx] = cell
-  const cell_content = `${row_idx}, ${col_idx}`
-  return {
-    kind: GridCellKind.Text,
-    allowOverlay: false,
-    displayData: cell_content,
-    data: cell_content,
-  }
-}
+import { DataEditor } from '@glideapps/glide-data-grid'
 
 const testColumns = [
-  {
-    id: "test_column_0",
-    title: "test title 0"
-  },
-  {
-    id: "test_column_1",
-    title: "test title 1"
-  }
+    {
+        id: 'test_column_0',
+        title: 'test title 0'
+    },
+    {
+        id: 'test_column_1',
+        title: 'test title 1'
+    }
 ]
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function MockTable(props: any) {
-  return <div className='mock'></div>
+    return <div className="mock"></div>
 }
 
-describe("table from state", () => {
-
-
-  test("should show error", () => {
-    const state = new TableState({ columns: [], errorMsg: "test error", isLoading: false })
-    render(<DataTable state={state} />)
-    screen.getByText("test error", { exact: false })
-  })
-  test("should show loading with empty args", () => {
-    const state = new TableState({ columns: [] })
-    const { container } = render(<DataTable state={state} />)
-    const shimmers = container.getElementsByClassName("shimmer")
-    expect(shimmers.length).toBe(1)
-  })
-  test("should show loading with loading set", () => {
-    const state = new TableState({ columns: [], isLoading: true })
-    const { container } = render(<DataTable state={state} />)
-    const shimmers = container.getElementsByClassName("shimmer")
-    expect(shimmers.length).toBe(1)
-  })
-  test("renders table", () => {
-    const state = new TableState({ columns: testColumns, row_objects: [{}, {}, {}], isLoading: false })
-    const mock_fn = () => { }
-    const { container } = render(<DataTable state={state} cellContent={mock_fn} />)
-    const outer = container.getElementsByClassName("cosmae-table-container-outer")
-    expect(outer.length).toBe(1)
-    const inner = outer[0].getElementsByClassName("cosmae-table-container-inner")
-    expect(inner.length).toBe(1)
-    const mock = inner[0].getElementsByClassName("mock")
-    expect(mock.length).toBe(1)
-    expect((DataEditor as any).mock.calls).toEqual([[{
-      rows: 3,
-      width: "100%",
-      height: "100%",
-      getCellContent: mock_fn,
-      columns: testColumns
-    }, {}]])
-  })
+describe('table from state', () => {
+    test('should show error', () => {
+        const state = new TableState({
+            columns: [],
+            errorMsg: 'test error',
+            isLoading: false
+        })
+        render(<DataTable state={state} />)
+        screen.getByText('test error', { exact: false })
+    })
+    test('should show loading with empty args', () => {
+        const state = new TableState({ columns: [] })
+        const { container } = render(<DataTable state={state} />)
+        const shimmers = container.getElementsByClassName('shimmer')
+        expect(shimmers.length).toBe(1)
+    })
+    test('should show loading with loading set', () => {
+        const state = new TableState({ columns: [], isLoading: true })
+        const { container } = render(<DataTable state={state} />)
+        const shimmers = container.getElementsByClassName('shimmer')
+        expect(shimmers.length).toBe(1)
+    })
+    test('renders table', () => {
+        const state = new TableState({
+            columns: testColumns,
+            row_objects: [{}, {}, {}],
+            isLoading: false
+        })
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const mock_fn = () => {}
+        const { container } = render(<DataTable state={state} cellContent={mock_fn} />)
+        const outer = container.getElementsByClassName('cosmae-table-container-outer')
+        expect(outer.length).toBe(1)
+        const inner = outer[0].getElementsByClassName('cosmae-table-container-inner')
+        expect(inner.length).toBe(1)
+        const mock = inner[0].getElementsByClassName('mock')
+        expect(mock.length).toBe(1)
+        expect((DataEditor as unknown as jest.Mock).mock.calls).toEqual([
+            [
+                {
+                    rows: 3,
+                    width: '100%',
+                    height: '100%',
+                    getCellContent: mock_fn,
+                    columns: testColumns
+                },
+                {}
+            ]
+        ])
+    })
 })

ui/src/table/components.tsx
@@ -1,58 +1,75 @@
-import { DataEditor, GridCell, GridCellKind, Item } from "@glideapps/glide-data-grid";
-import { useEffect, useCallback } from "react";
-import { tableReducer } from "./reducer";
-import { TableState } from "./state";
-import { GetTableAsyncAction } from "./async_actions"
-import { useThunkReducer } from "../util/state"
+import { DataEditor, GridCell, GridCellKind, Item } from '@glideapps/glide-data-grid'
+import { useEffect, useCallback } from 'react'
+import { tableReducer } from './reducer'
+import { TableState } from './state'
+import { GetTableAsyncAction } from './async_actions'
+import { useThunkReducer } from '../util/state'
 
-
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function RemoteDataTable(props: any) {
     const [state, dispatch] = useThunkReducer(
-        tableReducer, new TableState({ columns: props.initialColumns }))
+        tableReducer,
+        new TableState({ columns: props.initialColumns })
+    )
     useEffect(() => {
         dispatch(new GetTableAsyncAction(props.base_url))
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    const cellContent = useCallback((cell: Item): GridCell => {
-        const [col_idx, row_idx] = cell
-        const row = state.row_objects![row_idx]
-        let cell_content = row[state.columns[col_idx].id as string]
-        if (cell_content === undefined || cell_content === null) {
-            cell_content = ""
-        }
-        return {
-            kind: GridCellKind.Text,
-            allowOverlay: false,
-            displayData: cell_content,
-            data: cell_content,
-        }
-    }, [state.row_objects])
+    const cellContent = useCallback(
+        (cell: Item): GridCell => {
+            const [col_idx, row_idx] = cell
+            const row = state.row_objects?.[row_idx]
+            let cell_content = row[state.columns[col_idx].id as string]
+            if (cell_content === undefined || cell_content === null) {
+                cell_content = ''
+            }
+            return {
+                kind: GridCellKind.Text,
+                allowOverlay: false,
+                displayData: cell_content,
+                data: cell_content
+            }
+        },
+        [state.row_objects, state.columns]
+    )
     return <DataTable state={state} cellContent={cellContent} />
-
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function DataTable(props: any) {
     const state = props.state
     if (state.isLoading === undefined || state.isLoading === null || state.isLoading) {
-        return <div className="cosmae-table-container-outer">
-            <div className="cosmae-table-container-inner">
-                <div className="shimmer"></div>
+        return (
+            <div className="cosmae-table-container-outer">
+                <div className="cosmae-table-container-inner">
+                    <div className="shimmer"></div>
+                </div>
             </div>
-        </div>
-    } else if (!(state.errorMsg === undefined || state.errorMsg === null || state.errorMsg === "")) {
-        return <div><p>Error: {state.errorMsg}</p></div>
-
+        )
+    } else if (
+        !(
+            state.errorMsg === undefined ||
+            state.errorMsg === null ||
+            state.errorMsg === ''
+        )
+    ) {
+        return (
+            <div>
+                <p>Error: {state.errorMsg}</p>
+            </div>
+        )
     } else {
-        return <div className="cosmae-table-container-outer">
-            <div className="cosmae-table-container-inner">
-                <DataEditor
-                    rows={state.row_objects!.length}
-                    columns={state.columns}
-                    getCellContent={props.cellContent}
-                    width="100%"
-                    height="100%"
-                />
-            </div >
-        </div >
+        return (
+            <div className="cosmae-table-container-outer">
+                <div className="cosmae-table-container-inner">
+                    <DataEditor
+                        rows={state.row_objects?.length ?? 0}
+                        columns={state.columns}
+                        getCellContent={props.cellContent}
+                        width="100%"
+                        height="100%"
+                    />
+                </div>
+            </div>
+        )
     }
 }