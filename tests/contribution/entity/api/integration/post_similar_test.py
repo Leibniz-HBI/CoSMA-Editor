Leibniz-HBI/DP-REX

Add endpoint for adding duplicate information to backend.

tests/contribution/entity/api/integration/post_similar_test.py
@@ -2,9 +2,12 @@
 from unittest.mock import MagicMock, patch
 from uuid import uuid4
 
+import pytest
+
 import tests.contribution.entity.api.requests as r
 import tests.contribution.entity.common as c
 import tests.entity.common as ce
+from vran.contribution.entity.models_django import EntityDuplicate
 
 
 def test_no_cookies(auth_server):
@@ -40,9 +43,10 @@ def test_unknown_entities(auth_server, contribution_candidate):
     assert rsp.json() == {"msg": "Entity does not exist."}
 
 
-def test_similar_entities(auth_server, contribution_candidate, entities):
-    similarMock = MagicMock()
-    similarMock.return_value = [
+@pytest.fixture
+def similar_mock():
+    mock = MagicMock()
+    mock.return_value = [
         {
             "similarity": 0.95,
             "id_persistent": ce.id_persistent_test_0,
@@ -56,8 +60,14 @@ def test_similar_entities(auth_server, contribution_candidate, entities):
             "id": 1,
         },
     ]
+    return mock
+
+
+def test_similar_entities_no_duplicate(
+    auth_server, contribution_candidate, entities, similar_mock
+):
     live_server, cookies = auth_server
-    with patch("vran.contribution.entity.api.find_matches", similarMock):
+    with patch("vran.contribution.entity.api.find_matches", similar_mock):
         rsp = r.post_similar(
             live_server.url,
             contribution_candidate.id_persistent,
@@ -67,23 +77,74 @@ def test_similar_entities(auth_server, contribution_candidate, entities):
     assert rsp.status_code == 200
     assert rsp.json() == {
         "matches": {
-            c.id_persistent_entity_duplicate_test: [
-                {
-                    "similarity": 0.95,
-                    "entity": {
-                        "id_persistent": ce.id_persistent_test_0,
-                        "display_txt": ce.display_txt_test0,
-                        "version": 0,
+            c.id_persistent_entity_duplicate_test: {
+                "matches": [
+                    {
+                        "similarity": 0.95,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_0,
+                            "display_txt": ce.display_txt_test0,
+                            "version": 0,
+                        },
                     },
-                },
-                {
-                    "similarity": 0.90,
-                    "entity": {
-                        "id_persistent": ce.id_persistent_test_1,
-                        "display_txt": ce.display_txt_test1,
-                        "version": 1,
+                    {
+                        "similarity": 0.90,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_1,
+                            "display_txt": ce.display_txt_test1,
+                            "version": 1,
+                        },
                     },
+                ],
+                "assigned_duplicate": None,
+            }
+        }
+    }
+
+
+def test_similar_entities_with_duplicate(
+    auth_server, contribution_candidate, entities, similar_mock
+):
+    live_server, cookies = auth_server
+    EntityDuplicate.objects.create(  # pylint: disable=no-member
+        id_origin_persistent=entities[2].id_persistent,
+        id_destination_persistent=entities[1].id_persistent,
+        contribution_candidate=contribution_candidate,
+    )
+    with patch("vran.contribution.entity.api.find_matches", similar_mock):
+        rsp = r.post_similar(
+            live_server.url,
+            contribution_candidate.id_persistent,
+            [c.id_persistent_entity_duplicate_test],
+            cookies,
+        )
+    assert rsp.status_code == 200
+    assert rsp.json() == {
+        "matches": {
+            c.id_persistent_entity_duplicate_test: {
+                "matches": [
+                    {
+                        "similarity": 0.95,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_0,
+                            "display_txt": ce.display_txt_test0,
+                            "version": 0,
+                        },
+                    },
+                    {
+                        "similarity": 0.90,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_1,
+                            "display_txt": ce.display_txt_test1,
+                            "version": 1,
+                        },
+                    },
+                ],
+                "assigned_duplicate": {
+                    "id_persistent": ce.id_persistent_test_1,
+                    "display_txt": ce.display_txt_test1,
+                    "version": entities[1].id,
                 },
-            ]
+            }
         }
     }

Leibniz-HBI/CoSMA-Editor

tests/contribution/entity/api/integration/post_similar_test.py
@@ -2,9 +2,12 @@
 from unittest.mock import MagicMock, patch
 from uuid import uuid4
 
+import pytest
+
 import tests.contribution.entity.api.requests as r
 import tests.contribution.entity.common as c
 import tests.entity.common as ce
+from cosmae.contribution.entity.models_django import EntityDuplicate
 
 
 def test_no_cookies(auth_server):
@@ -40,9 +43,10 @@ def test_unknown_entities(auth_server, contribution_candidate):
     assert rsp.json() == {"msg": "Entity does not exist."}
 
 
-def test_similar_entities(auth_server, contribution_candidate, entities):
-    similarMock = MagicMock()
-    similarMock.return_value = [
+@pytest.fixture
+def similar_mock():
+    mock = MagicMock()
+    mock.return_value = [
         {
             "similarity": 0.95,
             "id_persistent": ce.id_persistent_test_0,
@@ -56,8 +60,14 @@ def test_similar_entities(auth_server, contribution_candidate, entities):
             "id": 1,
         },
     ]
+    return mock
+
+
+def test_similar_entities_no_duplicate(
+    auth_server, contribution_candidate, entities, similar_mock
+):
     live_server, cookies = auth_server
-    with patch("cosmae.contribution.entity.api.find_matches", similarMock):
+    with patch("cosmae.contribution.entity.api.find_matches", similar_mock):
         rsp = r.post_similar(
             live_server.url,
             contribution_candidate.id_persistent,
@@ -67,23 +77,74 @@ def test_similar_entities(auth_server, contribution_candidate, entities):
     assert rsp.status_code == 200
     assert rsp.json() == {
         "matches": {
-            c.id_persistent_entity_duplicate_test: [
-                {
-                    "similarity": 0.95,
-                    "entity": {
-                        "id_persistent": ce.id_persistent_test_0,
-                        "display_txt": ce.display_txt_test0,
-                        "version": 0,
+            c.id_persistent_entity_duplicate_test: {
+                "matches": [
+                    {
+                        "similarity": 0.95,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_0,
+                            "display_txt": ce.display_txt_test0,
+                            "version": 0,
+                        },
                     },
-                },
-                {
-                    "similarity": 0.90,
-                    "entity": {
-                        "id_persistent": ce.id_persistent_test_1,
-                        "display_txt": ce.display_txt_test1,
-                        "version": 1,
+                    {
+                        "similarity": 0.90,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_1,
+                            "display_txt": ce.display_txt_test1,
+                            "version": 1,
+                        },
                     },
+                ],
+                "assigned_duplicate": None,
+            }
+        }
+    }
+
+
+def test_similar_entities_with_duplicate(
+    auth_server, contribution_candidate, entities, similar_mock
+):
+    live_server, cookies = auth_server
+    EntityDuplicate.objects.create(  # pylint: disable=no-member
+        id_origin_persistent=entities[2].id_persistent,
+        id_destination_persistent=entities[1].id_persistent,
+        contribution_candidate=contribution_candidate,
+    )
+    with patch("cosmae.contribution.entity.api.find_matches", similar_mock):
+        rsp = r.post_similar(
+            live_server.url,
+            contribution_candidate.id_persistent,
+            [c.id_persistent_entity_duplicate_test],
+            cookies,
+        )
+    assert rsp.status_code == 200
+    assert rsp.json() == {
+        "matches": {
+            c.id_persistent_entity_duplicate_test: {
+                "matches": [
+                    {
+                        "similarity": 0.95,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_0,
+                            "display_txt": ce.display_txt_test0,
+                            "version": 0,
+                        },
+                    },
+                    {
+                        "similarity": 0.90,
+                        "entity": {
+                            "id_persistent": ce.id_persistent_test_1,
+                            "display_txt": ce.display_txt_test1,
+                            "version": 1,
+                        },
+                    },
+                ],
+                "assigned_duplicate": {
+                    "id_persistent": ce.id_persistent_test_1,
+                    "display_txt": ce.display_txt_test1,
+                    "version": entities[1].id,
                 },
-            ]
+            }
         }
     }