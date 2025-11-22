Leibniz-HBI/DP-REX

Define containers and their orchestration.

vran/apps.py
@@ -3,6 +3,7 @@ import logging
 from typing import List
 
 from django.apps import AppConfig, apps
+from django.db.backends.signals import connection_created
 from django.db.models.signals import post_migrate
 
 logger = logging.getLogger("vran.app_config")
@@ -59,6 +60,27 @@ def add_permissions(
             return
 
 
+def add_superuser(
+    sender, connection, verbosity=2, **kwargs
+):  # pylint: disable=unused-argument
+    "Add superuser if no users exist"
+    try:
+        user_model = apps.get_model("vran", "vranuser")
+        if user_model.objects.count() == 0:
+            username = "admin"
+            email = "mail@test.url"
+            password = "changeme"
+            print(f"Creating account for {username} ({email})")
+            admin = user_model.objects.create_superuser(
+                email=email, username=username, password=password
+            )
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+    except Exception:  # pylint: disable=broad-except
+        pass
+
+
 class VranConfig(AppConfig):
     """Configuration for the VrAN Django app"""
 
@@ -67,4 +89,7 @@ class VranConfig(AppConfig):
 
     def ready(self) -> None:
         post_migrate.connect(add_permissions, dispatch_uid="vran.create_groups")
+        connection_created.connect(
+            add_superuser, dispatch_uid="vran.create_initial_superuser"
+        )
         super().ready()

Leibniz-HBI/CoSMA-Editor

cosmae/apps.py
@@ -3,6 +3,7 @@ import logging
 from typing import List
 
 from django.apps import AppConfig, apps
+from django.db.backends.signals import connection_created
 from django.db.models.signals import post_migrate
 
 logger = logging.getLogger("cosmae.app_config")
@@ -59,12 +60,36 @@ def add_permissions(
             return
 
 
+def add_superuser(
+    sender, connection, verbosity=2, **kwargs
+):  # pylint: disable=unused-argument
+    "Add superuser if no users exist"
+    try:
+        user_model = apps.get_model("cosmae", "cosmaeuser")
+        if user_model.objects.count() == 0:
+            username = "admin"
+            email = "mail@test.url"
+            password = "changeme"
+            print(f"Creating account for {username} ({email})")
+            admin = user_model.objects.create_superuser(
+                email=email, username=username, password=password
+            )
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+    except Exception:  # pylint: disable=broad-except
+        pass
+
+
 class CosmaeConfig(AppConfig):
-    """Configuration for the CoSMA-E Django app"""
+    """Configuration for the CoSMA-Editor Django app"""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "cosmae"
 
     def ready(self) -> None:
         post_migrate.connect(add_permissions, dispatch_uid="cosmae.create_groups")
+        connection_created.connect(
+            add_superuser, dispatch_uid="cosmae.create_initial_superuser"
+        )
         super().ready()