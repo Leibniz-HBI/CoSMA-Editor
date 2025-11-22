Leibniz-HBI/DP-REX

Add backend functionality for resolving merge requests.

vran/migrations/0016_alter_mergerequest_state_conflictresolution.py
@@ -1,0 +1,92 @@
+# Generated by Django 4.2.2 on 2023-07-21 09:43
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0015_alter_mergerequest_contribution_candidate"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="mergerequest",
+            name="state",
+            field=models.TextField(
+                choices=[
+                    ("OPN", "open"),
+                    ("CNF", "conflicts"),
+                    ("CLS", "closed"),
+                    ("RSL", "resolved"),
+                    ("MRG", "merged"),
+                    ("ERR", "error"),
+                ],
+                default="OPN",
+                max_length=3,
+            ),
+        ),
+        migrations.CreateModel(
+            name="ConflictResolution",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("replace", models.BooleanField()),
+                (
+                    "entity",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="vran.entity",
+                    ),
+                ),
+                (
+                    "merge_request",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.mergerequest",
+                    ),
+                ),
+                (
+                    "tag_definition_destination",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="vran.tagdefinition",
+                    ),
+                ),
+                (
+                    "tag_instance_destination",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="vran.taginstance",
+                    ),
+                ),
+                (
+                    "tag_definition_origin",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="vran.tagdefinition",
+                    ),
+                ),
+                (
+                    "tag_instance_origin",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="vran.taginstance",
+                    ),
+                ),
+            ],
+        ),
+    ]

vran/migrations/0017_alter_conflictresolution_tag_instance_destination.py
@@ -1,0 +1,25 @@
+# Generated by Django 4.2.2 on 2023-07-31 15:53
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0016_alter_mergerequest_state_conflictresolution"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="conflictresolution",
+            name="tag_instance_destination",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="+",
+                to="vran.taginstance",
+            ),
+        ),
+    ]

tests/merge_request/api/integration/requests.py
@@ -4,3 +4,11 @@ import requests
 
 def get_merge_requests(url, cookies=None):
     return requests.get(url + "/vran/api/merge_requests", cookies=cookies, timeout=900)
+
+
+def get_conflicts(url, id_merge_request_persistent, cookies=None):
+    return requests.get(
+        url + f"/vran/api/merge_requests/{id_merge_request_persistent}/conflicts",
+        cookies=cookies,
+        timeout=900,
+    )

tests/merge_request/models_django_test.py
@@ -1,5 +1,8 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
-from vran.merge_request.models_django import MergeRequest
+from datetime import datetime
+
+from vran.entity.models_django import Entity
+from vran.merge_request.models_django import ConflictResolution, MergeRequest
 
 
 def test_created_by_user(user, merge_request_user, merge_request_user1):
@@ -10,3 +13,127 @@ def test_created_by_user(user, merge_request_user, merge_request_user1):
 def test_assigned_to_user(user, merge_request_user, merge_request_user1):
     mr = MergeRequest.assigned_to_user(user).get()
     assert str(mr.id_persistent) == str(merge_request_user.id_persistent)
+
+
+def test_non_recent_no_change(merge_request_user, conflict_resolution_replace):
+    non_recent = ConflictResolution.non_recent()
+    assert len(non_recent) == 0
+
+
+def test_recent_no_change(merge_request_user, conflict_resolution_replace):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 1
+
+
+def test_includes_no_value_at_destination(
+    merge_request_user, instances_merge_request_origin_user
+):
+    conflicts = list(merge_request_user.instance_conflicts_all())
+    assert len(conflicts) == 2
+
+
+def test_non_recent_change_entity(
+    merge_request_user, entity1_changed, conflict_resolution_replace
+):
+    non_recent = ConflictResolution.non_recent()
+    assert len(non_recent) == 1
+
+
+def test_recent_change_entity(
+    merge_request_user, entity1_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_recent_change_definition_origin(
+    merge_request_user, origin_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_definition_origin(
+    merge_request_user, origin_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_definition_destination(
+    merge_request_user, destination_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_definition_destination(
+    merge_request_user, destination_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_instance_destination(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_instance_destination(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_instance_origin(
+    merge_request_user,
+    instance_merge_request_origin_user_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_instance_origin(
+    merge_request_user,
+    instance_merge_request_origin_user_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_all(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    destination_tag_def_for_mr_changed,
+    conflict_resolution_replace,
+):
+    old_entity = conflict_resolution_replace.entity
+    Entity.change_or_create(
+        id_persistent=old_entity.id_persistent,
+        time_edit=datetime(1912, 4, 7),
+        display_txt="edited_entity",
+        version=old_entity.id,
+    )[0].save()
+
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_all(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    destination_tag_def_for_mr_changed,
+    entity1_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1

tests/merge_request/queue_test.py
@@ -1,0 +1,64 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+import vran.merge_request.queue as q
+from vran.merge_request.models_django import MergeRequest
+
+
+def test_fast_forward_destination_empty(
+    merge_request_user_fast_forward, instances_merge_request_origin_user
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.MERGED
+
+
+def test_fast_forward_origin_empty(
+    merge_request_user_fast_forward, instance_merge_request_destination_user_no_conflict
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.MERGED
+
+
+def test_fast_forward_no_value(
+    merge_request_user_fast_forward,
+    instances_merge_request_origin_user,
+    instance_merge_request_destination_user_no_conflict_fast_forward,
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.CONFLICTS
+
+
+def test_fast_forward_conflict(
+    merge_request_user_fast_forward,
+    instances_merge_request_origin_user,
+    instance_merge_request_destination_user_conflict_fast_forward,
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.CONFLICTS
+
+
+def test_fast_forward_no_conflict_same_value(
+    merge_request_user_fast_forward,
+    instances_merge_request_origin_user,
+    instance_merge_request_destination_user_same_value,
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.MERGED

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0016_alter_mergerequest_state_conflictresolution.py
@@ -1,0 +1,92 @@
+# Generated by Django 4.2.2 on 2023-07-21 09:43
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0015_alter_mergerequest_contribution_candidate"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="mergerequest",
+            name="state",
+            field=models.TextField(
+                choices=[
+                    ("OPN", "open"),
+                    ("CNF", "conflicts"),
+                    ("CLS", "closed"),
+                    ("RSL", "resolved"),
+                    ("MRG", "merged"),
+                    ("ERR", "error"),
+                ],
+                default="OPN",
+                max_length=3,
+            ),
+        ),
+        migrations.CreateModel(
+            name="ConflictResolution",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("replace", models.BooleanField()),
+                (
+                    "entity",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="cosmae.entity",
+                    ),
+                ),
+                (
+                    "merge_request",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.mergerequest",
+                    ),
+                ),
+                (
+                    "tag_definition_destination",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="cosmae.tagdefinition",
+                    ),
+                ),
+                (
+                    "tag_instance_destination",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="cosmae.taginstance",
+                    ),
+                ),
+                (
+                    "tag_definition_origin",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="cosmae.tagdefinition",
+                    ),
+                ),
+                (
+                    "tag_instance_origin",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to="cosmae.taginstance",
+                    ),
+                ),
+            ],
+        ),
+    ]

cosmae/migrations/0017_alter_conflictresolution_tag_instance_destination.py
@@ -1,0 +1,25 @@
+# Generated by Django 4.2.2 on 2023-07-31 15:53
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0016_alter_mergerequest_state_conflictresolution"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="conflictresolution",
+            name="tag_instance_destination",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="+",
+                to="cosmae.taginstance",
+            ),
+        ),
+    ]

tests/merge_request/api/integration/requests.py
@@ -4,3 +4,11 @@ import requests
 
 def get_merge_requests(url, cookies=None):
     return requests.get(url + "/cosmae/api/merge_requests", cookies=cookies, timeout=900)
+
+
+def get_conflicts(url, id_merge_request_persistent, cookies=None):
+    return requests.get(
+        url + f"/cosmae/api/merge_requests/{id_merge_request_persistent}/conflicts",
+        cookies=cookies,
+        timeout=900,
+    )

tests/merge_request/models_django_test.py
@@ -1,5 +1,8 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
-from cosmae.merge_request.models_django import MergeRequest
+from datetime import datetime
+
+from cosmae.entity.models_django import Entity
+from cosmae.merge_request.models_django import ConflictResolution, MergeRequest
 
 
 def test_created_by_user(user, merge_request_user, merge_request_user1):
@@ -10,3 +13,127 @@ def test_created_by_user(user, merge_request_user, merge_request_user1):
 def test_assigned_to_user(user, merge_request_user, merge_request_user1):
     mr = MergeRequest.assigned_to_user(user).get()
     assert str(mr.id_persistent) == str(merge_request_user.id_persistent)
+
+
+def test_non_recent_no_change(merge_request_user, conflict_resolution_replace):
+    non_recent = ConflictResolution.non_recent()
+    assert len(non_recent) == 0
+
+
+def test_recent_no_change(merge_request_user, conflict_resolution_replace):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 1
+
+
+def test_includes_no_value_at_destination(
+    merge_request_user, instances_merge_request_origin_user
+):
+    conflicts = list(merge_request_user.instance_conflicts_all())
+    assert len(conflicts) == 2
+
+
+def test_non_recent_change_entity(
+    merge_request_user, entity1_changed, conflict_resolution_replace
+):
+    non_recent = ConflictResolution.non_recent()
+    assert len(non_recent) == 1
+
+
+def test_recent_change_entity(
+    merge_request_user, entity1_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_recent_change_definition_origin(
+    merge_request_user, origin_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_definition_origin(
+    merge_request_user, origin_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_definition_destination(
+    merge_request_user, destination_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_definition_destination(
+    merge_request_user, destination_tag_def_for_mr_changed, conflict_resolution_replace
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_instance_destination(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_instance_destination(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_instance_origin(
+    merge_request_user,
+    instance_merge_request_origin_user_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_instance_origin(
+    merge_request_user,
+    instance_merge_request_origin_user_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1
+
+
+def test_recent_change_all(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    destination_tag_def_for_mr_changed,
+    conflict_resolution_replace,
+):
+    old_entity = conflict_resolution_replace.entity
+    Entity.change_or_create(
+        id_persistent=old_entity.id_persistent,
+        time_edit=datetime(1912, 4, 7),
+        display_txt="edited_entity",
+        version=old_entity.id,
+    )[0].save()
+
+    recent = ConflictResolution.only_recent()
+    assert len(recent) == 0
+
+
+def test_non_recent_change_all(
+    merge_request_user,
+    instance_merge_request_destination_user_conflict_changed,
+    destination_tag_def_for_mr_changed,
+    entity1_changed,
+    conflict_resolution_replace,
+):
+    recent = ConflictResolution.non_recent()
+    assert len(recent) == 1

tests/merge_request/queue_test.py
@@ -1,0 +1,64 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+import cosmae.merge_request.queue as q
+from cosmae.merge_request.models_django import MergeRequest
+
+
+def test_fast_forward_destination_empty(
+    merge_request_user_fast_forward, instances_merge_request_origin_user
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.MERGED
+
+
+def test_fast_forward_origin_empty(
+    merge_request_user_fast_forward, instance_merge_request_destination_user_no_conflict
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.MERGED
+
+
+def test_fast_forward_no_value(
+    merge_request_user_fast_forward,
+    instances_merge_request_origin_user,
+    instance_merge_request_destination_user_no_conflict_fast_forward,
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.CONFLICTS
+
+
+def test_fast_forward_conflict(
+    merge_request_user_fast_forward,
+    instances_merge_request_origin_user,
+    instance_merge_request_destination_user_conflict_fast_forward,
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.CONFLICTS
+
+
+def test_fast_forward_no_conflict_same_value(
+    merge_request_user_fast_forward,
+    instances_merge_request_origin_user,
+    instance_merge_request_destination_user_same_value,
+):
+    q.merge_request_fast_forward(merge_request_user_fast_forward.id_persistent)
+    merge_request_after = MergeRequest.by_id_persistent(
+        merge_request_user_fast_forward.id_persistent,
+        merge_request_user_fast_forward.created_by,
+    )
+    assert merge_request_after.state == MergeRequest.MERGED