Leibniz-HBI/DP-REX

Add functionality for curation and tag ownership to backend.

vran/urls.py
@@ -9,7 +9,7 @@ from ninja.constants import NOT_SET
 from vran.contribution.api import router as contribution_router
 from vran.merge_request.api import router as merge_request_router
 from vran.person.api import router as person_router
-from vran.tag.api import router as tag_router
+from vran.tag.api.router import router as tag_router
 from vran.user.api import router as user_router
 from vran.util.auth import vran_auth
 

tests/conftest.py
@@ -152,6 +152,42 @@ def user1(db):  # pylint: disable=unused-argument
 
 
 @pytest.fixture
+def user_commissioner(db):  # pylint: disable=unused-argument
+    try:
+        user = VranUser.objects.create_user(
+            username=cu.test_username_commissioner,
+            password=cu.test_password_commissioner,
+            email=cu.test_email_commissioner,
+            first_name=cu.test_names_personal_commissioner,
+            id_persistent=cu.test_uuid_commissioner,
+            permission_group=VranUser.COMMISSIONER,
+        )
+        return user
+    except IntegrityError:
+        return VranUser.objects.get(
+            email=cu.test_email_commissioner
+        )  # pylint: disable=no-member
+
+
+@pytest.fixture
+def user_editor(db):  # pylint: disable=unused-argument
+    try:
+        user = VranUser.objects.create_user(
+            username=cu.test_username_editor,
+            password=cu.test_password_editor,
+            email=cu.test_email_editor,
+            first_name=cu.test_names_personal_editor,
+            id_persistent=cu.test_uuid_editor,
+            permission_group=VranUser.EDITOR,
+        )
+        return user
+    except IntegrityError:
+        return VranUser.objects.get(  # pylint: disable=no-member
+            email=cu.test_email_editor
+        )
+
+
+@pytest.fixture
 def super_user(db):  # pylint: disable=unused-argument
     super_user = VranUser.objects.create_superuser(
         email=cu.test_email_super,

tests/tag/api/integration/delete_ownership_request_test.py
@@ -1,0 +1,53 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.tag.api.integration.requests as req
+import tests.tag.common as c
+from vran.exception import NotAuthenticatedException
+from vran.tag.models_django import OwnershipRequest
+
+
+def test_unknown_user(auth_server):
+    "Make sure that an unauthenticated user gets the correct status code."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.tag.api.permissions.check_user", mock):
+        rsp = req.delete_ownership(server.url, c.id_ownership_request_test, cookies)
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Make sure that without cookies the response has a unauthenticated status."
+    server, _ = auth_server
+    rsp = req.delete_ownership(server.url, c.id_ownership_request_test)
+    assert rsp.status_code == 401
+
+
+def test_wrong_user(auth_server1, ownership_request_user):
+    "Make sure the wrong user can not delete an ownership request."
+    server, _, cookies = auth_server1
+    rsp = req.delete_ownership(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_correct_user(auth_server, ownership_request_user):
+    "Make sure the petitioner can delete an ownership request."
+    server, cookies = auth_server
+    rsp = req.delete_ownership(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 200
+    ownership_requests = OwnershipRequest.objects.all()  # pylint: disable=no-member
+    assert 0 == len(ownership_requests)
+
+
+def test_commissioner_curated(
+    auth_server_commissioner, ownership_request_curated_editor
+):
+    "Make sure a commissioner can delete ownership requests for curated tags."
+    server, cookies = auth_server_commissioner
+    rsp = req.delete_ownership(
+        server.url, c.id_ownership_request_curated_test, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    ownership_requests = OwnershipRequest.objects.all()  # pylint: disable=no-member
+    assert 0 == len(ownership_requests)

tests/tag/api/integration/post_accept_request_test.py
@@ -1,0 +1,57 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.tag.api.integration.requests as req
+import tests.tag.common as c
+from vran.exception import NotAuthenticatedException
+from vran.tag.models_django import TagDefinition
+
+
+def test_unknown_user(auth_server):
+    "Make sure that an unauthenticated user gets the correct status code."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.tag.api.permissions.check_user", mock):
+        rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies)
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Make sure that without cookies the response has a unauthenticated status."
+    server, _ = auth_server
+    rsp = req.post_accept(server.url, c.id_ownership_request_test)
+    assert rsp.status_code == 401
+
+
+def test_wrong_user(auth_server, ownership_request_user):
+    "Make sure the wrong user can not accept an ownership request."
+    server, cookies = auth_server
+    rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_correct_user(auth_server1, ownership_request_user):
+    "Make sure the correct user can accept an ownership request and ownership is transferred."
+    server, _, cookies = auth_server1
+    rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 200
+    tag_definition = TagDefinition.most_recent_by_id(
+        ownership_request_user.id_tag_definition_persistent
+    )
+    assert tag_definition.owner == ownership_request_user.receiver
+
+
+def test_correct_user_curated(auth_server, ownership_request_curated):
+    """Make sure the correct user can accept an ownership request
+    for a previously curated tag definition and ownership is transferred."""
+    server, cookies = auth_server
+    rsp = req.post_accept(
+        server.url, c.id_ownership_request_curated_test, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    tag_definition = TagDefinition.most_recent_by_id(
+        ownership_request_curated.id_tag_definition_persistent
+    )
+    assert tag_definition.owner == ownership_request_curated.receiver
+    assert not tag_definition.curated

tests/tag/api/integration/post_set_owner_test.py
@@ -1,0 +1,115 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.tag.api.integration.requests as req
+import tests.tag.common as c
+import tests.user.common as cu
+from vran.exception import NotAuthenticatedException
+from vran.tag.models_django import OwnershipRequest as OwnershipRequestDb
+from vran.tag.models_django import TagDefinition as TagDefinitionDb
+
+
+def test_unknown_user(auth_server):
+    "Test the response when the user is not authenticated."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.tag.api.permissions.check_user", mock):
+        rsp = req.post_owner(
+            server.url, c.id_tag_def_persistent_test, cu.test_uuid, cookies=cookies
+        )
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Check 401 response for missing cookies."
+    server, _ = auth_server
+    rsp = req.post_owner(server.url, c.id_tag_def_persistent_test, cu.test_uuid)
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server, tag_def_curated):
+    "Check correct status code for normal users for a curated tag definition."
+    server, cookies = auth_server
+    rsp = req.post_owner(
+        server.url, tag_def_curated.id_persistent, cu.test_uuid1, cookies=cookies
+    )
+    assert rsp.status_code == 403
+
+
+def test_set_owner_non_curated(auth_server, tag_def_user, user1):
+    "Check correct status code for normal users for a curated tag definition."
+    server, cookies = auth_server
+    rsp = req.post_owner(
+        server.url, tag_def_user.id_persistent, user1.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    ownership_request_list = list(
+        OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=tag_def_user.id_persistent
+        )
+    )
+    assert len(ownership_request_list) == 1
+    ownership_request = ownership_request_list[0]
+    assert ownership_request.petitioner == tag_def_user.owner
+    assert ownership_request.receiver == user1
+
+
+def test_replaces_existing_request(auth_server, tag_def_user, user1, user_commissioner):
+    "Test whether an existing ownership request ist replaced."
+    server, cookies = auth_server
+    rsp = req.post_owner(
+        server.url,
+        tag_def_user.id_persistent,
+        user_commissioner.id_persistent,
+        cookies=cookies,
+    )
+    assert rsp.status_code == 200
+    rsp = req.post_owner(
+        server.url, tag_def_user.id_persistent, user1.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    ownership_request_list = list(
+        OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=tag_def_user.id_persistent
+        )
+    )
+    assert len(ownership_request_list) == 1
+    ownership_request = ownership_request_list[0]
+    assert ownership_request.petitioner == tag_def_user.owner
+    assert ownership_request.receiver == user1
+
+
+def test_curated_commissioner(auth_server_commissioner, user, tag_def_curated):
+    "Test wether a commissioner can make an ownership request for a curated tag definition."
+    server, cookies = auth_server_commissioner
+    rsp = req.post_owner(
+        server.url, tag_def_curated.id_persistent, user.id_persistent, cookies
+    )
+    assert rsp.status_code == 200
+    OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+        id_tag_definition_persistent=tag_def_curated.id_persistent
+    ).get()
+
+
+def test_curated_commissioner_to_self(
+    auth_server_commissioner,
+    user_commissioner,
+    tag_def_curated,
+):
+    "Test wether a commissioner can make an ownership request for a curated tag definition."
+    server, cookies = auth_server_commissioner
+    rsp = req.post_owner(
+        server.url,
+        tag_def_curated.id_persistent,
+        user_commissioner.id_persistent,
+        cookies,
+    )
+    assert rsp.status_code == 200
+    assert 0 == len(
+        OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=tag_def_curated.id_persistent
+        )
+    )
+    tag_def = TagDefinitionDb.most_recent_by_id(tag_def_curated.id_persistent)
+    assert tag_def.owner == user_commissioner

Leibniz-HBI/CoSMA-Editor

cosmae/urls.py
@@ -9,7 +9,7 @@ from ninja.constants import NOT_SET
 from cosmae.contribution.api import router as contribution_router
 from cosmae.merge_request.api import router as merge_request_router
 from cosmae.person.api import router as person_router
-from cosmae.tag.api import router as tag_router
+from cosmae.tag.api.router import router as tag_router
 from cosmae.user.api import router as user_router
 from cosmae.util.auth import cosmae_auth
 

tests/conftest.py
@@ -152,6 +152,42 @@ def user1(db):  # pylint: disable=unused-argument
 
 
 @pytest.fixture
+def user_commissioner(db):  # pylint: disable=unused-argument
+    try:
+        user = CosmaeUser.objects.create_user(
+            username=cu.test_username_commissioner,
+            password=cu.test_password_commissioner,
+            email=cu.test_email_commissioner,
+            first_name=cu.test_names_personal_commissioner,
+            id_persistent=cu.test_uuid_commissioner,
+            permission_group=CosmaeUser.COMMISSIONER,
+        )
+        return user
+    except IntegrityError:
+        return CosmaeUser.objects.get(
+            email=cu.test_email_commissioner
+        )  # pylint: disable=no-member
+
+
+@pytest.fixture
+def user_editor(db):  # pylint: disable=unused-argument
+    try:
+        user = CosmaeUser.objects.create_user(
+            username=cu.test_username_editor,
+            password=cu.test_password_editor,
+            email=cu.test_email_editor,
+            first_name=cu.test_names_personal_editor,
+            id_persistent=cu.test_uuid_editor,
+            permission_group=CosmaeUser.EDITOR,
+        )
+        return user
+    except IntegrityError:
+        return CosmaeUser.objects.get(  # pylint: disable=no-member
+            email=cu.test_email_editor
+        )
+
+
+@pytest.fixture
 def super_user(db):  # pylint: disable=unused-argument
     super_user = CosmaeUser.objects.create_superuser(
         email=cu.test_email_super,

tests/tag/api/integration/delete_ownership_request_test.py
@@ -1,0 +1,53 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.tag.api.integration.requests as req
+import tests.tag.common as c
+from cosmae.exception import NotAuthenticatedException
+from cosmae.tag.models_django import OwnershipRequest
+
+
+def test_unknown_user(auth_server):
+    "Make sure that an unauthenticated user gets the correct status code."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.tag.api.permissions.check_user", mock):
+        rsp = req.delete_ownership(server.url, c.id_ownership_request_test, cookies)
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Make sure that without cookies the response has a unauthenticated status."
+    server, _ = auth_server
+    rsp = req.delete_ownership(server.url, c.id_ownership_request_test)
+    assert rsp.status_code == 401
+
+
+def test_wrong_user(auth_server1, ownership_request_user):
+    "Make sure the wrong user can not delete an ownership request."
+    server, _, cookies = auth_server1
+    rsp = req.delete_ownership(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_correct_user(auth_server, ownership_request_user):
+    "Make sure the petitioner can delete an ownership request."
+    server, cookies = auth_server
+    rsp = req.delete_ownership(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 200
+    ownership_requests = OwnershipRequest.objects.all()  # pylint: disable=no-member
+    assert 0 == len(ownership_requests)
+
+
+def test_commissioner_curated(
+    auth_server_commissioner, ownership_request_curated_editor
+):
+    "Make sure a commissioner can delete ownership requests for curated tags."
+    server, cookies = auth_server_commissioner
+    rsp = req.delete_ownership(
+        server.url, c.id_ownership_request_curated_test, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    ownership_requests = OwnershipRequest.objects.all()  # pylint: disable=no-member
+    assert 0 == len(ownership_requests)

tests/tag/api/integration/post_accept_request_test.py
@@ -1,0 +1,57 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.tag.api.integration.requests as req
+import tests.tag.common as c
+from cosmae.exception import NotAuthenticatedException
+from cosmae.tag.models_django import TagDefinition
+
+
+def test_unknown_user(auth_server):
+    "Make sure that an unauthenticated user gets the correct status code."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.tag.api.permissions.check_user", mock):
+        rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies)
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Make sure that without cookies the response has a unauthenticated status."
+    server, _ = auth_server
+    rsp = req.post_accept(server.url, c.id_ownership_request_test)
+    assert rsp.status_code == 401
+
+
+def test_wrong_user(auth_server, ownership_request_user):
+    "Make sure the wrong user can not accept an ownership request."
+    server, cookies = auth_server
+    rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_correct_user(auth_server1, ownership_request_user):
+    "Make sure the correct user can accept an ownership request and ownership is transferred."
+    server, _, cookies = auth_server1
+    rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 200
+    tag_definition = TagDefinition.most_recent_by_id(
+        ownership_request_user.id_tag_definition_persistent
+    )
+    assert tag_definition.owner == ownership_request_user.receiver
+
+
+def test_correct_user_curated(auth_server, ownership_request_curated):
+    """Make sure the correct user can accept an ownership request
+    for a previously curated tag definition and ownership is transferred."""
+    server, cookies = auth_server
+    rsp = req.post_accept(
+        server.url, c.id_ownership_request_curated_test, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    tag_definition = TagDefinition.most_recent_by_id(
+        ownership_request_curated.id_tag_definition_persistent
+    )
+    assert tag_definition.owner == ownership_request_curated.receiver
+    assert not tag_definition.curated

tests/tag/api/integration/post_set_owner_test.py
@@ -1,0 +1,115 @@
+# pylint: disable=missing-module-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.tag.api.integration.requests as req
+import tests.tag.common as c
+import tests.user.common as cu
+from cosmae.exception import NotAuthenticatedException
+from cosmae.tag.models_django import OwnershipRequest as OwnershipRequestDb
+from cosmae.tag.models_django import TagDefinition as TagDefinitionDb
+
+
+def test_unknown_user(auth_server):
+    "Test the response when the user is not authenticated."
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.tag.api.permissions.check_user", mock):
+        rsp = req.post_owner(
+            server.url, c.id_tag_def_persistent_test, cu.test_uuid, cookies=cookies
+        )
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    "Check 401 response for missing cookies."
+    server, _ = auth_server
+    rsp = req.post_owner(server.url, c.id_tag_def_persistent_test, cu.test_uuid)
+    assert rsp.status_code == 401
+
+
+def test_insufficient_permissions(auth_server, tag_def_curated):
+    "Check correct status code for normal users for a curated tag definition."
+    server, cookies = auth_server
+    rsp = req.post_owner(
+        server.url, tag_def_curated.id_persistent, cu.test_uuid1, cookies=cookies
+    )
+    assert rsp.status_code == 403
+
+
+def test_set_owner_non_curated(auth_server, tag_def_user, user1):
+    "Check correct status code for normal users for a curated tag definition."
+    server, cookies = auth_server
+    rsp = req.post_owner(
+        server.url, tag_def_user.id_persistent, user1.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    ownership_request_list = list(
+        OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=tag_def_user.id_persistent
+        )
+    )
+    assert len(ownership_request_list) == 1
+    ownership_request = ownership_request_list[0]
+    assert ownership_request.petitioner == tag_def_user.owner
+    assert ownership_request.receiver == user1
+
+
+def test_replaces_existing_request(auth_server, tag_def_user, user1, user_commissioner):
+    "Test whether an existing ownership request ist replaced."
+    server, cookies = auth_server
+    rsp = req.post_owner(
+        server.url,
+        tag_def_user.id_persistent,
+        user_commissioner.id_persistent,
+        cookies=cookies,
+    )
+    assert rsp.status_code == 200
+    rsp = req.post_owner(
+        server.url, tag_def_user.id_persistent, user1.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    ownership_request_list = list(
+        OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=tag_def_user.id_persistent
+        )
+    )
+    assert len(ownership_request_list) == 1
+    ownership_request = ownership_request_list[0]
+    assert ownership_request.petitioner == tag_def_user.owner
+    assert ownership_request.receiver == user1
+
+
+def test_curated_commissioner(auth_server_commissioner, user, tag_def_curated):
+    "Test wether a commissioner can make an ownership request for a curated tag definition."
+    server, cookies = auth_server_commissioner
+    rsp = req.post_owner(
+        server.url, tag_def_curated.id_persistent, user.id_persistent, cookies
+    )
+    assert rsp.status_code == 200
+    OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+        id_tag_definition_persistent=tag_def_curated.id_persistent
+    ).get()
+
+
+def test_curated_commissioner_to_self(
+    auth_server_commissioner,
+    user_commissioner,
+    tag_def_curated,
+):
+    "Test wether a commissioner can make an ownership request for a curated tag definition."
+    server, cookies = auth_server_commissioner
+    rsp = req.post_owner(
+        server.url,
+        tag_def_curated.id_persistent,
+        user_commissioner.id_persistent,
+        cookies,
+    )
+    assert rsp.status_code == 200
+    assert 0 == len(
+        OwnershipRequestDb.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=tag_def_curated.id_persistent
+        )
+    )
+    tag_def = TagDefinitionDb.most_recent_by_id(tag_def_curated.id_persistent)
+    assert tag_def.owner == user_commissioner