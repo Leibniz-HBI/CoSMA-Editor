Leibniz-HBI/DP-REX

Add entity similarity for contributions to backend.

vran/contribution/entity/match_entities.py
@@ -1,0 +1,21 @@
+"Queue method for finding duplicates in entity names for contribution candidate."
+from django.contrib.postgres.search import TrigramSimilarity
+
+from vran.entity.models_django import Entity
+
+
+def find_matches(entity: Entity):
+    """Finds entities without a contribution candidate
+    having similar display txt, according to trigram similarity"""
+
+    return (
+        Entity.most_recent(
+            Entity.objects.filter(  # pylint: disable=no-member
+                contribution_candidate=None
+            )
+        )
+        .annotate(similarity=TrigramSimilarity("display_txt", entity.display_txt))
+        .filter(similarity__gt=0.85)
+        .order_by("-similarity")[:10]
+        .values()
+    )

vran/migrations/0006_tagdefinition_curated_tagdefinition_owner_and_more.py
@@ -1,0 +1,55 @@
+# Generated by Django 4.2 on 2023-06-20 09:57
+
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0005_remove_tagdefinitioncontribution_id_parent_persistent_and_more"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="tagdefinition",
+            name="curated",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name="tagdefinition",
+            name="owner",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+        migrations.CreateModel(
+            name="MergeRequest",
+            fields=[
+                ("id_destination_persistent", models.TextField()),
+                ("id_origin_persistent", models.TextField()),
+                ("created_at", models.DateTimeField()),
+                ("id_persistent", models.UUIDField(primary_key=True, serialize=False)),
+                (
+                    "assigned_to",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="assigned_merge_requests",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="created_merge_requests",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+        ),
+    ]

vran/migrations/0010_entityduplicate.py
@@ -1,0 +1,37 @@
+# Generated by Django 4.2.2 on 2023-06-23 10:29
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0009_entity_vran_entity_id_pers_0f2b92_idx_and_more"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="EntityDuplicate",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("id_origin_persistent", models.TextField()),
+                ("id_destination_persistent", models.TextField()),
+                (
+                    "contribution_candidate",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.contributioncandidate",
+                    ),
+                ),
+            ],
+        ),
+    ]

vran/models.py
@@ -1,11 +1,12 @@
 """Union of all DB models."""
 # pylint: disable=unused-import
+from vran.contribution.entity.models_django import EntityDuplicate
 from vran.contribution.models_django import ContributionCandidate
 from vran.contribution.tag_definition.models_django import (
     TagDefinitionContribution,
     TagInstanceContribution,
 )
 from vran.entity.models_django import Entity
-from vran.person.models_django import Person
+from vran.merge_request.models_django import MergeRequest
 from vran.tag.models_django import TagDefinition, TagInstance
 from vran.util import VranUser

tests/merge_request/conftest.py
@@ -1,0 +1,107 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+import pytest
+
+import tests.merge_request.common as c
+from vran.contribution.models_django import ContributionCandidate
+from vran.merge_request.models_django import MergeRequest
+from vran.tag.models_django import TagDefinition
+
+
+@pytest.fixture
+def destination_tag_def_for_mr(db, user):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_destination,
+        id_persistent=c.id_persistent_tag_def_destination,
+        type=TagDefinition.FLOAT,
+        time_edit=c.time_tag_def_destination,
+        owner=user,
+    )
+
+
+@pytest.fixture
+def origin_tag_def_for_mr(db, user1):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_origin,
+        id_persistent=c.id_persistent_tag_def_origin,
+        type=TagDefinition.FLOAT,
+        time_edit=c.time_tag_def_origin,
+        owner=user1,
+    )
+
+
+@pytest.fixture
+def contribution_for_mr(db, user1):
+    return ContributionCandidate.objects.create(  # pylint: disable=no-member
+        name=c.name_contribution,
+        description=c.description_contribution,
+        id_persistent=c.id_persistent_contribution,
+        anonymous=True,
+        has_header=True,
+        created_by=user1,
+        file_name="tmp_file.csv",
+    )
+
+
+@pytest.fixture
+def merge_request_user(
+    db, origin_tag_def_for_mr, destination_tag_def_for_mr, contribution_for_mr
+):
+    return MergeRequest.objects.create(  # pylint: disable=no-member
+        id_origin_persistent=origin_tag_def_for_mr.id_persistent,
+        id_destination_persistent=destination_tag_def_for_mr.id_persistent,
+        created_by=origin_tag_def_for_mr.owner,
+        assigned_to=destination_tag_def_for_mr.owner,
+        created_at=c.time_merge_request,
+        id_persistent=c.id_persistent_merge_request,
+        contribution_candidate=contribution_for_mr,
+    )
+
+
+@pytest.fixture
+def destination_tag_def_for_mr1(db, user1):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_destination1,
+        id_persistent=c.id_persistent_tag_def_destination1,
+        type=TagDefinition.STRING,
+        time_edit=c.time_tag_def_destination1,
+        owner=user1,
+    )
+
+
+@pytest.fixture
+def origin_tag_def_for_mr1(db, user):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_origin1,
+        id_persistent=c.id_persistent_tag_def_origin1,
+        type=TagDefinition.STRING,
+        time_edit=c.time_tag_def_origin1,
+        owner=user,
+    )
+
+
+@pytest.fixture
+def contribution_for_mr1(db, user):
+    return ContributionCandidate.objects.create(  # pylint: disable=no-member
+        name=c.name_contribution1,
+        description=c.description_contribution1,
+        id_persistent=c.id_persistent_contribution1,
+        anonymous=True,
+        has_header=True,
+        created_by=user,
+        file_name="tmp_file.csv",
+    )
+
+
+@pytest.fixture
+def merge_request_user1(
+    db, destination_tag_def_for_mr1, origin_tag_def_for_mr1, contribution_for_mr1
+):
+    return MergeRequest.objects.create(  # pylint: disable=no-member
+        id_destination_persistent=destination_tag_def_for_mr1.id_persistent,
+        id_origin_persistent=origin_tag_def_for_mr1.id_persistent,
+        created_by=origin_tag_def_for_mr1.owner,
+        assigned_to=destination_tag_def_for_mr1.owner,
+        created_at=c.time_merge_request1,
+        id_persistent=c.id_persistent_merge_request1,
+        contribution_candidate=contribution_for_mr1,
+    )

tests/user/api/integration/post_register_test.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
 from unittest.mock import MagicMock, patch
+from uuid import uuid4
 
 from django.db import DatabaseError
 
@@ -39,15 +40,18 @@ def test_same_email(auth_server):
 
 def test_same_names(auth_server):
     live_server, _ = auth_server
-    rsp = post_register(
-        live_server.url,
-        {
-            "user_name": "other",
-            "email": "other@test.org",
-            "names_personal": c.test_names_personal,
-            "password": c.test_password,
-        },
-    )
+    uuid = uuid4()
+    uuidMock = MagicMock(return_value=uuid)
+    with patch("vran.user.api.uuid4", uuidMock):
+        rsp = post_register(
+            live_server.url,
+            {
+                "user_name": "other",
+                "email": "other@test.org",
+                "names_personal": c.test_names_personal,
+                "password": c.test_password,
+            },
+        )
     assert rsp.status_code == 200
     assert rsp.json() == {
         "user_name": "other",
@@ -55,6 +59,7 @@ def test_same_names(auth_server):
         "names_personal": c.test_names_personal,
         "names_family": "",
         "columns": [],
+        "id_persistent": str(uuid),
     }
 
 

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/entity/match_entities.py
@@ -1,0 +1,21 @@
+"Queue method for finding duplicates in entity names for contribution candidate."
+from django.contrib.postgres.search import TrigramSimilarity
+
+from cosmae.entity.models_django import Entity
+
+
+def find_matches(entity: Entity):
+    """Finds entities without a contribution candidate
+    having similar display txt, according to trigram similarity"""
+
+    return (
+        Entity.most_recent(
+            Entity.objects.filter(  # pylint: disable=no-member
+                contribution_candidate=None
+            )
+        )
+        .annotate(similarity=TrigramSimilarity("display_txt", entity.display_txt))
+        .filter(similarity__gt=0.85)
+        .order_by("-similarity")[:10]
+        .values()
+    )

cosmae/migrations/0006_tagdefinition_curated_tagdefinition_owner_and_more.py
@@ -1,0 +1,55 @@
+# Generated by Django 4.2 on 2023-06-20 09:57
+
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0005_remove_tagdefinitioncontribution_id_parent_persistent_and_more"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="tagdefinition",
+            name="curated",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name="tagdefinition",
+            name="owner",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+        migrations.CreateModel(
+            name="MergeRequest",
+            fields=[
+                ("id_destination_persistent", models.TextField()),
+                ("id_origin_persistent", models.TextField()),
+                ("created_at", models.DateTimeField()),
+                ("id_persistent", models.UUIDField(primary_key=True, serialize=False)),
+                (
+                    "assigned_to",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="assigned_merge_requests",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="created_merge_requests",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+        ),
+    ]

cosmae/migrations/0010_entityduplicate.py
@@ -1,0 +1,37 @@
+# Generated by Django 4.2.2 on 2023-06-23 10:29
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0009_entity_cosmae_entity_id_pers_0f2b92_idx_and_more"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="EntityDuplicate",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("id_origin_persistent", models.TextField()),
+                ("id_destination_persistent", models.TextField()),
+                (
+                    "contribution_candidate",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.contributioncandidate",
+                    ),
+                ),
+            ],
+        ),
+    ]

cosmae/models.py
@@ -1,11 +1,12 @@
 """Union of all DB models."""
 # pylint: disable=unused-import
+from cosmae.contribution.entity.models_django import EntityDuplicate
 from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.contribution.tag_definition.models_django import (
     TagDefinitionContribution,
     TagInstanceContribution,
 )
 from cosmae.entity.models_django import Entity
-from cosmae.person.models_django import Person
+from cosmae.merge_request.models_django import MergeRequest
 from cosmae.tag.models_django import TagDefinition, TagInstance
 from cosmae.util import CosmaeUser

tests/merge_request/conftest.py
@@ -1,0 +1,107 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
+import pytest
+
+import tests.merge_request.common as c
+from cosmae.contribution.models_django import ContributionCandidate
+from cosmae.merge_request.models_django import MergeRequest
+from cosmae.tag.models_django import TagDefinition
+
+
+@pytest.fixture
+def destination_tag_def_for_mr(db, user):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_destination,
+        id_persistent=c.id_persistent_tag_def_destination,
+        type=TagDefinition.FLOAT,
+        time_edit=c.time_tag_def_destination,
+        owner=user,
+    )
+
+
+@pytest.fixture
+def origin_tag_def_for_mr(db, user1):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_origin,
+        id_persistent=c.id_persistent_tag_def_origin,
+        type=TagDefinition.FLOAT,
+        time_edit=c.time_tag_def_origin,
+        owner=user1,
+    )
+
+
+@pytest.fixture
+def contribution_for_mr(db, user1):
+    return ContributionCandidate.objects.create(  # pylint: disable=no-member
+        name=c.name_contribution,
+        description=c.description_contribution,
+        id_persistent=c.id_persistent_contribution,
+        anonymous=True,
+        has_header=True,
+        created_by=user1,
+        file_name="tmp_file.csv",
+    )
+
+
+@pytest.fixture
+def merge_request_user(
+    db, origin_tag_def_for_mr, destination_tag_def_for_mr, contribution_for_mr
+):
+    return MergeRequest.objects.create(  # pylint: disable=no-member
+        id_origin_persistent=origin_tag_def_for_mr.id_persistent,
+        id_destination_persistent=destination_tag_def_for_mr.id_persistent,
+        created_by=origin_tag_def_for_mr.owner,
+        assigned_to=destination_tag_def_for_mr.owner,
+        created_at=c.time_merge_request,
+        id_persistent=c.id_persistent_merge_request,
+        contribution_candidate=contribution_for_mr,
+    )
+
+
+@pytest.fixture
+def destination_tag_def_for_mr1(db, user1):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_destination1,
+        id_persistent=c.id_persistent_tag_def_destination1,
+        type=TagDefinition.STRING,
+        time_edit=c.time_tag_def_destination1,
+        owner=user1,
+    )
+
+
+@pytest.fixture
+def origin_tag_def_for_mr1(db, user):
+    return TagDefinition.objects.create(  # pylint: disable=no-member
+        name=c.name_tag_def_origin1,
+        id_persistent=c.id_persistent_tag_def_origin1,
+        type=TagDefinition.STRING,
+        time_edit=c.time_tag_def_origin1,
+        owner=user,
+    )
+
+
+@pytest.fixture
+def contribution_for_mr1(db, user):
+    return ContributionCandidate.objects.create(  # pylint: disable=no-member
+        name=c.name_contribution1,
+        description=c.description_contribution1,
+        id_persistent=c.id_persistent_contribution1,
+        anonymous=True,
+        has_header=True,
+        created_by=user,
+        file_name="tmp_file.csv",
+    )
+
+
+@pytest.fixture
+def merge_request_user1(
+    db, destination_tag_def_for_mr1, origin_tag_def_for_mr1, contribution_for_mr1
+):
+    return MergeRequest.objects.create(  # pylint: disable=no-member
+        id_destination_persistent=destination_tag_def_for_mr1.id_persistent,
+        id_origin_persistent=origin_tag_def_for_mr1.id_persistent,
+        created_by=origin_tag_def_for_mr1.owner,
+        assigned_to=destination_tag_def_for_mr1.owner,
+        created_at=c.time_merge_request1,
+        id_persistent=c.id_persistent_merge_request1,
+        contribution_candidate=contribution_for_mr1,
+    )

tests/user/api/integration/post_register_test.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
 from unittest.mock import MagicMock, patch
+from uuid import uuid4
 
 from django.db import DatabaseError
 
@@ -39,15 +40,18 @@ def test_same_email(auth_server):
 
 def test_same_names(auth_server):
     live_server, _ = auth_server
-    rsp = post_register(
-        live_server.url,
-        {
-            "user_name": "other",
-            "email": "other@test.org",
-            "names_personal": c.test_names_personal,
-            "password": c.test_password,
-        },
-    )
+    uuid = uuid4()
+    uuidMock = MagicMock(return_value=uuid)
+    with patch("cosmae.user.api.uuid4", uuidMock):
+        rsp = post_register(
+            live_server.url,
+            {
+                "user_name": "other",
+                "email": "other@test.org",
+                "names_personal": c.test_names_personal,
+                "password": c.test_password,
+            },
+        )
     assert rsp.status_code == 200
     assert rsp.json() == {
         "user_name": "other",
@@ -55,6 +59,7 @@ def test_same_names(auth_server):
         "names_personal": c.test_names_personal,
         "names_family": "",
         "columns": [],
+        "id_persistent": str(uuid),
     }
 
 