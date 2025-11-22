Leibniz-HBI/DP-REX

Add functionality to backend for storing the last selected tag definitions in the user profile.

vran/apps.py
@@ -6,43 +6,32 @@ from django.apps import AppConfig, apps
 from django.core.exceptions import AppRegistryNotReady
 from django.db.backends.signals import connection_created
 from django.db.models.signals import post_migrate, post_save
+from django.db.utils import OperationalError
 
 logger = logging.getLogger("vran.app_config")
 
-READ_PERMISSIONS = [
-    "view_person",
-    "view_entity",
-    "view_taginstance",
-    "view_tagdefinition",
-]
-
-WRITE_PERMISSIONS = [
-    "add_person",
-    "add_entity",
-    "add_taginstance",
-    "add_tagdefinition",
-]
 
 added_permissions = {
     "VranGroup.APPLICANT": [],
-    "VranGroup.READER": [READ_PERMISSIONS],
-    "VranGroup.CONTRIBUTOR": [READ_PERMISSIONS],
-    "VranGroup.EDITOR": [READ_PERMISSIONS, WRITE_PERMISSIONS],
-    "VranGroup.COMMISSIONER": [READ_PERMISSIONS, WRITE_PERMISSIONS],
+    "VranGroup.READER": [],
+    "VranGroup.CONTRIBUTOR": [],
+    "VranGroup.EDITOR": [],
+    "VranGroup.COMMISSIONER": [],
 }
 
 
 def add_permission_for_group(group: str, permission_list_list: List[List[str]]):
     "Method for granting permissions to groups."
     group_model = apps.get_model("auth", "Group")
-    permission_model = apps.get_model("auth", "Permission")
+    permission_model = apps.get_model("auth", "group_permissions")
     group_name = str(group)
 
     group, _created_group = group_model.objects.get_or_create(name=group_name)
     for permission_list in permission_list_list:
         for permission_name in permission_list:
-            permission = permission_model.objects.get(codename=permission_name)
-            group.permissions.add(permission)
+            permission = permission_model.objects.filter(codename=permission_name).get()
+            if not permission in group.permissions:
+                group.permissions.add(permission)
     group.save()
 
 
@@ -110,6 +99,25 @@ class VranConfig(AppConfig):
                 dispatch_merge_request_queue_process,
                 sender=MergeRequest,
                 dispatch_uid="vran_merge_request_queue",
+            )
+            from django_rq import enqueue
+
+            from vran.tag.models_django import TagDefinition
+            from vran.tag.queue import (
+                dispatch_tag_definition_queue_process,
+                update_tag_definition_name_path,
+            )
+
+            try:
+                roots = TagDefinition.most_recent_children(None)
+                for root in roots:
+                    enqueue(update_tag_definition_name_path, root.id_persistent, [])
+            except OperationalError:
+                pass  #
+            post_save.connect(
+                dispatch_tag_definition_queue_process,
+                sender=TagDefinition,
+                dispatch_uid="vran_tag_definition_queue",
             )
         except AppRegistryNotReady:
             pass

vran/tag/queue.py
@@ -1,0 +1,66 @@
+"""Queue methods for tag instances and tag definitions."""
+from typing import List, Optional
+
+from django.core.cache import caches
+from django.db import transaction
+from django.db.utils import OperationalError
+from django_rq import enqueue
+
+from vran.tag.models_django import TagDefinition
+
+tag_definition_name_path_cache = caches["tag_definition_name_paths"]
+
+
+def get_tag_definition_name_path(tag_definition: TagDefinition):
+    """Get the name path of a tag definition.
+    This will retrieve the name path from the cache if present.
+    Otherwise only the name is returned and an update to the cache is triggered."""
+    name_path = tag_definition_name_path_cache.get(tag_definition.id_persistent)
+    if name_path is None:
+        name_path = [tag_definition.name]
+        enqueue(update_tag_definition_name_path, tag_definition.id_persistent)
+    return name_path
+
+
+def update_tag_definition_name_path(
+    id_tag_definition_persistent, parent_name_path: Optional[List[str]] = None
+):
+    """Update the name path cache entry for the tag definition references by its persistent id.
+    If the name path of the parent is already known it can be provided as an optional parameter."""
+    tag_definition_query = TagDefinition.most_recent_by_id_query_set(
+        id_tag_definition_persistent
+    )
+    try:
+        with transaction.atomic():
+            try:
+                tag_definition = tag_definition_query.get()
+            except OperationalError:
+                return
+            if parent_name_path is None:
+                if tag_definition.id_parent_persistent is None:
+                    parent_name_path = []
+                else:
+                    parent_name_path = tag_definition_name_path_cache.get(
+                        tag_definition.id_parent_persistent
+                    )
+            name_path = parent_name_path + [tag_definition.name]
+            tag_definition_name_path_cache.set(tag_definition.id_persistent, name_path)
+            children = TagDefinition.most_recent_children(tag_definition.id_persistent)
+            for child in children:
+                enqueue(update_tag_definition_name_path, child.id_persistent, name_path)
+
+    except Exception:  # pylint: disable=broad-except
+        return
+
+
+def dispatch_tag_definition_queue_process(
+    sender,
+    instance,
+    created,
+    update_fields,
+    **kwargs  # pylint: disable=unused-argument
+):
+    "Dispatches queue methods for tag definitions on save."
+    if not (created or (update_fields and "id_persistent" in update_fields)):
+        return
+    enqueue(update_tag_definition_name_path, str(instance.id_persistent))

django_project/settings/settings_production.py
@@ -41,7 +41,9 @@ def get_docker_compose_secret(secret_name):
     with open(f"/run/secrets/{secret_name}", encoding="utf-8") as key_file:
         key = key_file.readline().rstrip("\n")
     if not key:
-        raise Exception(f"Please set the {secret_name} secret.")
+        raise Exception(  # pylint: disable=broad-exception-raised
+            f"Please set the {secret_name} secret."
+        )
     return key
 
 
@@ -170,4 +172,16 @@ RQ_QUEUES = {
         "DEFAULT_TIMEOUT": 360,
         "PASSWORD": get_docker_compose_secret("redis_password"),
     }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": f"redis://default:{get_docker_compose_secret}@vran_redis:6379",
+    },
+    "tag_definition_name_paths": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": f"redis://default:{get_docker_compose_secret}@vran_redis:6379",
+        "KEY_PREFIX": "tag_definition_name_path",
+    },
 }

tests/tag/queue_test.py
@@ -1,0 +1,58 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, call, patch
+
+import pytest
+
+import vran.tag.queue as q
+
+
+def test_get_name_from_cache(tag_def):
+    path = ["name", "path", "test"]
+    q.tag_definition_name_path_cache.set(tag_def.id_persistent, path)
+    result = q.get_tag_definition_name_path(tag_def)
+    assert result == path
+
+
+def test_name_as_default_path_and_enqueues(tag_def_user):
+    mock = MagicMock()
+    with patch("vran.tag.queue.enqueue", mock):
+        result = q.get_tag_definition_name_path(tag_def_user)
+    assert result == [tag_def_user.name]
+    mock.assert_called_once_with(
+        q.update_tag_definition_name_path, tag_def_user.id_persistent
+    )
+
+
+def test_enqueues_children(tag_def_parent, tag_def_child_0, tag_def_child_1):
+    tag_def_child_0.save()
+    tag_def_child_1.save()
+    mock = MagicMock()
+    with patch("vran.tag.queue.enqueue", mock):
+        q.update_tag_definition_name_path(tag_def_parent.id_persistent)
+    assert q.tag_definition_name_path_cache.get(tag_def_parent.id_persistent) == [
+        tag_def_parent.name
+    ]
+    mock.assert_has_calls(
+        [
+            call(
+                q.update_tag_definition_name_path,
+                tag_def_child_0.id_persistent,
+                [tag_def_parent.name],
+            ),
+            call(
+                q.update_tag_definition_name_path,
+                tag_def_child_1.id_persistent,
+                [tag_def_parent.name],
+            ),
+        ]
+    )
+
+
+@pytest.mark.django_db
+def test_uses_provided_parent_path(tag_def_child_0):
+    tag_def_child_0.save()
+    q.update_tag_definition_name_path(
+        tag_def_child_0.id_persistent, ["name", "path", "parent", "test"]
+    )
+    from_cache = q.get_tag_definition_name_path(tag_def_child_0)
+    assert from_cache == ["name", "path", "parent", "test", tag_def_child_0.name]

tests/user/api/integration/post_append_id_tag_definition_test.py
@@ -1,0 +1,66 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.user.api.integration.requests as req
+from vran.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("vran.user.api.check_user", mock):
+        rsp = req.post_append_id_tag_definition_persistent(
+            server.url, "id", cookies=cookies
+        )
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(server.url, "id")
+    assert rsp.status_code == 401
+
+
+def test_unknown_tag_instance(auth_server):
+    server, cookies = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, "id", cookies=cookies
+    )
+    assert rsp.status_code == 400  # pylint: disable=duplicate-code
+
+
+def test_existing_tag_definition(auth_server, tag_def_user_profile):
+    # pylint: disable=duplicate-code
+    server, cookies = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, tag_def_user_profile.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    rsp = req.get_refresh(server.url, cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert [tag_def["id_persistent"] for tag_def in json["tag_definition_list"]] == [
+        tag_def_user_profile.id_persistent
+    ]
+
+
+def test_existing_tag_definition_multiple(
+    auth_server, tag_def_user_profile, tag_def_user_profile1
+):
+    # pylint: disable=duplicate-code
+    server, cookies = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, tag_def_user_profile.id_persistent, cookies=cookies
+    )
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, tag_def_user_profile1.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    rsp = req.get_refresh(server.url, cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert [tag_def["id_persistent"] for tag_def in json["tag_definition_list"]] == [
+        tag_def_user_profile.id_persistent,
+        tag_def_user_profile1.id_persistent,
+    ]

tests/user/conftest.py
@@ -1,0 +1,66 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,no-member,redefined-outer-name
+import pytest
+
+import tests.user.common as c
+from vran.tag.models_django import TagDefinition
+
+
+@pytest.fixture
+def tag_def_user_profile():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def,
+        id_persistent=c.id_tag_def_persistent,
+        time_edit=c.time_edit_tag_def,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def tag_def_user_profile1():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def1,
+        id_persistent=c.id_tag_def_persistent1,
+        time_edit=c.time_edit_tag_def1,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def tag_def_user_profile2():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def2,
+        id_persistent=c.id_tag_def_persistent2,
+        time_edit=c.time_edit_tag_def2,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def tag_def_user_profile3():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def3,
+        id_persistent=c.id_tag_def_persistent3,
+        time_edit=c.time_edit_tag_def3,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def user_with_tag_defs(
+    user,
+    tag_def_user_profile,
+    tag_def_user_profile1,
+    tag_def_user_profile2,
+    tag_def_user_profile3,
+):
+    user.tag_definitions = [
+        tag_def.id_persistent
+        for tag_def in [
+            tag_def_user_profile,
+            tag_def_user_profile1,
+            tag_def_user_profile2,
+            tag_def_user_profile3,
+        ]
+    ]
+    user.save()
+    return user

Leibniz-HBI/CoSMA-Editor

cosmae/apps.py
@@ -6,43 +6,32 @@ from django.apps import AppConfig, apps
 from django.core.exceptions import AppRegistryNotReady
 from django.db.backends.signals import connection_created
 from django.db.models.signals import post_migrate, post_save
+from django.db.utils import OperationalError
 
 logger = logging.getLogger("cosmae.app_config")
 
-READ_PERMISSIONS = [
-    "view_person",
-    "view_entity",
-    "view_taginstance",
-    "view_tagdefinition",
-]
-
-WRITE_PERMISSIONS = [
-    "add_person",
-    "add_entity",
-    "add_taginstance",
-    "add_tagdefinition",
-]
 
 added_permissions = {
     "CosmaeGroup.APPLICANT": [],
-    "CosmaeGroup.READER": [READ_PERMISSIONS],
-    "CosmaeGroup.CONTRIBUTOR": [READ_PERMISSIONS],
-    "CosmaeGroup.EDITOR": [READ_PERMISSIONS, WRITE_PERMISSIONS],
-    "CosmaeGroup.COMMISSIONER": [READ_PERMISSIONS, WRITE_PERMISSIONS],
+    "CosmaeGroup.READER": [],
+    "CosmaeGroup.CONTRIBUTOR": [],
+    "CosmaeGroup.EDITOR": [],
+    "CosmaeGroup.COMMISSIONER": [],
 }
 
 
 def add_permission_for_group(group: str, permission_list_list: List[List[str]]):
     "Method for granting permissions to groups."
     group_model = apps.get_model("auth", "Group")
-    permission_model = apps.get_model("auth", "Permission")
+    permission_model = apps.get_model("auth", "group_permissions")
     group_name = str(group)
 
     group, _created_group = group_model.objects.get_or_create(name=group_name)
     for permission_list in permission_list_list:
         for permission_name in permission_list:
-            permission = permission_model.objects.get(codename=permission_name)
-            group.permissions.add(permission)
+            permission = permission_model.objects.filter(codename=permission_name).get()
+            if not permission in group.permissions:
+                group.permissions.add(permission)
     group.save()
 
 
@@ -110,6 +99,25 @@ class CosmaeConfig(AppConfig):
                 dispatch_merge_request_queue_process,
                 sender=MergeRequest,
                 dispatch_uid="cosmae_merge_request_queue",
+            )
+            from django_rq import enqueue
+
+            from cosmae.tag.models_django import TagDefinition
+            from cosmae.tag.queue import (
+                dispatch_tag_definition_queue_process,
+                update_tag_definition_name_path,
+            )
+
+            try:
+                roots = TagDefinition.most_recent_children(None)
+                for root in roots:
+                    enqueue(update_tag_definition_name_path, root.id_persistent, [])
+            except OperationalError:
+                pass  #
+            post_save.connect(
+                dispatch_tag_definition_queue_process,
+                sender=TagDefinition,
+                dispatch_uid="cosmae_tag_definition_queue",
             )
         except AppRegistryNotReady:
             pass

cosmae/tag/queue.py
@@ -1,0 +1,66 @@
+"""Queue methods for tag instances and tag definitions."""
+from typing import List, Optional
+
+from django.core.cache import caches
+from django.db import transaction
+from django.db.utils import OperationalError
+from django_rq import enqueue
+
+from cosmae.tag.models_django import TagDefinition
+
+tag_definition_name_path_cache = caches["tag_definition_name_paths"]
+
+
+def get_tag_definition_name_path(tag_definition: TagDefinition):
+    """Get the name path of a tag definition.
+    This will retrieve the name path from the cache if present.
+    Otherwise only the name is returned and an update to the cache is triggered."""
+    name_path = tag_definition_name_path_cache.get(tag_definition.id_persistent)
+    if name_path is None:
+        name_path = [tag_definition.name]
+        enqueue(update_tag_definition_name_path, tag_definition.id_persistent)
+    return name_path
+
+
+def update_tag_definition_name_path(
+    id_tag_definition_persistent, parent_name_path: Optional[List[str]] = None
+):
+    """Update the name path cache entry for the tag definition references by its persistent id.
+    If the name path of the parent is already known it can be provided as an optional parameter."""
+    tag_definition_query = TagDefinition.most_recent_by_id_query_set(
+        id_tag_definition_persistent
+    )
+    try:
+        with transaction.atomic():
+            try:
+                tag_definition = tag_definition_query.get()
+            except OperationalError:
+                return
+            if parent_name_path is None:
+                if tag_definition.id_parent_persistent is None:
+                    parent_name_path = []
+                else:
+                    parent_name_path = tag_definition_name_path_cache.get(
+                        tag_definition.id_parent_persistent
+                    )
+            name_path = parent_name_path + [tag_definition.name]
+            tag_definition_name_path_cache.set(tag_definition.id_persistent, name_path)
+            children = TagDefinition.most_recent_children(tag_definition.id_persistent)
+            for child in children:
+                enqueue(update_tag_definition_name_path, child.id_persistent, name_path)
+
+    except Exception:  # pylint: disable=broad-except
+        return
+
+
+def dispatch_tag_definition_queue_process(
+    sender,
+    instance,
+    created,
+    update_fields,
+    **kwargs  # pylint: disable=unused-argument
+):
+    "Dispatches queue methods for tag definitions on save."
+    if not (created or (update_fields and "id_persistent" in update_fields)):
+        return
+    enqueue(update_tag_definition_name_path, str(instance.id_persistent))

django_project/settings/settings_production.py
@@ -41,7 +41,9 @@ def get_docker_compose_secret(secret_name):
     with open(f"/run/secrets/{secret_name}", encoding="utf-8") as key_file:
         key = key_file.readline().rstrip("\n")
     if not key:
-        raise Exception(f"Please set the {secret_name} secret.")
+        raise Exception(  # pylint: disable=broad-exception-raised
+            f"Please set the {secret_name} secret."
+        )
     return key
 
 
@@ -170,4 +172,16 @@ RQ_QUEUES = {
         "DEFAULT_TIMEOUT": 360,
         "PASSWORD": get_docker_compose_secret("redis_password"),
     }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": f"redis://default:{get_docker_compose_secret}@cosmae_redis:6379",
+    },
+    "tag_definition_name_paths": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": f"redis://default:{get_docker_compose_secret}@cosmae_redis:6379",
+        "KEY_PREFIX": "tag_definition_name_path",
+    },
 }

tests/tag/queue_test.py
@@ -1,0 +1,58 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, call, patch
+
+import pytest
+
+import cosmae.tag.queue as q
+
+
+def test_get_name_from_cache(tag_def):
+    path = ["name", "path", "test"]
+    q.tag_definition_name_path_cache.set(tag_def.id_persistent, path)
+    result = q.get_tag_definition_name_path(tag_def)
+    assert result == path
+
+
+def test_name_as_default_path_and_enqueues(tag_def_user):
+    mock = MagicMock()
+    with patch("cosmae.tag.queue.enqueue", mock):
+        result = q.get_tag_definition_name_path(tag_def_user)
+    assert result == [tag_def_user.name]
+    mock.assert_called_once_with(
+        q.update_tag_definition_name_path, tag_def_user.id_persistent
+    )
+
+
+def test_enqueues_children(tag_def_parent, tag_def_child_0, tag_def_child_1):
+    tag_def_child_0.save()
+    tag_def_child_1.save()
+    mock = MagicMock()
+    with patch("cosmae.tag.queue.enqueue", mock):
+        q.update_tag_definition_name_path(tag_def_parent.id_persistent)
+    assert q.tag_definition_name_path_cache.get(tag_def_parent.id_persistent) == [
+        tag_def_parent.name
+    ]
+    mock.assert_has_calls(
+        [
+            call(
+                q.update_tag_definition_name_path,
+                tag_def_child_0.id_persistent,
+                [tag_def_parent.name],
+            ),
+            call(
+                q.update_tag_definition_name_path,
+                tag_def_child_1.id_persistent,
+                [tag_def_parent.name],
+            ),
+        ]
+    )
+
+
+@pytest.mark.django_db
+def test_uses_provided_parent_path(tag_def_child_0):
+    tag_def_child_0.save()
+    q.update_tag_definition_name_path(
+        tag_def_child_0.id_persistent, ["name", "path", "parent", "test"]
+    )
+    from_cache = q.get_tag_definition_name_path(tag_def_child_0)
+    assert from_cache == ["name", "path", "parent", "test", tag_def_child_0.name]

tests/user/api/integration/post_append_id_tag_definition_test.py
@@ -1,0 +1,66 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+import tests.user.api.integration.requests as req
+from cosmae.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server
+    with patch("cosmae.user.api.check_user", mock):
+        rsp = req.post_append_id_tag_definition_persistent(
+            server.url, "id", cookies=cookies
+        )
+    assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server):
+    server, _ = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(server.url, "id")
+    assert rsp.status_code == 401
+
+
+def test_unknown_tag_instance(auth_server):
+    server, cookies = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, "id", cookies=cookies
+    )
+    assert rsp.status_code == 400  # pylint: disable=duplicate-code
+
+
+def test_existing_tag_definition(auth_server, tag_def_user_profile):
+    # pylint: disable=duplicate-code
+    server, cookies = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, tag_def_user_profile.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    rsp = req.get_refresh(server.url, cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert [tag_def["id_persistent"] for tag_def in json["tag_definition_list"]] == [
+        tag_def_user_profile.id_persistent
+    ]
+
+
+def test_existing_tag_definition_multiple(
+    auth_server, tag_def_user_profile, tag_def_user_profile1
+):
+    # pylint: disable=duplicate-code
+    server, cookies = auth_server
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, tag_def_user_profile.id_persistent, cookies=cookies
+    )
+    rsp = req.post_append_id_tag_definition_persistent(
+        server.url, tag_def_user_profile1.id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    rsp = req.get_refresh(server.url, cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert [tag_def["id_persistent"] for tag_def in json["tag_definition_list"]] == [
+        tag_def_user_profile.id_persistent,
+        tag_def_user_profile1.id_persistent,
+    ]

tests/user/conftest.py
@@ -1,0 +1,66 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,no-member,redefined-outer-name
+import pytest
+
+import tests.user.common as c
+from cosmae.tag.models_django import TagDefinition
+
+
+@pytest.fixture
+def tag_def_user_profile():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def,
+        id_persistent=c.id_tag_def_persistent,
+        time_edit=c.time_edit_tag_def,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def tag_def_user_profile1():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def1,
+        id_persistent=c.id_tag_def_persistent1,
+        time_edit=c.time_edit_tag_def1,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def tag_def_user_profile2():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def2,
+        id_persistent=c.id_tag_def_persistent2,
+        time_edit=c.time_edit_tag_def2,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def tag_def_user_profile3():
+    return TagDefinition.objects.create(
+        name=c.name_tag_def3,
+        id_persistent=c.id_tag_def_persistent3,
+        time_edit=c.time_edit_tag_def3,
+        type=TagDefinition.STRING,
+    )
+
+
+@pytest.fixture
+def user_with_tag_defs(
+    user,
+    tag_def_user_profile,
+    tag_def_user_profile1,
+    tag_def_user_profile2,
+    tag_def_user_profile3,
+):
+    user.tag_definitions = [
+        tag_def.id_persistent
+        for tag_def in [
+            tag_def_user_profile,
+            tag_def_user_profile1,
+            tag_def_user_profile2,
+            tag_def_user_profile3,
+        ]
+    ]
+    user.save()
+    return user