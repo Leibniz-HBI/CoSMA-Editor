Leibniz-HBI/DP-REX

Add method for getting tag values by entity and tag definitions in context of contributions to backend.

vran/migrations/0015_alter_mergerequest_contribution_candidate.py
@@ -1,0 +1,24 @@
+# Generated by Django 4.2.2 on 2023-07-19 10:21
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0014_mergerequest_state"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="mergerequest",
+            name="contribution_candidate",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="vran.contributioncandidate",
+            ),
+        ),
+    ]

vran/tag/api/instances.py
@@ -4,6 +4,8 @@ from typing import List, Optional
 from uuid import uuid4
 
 from django.db import IntegrityError
+from django.db.models import Value
+from django.http import HttpRequest
 from ninja import Router, Schema
 
 from vran.exception import (
@@ -11,11 +13,14 @@ from vran.exception import (
     EntityMissingException,
     EntityUpdatedException,
     InvalidTagValueException,
+    NotAuthenticatedException,
     TagDefinitionMissingException,
     TagInstanceExistsException,
     ValidationException,
 )
+from vran.merge_request.models_django import MergeRequest
 from vran.tag.models_django import TagInstance as TagInstanceDb
+from vran.util.auth import check_user
 from vran.util.django import save_many_atomic
 
 router = Router()
@@ -42,15 +47,29 @@ class TagInstancePostList(Schema):
 
 class TagInstancePostChunkRequest(Schema):
     # pylint: disable=too-few-public-methods
-    "Request body for geting instances of a tag."
+    "Request body for getting instances of a tag."
     id_tag_definition_persistent: str
     offset: int
     limit: int
 
 
+class TagInstanceForEntitiesPostRequest(Schema):
+    "Request body for getting tag instances for a set of entities"
+    # pylint: disable=too-few-public-methods
+    id_tag_definition_persistent_list: List[str]
+    id_entity_persistent_list: List[str]
+    id_merge_request_persistent: Optional[str]
+    """When a merge request is referenced,
+    the values of the origin tag are also returned. when querying for the destination."""
+    id_contribution_persistent: Optional[str]
+    """When a contribution is referenced, all merge requests contained are considered.
+    I.e., for all merge requests of the contribution,
+    when the destination tag is queried values for the origin tag are also returned."""
+
+
 class TagInstanceValueRequest(Schema):
     # pylint: disable=too-few-public-methods
-    "Reuqest body for getting a specific value"
+    "Request body for getting a specific value"
     id_entity_persistent: str
     id_tag_definition_persistent: str
 
@@ -69,15 +88,30 @@ class TagInstanceValueResponse(Schema):
     values: List[TagInstancePost]
 
 
+class TagInstanceValueWithExistingFlagResponse(TagInstancePost):
+    # pylint: disable= too-few-public-methods
+    """Tag instance value with a flag for marking data as existing.
+    This is used in context of retrieving values of tag definitions
+    that are part of a merge request or the entity review of contributions."""
+    is_existing: bool
+    id_tag_definition_requested_persistent: str
+
+
 class TagInstanceValueResponseList(Schema):
     # pylint: disable=too-few-public-methods
     "Multiple Value request responses"
     value_responses: List[TagInstanceValueResponse]
 
 
+class TagInstanceForEntitiesPostResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    "Response body for getting tag instances for a set of entities."
+    value_responses: List[TagInstanceValueWithExistingFlagResponse]
+
+
 class TagInstanceUpdatedResponse(Schema):
     # pylint: disable=too-few-public-methods
-    "Informatation on updates when submitting values"
+    "Information on updates when submitting values"
     msg: str
     tag_instances: List[TagInstancePost]
 
@@ -191,8 +225,68 @@ def post_tag_instance_values(_, values_req: TagInstanceValueRequestList):
         return 500, ApiError(msg="Could not get requested values.")
 
 
+@router.post(
+    "entities",
+    response={
+        200: TagInstanceForEntitiesPostResponse,
+        400: ApiError,
+        401: ApiError,
+        404: ApiError,
+        500: ApiError,
+    },
+)
+def post_tag_instances_for_entities(
+    request: HttpRequest, request_data: TagInstanceForEntitiesPostRequest
+):
+    """API method for getting tag instances for a list of entities.
+    Also include tag instances of tag definitions that are related by
+    contribution or merge request."""
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated")
+    if (
+        len(request_data.id_entity_persistent_list) == 0
+        or len(request_data.id_tag_definition_persistent_list) == 0
+    ):
+        return 200, TagInstanceForEntitiesPostResponse(value_responses=[])
+    try:
+        instances_all = TagInstanceDb.objects.none()  # pylint: disable=no-member
+        for (
+            id_tag_definition_persistent
+        ) in request_data.id_tag_definition_persistent_list:
+            tag_definitions = MergeRequest.get_tag_definitions_for_entities_request(
+                id_tag_definition_persistent,
+                request_data.id_contribution_persistent,
+                request_data.id_merge_request_persistent,
+                user,
+            )
+            for id_tag_def, is_existing in tag_definitions:
+                instances_for_tag = TagInstanceDb.for_entities(
+                    id_tag_def,
+                    request_data.id_entity_persistent_list,
+                ).annotate(
+                    is_existing=Value(is_existing),
+                    id_tag_definition_requested_persistent=Value(
+                        id_tag_definition_persistent
+                    ),
+                )
+                instances_all = instances_all.union(instances_for_tag)
+        return 200, TagInstanceForEntitiesPostResponse(
+            value_responses=[
+                tag_instance_with_existing_db_to_api(tag_instance)
+                for tag_instance in instances_all
+            ]
+        )
+
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not get the tag instances for the provided entities."
+        )
+
+
 def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
-    "Convert a tag instance from API to databse representation."
+    "Convert a tag instance from API to database representation."
     if tag_api.id_persistent:
         persistent_id = tag_api.id_persistent
         if tag_api.version is None:
@@ -219,11 +313,24 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
 
 
 def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
-    "Convert tag istances from databse to API representation."
+    "Convert tag instances from database to API representation."
     return TagInstancePost(
         id_persistent=tag_db.id_persistent,
         id_entity_persistent=tag_db.id_entity_persistent,
         id_tag_definition_persistent=tag_db.id_tag_definition_persistent,
         value=tag_db.value,
         version=tag_db.id,
+    )
+
+
+def tag_instance_with_existing_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+    "Convert tag instances from database to API representation."
+    return TagInstanceValueWithExistingFlagResponse(
+        id_persistent=tag_db.id_persistent,
+        id_entity_persistent=tag_db.id_entity_persistent,
+        id_tag_definition_persistent=tag_db.id_tag_definition_persistent,
+        value=tag_db.value,
+        version=tag_db.id,
+        is_existing=tag_db.is_existing,
+        id_tag_definition_requested_persistent=tag_db.id_tag_definition_requested_persistent,
     )

tests/tag/api/integration/requests.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,too-many-arguments
 from urllib.parse import urljoin
 
 import requests
@@ -79,6 +79,27 @@ def post_tag_instances_values(url, id_persistent_pairs, cookies):
                 }
                 for id_entity_persistent, id_tag_definition_persistent in id_persistent_pairs
             ]
+        },
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def post_tag_instances_for_entities(
+    url,
+    id_entity_persistent_list,
+    id_tag_definition_persistent_list,
+    id_contribution_persistent=None,
+    id_merge_request_persistent=None,
+    cookies=None,
+):
+    return requests.post(
+        urljoin(url, "vran/api/tags/entities"),
+        json={
+            "id_tag_definition_persistent_list": id_tag_definition_persistent_list,
+            "id_entity_persistent_list": id_entity_persistent_list,
+            "id_contribution_persistent": id_contribution_persistent,
+            "id_merge_request_persistent": id_merge_request_persistent,
         },
         cookies=cookies,
         timeout=900,

tests/tag/conftest.py
@@ -3,8 +3,9 @@ from datetime import timedelta
 
 import pytest
 
-from tests.tag import common as c
-from vran.tag.models_django import TagDefinition
+import tests.entity.common as ce
+import tests.tag.common as c
+from vran.tag.models_django import TagDefinition, TagInstance
 
 
 @pytest.fixture
@@ -21,14 +22,30 @@ def tag_def():
 
 @pytest.fixture
 def tag_def_user(user):
-    return TagDefinition(
-        id_persistent=c.id_tag_def_persistent_test + "user",
+    tag_def = TagDefinition(  # pylint: disable=no-member
+        id_persistent=c.id_tag_def_persistent_test_user,
         type=TagDefinition.FLOAT,
         id_parent_persistent=None,
         name=c.name_tag_def_test,
         time_edit=c.time_edit_test,
         owner=user,
     )
+    tag_def.save()
+    return tag_def
+
+
+@pytest.fixture
+def tag_def_user1(user):
+    tag_def = TagDefinition(  # pylint: disable=no-member
+        id_persistent=c.id_tag_def_persistent_test_user1,
+        type=TagDefinition.FLOAT,
+        id_parent_persistent=None,
+        name=c.name_tag_def_test1,
+        time_edit=c.time_edit_test,
+        owner=user,
+    )
+    tag_def.save()
+    return tag_def
 
 
 @pytest.fixture
@@ -45,7 +62,7 @@ def tag_def_child_0():
 
 @pytest.fixture
 def tag_def_child_1():
-    "Aanother shared child tag definition for tests"
+    "Another shared child tag definition for tests"
     return TagDefinition(
         id_persistent=c.id_tag_def_persistent_child_1,
         type=TagDefinition.FLOAT,
@@ -53,3 +70,39 @@ def tag_def_child_1():
         name="test tag definition child 1",
         time_edit=c.time_edit_test + timedelta(seconds=10),
     )
+
+
+@pytest.fixture
+def tag_instances_user():
+    tag_inst = TagInstance(
+        id_persistent=c.id_instance_test0,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
+        id_entity_persistent=ce.id_persistent_test_0,
+        value="value",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_inst1 = TagInstance(
+        id_persistent=c.id_instance_test1,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
+        id_entity_persistent=ce.id_persistent_test_1,
+        value="value 1",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_inst2 = TagInstance(
+        id_persistent=c.id_instance_test2,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user1,
+        id_entity_persistent=ce.id_persistent_test_0,
+        value="value 2",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_inst3 = TagInstance(
+        id_persistent=c.id_instance_test3,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user1,
+        id_entity_persistent=ce.id_persistent_test_1,
+        value="value 3",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_instances = [tag_inst, tag_inst1, tag_inst2, tag_inst3]
+    for inst in tag_instances:
+        inst.save()
+    return tag_instances

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0015_alter_mergerequest_contribution_candidate.py
@@ -1,0 +1,24 @@
+# Generated by Django 4.2.2 on 2023-07-19 10:21
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0014_mergerequest_state"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="mergerequest",
+            name="contribution_candidate",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="cosmae.contributioncandidate",
+            ),
+        ),
+    ]

cosmae/tag/api/instances.py
@@ -4,6 +4,8 @@ from typing import List, Optional
 from uuid import uuid4
 
 from django.db import IntegrityError
+from django.db.models import Value
+from django.http import HttpRequest
 from ninja import Router, Schema
 
 from cosmae.exception import (
@@ -11,11 +13,14 @@ from cosmae.exception import (
     EntityMissingException,
     EntityUpdatedException,
     InvalidTagValueException,
+    NotAuthenticatedException,
     TagDefinitionMissingException,
     TagInstanceExistsException,
     ValidationException,
 )
+from cosmae.merge_request.models_django import MergeRequest
 from cosmae.tag.models_django import TagInstance as TagInstanceDb
+from cosmae.util.auth import check_user
 from cosmae.util.django import save_many_atomic
 
 router = Router()
@@ -42,15 +47,29 @@ class TagInstancePostList(Schema):
 
 class TagInstancePostChunkRequest(Schema):
     # pylint: disable=too-few-public-methods
-    "Request body for geting instances of a tag."
+    "Request body for getting instances of a tag."
     id_tag_definition_persistent: str
     offset: int
     limit: int
 
 
+class TagInstanceForEntitiesPostRequest(Schema):
+    "Request body for getting tag instances for a set of entities"
+    # pylint: disable=too-few-public-methods
+    id_tag_definition_persistent_list: List[str]
+    id_entity_persistent_list: List[str]
+    id_merge_request_persistent: Optional[str]
+    """When a merge request is referenced,
+    the values of the origin tag are also returned. when querying for the destination."""
+    id_contribution_persistent: Optional[str]
+    """When a contribution is referenced, all merge requests contained are considered.
+    I.e., for all merge requests of the contribution,
+    when the destination tag is queried values for the origin tag are also returned."""
+
+
 class TagInstanceValueRequest(Schema):
     # pylint: disable=too-few-public-methods
-    "Reuqest body for getting a specific value"
+    "Request body for getting a specific value"
     id_entity_persistent: str
     id_tag_definition_persistent: str
 
@@ -69,15 +88,30 @@ class TagInstanceValueResponse(Schema):
     values: List[TagInstancePost]
 
 
+class TagInstanceValueWithExistingFlagResponse(TagInstancePost):
+    # pylint: disable= too-few-public-methods
+    """Tag instance value with a flag for marking data as existing.
+    This is used in context of retrieving values of tag definitions
+    that are part of a merge request or the entity review of contributions."""
+    is_existing: bool
+    id_tag_definition_requested_persistent: str
+
+
 class TagInstanceValueResponseList(Schema):
     # pylint: disable=too-few-public-methods
     "Multiple Value request responses"
     value_responses: List[TagInstanceValueResponse]
 
 
+class TagInstanceForEntitiesPostResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    "Response body for getting tag instances for a set of entities."
+    value_responses: List[TagInstanceValueWithExistingFlagResponse]
+
+
 class TagInstanceUpdatedResponse(Schema):
     # pylint: disable=too-few-public-methods
-    "Informatation on updates when submitting values"
+    "Information on updates when submitting values"
     msg: str
     tag_instances: List[TagInstancePost]
 
@@ -191,8 +225,68 @@ def post_tag_instance_values(_, values_req: TagInstanceValueRequestList):
         return 500, ApiError(msg="Could not get requested values.")
 
 
+@router.post(
+    "entities",
+    response={
+        200: TagInstanceForEntitiesPostResponse,
+        400: ApiError,
+        401: ApiError,
+        404: ApiError,
+        500: ApiError,
+    },
+)
+def post_tag_instances_for_entities(
+    request: HttpRequest, request_data: TagInstanceForEntitiesPostRequest
+):
+    """API method for getting tag instances for a list of entities.
+    Also include tag instances of tag definitions that are related by
+    contribution or merge request."""
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated")
+    if (
+        len(request_data.id_entity_persistent_list) == 0
+        or len(request_data.id_tag_definition_persistent_list) == 0
+    ):
+        return 200, TagInstanceForEntitiesPostResponse(value_responses=[])
+    try:
+        instances_all = TagInstanceDb.objects.none()  # pylint: disable=no-member
+        for (
+            id_tag_definition_persistent
+        ) in request_data.id_tag_definition_persistent_list:
+            tag_definitions = MergeRequest.get_tag_definitions_for_entities_request(
+                id_tag_definition_persistent,
+                request_data.id_contribution_persistent,
+                request_data.id_merge_request_persistent,
+                user,
+            )
+            for id_tag_def, is_existing in tag_definitions:
+                instances_for_tag = TagInstanceDb.for_entities(
+                    id_tag_def,
+                    request_data.id_entity_persistent_list,
+                ).annotate(
+                    is_existing=Value(is_existing),
+                    id_tag_definition_requested_persistent=Value(
+                        id_tag_definition_persistent
+                    ),
+                )
+                instances_all = instances_all.union(instances_for_tag)
+        return 200, TagInstanceForEntitiesPostResponse(
+            value_responses=[
+                tag_instance_with_existing_db_to_api(tag_instance)
+                for tag_instance in instances_all
+            ]
+        )
+
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(
+            msg="Could not get the tag instances for the provided entities."
+        )
+
+
 def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
-    "Convert a tag instance from API to databse representation."
+    "Convert a tag instance from API to database representation."
     if tag_api.id_persistent:
         persistent_id = tag_api.id_persistent
         if tag_api.version is None:
@@ -219,11 +313,24 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
 
 
 def tag_instance_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
-    "Convert tag istances from databse to API representation."
+    "Convert tag instances from database to API representation."
     return TagInstancePost(
         id_persistent=tag_db.id_persistent,
         id_entity_persistent=tag_db.id_entity_persistent,
         id_tag_definition_persistent=tag_db.id_tag_definition_persistent,
         value=tag_db.value,
         version=tag_db.id,
+    )
+
+
+def tag_instance_with_existing_db_to_api(tag_db: TagInstanceDb) -> TagInstancePost:
+    "Convert tag instances from database to API representation."
+    return TagInstanceValueWithExistingFlagResponse(
+        id_persistent=tag_db.id_persistent,
+        id_entity_persistent=tag_db.id_entity_persistent,
+        id_tag_definition_persistent=tag_db.id_tag_definition_persistent,
+        value=tag_db.value,
+        version=tag_db.id,
+        is_existing=tag_db.is_existing,
+        id_tag_definition_requested_persistent=tag_db.id_tag_definition_requested_persistent,
     )

tests/tag/api/integration/requests.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,too-many-arguments
 from urllib.parse import urljoin
 
 import requests
@@ -79,6 +79,27 @@ def post_tag_instances_values(url, id_persistent_pairs, cookies):
                 }
                 for id_entity_persistent, id_tag_definition_persistent in id_persistent_pairs
             ]
+        },
+        cookies=cookies,
+        timeout=900,
+    )
+
+
+def post_tag_instances_for_entities(
+    url,
+    id_entity_persistent_list,
+    id_tag_definition_persistent_list,
+    id_contribution_persistent=None,
+    id_merge_request_persistent=None,
+    cookies=None,
+):
+    return requests.post(
+        urljoin(url, "cosmae/api/tags/entities"),
+        json={
+            "id_tag_definition_persistent_list": id_tag_definition_persistent_list,
+            "id_entity_persistent_list": id_entity_persistent_list,
+            "id_contribution_persistent": id_contribution_persistent,
+            "id_merge_request_persistent": id_merge_request_persistent,
         },
         cookies=cookies,
         timeout=900,

tests/tag/conftest.py
@@ -3,8 +3,9 @@ from datetime import timedelta
 
 import pytest
 
-from tests.tag import common as c
-from cosmae.tag.models_django import TagDefinition
+import tests.entity.common as ce
+import tests.tag.common as c
+from cosmae.tag.models_django import TagDefinition, TagInstance
 
 
 @pytest.fixture
@@ -21,14 +22,30 @@ def tag_def():
 
 @pytest.fixture
 def tag_def_user(user):
-    return TagDefinition(
-        id_persistent=c.id_tag_def_persistent_test + "user",
+    tag_def = TagDefinition(  # pylint: disable=no-member
+        id_persistent=c.id_tag_def_persistent_test_user,
         type=TagDefinition.FLOAT,
         id_parent_persistent=None,
         name=c.name_tag_def_test,
         time_edit=c.time_edit_test,
         owner=user,
     )
+    tag_def.save()
+    return tag_def
+
+
+@pytest.fixture
+def tag_def_user1(user):
+    tag_def = TagDefinition(  # pylint: disable=no-member
+        id_persistent=c.id_tag_def_persistent_test_user1,
+        type=TagDefinition.FLOAT,
+        id_parent_persistent=None,
+        name=c.name_tag_def_test1,
+        time_edit=c.time_edit_test,
+        owner=user,
+    )
+    tag_def.save()
+    return tag_def
 
 
 @pytest.fixture
@@ -45,7 +62,7 @@ def tag_def_child_0():
 
 @pytest.fixture
 def tag_def_child_1():
-    "Aanother shared child tag definition for tests"
+    "Another shared child tag definition for tests"
     return TagDefinition(
         id_persistent=c.id_tag_def_persistent_child_1,
         type=TagDefinition.FLOAT,
@@ -53,3 +70,39 @@ def tag_def_child_1():
         name="test tag definition child 1",
         time_edit=c.time_edit_test + timedelta(seconds=10),
     )
+
+
+@pytest.fixture
+def tag_instances_user():
+    tag_inst = TagInstance(
+        id_persistent=c.id_instance_test0,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
+        id_entity_persistent=ce.id_persistent_test_0,
+        value="value",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_inst1 = TagInstance(
+        id_persistent=c.id_instance_test1,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
+        id_entity_persistent=ce.id_persistent_test_1,
+        value="value 1",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_inst2 = TagInstance(
+        id_persistent=c.id_instance_test2,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user1,
+        id_entity_persistent=ce.id_persistent_test_0,
+        value="value 2",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_inst3 = TagInstance(
+        id_persistent=c.id_instance_test3,
+        id_tag_definition_persistent=c.id_tag_def_persistent_test_user1,
+        id_entity_persistent=ce.id_persistent_test_1,
+        value="value 3",
+        time_edit=c.time_edit_instance_test,
+    )
+    tag_instances = [tag_inst, tag_inst1, tag_inst2, tag_inst3]
+    for inst in tag_instances:
+        inst.save()
+    return tag_instances