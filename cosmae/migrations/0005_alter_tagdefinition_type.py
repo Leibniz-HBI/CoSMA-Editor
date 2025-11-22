Leibniz-HBI/DP-REX

Change API value type to string.

vran/migrations/0005_alter_tagdefinition_type.py
@@ -1,0 +1,22 @@
+# Generated by Django 4.1.3 on 2023-01-30 13:29
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0004_alter_tagdefinition_type"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tagdefinition",
+            name="type",
+            field=models.CharField(
+                choices=[("INR", "inner"), ("FLT", "float"), ("STR", "string")],
+                default="INR",
+                max_length=3,
+            ),
+        ),
+    ]

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0005_alter_tagdefinition_type.py
@@ -1,0 +1,22 @@
+# Generated by Django 4.1.3 on 2023-01-30 13:29
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0004_alter_tagdefinition_type"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="tagdefinition",
+            name="type",
+            field=models.CharField(
+                choices=[("INR", "inner"), ("FLT", "float"), ("STR", "string")],
+                default="INR",
+                max_length=3,
+            ),
+        ),
+    ]