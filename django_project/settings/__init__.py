Leibniz-HBI/DP-REX

Add embedded redis to tests.

django_project/settings/__init__.py
@@ -8,7 +8,11 @@ try:
 except ImportError:
     pass
 
-if environ.get("VRAN_DEBUG", "false").lower() == "true":
+if environ.get("VRAN_CI", "false").lower() == "true":
+    print("import CI config")
+    from django_project.settings.settings_ci import *
+elif environ.get("VRAN_DEBUG", "false").lower() == "true":
+    print("import test config")
     from django_project.settings.settings_test import *
 else:
     from django_project.settings.settings_production import *

django_project/settings/settings_ci.py
@@ -39,13 +39,9 @@ def SECRET_KEY():  # pylint: disable=invalid-name
 
 def get_docker_compose_secret(secret_name):
     "Get secret as provided by docker_compose"
-    try:
-        with open(f"/run/secrets/{secret_name}", encoding="utf-8") as key_file:
-            key = key_file.readline().rstrip("\n")
-    except FileNotFoundError:
-        env_var_name = secret_name.upper()
-        key = environ.get(env_var_name)
-    if not key:
+    env_var_name = secret_name.upper()
+    key = environ.get(env_var_name)
+    if key is None:
         raise Exception(  # pylint: disable=broad-exception-raised
             f"Please set the {secret_name} secret."
         )
@@ -172,8 +168,8 @@ AUTH_USER_MODEL = "vran.VranUser"
 
 RQ_QUEUES = {
     "default": {
-        "HOST": "vran_redis",
-        "PORT": 6379,
+        "HOST": "localhost",
+        "PORT": get_docker_compose_secret("vran_redis_port"),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
         "PASSWORD": get_docker_compose_secret("vran_redis_password"),

django_project/settings/settings_test.py
@@ -11,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,10 +41,14 @@ def SECRET_KEY():  # pylint: disable=invalid-name
 
 def get_file_secret(secret_name):
     "Get secret as provided by docker_compose"
-    with open(
-        f"{BASE_DIR.absolute()}/.secrets_local/{secret_name}", encoding="utf-8"
-    ) as key_file:
-        key = key_file.readline().rstrip("\n")
+    try:
+        with open(
+            f"{BASE_DIR.absolute()}/.secrets_local/{secret_name}", encoding="utf-8"
+        ) as key_file:
+            key = key_file.readline().rstrip("\n")
+    except FileNotFoundError:
+        env_var_name = secret_name.upper()
+        key = environ.get(env_var_name)
     if not key:
         raise Exception(  # pylint: disable=broad-exception-raised
             f"Please set the {secret_name} secret."
@@ -179,7 +184,7 @@ AUTH_USER_MODEL = "vran.VranUser"
 RQ_QUEUES = {
     "default": {
         "HOST": "localhost",
-        "PORT": 6379,
+        "PORT": get_file_secret("vran_redis_port"),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
     }

tests/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,7 +132,7 @@ CONTRIBUTION_DIRECTORY = "/tmp"
 RQ_QUEUES = {
     "default": {
         "HOST": "localhost",
-        "PORT": 6379,
+        "PORT": environ.get("VRAN_REDIS_PORT", 6379),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
     }

Leibniz-HBI/CoSMA-Editor

django_project/settings/__init__.py
@@ -8,7 +8,11 @@ try:
 except ImportError:
     pass
 
-if environ.get("COSMAE_DEBUG", "false").lower() == "true":
+if environ.get("COSMAE_CI", "false").lower() == "true":
+    print("import CI config")
+    from django_project.settings.settings_ci import *
+elif environ.get("COSMAE_DEBUG", "false").lower() == "true":
+    print("import test config")
     from django_project.settings.settings_test import *
 else:
     from django_project.settings.settings_production import *

django_project/settings/settings_ci.py
@@ -39,13 +39,9 @@ def SECRET_KEY():  # pylint: disable=invalid-name
 
 def get_docker_compose_secret(secret_name):
     "Get secret as provided by docker_compose"
-    try:
-        with open(f"/run/secrets/{secret_name}", encoding="utf-8") as key_file:
-            key = key_file.readline().rstrip("\n")
-    except FileNotFoundError:
-        env_var_name = secret_name.upper()
-        key = environ.get(env_var_name)
-    if not key:
+    env_var_name = secret_name.upper()
+    key = environ.get(env_var_name)
+    if key is None:
         raise Exception(  # pylint: disable=broad-exception-raised
             f"Please set the {secret_name} secret."
         )
@@ -172,8 +168,8 @@ AUTH_USER_MODEL = "cosmae.CosmaeUser"
 
 RQ_QUEUES = {
     "default": {
-        "HOST": "cosmae_redis",
-        "PORT": 6379,
+        "HOST": "localhost",
+        "PORT": get_docker_compose_secret("cosmae_redis_port"),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
         "PASSWORD": get_docker_compose_secret("cosmae_redis_password"),

django_project/settings/settings_test.py
@@ -11,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,10 +41,14 @@ def SECRET_KEY():  # pylint: disable=invalid-name
 
 def get_file_secret(secret_name):
     "Get secret as provided by docker_compose"
-    with open(
-        f"{BASE_DIR.absolute()}/.secrets_local/{secret_name}", encoding="utf-8"
-    ) as key_file:
-        key = key_file.readline().rstrip("\n")
+    try:
+        with open(
+            f"{BASE_DIR.absolute()}/.secrets_local/{secret_name}", encoding="utf-8"
+        ) as key_file:
+            key = key_file.readline().rstrip("\n")
+    except FileNotFoundError:
+        env_var_name = secret_name.upper()
+        key = environ.get(env_var_name)
     if not key:
         raise Exception(  # pylint: disable=broad-exception-raised
             f"Please set the {secret_name} secret."
@@ -179,7 +184,7 @@ AUTH_USER_MODEL = "cosmae.CosmaeUser"
 RQ_QUEUES = {
     "default": {
         "HOST": "localhost",
-        "PORT": 6379,
+        "PORT": get_file_secret("cosmae_redis_port"),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
     }

tests/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,7 +132,7 @@ CONTRIBUTION_DIRECTORY = "/tmp"
 RQ_QUEUES = {
     "default": {
         "HOST": "localhost",
-        "PORT": 6379,
+        "PORT": environ.get("COSMAE_REDIS_PORT", 6379),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
     }