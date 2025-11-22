Leibniz-HBI/DP-REX

Add methods for entity merge requests to API.

vran/contribution/tag_definition/queue/ingest.py
@@ -10,7 +10,7 @@ from vran.contribution.models_django import ContributionCandidate
 from vran.contribution.tag_definition.models_django import TagDefinitionContribution
 from vran.contribution.tag_definition.queue.util import read_csv_of_candidate
 from vran.entity.models_django import Entity
-from vran.merge_request.models_django import MergeRequest
+from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.models_django import TagDefinition, TagInstance
 
 
@@ -93,7 +93,7 @@ def ingest_values_from_csv(id_contribution_persistent):
                     )
                     tag_instance.save()
             for origin, destination in tag_definition_pairs:
-                MergeRequest(
+                TagMergeRequest(
                     id_persistent=uuid4(),
                     id_origin_persistent=origin.id_persistent,
                     id_destination_persistent=destination.id_persistent,

vran/migrations/0023_rename_mergerequest_tagmergerequest_entity_disabled.py
@@ -1,0 +1,21 @@
+# Generated by Django 4.2.2 on 2023-11-10 08:15
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("vran", "0022_contributioncandidate_error_msg_and_more"),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name="MergeRequest",
+            new_name="TagMergeRequest",
+        ),
+        migrations.AddField(
+            model_name="entity",
+            name="disabled",
+            field=models.BooleanField(default=False),
+        ),
+    ]

vran/models.py
@@ -7,6 +7,10 @@ from vran.contribution.tag_definition.models_django import (
     TagInstanceContribution,
 )
 from vran.entity.models_django import Entity
-from vran.merge_request.models_django import ConflictResolution, MergeRequest
+from vran.merge_request.entity.models_django import (
+    EntityConflictResolution,
+    EntityMergeRequest,
+)
+from vran.merge_request.models_django import TagConflictResolution, TagMergeRequest
 from vran.tag.models_django import TagDefinition, TagInstance
 from vran.util import VranUser

vran/tag/api/instances.py
@@ -19,7 +19,7 @@ from vran.exception import (
     TagInstanceExistsException,
     ValidationException,
 )
-from vran.merge_request.models_django import MergeRequest
+from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.api.definitions import tag_definition_db_to_api
 from vran.tag.api.models_api import TagDefinitionResponse
 from vran.tag.models_django import TagInstance as TagInstanceDb
@@ -267,7 +267,7 @@ def post_tag_instances_for_entities(
         for (
             id_tag_definition_persistent
         ) in request_data.id_tag_definition_persistent_list:
-            tag_definitions = MergeRequest.get_tag_definitions_for_entities_request(
+            tag_definitions = TagMergeRequest.get_tag_definitions_for_entities_request(
                 id_tag_definition_persistent,
                 request_data.id_contribution_persistent,
                 request_data.id_merge_request_persistent,

tests/merge_request/conftest.py
@@ -4,7 +4,7 @@ import pytest
 import tests.entity.common as ce
 import tests.merge_request.common as c
 from vran.contribution.models_django import ContributionCandidate
-from vran.merge_request.models_django import ConflictResolution, MergeRequest
+from vran.merge_request.models_django import TagConflictResolution, TagMergeRequest
 from vran.tag.models_django import TagDefinition, TagInstance
 
 
@@ -82,7 +82,7 @@ def contribution_for_mr(db, user1):
 def merge_request_user_fast_forward(
     db, origin_tag_def_for_mr, destination_tag_def_for_mr_user1, contribution_for_mr
 ):
-    return MergeRequest.objects.create(  # pylint: disable=no-member
+    return TagMergeRequest.objects.create(  # pylint: disable=no-member
         id_origin_persistent=origin_tag_def_for_mr.id_persistent,
         id_destination_persistent=destination_tag_def_for_mr_user1.id_persistent,
         created_by=origin_tag_def_for_mr.owner,
@@ -97,7 +97,7 @@ def merge_request_user_fast_forward(
 def merge_request_user(
     db, origin_tag_def_for_mr, destination_tag_def_for_mr, contribution_for_mr
 ):
-    return MergeRequest.objects.create(  # pylint: disable=no-member
+    return TagMergeRequest.objects.create(  # pylint: disable=no-member
         id_origin_persistent=origin_tag_def_for_mr.id_persistent,
         id_destination_persistent=destination_tag_def_for_mr.id_persistent,
         created_by=origin_tag_def_for_mr.owner,
@@ -147,7 +147,7 @@ def contribution_for_mr1(db, user):
 def merge_request_user1(
     db, destination_tag_def_for_mr1, origin_tag_def_for_mr1, contribution_for_mr1
 ):
-    return MergeRequest.objects.create(  # pylint: disable=no-member
+    return TagMergeRequest.objects.create(  # pylint: disable=no-member
         id_destination_persistent=destination_tag_def_for_mr1.id_persistent,
         id_origin_persistent=origin_tag_def_for_mr1.id_persistent,
         created_by=origin_tag_def_for_mr1.owner,
@@ -289,7 +289,7 @@ def conflict_resolution_replace(
     instances_merge_request_origin_user,
     instance_merge_request_destination_user_conflict,
 ):
-    return ConflictResolution.objects.create(  # pylint: disable=no-member
+    return TagConflictResolution.objects.create(  # pylint: disable=no-member
         entity=entity1,
         tag_definition_origin=origin_tag_def_for_mr,
         tag_definition_destination=destination_tag_def_for_mr,
@@ -309,7 +309,7 @@ def conflict_resolution_keep(
     instances_merge_request_origin_user,
     instance_merge_request_destination_user_conflict,
 ):
-    return ConflictResolution.objects.create(  # pylint: disable=no-member
+    return TagConflictResolution.objects.create(  # pylint: disable=no-member
         entity=entity0,
         tag_definition_origin=origin_tag_def_for_mr,
         tag_definition_destination=destination_tag_def_for_mr,
@@ -329,7 +329,7 @@ def conflict_resolution_keep_fast_forward(
     instances_merge_request_origin_user,
     instance_merge_request_destination_user_conflict_fast_forward,
 ):
-    return ConflictResolution.objects.create(  # pylint: disable=no-member
+    return TagConflictResolution.objects.create(  # pylint: disable=no-member
         entity=entity1,
         tag_definition_origin=origin_tag_def_for_mr,
         tag_definition_destination=destination_tag_def_for_mr,

tests/merge_request/entity/api/integration/get_entity_merge_requests_test.py
@@ -1,0 +1,68 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+from tests.merge_request.entity import common as c
+from tests.merge_request.entity.api.integration import requests as req
+from tests.user import common as cu
+from tests.utils import assert_versioned
+from vran.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server_commissioner):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server_commissioner
+    with patch("vran.merge_request.entity.api.check_user", mock):
+        rsp = req.get_merge_requests(server.url, cookies=cookies)
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server_commissioner):
+    server, _ = auth_server_commissioner
+    rsp = req.get_merge_requests(server.url)
+    assert rsp.status_code == 401
+
+
+def test_normal_user(auth_server):
+    server, cookies = auth_server
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_no_mr(auth_server_commissioner):
+    server, cookies = auth_server_commissioner
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert json == {"entity_merge_requests": []}
+
+
+def test_success(auth_server_commissioner, merge_request_user):
+    server, cookies = auth_server_commissioner
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert_versioned(
+        json,
+        {
+            "entity_merge_requests": [
+                {
+                    "id_persistent": c.id_merge_request_persistent,
+                    "origin": {
+                        "display_txt": c.display_txt_entity_origin,
+                        "id_persistent": c.id_entity_origin_persistent,
+                    },
+                    "destination": {
+                        "id_persistent": c.id_entity_destination_persistent,
+                        "display_txt": c.display_txt_entity_destination,
+                    },
+                    "created_by": {
+                        "id_persistent": cu.test_uuid_commissioner,
+                        "user_name": cu.test_username_commissioner,
+                        "permission_group": "COMMISSIONER",
+                    },
+                    "state": "OPEN",
+                },
+            ]
+        },
+    )

tests/merge_request/entity/api/integration/get_merge_request_test.py
@@ -1,0 +1,73 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+from tests.merge_request.entity import common as c
+from tests.merge_request.entity.api.integration import requests as req
+from tests.user import common as cu
+from tests.utils import assert_versioned
+from vran.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server_commissioner):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server_commissioner
+    with patch("vran.merge_request.entity.api.check_user", mock):
+        rsp = req.get_merge_request(
+            server.url,
+            c.id_merge_request_persistent,
+            cookies=cookies,
+        )
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server_commissioner):
+    server, _ = auth_server_commissioner
+    rsp = req.get_merge_request(
+        server.url,
+        c.id_merge_request_persistent,
+    )
+    assert rsp.status_code == 401
+
+
+def test_normal_user(auth_server):
+    server, cookies = auth_server
+    rsp = req.get_merge_request(
+        server.url,
+        c.id_merge_request_persistent,
+        cookies=cookies,
+    )
+    assert rsp.status_code == 403
+
+
+def test_get_entity_merge_request(auth_server_commissioner, merge_request_user):
+    server, cookies = auth_server_commissioner
+    uuidMock = MagicMock()
+    uuidMock.return_value = c.id_merge_request_persistent
+    rsp = req.get_merge_request(
+        server.url,
+        merge_request_user.id_persistent,
+        cookies=cookies,
+    )
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert_versioned(
+        json,
+        {
+            "id_persistent": c.id_merge_request_persistent,
+            "origin": {
+                "display_txt": c.display_txt_entity_origin,
+                "id_persistent": c.id_entity_origin_persistent,
+            },
+            "destination": {
+                "id_persistent": c.id_entity_destination_persistent,
+                "display_txt": c.display_txt_entity_destination,
+            },
+            "created_by": {
+                "id_persistent": cu.test_uuid_commissioner,
+                "user_name": cu.test_username_commissioner,
+                "permission_group": "COMMISSIONER",
+            },
+            "state": "OPEN",
+        },
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/tag_definition/queue/ingest.py
@@ -10,7 +10,7 @@ from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.contribution.tag_definition.models_django import TagDefinitionContribution
 from cosmae.contribution.tag_definition.queue.util import read_csv_of_candidate
 from cosmae.entity.models_django import Entity
-from cosmae.merge_request.models_django import MergeRequest
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.models_django import TagDefinition, TagInstance
 
 
@@ -93,7 +93,7 @@ def ingest_values_from_csv(id_contribution_persistent):
                     )
                     tag_instance.save()
             for origin, destination in tag_definition_pairs:
-                MergeRequest(
+                TagMergeRequest(
                     id_persistent=uuid4(),
                     id_origin_persistent=origin.id_persistent,
                     id_destination_persistent=destination.id_persistent,

cosmae/migrations/0023_rename_mergerequest_tagmergerequest_entity_disabled.py
@@ -1,0 +1,21 @@
+# Generated by Django 4.2.2 on 2023-11-10 08:15
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("cosmae", "0022_contributioncandidate_error_msg_and_more"),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name="MergeRequest",
+            new_name="TagMergeRequest",
+        ),
+        migrations.AddField(
+            model_name="entity",
+            name="disabled",
+            field=models.BooleanField(default=False),
+        ),
+    ]

cosmae/models.py
@@ -7,6 +7,10 @@ from cosmae.contribution.tag_definition.models_django import (
     TagInstanceContribution,
 )
 from cosmae.entity.models_django import Entity
-from cosmae.merge_request.models_django import ConflictResolution, MergeRequest
+from cosmae.merge_request.entity.models_django import (
+    EntityConflictResolution,
+    EntityMergeRequest,
+)
+from cosmae.merge_request.models_django import TagConflictResolution, TagMergeRequest
 from cosmae.tag.models_django import TagDefinition, TagInstance
 from cosmae.util import CosmaeUser

cosmae/tag/api/instances.py
@@ -19,7 +19,7 @@ from cosmae.exception import (
     TagInstanceExistsException,
     ValidationException,
 )
-from cosmae.merge_request.models_django import MergeRequest
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.api.definitions import tag_definition_db_to_api
 from cosmae.tag.api.models_api import TagDefinitionResponse
 from cosmae.tag.models_django import TagInstance as TagInstanceDb
@@ -267,7 +267,7 @@ def post_tag_instances_for_entities(
         for (
             id_tag_definition_persistent
         ) in request_data.id_tag_definition_persistent_list:
-            tag_definitions = MergeRequest.get_tag_definitions_for_entities_request(
+            tag_definitions = TagMergeRequest.get_tag_definitions_for_entities_request(
                 id_tag_definition_persistent,
                 request_data.id_contribution_persistent,
                 request_data.id_merge_request_persistent,

tests/merge_request/conftest.py
@@ -4,7 +4,7 @@ import pytest
 import tests.entity.common as ce
 import tests.merge_request.common as c
 from cosmae.contribution.models_django import ContributionCandidate
-from cosmae.merge_request.models_django import ConflictResolution, MergeRequest
+from cosmae.merge_request.models_django import TagConflictResolution, TagMergeRequest
 from cosmae.tag.models_django import TagDefinition, TagInstance
 
 
@@ -82,7 +82,7 @@ def contribution_for_mr(db, user1):
 def merge_request_user_fast_forward(
     db, origin_tag_def_for_mr, destination_tag_def_for_mr_user1, contribution_for_mr
 ):
-    return MergeRequest.objects.create(  # pylint: disable=no-member
+    return TagMergeRequest.objects.create(  # pylint: disable=no-member
         id_origin_persistent=origin_tag_def_for_mr.id_persistent,
         id_destination_persistent=destination_tag_def_for_mr_user1.id_persistent,
         created_by=origin_tag_def_for_mr.owner,
@@ -97,7 +97,7 @@ def merge_request_user_fast_forward(
 def merge_request_user(
     db, origin_tag_def_for_mr, destination_tag_def_for_mr, contribution_for_mr
 ):
-    return MergeRequest.objects.create(  # pylint: disable=no-member
+    return TagMergeRequest.objects.create(  # pylint: disable=no-member
         id_origin_persistent=origin_tag_def_for_mr.id_persistent,
         id_destination_persistent=destination_tag_def_for_mr.id_persistent,
         created_by=origin_tag_def_for_mr.owner,
@@ -147,7 +147,7 @@ def contribution_for_mr1(db, user):
 def merge_request_user1(
     db, destination_tag_def_for_mr1, origin_tag_def_for_mr1, contribution_for_mr1
 ):
-    return MergeRequest.objects.create(  # pylint: disable=no-member
+    return TagMergeRequest.objects.create(  # pylint: disable=no-member
         id_destination_persistent=destination_tag_def_for_mr1.id_persistent,
         id_origin_persistent=origin_tag_def_for_mr1.id_persistent,
         created_by=origin_tag_def_for_mr1.owner,
@@ -289,7 +289,7 @@ def conflict_resolution_replace(
     instances_merge_request_origin_user,
     instance_merge_request_destination_user_conflict,
 ):
-    return ConflictResolution.objects.create(  # pylint: disable=no-member
+    return TagConflictResolution.objects.create(  # pylint: disable=no-member
         entity=entity1,
         tag_definition_origin=origin_tag_def_for_mr,
         tag_definition_destination=destination_tag_def_for_mr,
@@ -309,7 +309,7 @@ def conflict_resolution_keep(
     instances_merge_request_origin_user,
     instance_merge_request_destination_user_conflict,
 ):
-    return ConflictResolution.objects.create(  # pylint: disable=no-member
+    return TagConflictResolution.objects.create(  # pylint: disable=no-member
         entity=entity0,
         tag_definition_origin=origin_tag_def_for_mr,
         tag_definition_destination=destination_tag_def_for_mr,
@@ -329,7 +329,7 @@ def conflict_resolution_keep_fast_forward(
     instances_merge_request_origin_user,
     instance_merge_request_destination_user_conflict_fast_forward,
 ):
-    return ConflictResolution.objects.create(  # pylint: disable=no-member
+    return TagConflictResolution.objects.create(  # pylint: disable=no-member
         entity=entity1,
         tag_definition_origin=origin_tag_def_for_mr,
         tag_definition_destination=destination_tag_def_for_mr,

tests/merge_request/entity/api/integration/get_entity_merge_requests_test.py
@@ -1,0 +1,68 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+from tests.merge_request.entity import common as c
+from tests.merge_request.entity.api.integration import requests as req
+from tests.user import common as cu
+from tests.utils import assert_versioned
+from cosmae.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server_commissioner):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server_commissioner
+    with patch("cosmae.merge_request.entity.api.check_user", mock):
+        rsp = req.get_merge_requests(server.url, cookies=cookies)
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server_commissioner):
+    server, _ = auth_server_commissioner
+    rsp = req.get_merge_requests(server.url)
+    assert rsp.status_code == 401
+
+
+def test_normal_user(auth_server):
+    server, cookies = auth_server
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 403
+
+
+def test_no_mr(auth_server_commissioner):
+    server, cookies = auth_server_commissioner
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert json == {"entity_merge_requests": []}
+
+
+def test_success(auth_server_commissioner, merge_request_user):
+    server, cookies = auth_server_commissioner
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert_versioned(
+        json,
+        {
+            "entity_merge_requests": [
+                {
+                    "id_persistent": c.id_merge_request_persistent,
+                    "origin": {
+                        "display_txt": c.display_txt_entity_origin,
+                        "id_persistent": c.id_entity_origin_persistent,
+                    },
+                    "destination": {
+                        "id_persistent": c.id_entity_destination_persistent,
+                        "display_txt": c.display_txt_entity_destination,
+                    },
+                    "created_by": {
+                        "id_persistent": cu.test_uuid_commissioner,
+                        "user_name": cu.test_username_commissioner,
+                        "permission_group": "COMMISSIONER",
+                    },
+                    "state": "OPEN",
+                },
+            ]
+        },
+    )

tests/merge_request/entity/api/integration/get_merge_request_test.py
@@ -1,0 +1,73 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-locals,too-many-arguments,too-many-statements
+from unittest.mock import MagicMock, patch
+
+from tests.merge_request.entity import common as c
+from tests.merge_request.entity.api.integration import requests as req
+from tests.user import common as cu
+from tests.utils import assert_versioned
+from cosmae.exception import NotAuthenticatedException
+
+
+def test_unknown_user(auth_server_commissioner):
+    mock = MagicMock()
+    mock.side_effect = NotAuthenticatedException()
+    server, cookies = auth_server_commissioner
+    with patch("cosmae.merge_request.entity.api.check_user", mock):
+        rsp = req.get_merge_request(
+            server.url,
+            c.id_merge_request_persistent,
+            cookies=cookies,
+        )
+        assert rsp.status_code == 401
+
+
+def test_no_cookies(auth_server_commissioner):
+    server, _ = auth_server_commissioner
+    rsp = req.get_merge_request(
+        server.url,
+        c.id_merge_request_persistent,
+    )
+    assert rsp.status_code == 401
+
+
+def test_normal_user(auth_server):
+    server, cookies = auth_server
+    rsp = req.get_merge_request(
+        server.url,
+        c.id_merge_request_persistent,
+        cookies=cookies,
+    )
+    assert rsp.status_code == 403
+
+
+def test_get_entity_merge_request(auth_server_commissioner, merge_request_user):
+    server, cookies = auth_server_commissioner
+    uuidMock = MagicMock()
+    uuidMock.return_value = c.id_merge_request_persistent
+    rsp = req.get_merge_request(
+        server.url,
+        merge_request_user.id_persistent,
+        cookies=cookies,
+    )
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert_versioned(
+        json,
+        {
+            "id_persistent": c.id_merge_request_persistent,
+            "origin": {
+                "display_txt": c.display_txt_entity_origin,
+                "id_persistent": c.id_entity_origin_persistent,
+            },
+            "destination": {
+                "id_persistent": c.id_entity_destination_persistent,
+                "display_txt": c.display_txt_entity_destination,
+            },
+            "created_by": {
+                "id_persistent": cu.test_uuid_commissioner,
+                "user_name": cu.test_username_commissioner,
+                "permission_group": "COMMISSIONER",
+            },
+            "state": "OPEN",
+        },
+    )