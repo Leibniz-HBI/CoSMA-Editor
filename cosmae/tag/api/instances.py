Leibniz-HBI/DP-REX

Check owner when entering data.

vran/tag/api/instances.py
@@ -15,11 +15,15 @@ from vran.exception import (
     InvalidTagValueException,
     NotAuthenticatedException,
     TagDefinitionMissingException,
+    TagDefinitionPermissionException,
     TagInstanceExistsException,
     ValidationException,
 )
 from vran.merge_request.models_django import MergeRequest
+from vran.tag.api.definitions import tag_definition_db_to_api
+from vran.tag.api.models_api import TagDefinitionResponse
 from vran.tag.models_django import TagInstance as TagInstanceDb
+from vran.util import VranUser
 from vran.util.auth import check_user
 from vran.util.django import save_many_atomic
 
@@ -121,17 +125,23 @@ class TagInstanceUpdatedResponse(Schema):
     response={
         200: TagInstancePostList,
         400: ApiError,
+        401: ApiError,
+        403: TagDefinitionResponse,
         409: TagInstanceUpdatedResponse,
         500: ApiError,
     },
 )
-def post_tag_instance(_, tag_list: TagInstancePostList):
+def post_tag_instance(request: HttpRequest, tag_list: TagInstancePostList):
     "Create or change a tag instance."
     # pylint: disable=too-many-return-statements
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
     tag_apis = tag_list.tag_instances
     now = datetime.utcnow()
     try:
-        tag_dbs = [tag_instance_api_to_db(tag, now) for tag in tag_apis]
+        tag_dbs = [tag_instance_api_to_db(tag, user, now) for tag in tag_apis]
     except ValidationException as exc:
         return 400, ApiError(msg=str(exc))
     except TagInstanceExistsException as exc:
@@ -154,6 +164,8 @@ def post_tag_instance(_, tag_list: TagInstancePostList):
             msg=f"Value {exc.value} should be of type {exc.type_name} "
             f"for tag with id_persistent {exc.tag_id_persistent}."
         )
+    except TagDefinitionPermissionException as exc:
+        return 403, tag_definition_db_to_api(exc.tag_definition)
     except EntityUpdatedException as exc:
         return 409, TagInstanceUpdatedResponse(
             msg="There has been a concurrent modification "
@@ -285,7 +297,7 @@ def post_tag_instances_for_entities(
         )
 
 
-def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
+def tag_instance_api_to_db(tag_api: TagInstancePost, user: VranUser, time: datetime):
     "Convert a tag instance from API to database representation."
     if tag_api.id_persistent:
         persistent_id = tag_api.id_persistent
@@ -306,6 +318,7 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
         id_persistent=persistent_id,
         id_entity_persistent=tag_api.id_entity_persistent,
         id_tag_definition_persistent=tag_api.id_tag_definition_persistent,
+        user=user,
         value=tag_api.value,
         time_edit=time,
         version=tag_api.version,

vran/tag/models_django.py
@@ -15,6 +15,7 @@ from vran.exception import (
     NoParentTagException,
     TagDefinitionExistsException,
     TagDefinitionMissingException,
+    TagDefinitionPermissionException,
     TagInstanceExistsException,
 )
 from vran.util import VranUser
@@ -117,9 +118,19 @@ class TagDefinition(models.Model):
             except TagDefinition.DoesNotExist as exc:  # pylint: disable=no-member
                 raise NoParentTagException(id_parent_persistent) from exc
         if version is None:
-            exists = TagDefinition.objects.filter(  # pylint: disable=no-member
-                name=name, id_parent_persistent=id_parent_persistent
-            ).exclude(id_persistent=id_persistent)
+            exists = (
+                TagDefinition.objects.filter(  # pylint: disable=no-member
+                    name=name, id_parent_persistent=id_parent_persistent
+                )
+                .annotate(
+                    next_version=models.Subquery(
+                        TagDefinition.objects.filter(  # pylint: disable=no-member
+                            previous_version=models.OuterRef("id")
+                        ).values("id")
+                    )
+                )
+                .exclude(id_persistent=id_persistent, next_version__isnull=True)
+            )
             if exists:
                 raise TagDefinitionExistsException(
                     name,
@@ -253,6 +264,7 @@ class TagInstance(models.Model):
         time_edit: datetime,
         id_entity_persistent: str,
         id_tag_definition_persistent: str,
+        user: VranUser,
         value: Optional[Union[int, float, List[str]]] = None,
         version: Optional[int] = None,
         **kwargs,
@@ -270,6 +282,8 @@ class TagInstance(models.Model):
             raise EntityMissingException(id_entity_persistent) from exc
         try:
             tag_def = TagDefinition.most_recent_by_id(id_tag_definition_persistent)
+            if not tag_def.has_write_access(user):
+                raise TagDefinitionPermissionException(tag_def)
             value = tag_def.check_value(value)
         except TagDefinition.DoesNotExist as exc:  # pylint: disable=no-member
             raise TagDefinitionMissingException(id_tag_definition_persistent) from exc

tests/tag/models_django_instance_test.py
@@ -5,7 +5,11 @@ import pytest
 
 import tests.person.common as cp
 import tests.tag.common as c
-from vran.exception import EntityMissingException, TagDefinitionMissingException
+from vran.exception import (
+    EntityMissingException,
+    TagDefinitionMissingException,
+    TagDefinitionPermissionException,
+)
 from vran.tag.models_django import TagDefinition, TagInstance
 
 
@@ -99,11 +103,12 @@ def test_get_most_recent_by_ids(tag):
 
 
 @pytest.mark.django_db
-def test_entity_missing():
+def test_entity_missing(user):
     with pytest.raises(EntityMissingException) as exc:
         TagInstance.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
+            user=user,
             id_entity_persistent=cp.id_persistent_test,
             id_tag_definition_persistent=c.id_tag_def_persistent_test,
         )
@@ -111,12 +116,13 @@ def test_entity_missing():
 
 
 @pytest.mark.django_db
-def test_tag_def_missing(entity0):
+def test_tag_def_missing(entity0, user):
     entity0.save()
     with pytest.raises(TagDefinitionMissingException) as exc:
         TagInstance.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
+            user=user,
             id_entity_persistent=entity0.id_persistent,
             id_tag_definition_persistent=c.id_tag_def_persistent_test,
         )
@@ -124,26 +130,43 @@ def test_tag_def_missing(entity0):
 
 
 @pytest.mark.django_db
-def test_add_tag_root(entity0, tag_def):
+def test_tag_def_no_permission(entity0, tag_def_user, user1):
     entity0.save()
-    tag_def.save()
+    tag_def_user.save()
+    with pytest.raises(TagDefinitionPermissionException) as exc:
+        TagInstance.change_or_create(
+            id_persistent=c.id_tag_persistent_test,
+            time_edit=c.time_edit_test,
+            user=user1,
+            value=2.0,
+            id_entity_persistent=entity0.id_persistent,
+            id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
+        )
+    assert exc.value.args[0] == tag_def_user
+
+
+@pytest.mark.django_db
+def test_add_tag_root(entity0, tag_def_user):
+    entity0.save()
+    tag_def_user.save()
     ret, _ = TagInstance.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
+        user=tag_def_user.owner,
         id_entity_persistent=entity0.id_persistent,
-        id_tag_definition_persistent=tag_def.id_persistent,
+        id_tag_definition_persistent=tag_def_user.id_persistent,
         value="2.0",
     )
     assert ret.value == "2.0"
     assert ret.id_persistent == c.id_tag_persistent_test
     assert ret.id_entity_persistent == entity0.id_persistent
-    assert ret.id_tag_definition_persistent == tag_def.id_persistent
+    assert ret.id_tag_definition_persistent == tag_def_user.id_persistent
     assert ret.time_edit == c.time_edit_test
     assert ret.previous_version is None
 
 
 @pytest.mark.django_db
-def test_add_tag_child(entity0, tag_def):
+def test_add_tag_child(entity0, tag_def_user):
     parent_tag, _ = TagDefinition.change_or_create(
         id_persistent=c.id_tag_def_parent_persistent_test,
         id_parent_persistent=None,
@@ -153,19 +176,20 @@ def test_add_tag_child(entity0, tag_def):
     )
     parent_tag.save()
     entity0.save()
-    tag_def.id_parent_persistent = c.id_tag_def_parent_persistent_test
-    tag_def.save()
+    tag_def_user.id_parent_persistent = c.id_tag_def_parent_persistent_test
+    tag_def_user.save()
     ret, _ = TagInstance.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
+        user=tag_def_user.owner,
         id_entity_persistent=entity0.id_persistent,
-        id_tag_definition_persistent=tag_def.id_persistent,
+        id_tag_definition_persistent=tag_def_user.id_persistent,
         value="2.0",
     )
     assert ret.value == "2.0"
     assert ret.id_persistent == c.id_tag_persistent_test
     assert ret.id_entity_persistent == entity0.id_persistent
-    assert ret.id_tag_definition_persistent == tag_def.id_persistent
+    assert ret.id_tag_definition_persistent == tag_def_user.id_persistent
     assert ret.time_edit == c.time_edit_test
     assert ret.previous_version is None
 

Leibniz-HBI/CoSMA-Editor

cosmae/tag/api/instances.py
@@ -15,11 +15,15 @@ from cosmae.exception import (
     InvalidTagValueException,
     NotAuthenticatedException,
     TagDefinitionMissingException,
+    TagDefinitionPermissionException,
     TagInstanceExistsException,
     ValidationException,
 )
 from cosmae.merge_request.models_django import MergeRequest
+from cosmae.tag.api.definitions import tag_definition_db_to_api
+from cosmae.tag.api.models_api import TagDefinitionResponse
 from cosmae.tag.models_django import TagInstance as TagInstanceDb
+from cosmae.util import CosmaeUser
 from cosmae.util.auth import check_user
 from cosmae.util.django import save_many_atomic
 
@@ -121,17 +125,23 @@ class TagInstanceUpdatedResponse(Schema):
     response={
         200: TagInstancePostList,
         400: ApiError,
+        401: ApiError,
+        403: TagDefinitionResponse,
         409: TagInstanceUpdatedResponse,
         500: ApiError,
     },
 )
-def post_tag_instance(_, tag_list: TagInstancePostList):
+def post_tag_instance(request: HttpRequest, tag_list: TagInstancePostList):
     "Create or change a tag instance."
     # pylint: disable=too-many-return-statements
+    try:
+        user = check_user(request)
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
     tag_apis = tag_list.tag_instances
     now = datetime.utcnow()
     try:
-        tag_dbs = [tag_instance_api_to_db(tag, now) for tag in tag_apis]
+        tag_dbs = [tag_instance_api_to_db(tag, user, now) for tag in tag_apis]
     except ValidationException as exc:
         return 400, ApiError(msg=str(exc))
     except TagInstanceExistsException as exc:
@@ -154,6 +164,8 @@ def post_tag_instance(_, tag_list: TagInstancePostList):
             msg=f"Value {exc.value} should be of type {exc.type_name} "
             f"for tag with id_persistent {exc.tag_id_persistent}."
         )
+    except TagDefinitionPermissionException as exc:
+        return 403, tag_definition_db_to_api(exc.tag_definition)
     except EntityUpdatedException as exc:
         return 409, TagInstanceUpdatedResponse(
             msg="There has been a concurrent modification "
@@ -285,7 +297,7 @@ def post_tag_instances_for_entities(
         )
 
 
-def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
+def tag_instance_api_to_db(tag_api: TagInstancePost, user: CosmaeUser, time: datetime):
     "Convert a tag instance from API to database representation."
     if tag_api.id_persistent:
         persistent_id = tag_api.id_persistent
@@ -306,6 +318,7 @@ def tag_instance_api_to_db(tag_api: TagInstancePost, time: datetime):
         id_persistent=persistent_id,
         id_entity_persistent=tag_api.id_entity_persistent,
         id_tag_definition_persistent=tag_api.id_tag_definition_persistent,
+        user=user,
         value=tag_api.value,
         time_edit=time,
         version=tag_api.version,

cosmae/tag/models_django.py
@@ -15,6 +15,7 @@ from cosmae.exception import (
     NoParentTagException,
     TagDefinitionExistsException,
     TagDefinitionMissingException,
+    TagDefinitionPermissionException,
     TagInstanceExistsException,
 )
 from cosmae.util import CosmaeUser
@@ -117,9 +118,19 @@ class TagDefinition(models.Model):
             except TagDefinition.DoesNotExist as exc:  # pylint: disable=no-member
                 raise NoParentTagException(id_parent_persistent) from exc
         if version is None:
-            exists = TagDefinition.objects.filter(  # pylint: disable=no-member
-                name=name, id_parent_persistent=id_parent_persistent
-            ).exclude(id_persistent=id_persistent)
+            exists = (
+                TagDefinition.objects.filter(  # pylint: disable=no-member
+                    name=name, id_parent_persistent=id_parent_persistent
+                )
+                .annotate(
+                    next_version=models.Subquery(
+                        TagDefinition.objects.filter(  # pylint: disable=no-member
+                            previous_version=models.OuterRef("id")
+                        ).values("id")
+                    )
+                )
+                .exclude(id_persistent=id_persistent, next_version__isnull=True)
+            )
             if exists:
                 raise TagDefinitionExistsException(
                     name,
@@ -253,6 +264,7 @@ class TagInstance(models.Model):
         time_edit: datetime,
         id_entity_persistent: str,
         id_tag_definition_persistent: str,
+        user: CosmaeUser,
         value: Optional[Union[int, float, List[str]]] = None,
         version: Optional[int] = None,
         **kwargs,
@@ -270,6 +282,8 @@ class TagInstance(models.Model):
             raise EntityMissingException(id_entity_persistent) from exc
         try:
             tag_def = TagDefinition.most_recent_by_id(id_tag_definition_persistent)
+            if not tag_def.has_write_access(user):
+                raise TagDefinitionPermissionException(tag_def)
             value = tag_def.check_value(value)
         except TagDefinition.DoesNotExist as exc:  # pylint: disable=no-member
             raise TagDefinitionMissingException(id_tag_definition_persistent) from exc

tests/tag/models_django_instance_test.py
@@ -5,7 +5,11 @@ import pytest
 
 import tests.person.common as cp
 import tests.tag.common as c
-from cosmae.exception import EntityMissingException, TagDefinitionMissingException
+from cosmae.exception import (
+    EntityMissingException,
+    TagDefinitionMissingException,
+    TagDefinitionPermissionException,
+)
 from cosmae.tag.models_django import TagDefinition, TagInstance
 
 
@@ -99,11 +103,12 @@ def test_get_most_recent_by_ids(tag):
 
 
 @pytest.mark.django_db
-def test_entity_missing():
+def test_entity_missing(user):
     with pytest.raises(EntityMissingException) as exc:
         TagInstance.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
+            user=user,
             id_entity_persistent=cp.id_persistent_test,
             id_tag_definition_persistent=c.id_tag_def_persistent_test,
         )
@@ -111,12 +116,13 @@ def test_entity_missing():
 
 
 @pytest.mark.django_db
-def test_tag_def_missing(entity0):
+def test_tag_def_missing(entity0, user):
     entity0.save()
     with pytest.raises(TagDefinitionMissingException) as exc:
         TagInstance.change_or_create(
             id_persistent=c.id_tag_persistent_test,
             time_edit=c.time_edit_test,
+            user=user,
             id_entity_persistent=entity0.id_persistent,
             id_tag_definition_persistent=c.id_tag_def_persistent_test,
         )
@@ -124,26 +130,43 @@ def test_tag_def_missing(entity0):
 
 
 @pytest.mark.django_db
-def test_add_tag_root(entity0, tag_def):
+def test_tag_def_no_permission(entity0, tag_def_user, user1):
     entity0.save()
-    tag_def.save()
+    tag_def_user.save()
+    with pytest.raises(TagDefinitionPermissionException) as exc:
+        TagInstance.change_or_create(
+            id_persistent=c.id_tag_persistent_test,
+            time_edit=c.time_edit_test,
+            user=user1,
+            value=2.0,
+            id_entity_persistent=entity0.id_persistent,
+            id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
+        )
+    assert exc.value.args[0] == tag_def_user
+
+
+@pytest.mark.django_db
+def test_add_tag_root(entity0, tag_def_user):
+    entity0.save()
+    tag_def_user.save()
     ret, _ = TagInstance.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
+        user=tag_def_user.owner,
         id_entity_persistent=entity0.id_persistent,
-        id_tag_definition_persistent=tag_def.id_persistent,
+        id_tag_definition_persistent=tag_def_user.id_persistent,
         value="2.0",
     )
     assert ret.value == "2.0"
     assert ret.id_persistent == c.id_tag_persistent_test
     assert ret.id_entity_persistent == entity0.id_persistent
-    assert ret.id_tag_definition_persistent == tag_def.id_persistent
+    assert ret.id_tag_definition_persistent == tag_def_user.id_persistent
     assert ret.time_edit == c.time_edit_test
     assert ret.previous_version is None
 
 
 @pytest.mark.django_db
-def test_add_tag_child(entity0, tag_def):
+def test_add_tag_child(entity0, tag_def_user):
     parent_tag, _ = TagDefinition.change_or_create(
         id_persistent=c.id_tag_def_parent_persistent_test,
         id_parent_persistent=None,
@@ -153,19 +176,20 @@ def test_add_tag_child(entity0, tag_def):
     )
     parent_tag.save()
     entity0.save()
-    tag_def.id_parent_persistent = c.id_tag_def_parent_persistent_test
-    tag_def.save()
+    tag_def_user.id_parent_persistent = c.id_tag_def_parent_persistent_test
+    tag_def_user.save()
     ret, _ = TagInstance.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
+        user=tag_def_user.owner,
         id_entity_persistent=entity0.id_persistent,
-        id_tag_definition_persistent=tag_def.id_persistent,
+        id_tag_definition_persistent=tag_def_user.id_persistent,
         value="2.0",
     )
     assert ret.value == "2.0"
     assert ret.id_persistent == c.id_tag_persistent_test
     assert ret.id_entity_persistent == entity0.id_persistent
-    assert ret.id_tag_definition_persistent == tag_def.id_persistent
+    assert ret.id_tag_definition_persistent == tag_def_user.id_persistent
     assert ret.time_edit == c.time_edit_test
     assert ret.previous_version is None
 