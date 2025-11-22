Leibniz-HBI/DP-REX

Add exact tag value match to backend.

vran/contribution/models_api.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 
 from ninja import Schema
 
+from vran.tag.api.definitions import TagDefinitionResponse
+
 
 class ContributionPostRequest(Schema):
     # pylint: disable=too-few-public-methods
@@ -38,6 +40,7 @@ class ContributionCandidate(Schema):
     author: Optional[str]
     error_msg: Optional[str]
     error_details: Optional[str]
+    match_tag_definition_list: Optional[List[TagDefinitionResponse]]
 
 
 class ContributionCandidatePatchRequest(Schema):

vran/contribution/models_conversion.py
@@ -3,6 +3,7 @@ from vran.contribution.models_api import ContributionCandidate
 from vran.contribution.models_django import (
     ContributionCandidate as ContributionCandidateDb,
 )
+from vran.tag.api.definitions import tag_definition_db_dict_to_api
 
 _contribution_state_mapping_db_to_api = {
     ContributionCandidateDb.UPLOADED: "UPLOADED",
@@ -24,6 +25,17 @@ def contribution_db_to_api(
         author = None
     else:
         author = contribution_db.created_by.username
+    if hasattr(contribution_db, "matched_tag_definitions"):
+        if contribution_db.matched_tag_definitions is None:
+            match_tag_definition_list = []
+        else:
+            match_tag_definition_list = [
+                tag_definition_db_dict_to_api(tag_def)
+                for tag_def in contribution_db.matched_tag_definitions
+                if tag_def is not None
+            ]
+    else:
+        match_tag_definition_list = None
     return ContributionCandidate(
         id_persistent=str(contribution_db.id_persistent),
         name=contribution_db.name,
@@ -34,4 +46,5 @@ def contribution_db_to_api(
         author=author,
         error_msg=contribution_db.error_msg,
         error_details=contribution_db.error_trace,
+        match_tag_definition_list=match_tag_definition_list,
     )

tests/contribution/api/integration/get_contribution_test.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 
 import tests.contribution.api.integration.common as c
 import tests.contribution.api.integration.requests as req_contrib
+from vran.merge_request.models_django import TagMergeRequest
 from vran.util.auth import NotAuthenticatedException
 
 
@@ -42,6 +43,48 @@ def test_get(auth_server):
     contribution = rsp.json()
     assert contribution["id_persistent"] == id_persistent
     contribution.pop("id_persistent")
+    assert contribution["match_tag_definition_list"] == []
+    contribution.pop("match_tag_definition_list")
+    assert contribution == c.contribution_test_upload0
+
+
+def test_get_with_match_tag_definition_list(auth_server, tag_def1, tag_def_curated):
+    live_server, cookies = auth_server
+    rsp = req_contrib.post_contribution(
+        live_server.url, c.contribution_post0, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    id_persistent = rsp.json()["id_persistent"]
+    TagMergeRequest.objects.create(  # pylint: disable=no-member
+        id_persistent=c.id_tag_merge_request_persistent,
+        id_origin_persistent=tag_def1.id_persistent,
+        id_destination_persistent=tag_def_curated.id_persistent,
+        contribution_candidate_id=id_persistent,
+        state=TagMergeRequest.OPEN,
+        created_by=tag_def1.owner,
+        created_at=c.time_edit_tag_merge_request,
+    )
+    rsp = req_contrib.get_contribution(
+        live_server.url, id_persistent=id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    contribution = rsp.json()
+    assert contribution["id_persistent"] == id_persistent
+    contribution.pop("id_persistent")
+    assert contribution["match_tag_definition_list"] == [
+        {
+            "id_persistent": tag_def_curated.id_persistent,
+            "name": tag_def_curated.name,
+            "curated": tag_def_curated.curated,
+            "id_parent_persistent": tag_def_curated.id_parent_persistent,
+            "version": tag_def_curated.id,
+            "name_path": [tag_def_curated.name],
+            "type": "STRING",
+            "hidden": tag_def_curated.hidden,
+            "owner": None,
+        }
+    ]
+    contribution.pop("match_tag_definition_list")
     assert contribution == c.contribution_test_upload0
 
 
@@ -62,4 +105,5 @@ def test_get_with_error(auth_server, contribution_error):
         "state": "ENTITIES_MATCHED",
         "error_msg": contribution_error.error_msg,
         "error_details": contribution_error.error_trace,
+        "match_tag_definition_list": [],
     }

tests/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent / "django_test_base"
-
+SECRET_DIR = Path(__file__).resolve().parent / ".secrets_local"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -73,13 +73,34 @@ TEMPLATES = [
 WSGI_APPLICATION = "django_project.wsgi.application"
 
 
+def get_file_secret(secret_name):
+    "Get secret as provided by docker_compose"
+    try:
+        with open(
+            f"{SECRET_DIR.absolute()}/{secret_name}", encoding="utf-8"
+        ) as key_file:
+            key = key_file.readline().rstrip("\n")
+    except FileNotFoundError:
+        env_var_name = secret_name.upper()
+        key = environ.get(env_var_name)
+    if not key:
+        raise Exception(  # pylint: disable=broad-exception-raised
+            f"Please set the {secret_name} secret."
+        )
+    return key
+
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": get_file_secret("vran_db_name"),
+        "USER": get_file_secret("vran_db_user"),
+        "PASSWORD": get_file_secret("vran_db_password"),
+        "HOST": "127.0.0.1",
+        "PORT": "5444",
     }
 }
 

tests/tag/api/integration/post_tag_instances_values_test.py
@@ -168,7 +168,7 @@ def test_bad_db(auth_server):
     mock = MagicMock()
     mock.side_effect = Exception()
     with patch(
-        "vran.tag.models_django.TagInstance.most_recents_by_entity_and_definition_ids",
+        "vran.tag.models_django.TagInstance.most_recent_by_entity_and_definition_id_query_set",
         mock,
     ):
         req = post_tag_instance_values(

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/models_api.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 
 from ninja import Schema
 
+from cosmae.tag.api.definitions import TagDefinitionResponse
+
 
 class ContributionPostRequest(Schema):
     # pylint: disable=too-few-public-methods
@@ -38,6 +40,7 @@ class ContributionCandidate(Schema):
     author: Optional[str]
     error_msg: Optional[str]
     error_details: Optional[str]
+    match_tag_definition_list: Optional[List[TagDefinitionResponse]]
 
 
 class ContributionCandidatePatchRequest(Schema):

cosmae/contribution/models_conversion.py
@@ -3,6 +3,7 @@ from cosmae.contribution.models_api import ContributionCandidate
 from cosmae.contribution.models_django import (
     ContributionCandidate as ContributionCandidateDb,
 )
+from cosmae.tag.api.definitions import tag_definition_db_dict_to_api
 
 _contribution_state_mapping_db_to_api = {
     ContributionCandidateDb.UPLOADED: "UPLOADED",
@@ -24,6 +25,17 @@ def contribution_db_to_api(
         author = None
     else:
         author = contribution_db.created_by.username
+    if hasattr(contribution_db, "matched_tag_definitions"):
+        if contribution_db.matched_tag_definitions is None:
+            match_tag_definition_list = []
+        else:
+            match_tag_definition_list = [
+                tag_definition_db_dict_to_api(tag_def)
+                for tag_def in contribution_db.matched_tag_definitions
+                if tag_def is not None
+            ]
+    else:
+        match_tag_definition_list = None
     return ContributionCandidate(
         id_persistent=str(contribution_db.id_persistent),
         name=contribution_db.name,
@@ -34,4 +46,5 @@ def contribution_db_to_api(
         author=author,
         error_msg=contribution_db.error_msg,
         error_details=contribution_db.error_trace,
+        match_tag_definition_list=match_tag_definition_list,
     )

tests/contribution/api/integration/get_contribution_test.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 
 import tests.contribution.api.integration.common as c
 import tests.contribution.api.integration.requests as req_contrib
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.util.auth import NotAuthenticatedException
 
 
@@ -42,6 +43,48 @@ def test_get(auth_server):
     contribution = rsp.json()
     assert contribution["id_persistent"] == id_persistent
     contribution.pop("id_persistent")
+    assert contribution["match_tag_definition_list"] == []
+    contribution.pop("match_tag_definition_list")
+    assert contribution == c.contribution_test_upload0
+
+
+def test_get_with_match_tag_definition_list(auth_server, tag_def1, tag_def_curated):
+    live_server, cookies = auth_server
+    rsp = req_contrib.post_contribution(
+        live_server.url, c.contribution_post0, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    id_persistent = rsp.json()["id_persistent"]
+    TagMergeRequest.objects.create(  # pylint: disable=no-member
+        id_persistent=c.id_tag_merge_request_persistent,
+        id_origin_persistent=tag_def1.id_persistent,
+        id_destination_persistent=tag_def_curated.id_persistent,
+        contribution_candidate_id=id_persistent,
+        state=TagMergeRequest.OPEN,
+        created_by=tag_def1.owner,
+        created_at=c.time_edit_tag_merge_request,
+    )
+    rsp = req_contrib.get_contribution(
+        live_server.url, id_persistent=id_persistent, cookies=cookies
+    )
+    assert rsp.status_code == 200
+    contribution = rsp.json()
+    assert contribution["id_persistent"] == id_persistent
+    contribution.pop("id_persistent")
+    assert contribution["match_tag_definition_list"] == [
+        {
+            "id_persistent": tag_def_curated.id_persistent,
+            "name": tag_def_curated.name,
+            "curated": tag_def_curated.curated,
+            "id_parent_persistent": tag_def_curated.id_parent_persistent,
+            "version": tag_def_curated.id,
+            "name_path": [tag_def_curated.name],
+            "type": "STRING",
+            "hidden": tag_def_curated.hidden,
+            "owner": None,
+        }
+    ]
+    contribution.pop("match_tag_definition_list")
     assert contribution == c.contribution_test_upload0
 
 
@@ -62,4 +105,5 @@ def test_get_with_error(auth_server, contribution_error):
         "state": "ENTITIES_MATCHED",
         "error_msg": contribution_error.error_msg,
         "error_details": contribution_error.error_trace,
+        "match_tag_definition_list": [],
     }

tests/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent / "django_test_base"
-
+SECRET_DIR = Path(__file__).resolve().parent / ".secrets_local"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -73,13 +73,34 @@ TEMPLATES = [
 WSGI_APPLICATION = "django_project.wsgi.application"
 
 
+def get_file_secret(secret_name):
+    "Get secret as provided by docker_compose"
+    try:
+        with open(
+            f"{SECRET_DIR.absolute()}/{secret_name}", encoding="utf-8"
+        ) as key_file:
+            key = key_file.readline().rstrip("\n")
+    except FileNotFoundError:
+        env_var_name = secret_name.upper()
+        key = environ.get(env_var_name)
+    if not key:
+        raise Exception(  # pylint: disable=broad-exception-raised
+            f"Please set the {secret_name} secret."
+        )
+    return key
+
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": get_file_secret("cosmae_db_name"),
+        "USER": get_file_secret("cosmae_db_user"),
+        "PASSWORD": get_file_secret("cosmae_db_password"),
+        "HOST": "127.0.0.1",
+        "PORT": "5444",
     }
 }
 

tests/tag/api/integration/post_tag_instances_values_test.py
@@ -168,7 +168,7 @@ def test_bad_db(auth_server):
     mock = MagicMock()
     mock.side_effect = Exception()
     with patch(
-        "cosmae.tag.models_django.TagInstance.most_recents_by_entity_and_definition_ids",
+        "cosmae.tag.models_django.TagInstance.most_recent_by_entity_and_definition_id_query_set",
         mock,
     ):
         req = post_tag_instance_values(