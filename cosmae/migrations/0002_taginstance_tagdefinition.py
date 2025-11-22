Leibniz-HBI/DP-REX

Add Django ORM models for tag definitions and instances.

vran/migrations/0002_taginstance_tagdefinition.py
@@ -1,0 +1,85 @@
+# Generated by Django 4.1.3 on 2022-12-20 09:55
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="TagInstance",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("id_persistent", models.TextField()),
+                ("id_entity_persistent", models.TextField()),
+                ("id_tag_definition_persistent", models.TextField()),
+                ("value", models.TextField()),
+                ("valid_from", models.DateField(blank=True, null=True)),
+                ("valid_to", models.DateField(blank=True, null=True)),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.taginstance",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="TagDefinition",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.TextField()),
+                ("id_persistent", models.TextField()),
+                ("id_parent_persistent", models.TextField(blank=True, null=True)),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[
+                            ("INR", "inner"),
+                            ("FLT", "float"),
+                            ("INT", "integer"),
+                        ],
+                        default="INR",
+                        max_length=3,
+                    ),
+                ),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.tagdefinition",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+    ]

vran/models.py
@@ -2,3 +2,4 @@
 # pylint: disable=unused-import
 from vran.entity.models_django import Entity
 from vran.person.models_django import Person
+from vran.tag.models_django import TagDefinition, TagInstance

tests/tag/conftest.py
@@ -1,0 +1,17 @@
+"Pytest fixtures for all tag tests."
+import pytest
+
+from tests.tag import common as c
+from vran.tag.models_django import TagDefinition
+
+
+@pytest.fixture()
+def tag_def():
+    "Shared tag defintion for tests."
+    return TagDefinition(
+        id_persistent=c.id_tag_def_persistent_test,
+        type=TagDefinition.FLOAT,
+        id_parent_persistent=None,
+        name=c.name_tag_def_test,
+        time_edit=c.time_edit_test,
+    )

tests/tag/models_django_definitions_test.py
@@ -1,0 +1,221 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+
+import pytest
+
+from tests.tag import common as c
+from vran.exception import (
+    InvalidTagDefinitionException,
+    InvalidTagValueException,
+    TagDefinitionExistsException,
+)
+from vran.tag.models_django import TagDefinition
+
+
+@pytest.mark.django_db
+def test_different_name(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        name="changed tag name",
+        type=tag_def.type,
+    )
+    assert tag_def.check_different_before_save(tag_def1)
+    assert tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_different_id_parent(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        id_parent_persistent=c.id_tag_def_parent_persistent_test,
+        name=c.name_tag_def_test,
+        type=tag_def.type,
+    )
+    assert tag_def.check_different_before_save(tag_def1)
+    assert tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_different_type(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        name=c.name_tag_def_test,
+        type=TagDefinition.INTEGER,
+    )
+    assert tag_def.check_different_before_save(tag_def1)
+    assert tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_same(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        name=c.name_tag_def_test,
+        type=TagDefinition.FLOAT,
+    )
+    assert not tag_def.check_different_before_save(tag_def1)
+    assert not tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_store_and_retrieve_tag_def(tag_def):
+    tag_def.save()
+    retrieved = TagDefinition.objects.get(  # pylint: disable=no-member
+        name=c.name_tag_def_test
+    )
+    assert retrieved == tag_def
+
+
+@pytest.mark.django_db
+def test_missing_parent():
+    with pytest.raises(InvalidTagDefinitionException) as exc:
+        TagDefinition.change_or_create(
+            c.id_tag_def_persistent_test,
+            c.time_edit_test,
+            c.name_tag_def_test,
+            c.id_tag_def_parent_persistent_test,
+        )
+    assert (
+        exc.value.args[0] == "There is no tag with "
+        f"persistent_id {c.id_tag_def_parent_persistent_test}."
+    )
+
+
+@pytest.mark.django_db
+def test_valid_parent_same_name(tag_def):
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.type = TagDefinition.INNER
+    tag_def.save()
+    ret, _ = TagDefinition.change_or_create(
+        c.id_tag_def_persistent_test,
+        c.time_edit_test,
+        c.name_tag_def_test,
+        c.id_tag_def_parent_persistent_test,
+    )
+    assert ret.id_parent_persistent == c.id_tag_def_parent_persistent_test
+
+
+@pytest.mark.django_db
+def test_invalid_parent(tag_def):
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.save()
+
+    with pytest.raises(InvalidTagDefinitionException) as exc:
+        TagDefinition.change_or_create(
+            c.id_tag_def_persistent_test,
+            c.time_edit_test,
+            c.name_tag_def_test,
+            c.id_tag_def_parent_persistent_test,
+        )
+    assert (
+        exc.value.args[0] == f"Tag {c.name_tag_def_test} "
+        "is not allowed to have child tags."
+    )
+
+
+@pytest.mark.django_db
+def test_tag_exists_root(tag_def):
+    tag_def.save()
+
+    with pytest.raises(TagDefinitionExistsException) as exc:
+        TagDefinition.change_or_create(
+            None,
+            c.time_edit_test,
+            c.name_tag_def_test,
+            None,
+        )
+    assert exc.value.args[0] == c.name_tag_def_test
+    assert exc.value.args[1] is None
+
+
+@pytest.mark.django_db
+def test_tag_exists_child(tag_def):
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.type = TagDefinition.INNER
+    tag_def.save()
+    old, _ = TagDefinition.change_or_create(
+        c.id_tag_def_persistent_test,
+        c.time_edit_test,
+        c.name_tag_def_test,
+        c.id_tag_def_parent_persistent_test,
+    )
+    old.save()
+
+    with pytest.raises(TagDefinitionExistsException) as exc:
+        TagDefinition.change_or_create(
+            "other_tag_def_id_test",
+            c.time_edit_test,
+            c.name_tag_def_test,
+            c.id_tag_def_parent_persistent_test,
+        )
+    assert exc.value.args[0] == c.name_tag_def_test
+    assert exc.value.args[1] == c.id_tag_def_parent_persistent_test
+
+
+@pytest.mark.django_db
+def test_tag_exists_rename(tag_def):
+    tag_def.type = TagDefinition.INNER
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.save()
+    TagDefinition.change_or_create(
+        c.id_tag_def_persistent_test,
+        c.time_edit_test,
+        c.name_tag_def_test,
+        c.id_tag_def_parent_persistent_test,
+    )
+    with pytest.raises(TagDefinitionExistsException) as exc:
+        TagDefinition.change_or_create(
+            c.id_tag_def_persistent_test, c.time_edit_test, c.name_tag_def_test, None
+        )
+
+    assert exc.value.args[0] == c.name_tag_def_test
+    assert exc.value.args[1] is None
+
+
+@pytest.mark.django_db
+def test_float_check_valid(tag_def):
+    tag_def.check_value(2.0)
+
+
+@pytest.mark.django_db
+def test_float_check_invalid(tag_def):
+    with pytest.raises(InvalidTagValueException) as exc:
+        tag_def.check_value(2)
+    assert exc.value.args[0] == (
+        f"Value has to be a float for tag {c.name_tag_def_test}."
+    )
+
+
+@pytest.mark.django_db
+def test_int_check_valid(tag_def):
+    tag_def.type = TagDefinition.INTEGER
+    tag_def.check_value(2)
+
+
+@pytest.mark.django_db
+def test_int_check_invalid(tag_def):
+    tag_def.type = TagDefinition.INTEGER
+    with pytest.raises(InvalidTagValueException) as exc:
+        tag_def.check_value(2.0)
+    assert exc.value.args[0] == (
+        f"Value has to be an int for tag {c.name_tag_def_test}."
+    )
+
+
+@pytest.mark.django_db
+def test_inner_check_valid(tag_def):
+    tag_def.type = TagDefinition.INNER
+    tag_def.check_value(None)
+
+
+@pytest.mark.django_db
+def test_inner_check_invalid(tag_def):
+    tag_def.type = TagDefinition.INNER
+    with pytest.raises(InvalidTagValueException) as exc:
+        tag_def.check_value(True)
+    assert exc.value.args[0] == (
+        f"Value has to be null for inner tag {c.name_tag_def_test}."
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0002_taginstance_tagdefinition.py
@@ -1,0 +1,85 @@
+# Generated by Django 4.1.3 on 2022-12-20 09:55
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="TagInstance",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("id_persistent", models.TextField()),
+                ("id_entity_persistent", models.TextField()),
+                ("id_tag_definition_persistent", models.TextField()),
+                ("value", models.TextField()),
+                ("valid_from", models.DateField(blank=True, null=True)),
+                ("valid_to", models.DateField(blank=True, null=True)),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.taginstance",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="TagDefinition",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.TextField()),
+                ("id_persistent", models.TextField()),
+                ("id_parent_persistent", models.TextField(blank=True, null=True)),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[
+                            ("INR", "inner"),
+                            ("FLT", "float"),
+                            ("INT", "integer"),
+                        ],
+                        default="INR",
+                        max_length=3,
+                    ),
+                ),
+                ("time_edit", models.DateTimeField()),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.tagdefinition",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+    ]

cosmae/models.py
@@ -2,3 +2,4 @@
 # pylint: disable=unused-import
 from cosmae.entity.models_django import Entity
 from cosmae.person.models_django import Person
+from cosmae.tag.models_django import TagDefinition, TagInstance

tests/tag/conftest.py
@@ -1,0 +1,17 @@
+"Pytest fixtures for all tag tests."
+import pytest
+
+from tests.tag import common as c
+from cosmae.tag.models_django import TagDefinition
+
+
+@pytest.fixture()
+def tag_def():
+    "Shared tag defintion for tests."
+    return TagDefinition(
+        id_persistent=c.id_tag_def_persistent_test,
+        type=TagDefinition.FLOAT,
+        id_parent_persistent=None,
+        name=c.name_tag_def_test,
+        time_edit=c.time_edit_test,
+    )

tests/tag/models_django_definitions_test.py
@@ -1,0 +1,221 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+
+import pytest
+
+from tests.tag import common as c
+from cosmae.exception import (
+    InvalidTagDefinitionException,
+    InvalidTagValueException,
+    TagDefinitionExistsException,
+)
+from cosmae.tag.models_django import TagDefinition
+
+
+@pytest.mark.django_db
+def test_different_name(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        name="changed tag name",
+        type=tag_def.type,
+    )
+    assert tag_def.check_different_before_save(tag_def1)
+    assert tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_different_id_parent(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        id_parent_persistent=c.id_tag_def_parent_persistent_test,
+        name=c.name_tag_def_test,
+        type=tag_def.type,
+    )
+    assert tag_def.check_different_before_save(tag_def1)
+    assert tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_different_type(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        name=c.name_tag_def_test,
+        type=TagDefinition.INTEGER,
+    )
+    assert tag_def.check_different_before_save(tag_def1)
+    assert tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_same(tag_def):
+    tag_def1 = TagDefinition(
+        id_persistent=tag_def.id_persistent,
+        time_edit=tag_def.time_edit,
+        name=c.name_tag_def_test,
+        type=TagDefinition.FLOAT,
+    )
+    assert not tag_def.check_different_before_save(tag_def1)
+    assert not tag_def1.check_different_before_save(tag_def)
+
+
+@pytest.mark.django_db
+def test_store_and_retrieve_tag_def(tag_def):
+    tag_def.save()
+    retrieved = TagDefinition.objects.get(  # pylint: disable=no-member
+        name=c.name_tag_def_test
+    )
+    assert retrieved == tag_def
+
+
+@pytest.mark.django_db
+def test_missing_parent():
+    with pytest.raises(InvalidTagDefinitionException) as exc:
+        TagDefinition.change_or_create(
+            c.id_tag_def_persistent_test,
+            c.time_edit_test,
+            c.name_tag_def_test,
+            c.id_tag_def_parent_persistent_test,
+        )
+    assert (
+        exc.value.args[0] == "There is no tag with "
+        f"persistent_id {c.id_tag_def_parent_persistent_test}."
+    )
+
+
+@pytest.mark.django_db
+def test_valid_parent_same_name(tag_def):
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.type = TagDefinition.INNER
+    tag_def.save()
+    ret, _ = TagDefinition.change_or_create(
+        c.id_tag_def_persistent_test,
+        c.time_edit_test,
+        c.name_tag_def_test,
+        c.id_tag_def_parent_persistent_test,
+    )
+    assert ret.id_parent_persistent == c.id_tag_def_parent_persistent_test
+
+
+@pytest.mark.django_db
+def test_invalid_parent(tag_def):
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.save()
+
+    with pytest.raises(InvalidTagDefinitionException) as exc:
+        TagDefinition.change_or_create(
+            c.id_tag_def_persistent_test,
+            c.time_edit_test,
+            c.name_tag_def_test,
+            c.id_tag_def_parent_persistent_test,
+        )
+    assert (
+        exc.value.args[0] == f"Tag {c.name_tag_def_test} "
+        "is not allowed to have child tags."
+    )
+
+
+@pytest.mark.django_db
+def test_tag_exists_root(tag_def):
+    tag_def.save()
+
+    with pytest.raises(TagDefinitionExistsException) as exc:
+        TagDefinition.change_or_create(
+            None,
+            c.time_edit_test,
+            c.name_tag_def_test,
+            None,
+        )
+    assert exc.value.args[0] == c.name_tag_def_test
+    assert exc.value.args[1] is None
+
+
+@pytest.mark.django_db
+def test_tag_exists_child(tag_def):
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.type = TagDefinition.INNER
+    tag_def.save()
+    old, _ = TagDefinition.change_or_create(
+        c.id_tag_def_persistent_test,
+        c.time_edit_test,
+        c.name_tag_def_test,
+        c.id_tag_def_parent_persistent_test,
+    )
+    old.save()
+
+    with pytest.raises(TagDefinitionExistsException) as exc:
+        TagDefinition.change_or_create(
+            "other_tag_def_id_test",
+            c.time_edit_test,
+            c.name_tag_def_test,
+            c.id_tag_def_parent_persistent_test,
+        )
+    assert exc.value.args[0] == c.name_tag_def_test
+    assert exc.value.args[1] == c.id_tag_def_parent_persistent_test
+
+
+@pytest.mark.django_db
+def test_tag_exists_rename(tag_def):
+    tag_def.type = TagDefinition.INNER
+    tag_def.id_persistent = c.id_tag_def_parent_persistent_test
+    tag_def.save()
+    TagDefinition.change_or_create(
+        c.id_tag_def_persistent_test,
+        c.time_edit_test,
+        c.name_tag_def_test,
+        c.id_tag_def_parent_persistent_test,
+    )
+    with pytest.raises(TagDefinitionExistsException) as exc:
+        TagDefinition.change_or_create(
+            c.id_tag_def_persistent_test, c.time_edit_test, c.name_tag_def_test, None
+        )
+
+    assert exc.value.args[0] == c.name_tag_def_test
+    assert exc.value.args[1] is None
+
+
+@pytest.mark.django_db
+def test_float_check_valid(tag_def):
+    tag_def.check_value(2.0)
+
+
+@pytest.mark.django_db
+def test_float_check_invalid(tag_def):
+    with pytest.raises(InvalidTagValueException) as exc:
+        tag_def.check_value(2)
+    assert exc.value.args[0] == (
+        f"Value has to be a float for tag {c.name_tag_def_test}."
+    )
+
+
+@pytest.mark.django_db
+def test_int_check_valid(tag_def):
+    tag_def.type = TagDefinition.INTEGER
+    tag_def.check_value(2)
+
+
+@pytest.mark.django_db
+def test_int_check_invalid(tag_def):
+    tag_def.type = TagDefinition.INTEGER
+    with pytest.raises(InvalidTagValueException) as exc:
+        tag_def.check_value(2.0)
+    assert exc.value.args[0] == (
+        f"Value has to be an int for tag {c.name_tag_def_test}."
+    )
+
+
+@pytest.mark.django_db
+def test_inner_check_valid(tag_def):
+    tag_def.type = TagDefinition.INNER
+    tag_def.check_value(None)
+
+
+@pytest.mark.django_db
+def test_inner_check_invalid(tag_def):
+    tag_def.type = TagDefinition.INNER
+    with pytest.raises(InvalidTagValueException) as exc:
+        tag_def.check_value(True)
+    assert exc.value.args[0] == (
+        f"Value has to be null for inner tag {c.name_tag_def_test}."
+    )