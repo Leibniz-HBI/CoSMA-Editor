Leibniz-HBI/DP-REX

Add Entity Merging Finalisation to Backend.

vran/migrations/0026_alter_entityconflictresolution_tag_instance_origin_and_more.py
@@ -1,0 +1,36 @@
+# Generated by Django 4.2.2 on 2023-11-27 15:20
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0025_alter_tagmergerequest_assigned_to_and_more"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="entityconflictresolution",
+            name="tag_instance_origin",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="+",
+                to="vran.taginstance",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="tagconflictresolution",
+            name="tag_instance_origin",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="+",
+                to="vran.taginstance",
+            ),
+        ),
+    ]

tests/merge_request/api/integration/get_merge_requests_test.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-statements
+from datetime import timedelta
 from unittest.mock import MagicMock, patch
 
 import tests.user.common as cu
@@ -6,6 +7,7 @@ from tests.merge_request import common as c
 from tests.merge_request.api.integration import requests as req
 from tests.utils import assert_versioned, format_datetime
 from vran.exception import NotAuthenticatedException
+from vran.tag.models_django import TagDefinition
 
 
 def test_unknown_user(auth_server):
@@ -56,6 +58,7 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user1",
             "curated": False,
+            "hidden": False,
         },
     )
     assert_versioned(
@@ -68,6 +71,7 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user",
             "curated": False,
+            "hidden": False,
         },
     )
     assigned_list = json["assigned"]
@@ -97,6 +101,7 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user",
             "curated": False,
+            "hidden": False,
         },
     )
     assert_versioned(
@@ -109,5 +114,114 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user1",
             "curated": False,
+            "hidden": False,
+        },
+    )
+
+
+def test_get_merge_requests_with_hidden(
+    auth_server, merge_request_user, merge_request_user1, origin_tag_def_for_mr1
+):
+    tag_def, _ = TagDefinition.change_or_create(
+        id_persistent=origin_tag_def_for_mr1.id_persistent,
+        id_parent_persistent=origin_tag_def_for_mr1.id_parent_persistent,
+        name=origin_tag_def_for_mr1.name,
+        owner=origin_tag_def_for_mr1.owner,
+        curated=origin_tag_def_for_mr1.curated,
+        hidden=True,
+        version=origin_tag_def_for_mr1.id,
+        time_edit=origin_tag_def_for_mr1.time_edit + timedelta(minutes=60),
+    )
+    tag_def.save()
+    assert TagDefinition.most_recent_by_id(origin_tag_def_for_mr1.id_persistent).hidden
+    server, cookies = auth_server
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert len(json) == 2
+    created_list = json["created"]
+    assert len(created_list) == 1
+    created = created_list[0]
+    assert len(created) == 7
+    assert created["created_at"] == format_datetime(c.time_merge_request1)
+    assert created["id_persistent"] == c.id_persistent_merge_request1
+    assert created["created_by"] == {
+        "user_name": cu.test_username,
+        "id_persistent": cu.test_uuid,
+        "permission_group": "APPLICANT",
+    }
+    assert created["assigned_to"] == {
+        "user_name": cu.test_username1,
+        "id_persistent": cu.test_uuid1,
+        "permission_group": "APPLICANT",
+    }
+    assert created["state"] == "OPEN"
+    assert_versioned(
+        created["destination"],
+        {
+            "id_persistent": c.id_persistent_tag_def_destination1,
+            "id_parent_persistent": None,
+            "name": c.name_tag_def_destination1,
+            "name_path": [c.name_tag_def_destination1],
+            "type": "STRING",
+            "owner": "test-user1",
+            "curated": False,
+            "hidden": False,
+        },
+    )
+    assert_versioned(
+        created["origin"],
+        {
+            "name": c.name_tag_def_origin1,
+            "name_path": [c.name_tag_def_origin1],
+            "id_parent_persistent": None,
+            "id_persistent": c.id_persistent_tag_def_origin1,
+            "type": "STRING",
+            "owner": "test-user",
+            "curated": False,
+            "hidden": True,
+        },
+    )
+    assigned_list = json["assigned"]
+    assert len(assigned_list) == 1
+    assigned = assigned_list[0]
+    assert len(assigned) == 7
+    assert assigned["created_at"] == format_datetime(c.time_merge_request)
+    assert assigned["id_persistent"] == c.id_persistent_merge_request
+    assert assigned["created_by"] == {
+        "user_name": cu.test_username1,
+        "id_persistent": cu.test_uuid1,
+        "permission_group": "APPLICANT",
+    }
+    assert assigned["assigned_to"] == {
+        "user_name": cu.test_username,
+        "id_persistent": cu.test_uuid,
+        "permission_group": "APPLICANT",
+    }
+    assert assigned["state"] == "OPEN"
+    assert_versioned(
+        assigned["destination"],
+        {
+            "id_persistent": c.id_persistent_tag_def_destination,
+            "id_parent_persistent": None,
+            "name": c.name_tag_def_destination,
+            "name_path": [c.name_tag_def_destination],
+            "type": "STRING",
+            "owner": "test-user",
+            "curated": False,
+            "hidden": False,
+        },
+    )
+    assert_versioned(
+        assigned["origin"],
+        {
+            "name": c.name_tag_def_origin,
+            "name_path": [c.name_tag_def_origin],
+            "id_persistent": c.id_persistent_tag_def_origin,
+            "id_parent_persistent": None,
+            "type": "STRING",
+            "owner": "test-user1",
+            "curated": False,
+            "hidden": False,
         },
     )

tests/settings.py
@@ -132,7 +132,7 @@ CONTRIBUTION_DIRECTORY = "/tmp"
 RQ_QUEUES = {
     "default": {
         "HOST": "localhost",
-        "PORT": environ.get("VRAN_REDIS_PORT", 6379),
+        "PORT": environ.get("VRAN_REDIS_PORT", 6333),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
     }

tests/tag/models_django_definitions_test.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import pytest
 
@@ -10,6 +10,7 @@ from vran.exception import (
     TagDefinitionExistsException,
 )
 from vran.tag.models_django import TagDefinition
+from vran.util import timestamp
 
 
 @pytest.mark.django_db
@@ -268,7 +269,7 @@ def test_most_recent_for_user(tag_def_user):
     tag_def_edited, _ = TagDefinition.change_or_create(
         id_persistent=tag_def_user.id_persistent,
         id_parent_persistent=None,
-        time_edit=datetime.utcnow(),
+        time_edit=timestamp(),
         name="new_name",
         owner=tag_def_user.owner,
         version=tag_def_user.id,
@@ -276,3 +277,18 @@ def test_most_recent_for_user(tag_def_user):
     tag_def_edited.save()
     ret = TagDefinition.for_user(tag_def_user.owner).get()
     assert ret == tag_def_edited
+
+
+@pytest.mark.django_db
+def test_can_create_hidden():
+    tag_def, _ = TagDefinition.change_or_create(
+        id_persistent=c.id_tag_persistent_test,
+        id_parent_persistent=None,
+        time_edit=timestamp(),
+        name="new_name",
+        owner=None,
+        hidden=True,
+    )
+    tag_def.save()
+    retrieved = TagDefinition.most_recent_by_id(c.id_tag_persistent_test)
+    assert retrieved.hidden

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0026_alter_entityconflictresolution_tag_instance_origin_and_more.py
@@ -1,0 +1,36 @@
+# Generated by Django 4.2.2 on 2023-11-27 15:20
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0025_alter_tagmergerequest_assigned_to_and_more"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="entityconflictresolution",
+            name="tag_instance_origin",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="+",
+                to="cosmae.taginstance",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="tagconflictresolution",
+            name="tag_instance_origin",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="+",
+                to="cosmae.taginstance",
+            ),
+        ),
+    ]

tests/merge_request/api/integration/get_merge_requests_test.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument,too-many-statements
+from datetime import timedelta
 from unittest.mock import MagicMock, patch
 
 import tests.user.common as cu
@@ -6,6 +7,7 @@ from tests.merge_request import common as c
 from tests.merge_request.api.integration import requests as req
 from tests.utils import assert_versioned, format_datetime
 from cosmae.exception import NotAuthenticatedException
+from cosmae.tag.models_django import TagDefinition
 
 
 def test_unknown_user(auth_server):
@@ -56,6 +58,7 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user1",
             "curated": False,
+            "hidden": False,
         },
     )
     assert_versioned(
@@ -68,6 +71,7 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user",
             "curated": False,
+            "hidden": False,
         },
     )
     assigned_list = json["assigned"]
@@ -97,6 +101,7 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user",
             "curated": False,
+            "hidden": False,
         },
     )
     assert_versioned(
@@ -109,5 +114,114 @@ def test_get_merge_requests(auth_server, merge_request_user, merge_request_user1
             "type": "STRING",
             "owner": "test-user1",
             "curated": False,
+            "hidden": False,
+        },
+    )
+
+
+def test_get_merge_requests_with_hidden(
+    auth_server, merge_request_user, merge_request_user1, origin_tag_def_for_mr1
+):
+    tag_def, _ = TagDefinition.change_or_create(
+        id_persistent=origin_tag_def_for_mr1.id_persistent,
+        id_parent_persistent=origin_tag_def_for_mr1.id_parent_persistent,
+        name=origin_tag_def_for_mr1.name,
+        owner=origin_tag_def_for_mr1.owner,
+        curated=origin_tag_def_for_mr1.curated,
+        hidden=True,
+        version=origin_tag_def_for_mr1.id,
+        time_edit=origin_tag_def_for_mr1.time_edit + timedelta(minutes=60),
+    )
+    tag_def.save()
+    assert TagDefinition.most_recent_by_id(origin_tag_def_for_mr1.id_persistent).hidden
+    server, cookies = auth_server
+    rsp = req.get_merge_requests(server.url, cookies=cookies)
+    assert rsp.status_code == 200
+    json = rsp.json()
+    assert len(json) == 2
+    created_list = json["created"]
+    assert len(created_list) == 1
+    created = created_list[0]
+    assert len(created) == 7
+    assert created["created_at"] == format_datetime(c.time_merge_request1)
+    assert created["id_persistent"] == c.id_persistent_merge_request1
+    assert created["created_by"] == {
+        "user_name": cu.test_username,
+        "id_persistent": cu.test_uuid,
+        "permission_group": "APPLICANT",
+    }
+    assert created["assigned_to"] == {
+        "user_name": cu.test_username1,
+        "id_persistent": cu.test_uuid1,
+        "permission_group": "APPLICANT",
+    }
+    assert created["state"] == "OPEN"
+    assert_versioned(
+        created["destination"],
+        {
+            "id_persistent": c.id_persistent_tag_def_destination1,
+            "id_parent_persistent": None,
+            "name": c.name_tag_def_destination1,
+            "name_path": [c.name_tag_def_destination1],
+            "type": "STRING",
+            "owner": "test-user1",
+            "curated": False,
+            "hidden": False,
+        },
+    )
+    assert_versioned(
+        created["origin"],
+        {
+            "name": c.name_tag_def_origin1,
+            "name_path": [c.name_tag_def_origin1],
+            "id_parent_persistent": None,
+            "id_persistent": c.id_persistent_tag_def_origin1,
+            "type": "STRING",
+            "owner": "test-user",
+            "curated": False,
+            "hidden": True,
+        },
+    )
+    assigned_list = json["assigned"]
+    assert len(assigned_list) == 1
+    assigned = assigned_list[0]
+    assert len(assigned) == 7
+    assert assigned["created_at"] == format_datetime(c.time_merge_request)
+    assert assigned["id_persistent"] == c.id_persistent_merge_request
+    assert assigned["created_by"] == {
+        "user_name": cu.test_username1,
+        "id_persistent": cu.test_uuid1,
+        "permission_group": "APPLICANT",
+    }
+    assert assigned["assigned_to"] == {
+        "user_name": cu.test_username,
+        "id_persistent": cu.test_uuid,
+        "permission_group": "APPLICANT",
+    }
+    assert assigned["state"] == "OPEN"
+    assert_versioned(
+        assigned["destination"],
+        {
+            "id_persistent": c.id_persistent_tag_def_destination,
+            "id_parent_persistent": None,
+            "name": c.name_tag_def_destination,
+            "name_path": [c.name_tag_def_destination],
+            "type": "STRING",
+            "owner": "test-user",
+            "curated": False,
+            "hidden": False,
+        },
+    )
+    assert_versioned(
+        assigned["origin"],
+        {
+            "name": c.name_tag_def_origin,
+            "name_path": [c.name_tag_def_origin],
+            "id_persistent": c.id_persistent_tag_def_origin,
+            "id_parent_persistent": None,
+            "type": "STRING",
+            "owner": "test-user1",
+            "curated": False,
+            "hidden": False,
         },
     )

tests/settings.py
@@ -132,7 +132,7 @@ CONTRIBUTION_DIRECTORY = "/tmp"
 RQ_QUEUES = {
     "default": {
         "HOST": "localhost",
-        "PORT": environ.get("COSMAE_REDIS_PORT", 6379),
+        "PORT": environ.get("COSMAE_REDIS_PORT", 6333),
         "DB": 0,
         "DEFAULT_TIMEOUT": 360,
     }

tests/tag/models_django_definitions_test.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import pytest
 
@@ -10,6 +10,7 @@ from cosmae.exception import (
     TagDefinitionExistsException,
 )
 from cosmae.tag.models_django import TagDefinition
+from cosmae.util import timestamp
 
 
 @pytest.mark.django_db
@@ -268,7 +269,7 @@ def test_most_recent_for_user(tag_def_user):
     tag_def_edited, _ = TagDefinition.change_or_create(
         id_persistent=tag_def_user.id_persistent,
         id_parent_persistent=None,
-        time_edit=datetime.utcnow(),
+        time_edit=timestamp(),
         name="new_name",
         owner=tag_def_user.owner,
         version=tag_def_user.id,
@@ -276,3 +277,18 @@ def test_most_recent_for_user(tag_def_user):
     tag_def_edited.save()
     ret = TagDefinition.for_user(tag_def_user.owner).get()
     assert ret == tag_def_edited
+
+
+@pytest.mark.django_db
+def test_can_create_hidden():
+    tag_def, _ = TagDefinition.change_or_create(
+        id_persistent=c.id_tag_persistent_test,
+        id_parent_persistent=None,
+        time_edit=timestamp(),
+        name="new_name",
+        owner=None,
+        hidden=True,
+    )
+    tag_def.save()
+    retrieved = TagDefinition.most_recent_by_id(c.id_tag_persistent_test)
+    assert retrieved.hidden