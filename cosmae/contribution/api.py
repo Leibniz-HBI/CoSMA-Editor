Leibniz-HBI/DP-REX

Add contribution creation to backend.

vran/contribution/api.py
@@ -1,0 +1,224 @@
+"API methods for handling contributions."
+import os
+from typing import List, Optional
+from uuid import uuid4
+
+from django.conf import settings
+from django.db import DatabaseError
+from ninja import File, Form, Router, Schema, UploadedFile
+
+from vran.contribution.models_django import (
+    ContributionCandidate as ContributionCandidateDb,
+)
+from vran.exception import ApiError, NotAuthenticatedException
+from vran.util import VranUser
+from vran.util.auth import check_user
+
+
+class ContributionPostRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "Request data for adding a new contribution"
+    name: str
+    description: str
+    anonymous: bool
+    has_header: bool
+
+
+class ContributionPostResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    "Response data for successful creation of a contribution"
+    id_persistent: str
+
+
+class ContributionChunkRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "Request body for requesting contribution candidates in chunks"
+    offset: int
+    limit: int
+
+
+class ContributionCandidate(Schema):
+    # pylint: disable=too-few-public-methods
+    "API model for contribution candidates"
+    id_persistent: str
+    name: str
+    description: str
+    anonymous: bool
+    has_header: bool
+    state: str
+    author: Optional[str]
+
+
+class ContributionCandidatePatchRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "API model for contribution candidate patch requests"
+    name: Optional[str]
+    description: Optional[str]
+    anonymous: Optional[bool]
+    has_header: Optional[bool]
+
+
+class ContributionChunkResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    "Response containing multiple contribution candidates."
+    contributions: List[ContributionCandidate]
+
+
+router = Router()
+
+ALLOWED_CONTENT_TYPES = ["text/csv"]
+
+
+@router.post(
+    "",
+    response={200: ContributionPostResponse, 400: ApiError, 500: ApiError},
+)
+def contribution_post(
+    request,
+    contribution: ContributionPostRequest = Form(...),
+    file: UploadedFile = File(...),
+):
+    "Create a new contribution"
+    try:
+        content_type = file.content_type
+        if content_type == "text/csv":
+            extension = ".csv"
+        else:
+            return 400, ApiError(
+                msg=f"Invalid content type. Only {','.join(ALLOWED_CONTENT_TYPES)} allowed."
+            )
+        contribution_db = mk_initial_contribution_candidate(contribution, request.user)
+        out_file_name = contribution_db.id_persistent + extension
+        out_file_path = os.path.join(settings.CONTRIBUTION_DIRECTORY, out_file_name)
+        contribution_db.file_name = out_file_name
+        try:
+            with open(out_file_path, "wb") as out_f:
+                out_f.write(file.read())
+        except IOError:
+            return 500, ApiError(msg="Could not save the uploaded file.")
+        try:
+            contribution_db.save()
+        except DatabaseError:
+            if os.path.exists(out_file_path):
+                os.remove(out_file_path)
+            return 500, ApiError(
+                msg="Could not store the contribution in the database."
+            )
+
+        return 200, ContributionPostResponse(
+            id_persistent=contribution_db.id_persistent
+        )
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not create contribution")
+
+
+@router.get(
+    "chunk/{start}/{offset}",
+    response={200: ContributionChunkResponse, 401: ApiError, 500: ApiError},
+)
+def contribution_chunk_get(request, start: int, offset: int):
+    "Get the contributions for a user."
+    try:
+        try:
+            user = check_user(request)
+        except NotAuthenticatedException:
+            return 401, ApiError(msg="Not authenticated.")
+        contributions_db = ContributionCandidateDb.chunk_for_user(user, start, offset)
+        contributions_api = [
+            contribution_db_to_api(contribution) for contribution in contributions_db
+        ]
+        return 200, ContributionChunkResponse(contributions=contributions_api)
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not get requested chunk.")
+
+
+@router.get(
+    "{id_persistent}",
+    response={200: ContributionCandidate, 401: ApiError, 500: ApiError, 404: ApiError},
+)
+def contribution_get(request, id_persistent: str):
+    "Get  details on a single contribution candidate"
+    try:
+        try:
+            contribution_db = ContributionCandidateDb.by_id_persistent(
+                id_persistent, check_user(request)
+            )
+        except NotAuthenticatedException:
+            return 401, ApiError(msg="Not authenticated.")
+        except ContributionCandidateDb.DoesNotExist:  # pylint: disable=no-member
+            return 404, ApiError(msg="Contribution does not exist.")
+
+        contribution_api = contribution_db_to_api(contribution_db)
+        return 200, contribution_api
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not get contribution")
+
+
+@router.patch(
+    "{id_persistent}",
+    response={200: ContributionCandidate, 401: ApiError, 500: ApiError, 404: ApiError},
+)
+def contribution_patch(
+    request, id_persistent: str, patch_data: ContributionCandidatePatchRequest
+):
+    "Update metadata of a contribution"
+    try:
+        user = check_user(request)
+        contribution_db = ContributionCandidateDb.update(
+            id_persistent, user, **patch_data.dict(exclude_unset=True)
+        )
+        return 200, contribution_db_to_api(contribution_db)
+
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    except ContributionCandidateDb.DoesNotExist:  # pylint: disable=no-member
+        return 404, ApiError(msg="Contribution does not exist.")
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not patch contribution")
+
+
+def mk_initial_contribution_candidate(
+    contribution_api: ContributionPostRequest, user: VranUser
+):
+    """Transform an API Contribution post to an initial database contribution candidate.
+    The result has no file name assigned."""
+    uuid = str(uuid4())
+    return ContributionCandidateDb(
+        name=contribution_api.name,
+        description=contribution_api.description,
+        anonymous=contribution_api.anonymous,
+        id_persistent=uuid,
+        has_header=contribution_api.has_header,
+        created_by=user,
+        state=ContributionCandidateDb.UPLOADED,
+    )
+
+
+def contribution_db_to_api(
+    contribution_db: ContributionCandidateDb,
+) -> ContributionCandidate:
+    "Transform a contribution candidate from DB to API representation."
+    if contribution_db.anonymous:
+        author = None
+    else:
+        author = contribution_db.created_by.username
+    return ContributionCandidate(
+        id_persistent=str(contribution_db.id_persistent),
+        name=contribution_db.name,
+        description=contribution_db.description,
+        anonymous=contribution_db.anonymous,
+        has_header=contribution_db.has_header,
+        state=_contribution_state_mapping_db_to_api[contribution_db.state],
+        author=author,
+    )
+
+
+_contribution_state_mapping_db_to_api = {
+    ContributionCandidateDb.UPLOADED: "UPLOADED",
+    ContributionCandidateDb.COLUMNS_EXTRACTED: "COLUMNS_EXTRACTED",
+    ContributionCandidateDb.COLUMNS_ASSIGNED: "COLUMNS_ASSIGNED",
+    ContributionCandidateDb.ENTITIES_MATCHED: "ENTITIES_MATCHED",
+    ContributionCandidateDb.ENTITIES_ASSIGNED: "ENTITIES_ASSIGNED",
+    ContributionCandidateDb.VALUES_ASSIGNED: "VALUES_ASSIGNED",
+    ContributionCandidateDb.MERGED: "MERGED",
+}

vran/contribution/models_django.py
@@ -1,0 +1,55 @@
+"Models for contribution proposals."
+from django.db import models
+
+from vran.util import VranUser
+from vran.util.django import patch_from_dict
+
+
+class ContributionCandidate(models.Model):
+    "ORM Model for maintaining proposed contributions."
+    UPLOADED = "UPLD"
+    COLUMNS_EXTRACTED = "CLXT"
+    COLUMNS_ASSIGNED = "CLAS"
+    ENTITIES_MATCHED = "NTMT"
+    ENTITIES_ASSIGNED = "NTAS"
+    VALUES_ASSIGNED = "VLAS"
+    MERGED = "MRGD"
+    TYPE_CHOICES = [
+        (UPLOADED, "uploaded"),
+        (COLUMNS_EXTRACTED, "columns extracted"),
+        (COLUMNS_ASSIGNED, "columns assigned"),
+        (ENTITIES_MATCHED, "entities matched"),
+        (ENTITIES_ASSIGNED, "entities assigned"),
+        (VALUES_ASSIGNED, "values assigned"),
+        (MERGED, "merged"),
+    ]
+
+    name = models.TextField()
+    description = models.TextField()
+    id_persistent = models.UUIDField()
+    anonymous = models.BooleanField()
+    has_header = models.BooleanField()
+    created_by = models.ForeignKey("VranUser", on_delete=models.CASCADE)
+    file_name = models.TextField()
+    state = models.CharField(max_length=4, choices=TYPE_CHOICES)
+
+    @classmethod
+    def chunk_for_user(cls, user, start, offset):
+        "Get the contribution candidates for a specific user."
+        return ContributionCandidate.objects.filter(  # pylint: disable=no-member
+            created_by=user
+        ).all()[start : start + offset]
+
+    @classmethod
+    def by_id_persistent(cls, id_persistent: str, user: VranUser):
+        "Get a single contribution candidate"
+        return ContributionCandidate.objects.get(  # pylint: disable=no-member
+            created_by=user, id_persistent=id_persistent
+        )
+
+    @classmethod
+    def update(cls, id_persistent: str, user: VranUser, **kwargs):
+        "Changes a contribution candidate."
+        candidate = ContributionCandidate.by_id_persistent(id_persistent, user)
+        patch_from_dict(candidate, **kwargs)
+        return candidate

vran/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 from ninja import NinjaAPI, Schema
 from ninja.constants import NOT_SET
 
+from vran.contribution.api import router as contribution_router
 from vran.person.api import router as person_router
 from vran.tag.api import router as tag_router
 from vran.user.api import router as user_router
@@ -15,6 +16,7 @@ ninja_api = NinjaAPI(csrf=False)
 ninja_api.add_router("user", user_router, auth=NOT_SET)
 ninja_api.add_router("persons", person_router, auth=vran_auth)
 ninja_api.add_router("tags", tag_router, auth=vran_auth)
+ninja_api.add_router("contributions", contribution_router, auth=vran_auth)
 
 
 class LoginRequest(Schema):

vran/util/auth.py
@@ -4,6 +4,9 @@ from enum import Enum
 from django.http import HttpRequest
 from ninja.security import django_auth
 
+from vran.exception import NotAuthenticatedException
+from vran.util import VranUser
+
 
 class VranPermission(Enum):
     # pylint: disable=too-few-public-methods
@@ -31,3 +34,11 @@ def vran_auth(request: HttpRequest):
     This is required to have sub paths without authorization
     where the parents use authorization."""
     return django_auth.authenticate(request, None)
+
+
+def check_user(request):
+    "Checks wether a request is authenticated, otherwise throws an exception."
+    user = request.user
+    if isinstance(user, VranUser):
+        return user
+    raise NotAuthenticatedException()

django_project/settings/settings_production.py
@@ -1,3 +1,4 @@
+# pylint: disable=duplicate-code
 """
 Django settings for django_project project.
 
@@ -22,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+CONTRIBUTION_DIRECTORY = "/srv/vran/contributions"
 
 
 ###################################################################

django_project/settings/settings_test.py
@@ -1,3 +1,4 @@
+# pylint: disable=duplicate-code
 """
 Django settings for django_project project.
 
@@ -23,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
+
+
+CONTRIBUTION_DIRECTORY = "/srv/vran/contributions"
 
 ###################################################################
 # SECURITY WARNING: keep the secret key used in production secret!

tests/conftest.py
@@ -5,6 +5,7 @@ from tests.entity import common as ce
 from tests.user import common as cu
 from tests.user.api.integration.requests import post_login, post_register
 from vran.entity.models_django import Entity
+from vran.util import VranUser
 
 
 @pytest.fixture
@@ -27,3 +28,42 @@ def auth_server(live_server):
         live_server.url, {"name": cu.test_username, "password": cu.test_password}
     )
     return live_server, rsp.cookies
+
+
+@pytest.fixture()
+def auth_server1(auth_server):
+    live_server, cookies_user0 = auth_server
+    url = live_server.url
+    rsp = post_register(
+        url,
+        {
+            "user_name": cu.test_username1,
+            "password": cu.test_password1,
+            "email": cu.test_email1,
+            "names_personal": cu.test_names_personal1,
+        },
+    )
+    rsp = post_login(url, {"name": cu.test_username1, "password": cu.test_password1})
+    return live_server, cookies_user0, rsp.cookies
+
+
+@pytest.fixture
+def user(db):  # pylint: disable=unused-argument
+    user = VranUser.objects.create_user(
+        username=cu.test_username,
+        password=cu.test_password,
+        email=cu.test_email,
+        first_name=cu.test_names_personal,
+    )
+    return user
+
+
+@pytest.fixture
+def user1(db):  # pylint: disable=unused-argument
+    user = VranUser.objects.create_user(
+        username=cu.test_username1,
+        password=cu.test_password1,
+        email=cu.test_email1,
+        first_name=cu.test_names_personal1,
+    )
+    return user

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/api.py
@@ -1,0 +1,224 @@
+"API methods for handling contributions."
+import os
+from typing import List, Optional
+from uuid import uuid4
+
+from django.conf import settings
+from django.db import DatabaseError
+from ninja import File, Form, Router, Schema, UploadedFile
+
+from cosmae.contribution.models_django import (
+    ContributionCandidate as ContributionCandidateDb,
+)
+from cosmae.exception import ApiError, NotAuthenticatedException
+from cosmae.util import CosmaeUser
+from cosmae.util.auth import check_user
+
+
+class ContributionPostRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "Request data for adding a new contribution"
+    name: str
+    description: str
+    anonymous: bool
+    has_header: bool
+
+
+class ContributionPostResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    "Response data for successful creation of a contribution"
+    id_persistent: str
+
+
+class ContributionChunkRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "Request body for requesting contribution candidates in chunks"
+    offset: int
+    limit: int
+
+
+class ContributionCandidate(Schema):
+    # pylint: disable=too-few-public-methods
+    "API model for contribution candidates"
+    id_persistent: str
+    name: str
+    description: str
+    anonymous: bool
+    has_header: bool
+    state: str
+    author: Optional[str]
+
+
+class ContributionCandidatePatchRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    "API model for contribution candidate patch requests"
+    name: Optional[str]
+    description: Optional[str]
+    anonymous: Optional[bool]
+    has_header: Optional[bool]
+
+
+class ContributionChunkResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    "Response containing multiple contribution candidates."
+    contributions: List[ContributionCandidate]
+
+
+router = Router()
+
+ALLOWED_CONTENT_TYPES = ["text/csv"]
+
+
+@router.post(
+    "",
+    response={200: ContributionPostResponse, 400: ApiError, 500: ApiError},
+)
+def contribution_post(
+    request,
+    contribution: ContributionPostRequest = Form(...),
+    file: UploadedFile = File(...),
+):
+    "Create a new contribution"
+    try:
+        content_type = file.content_type
+        if content_type == "text/csv":
+            extension = ".csv"
+        else:
+            return 400, ApiError(
+                msg=f"Invalid content type. Only {','.join(ALLOWED_CONTENT_TYPES)} allowed."
+            )
+        contribution_db = mk_initial_contribution_candidate(contribution, request.user)
+        out_file_name = contribution_db.id_persistent + extension
+        out_file_path = os.path.join(settings.CONTRIBUTION_DIRECTORY, out_file_name)
+        contribution_db.file_name = out_file_name
+        try:
+            with open(out_file_path, "wb") as out_f:
+                out_f.write(file.read())
+        except IOError:
+            return 500, ApiError(msg="Could not save the uploaded file.")
+        try:
+            contribution_db.save()
+        except DatabaseError:
+            if os.path.exists(out_file_path):
+                os.remove(out_file_path)
+            return 500, ApiError(
+                msg="Could not store the contribution in the database."
+            )
+
+        return 200, ContributionPostResponse(
+            id_persistent=contribution_db.id_persistent
+        )
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not create contribution")
+
+
+@router.get(
+    "chunk/{start}/{offset}",
+    response={200: ContributionChunkResponse, 401: ApiError, 500: ApiError},
+)
+def contribution_chunk_get(request, start: int, offset: int):
+    "Get the contributions for a user."
+    try:
+        try:
+            user = check_user(request)
+        except NotAuthenticatedException:
+            return 401, ApiError(msg="Not authenticated.")
+        contributions_db = ContributionCandidateDb.chunk_for_user(user, start, offset)
+        contributions_api = [
+            contribution_db_to_api(contribution) for contribution in contributions_db
+        ]
+        return 200, ContributionChunkResponse(contributions=contributions_api)
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not get requested chunk.")
+
+
+@router.get(
+    "{id_persistent}",
+    response={200: ContributionCandidate, 401: ApiError, 500: ApiError, 404: ApiError},
+)
+def contribution_get(request, id_persistent: str):
+    "Get  details on a single contribution candidate"
+    try:
+        try:
+            contribution_db = ContributionCandidateDb.by_id_persistent(
+                id_persistent, check_user(request)
+            )
+        except NotAuthenticatedException:
+            return 401, ApiError(msg="Not authenticated.")
+        except ContributionCandidateDb.DoesNotExist:  # pylint: disable=no-member
+            return 404, ApiError(msg="Contribution does not exist.")
+
+        contribution_api = contribution_db_to_api(contribution_db)
+        return 200, contribution_api
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not get contribution")
+
+
+@router.patch(
+    "{id_persistent}",
+    response={200: ContributionCandidate, 401: ApiError, 500: ApiError, 404: ApiError},
+)
+def contribution_patch(
+    request, id_persistent: str, patch_data: ContributionCandidatePatchRequest
+):
+    "Update metadata of a contribution"
+    try:
+        user = check_user(request)
+        contribution_db = ContributionCandidateDb.update(
+            id_persistent, user, **patch_data.dict(exclude_unset=True)
+        )
+        return 200, contribution_db_to_api(contribution_db)
+
+    except NotAuthenticatedException:
+        return 401, ApiError(msg="Not authenticated.")
+    except ContributionCandidateDb.DoesNotExist:  # pylint: disable=no-member
+        return 404, ApiError(msg="Contribution does not exist.")
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not patch contribution")
+
+
+def mk_initial_contribution_candidate(
+    contribution_api: ContributionPostRequest, user: CosmaeUser
+):
+    """Transform an API Contribution post to an initial database contribution candidate.
+    The result has no file name assigned."""
+    uuid = str(uuid4())
+    return ContributionCandidateDb(
+        name=contribution_api.name,
+        description=contribution_api.description,
+        anonymous=contribution_api.anonymous,
+        id_persistent=uuid,
+        has_header=contribution_api.has_header,
+        created_by=user,
+        state=ContributionCandidateDb.UPLOADED,
+    )
+
+
+def contribution_db_to_api(
+    contribution_db: ContributionCandidateDb,
+) -> ContributionCandidate:
+    "Transform a contribution candidate from DB to API representation."
+    if contribution_db.anonymous:
+        author = None
+    else:
+        author = contribution_db.created_by.username
+    return ContributionCandidate(
+        id_persistent=str(contribution_db.id_persistent),
+        name=contribution_db.name,
+        description=contribution_db.description,
+        anonymous=contribution_db.anonymous,
+        has_header=contribution_db.has_header,
+        state=_contribution_state_mapping_db_to_api[contribution_db.state],
+        author=author,
+    )
+
+
+_contribution_state_mapping_db_to_api = {
+    ContributionCandidateDb.UPLOADED: "UPLOADED",
+    ContributionCandidateDb.COLUMNS_EXTRACTED: "COLUMNS_EXTRACTED",
+    ContributionCandidateDb.COLUMNS_ASSIGNED: "COLUMNS_ASSIGNED",
+    ContributionCandidateDb.ENTITIES_MATCHED: "ENTITIES_MATCHED",
+    ContributionCandidateDb.ENTITIES_ASSIGNED: "ENTITIES_ASSIGNED",
+    ContributionCandidateDb.VALUES_ASSIGNED: "VALUES_ASSIGNED",
+    ContributionCandidateDb.MERGED: "MERGED",
+}

cosmae/contribution/models_django.py
@@ -1,0 +1,55 @@
+"Models for contribution proposals."
+from django.db import models
+
+from cosmae.util import CosmaeUser
+from cosmae.util.django import patch_from_dict
+
+
+class ContributionCandidate(models.Model):
+    "ORM Model for maintaining proposed contributions."
+    UPLOADED = "UPLD"
+    COLUMNS_EXTRACTED = "CLXT"
+    COLUMNS_ASSIGNED = "CLAS"
+    ENTITIES_MATCHED = "NTMT"
+    ENTITIES_ASSIGNED = "NTAS"
+    VALUES_ASSIGNED = "VLAS"
+    MERGED = "MRGD"
+    TYPE_CHOICES = [
+        (UPLOADED, "uploaded"),
+        (COLUMNS_EXTRACTED, "columns extracted"),
+        (COLUMNS_ASSIGNED, "columns assigned"),
+        (ENTITIES_MATCHED, "entities matched"),
+        (ENTITIES_ASSIGNED, "entities assigned"),
+        (VALUES_ASSIGNED, "values assigned"),
+        (MERGED, "merged"),
+    ]
+
+    name = models.TextField()
+    description = models.TextField()
+    id_persistent = models.UUIDField()
+    anonymous = models.BooleanField()
+    has_header = models.BooleanField()
+    created_by = models.ForeignKey("CosmaeUser", on_delete=models.CASCADE)
+    file_name = models.TextField()
+    state = models.CharField(max_length=4, choices=TYPE_CHOICES)
+
+    @classmethod
+    def chunk_for_user(cls, user, start, offset):
+        "Get the contribution candidates for a specific user."
+        return ContributionCandidate.objects.filter(  # pylint: disable=no-member
+            created_by=user
+        ).all()[start : start + offset]
+
+    @classmethod
+    def by_id_persistent(cls, id_persistent: str, user: CosmaeUser):
+        "Get a single contribution candidate"
+        return ContributionCandidate.objects.get(  # pylint: disable=no-member
+            created_by=user, id_persistent=id_persistent
+        )
+
+    @classmethod
+    def update(cls, id_persistent: str, user: CosmaeUser, **kwargs):
+        "Changes a contribution candidate."
+        candidate = ContributionCandidate.by_id_persistent(id_persistent, user)
+        patch_from_dict(candidate, **kwargs)
+        return candidate

cosmae/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 from ninja import NinjaAPI, Schema
 from ninja.constants import NOT_SET
 
+from cosmae.contribution.api import router as contribution_router
 from cosmae.person.api import router as person_router
 from cosmae.tag.api import router as tag_router
 from cosmae.user.api import router as user_router
@@ -15,6 +16,7 @@ ninja_api = NinjaAPI(csrf=False)
 ninja_api.add_router("user", user_router, auth=NOT_SET)
 ninja_api.add_router("persons", person_router, auth=cosmae_auth)
 ninja_api.add_router("tags", tag_router, auth=cosmae_auth)
+ninja_api.add_router("contributions", contribution_router, auth=cosmae_auth)
 
 
 class LoginRequest(Schema):

cosmae/util/auth.py
@@ -4,6 +4,9 @@ from enum import Enum
 from django.http import HttpRequest
 from ninja.security import django_auth
 
+from cosmae.exception import NotAuthenticatedException
+from cosmae.util import CosmaeUser
+
 
 class CosmaePermission(Enum):
     # pylint: disable=too-few-public-methods
@@ -31,3 +34,11 @@ def cosmae_auth(request: HttpRequest):
     This is required to have sub paths without authorization
     where the parents use authorization."""
     return django_auth.authenticate(request, None)
+
+
+def check_user(request):
+    "Checks wether a request is authenticated, otherwise throws an exception."
+    user = request.user
+    if isinstance(user, CosmaeUser):
+        return user
+    raise NotAuthenticatedException()

django_project/settings/settings_production.py
@@ -1,3 +1,4 @@
+# pylint: disable=duplicate-code
 """
 Django settings for django_project project.
 
@@ -22,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+CONTRIBUTION_DIRECTORY = "/srv/cosmae/contributions"
 
 
 ###################################################################

django_project/settings/settings_test.py
@@ -1,3 +1,4 @@
+# pylint: disable=duplicate-code
 """
 Django settings for django_project project.
 
@@ -23,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
+
+
+CONTRIBUTION_DIRECTORY = "/srv/cosmae/contributions"
 
 ###################################################################
 # SECURITY WARNING: keep the secret key used in production secret!

tests/conftest.py
@@ -5,6 +5,7 @@ from tests.entity import common as ce
 from tests.user import common as cu
 from tests.user.api.integration.requests import post_login, post_register
 from cosmae.entity.models_django import Entity
+from cosmae.util import CosmaeUser
 
 
 @pytest.fixture
@@ -27,3 +28,42 @@ def auth_server(live_server):
         live_server.url, {"name": cu.test_username, "password": cu.test_password}
     )
     return live_server, rsp.cookies
+
+
+@pytest.fixture()
+def auth_server1(auth_server):
+    live_server, cookies_user0 = auth_server
+    url = live_server.url
+    rsp = post_register(
+        url,
+        {
+            "user_name": cu.test_username1,
+            "password": cu.test_password1,
+            "email": cu.test_email1,
+            "names_personal": cu.test_names_personal1,
+        },
+    )
+    rsp = post_login(url, {"name": cu.test_username1, "password": cu.test_password1})
+    return live_server, cookies_user0, rsp.cookies
+
+
+@pytest.fixture
+def user(db):  # pylint: disable=unused-argument
+    user = CosmaeUser.objects.create_user(
+        username=cu.test_username,
+        password=cu.test_password,
+        email=cu.test_email,
+        first_name=cu.test_names_personal,
+    )
+    return user
+
+
+@pytest.fixture
+def user1(db):  # pylint: disable=unused-argument
+    user = CosmaeUser.objects.create_user(
+        username=cu.test_username1,
+        password=cu.test_password1,
+        email=cu.test_email1,
+        first_name=cu.test_names_personal1,
+    )
+    return user