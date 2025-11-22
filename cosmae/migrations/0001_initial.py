Leibniz-HBI/DP-REX

Add API for creating and retrieving persons.

vran/migrations/0001_initial.py
@@ -1,0 +1,54 @@
+# Generated by Django 4.1.3 on 2022-11-28 12:19
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Entity",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("proxy_name", models.TextField()),
+                ("display_txt", models.TextField()),
+                ("time_edit", models.DateTimeField()),
+                ("id_persistent", models.TextField()),
+                ("names_personal", models.TextField(null=True)),
+                ("names_family", models.TextField(null=True)),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="vran.entity",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="Person",
+            fields=[],
+            options={
+                "proxy": True,
+                "indexes": [],
+                "constraints": [],
+            },
+            bases=("vran.entity",),
+        ),
+    ]

vran/person/api.py
@@ -1,0 +1,158 @@
+"""API for handling natural persons."""
+from datetime import datetime
+from typing import List, Optional
+from uuid import uuid4
+
+from django.db import IntegrityError
+from ninja import Router, Schema
+
+from vran.exception import (
+    ApiError,
+    EntityExistsException,
+    EntityUpdatedException,
+    ValidationException,
+)
+from vran.person.models_django import Person as PersonNaturalDb
+from vran.util.django import save_many_atomic
+
+router = Router()
+
+
+class PersonNatural(Schema):
+    # pylint: disable=too-few-public-methods
+    """API model for a natural person."""
+
+    names_personal: str
+    names_family: Optional[str]
+    display_txt: str
+    version: Optional[int]
+    """The version of the person that the change is made on.
+    If null on POST, a new person is created."""
+    id_persistent: Optional[str]
+
+
+class PersonNaturalList(Schema):
+    # pylint: disable=too-few-public-methods
+    """API Model for multiple natural persons."""
+
+    persons: List[PersonNatural]
+
+
+class PersonsGetRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    """Model for return type of posting persons."""
+
+    modified_ids: List[str]
+
+
+class PersonCountResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    """Response for the count person request."""
+
+    count: int
+
+
+class ChunkRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    """Request body for chunks of persons"""
+
+    offset: int
+    limit: int
+
+
+@router.post("", response={200: PersonNaturalList, 400: ApiError, 500: ApiError})
+def persons_post(_, persons: PersonNaturalList):
+    """Add a person to the DB.
+    Returns:
+        PersonNaturalList: The updated persons.
+    """
+    now = datetime.utcnow()
+    try:
+        person_dbs = [person_api_to_db(person, now) for person in persons.persons]
+    except ValidationException as valid_x:
+        return 400, ApiError(msg=str(valid_x))
+    except EntityExistsException as exists_x:
+        return 500, ApiError(
+            msg=(
+                "Could not generate an id for person "
+                f"with display_txt {exists_x.display_txt}."
+            )
+        )
+    except EntityUpdatedException as updated_x:
+        return 400, ApiError(
+            msg="There has been a concurrent modification "
+            f"to the person with id_persistent {updated_x.id_affected}."
+        )
+
+    try:
+        save_many_atomic(person for person, do_write in person_dbs if do_write)
+    except IntegrityError:
+        return 500, ApiError(msg="Provided data not consistent with database.")
+    return 200, PersonNaturalList(
+        persons=[person_db_to_api(person) for person, _ in person_dbs]
+    )
+
+
+@router.get("count", response={200: PersonCountResponse, 500: ApiError})
+def persons_count_get(_):
+    """Get the number of unique persons."""
+    try:
+        count = PersonNaturalDb.get_count()
+        return PersonCountResponse(count=count)
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not count persons.")
+
+
+@router.post("chunk", response={200: PersonNaturalList, 400: ApiError, 500: ApiError})
+def persons_chunks_post(_, req_data: ChunkRequest):
+    """Get a chunk of persons.
+    Note:
+        The persons are ordered by the order of initial creation."""
+    chunk_limit = 1000
+    if req_data.limit > chunk_limit:
+        return 400, ApiError(msg=f"Please specify limit smaller than {chunk_limit}.")
+    try:
+        person_dbs = PersonNaturalDb.get_most_recent_chunked(
+            req_data.offset, req_data.limit
+        )
+        person_apis = [person_db_to_api(person) for person in person_dbs]
+        return 200, PersonNaturalList(persons=person_apis)
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not get requested chunk.")
+
+
+def person_api_to_db(person: PersonNatural, time_edit: datetime) -> PersonNaturalDb:
+    """Transform an natural person from API to DB model."""
+    if person.id_persistent:
+        persistent_id = person.id_persistent
+        if person.version is None:
+            raise ValidationException(
+                f"person with persistent_id {person.id_persistent} "
+                "has no previous version."
+            )
+    else:
+        if person.version:
+            raise ValidationException(
+                f"Person with display_txt {person.display_txt} "
+                "has version but no persistent_id."
+            )
+        persistent_id = str(uuid4())
+    return PersonNaturalDb.change_or_create(
+        display_txt=person.display_txt,
+        time_edit=time_edit,
+        id_persistent=persistent_id,
+        version=person.version,
+        names_personal=person.names_personal,
+        names_family=person.names_family,
+    )
+
+
+def person_db_to_api(person: PersonNaturalDb) -> PersonNatural:
+    """Transform a natural person from DB to API representation."""
+    return PersonNatural(
+        names_personal=person.names_personal,
+        names_family=person.names_family,
+        display_txt=person.display_txt,
+        version=person.id,
+        id_persistent=person.id_persistent,
+    )

tests/person/api/integration/count_get_test.py
@@ -1,0 +1,37 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, patch
+
+from django.db import IntegrityError
+
+from tests.person.api.integration.requests import get_count, post_person
+
+
+def test_get_count(live_server):
+    for i in range(10):
+        id_prev = None
+        version_prev = None
+        for j in range(3):
+            person = dict(
+                names_personal="test personal",
+                names_family=f"test family {i:02d}",
+                display_txt=f"display test {i:02d} {j}",
+            )
+            if id_prev and version_prev:
+                person["id_persistent"] = id_prev
+                person["version"] = version_prev
+            rsp = post_person(live_server.url, person)
+            assert rsp.status_code == 200
+            persons = rsp.json()
+            id_prev = persons["persons"][0]["id_persistent"]
+            version_prev = persons["persons"][0]["version"]
+    count = get_count(live_server.url)
+    assert count.json()["count"] == 10
+
+
+def test_bad_db(live_server):
+    mock = MagicMock()
+    mock.side_effect = IntegrityError()
+    with patch("vran.person.models_django.Person.get_count", mock):
+        rsp = get_count(live_server.url)
+    assert rsp.status_code == 500
+    assert rsp.json()["msg"] == "Could not count persons."

tests/person/django_models_test.py
@@ -1,0 +1,45 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+
+import pytest
+
+from tests.person import common as c
+from vran.entity.models_django import Entity
+from vran.person.models_django import Person
+
+
+@pytest.fixture
+def person():
+    return Person(
+        id_persistent=c.id_persistent_test,
+        names_family=c.names_family_test,
+        names_personal=c.names_personal_test,
+        display_txt=c.display_txt_test,
+        time_edit=c.time_edit_test,
+    )
+
+
+def test_create_person(person):
+    assert person.id_persistent == c.id_persistent_test
+    assert person.names_family == c.names_family_test
+    assert person.names_personal == c.names_personal_test
+    assert person.display_txt == c.display_txt_test
+    assert person.time_edit == c.time_edit_test
+
+
+@pytest.mark.django_db
+def test_store_and_retrieve_person(person):
+    person.save()
+    retrieved = Person.objects.get(names_family=c.names_family_test)
+    assert retrieved == person
+
+
+@pytest.mark.django_db
+def test_does_not_get_other_entities(person):
+    person.save()
+    entity = Entity(time_edit=c.time_edit_test)
+    entity.save()
+    persons = list(Person.objects.all())
+    assert len(persons) == 1
+    assert persons[0] == person
+    entities = list(Entity.objects.all())  # pylint: disable=no-member
+    assert len(entities) == 2

tests/util/django_test.py
@@ -1,0 +1,51 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from datetime import datetime
+
+import pytest
+from django.db import IntegrityError
+
+from vran.entity.models_django import Entity
+from vran.util import django as du
+
+
+@pytest.mark.django_db
+def test_store_multiple():
+    entity_test_0 = Entity(
+        id_persistent="test_id_0", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+    entity_test_1 = Entity(
+        id_persistent="test_id_1", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+    entity_test_2 = Entity(
+        id_persistent="test_id_2", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+
+    entities_test = [entity_test_0, entity_test_1, entity_test_2]
+    du.save_many_atomic(entities_test)
+    assert Entity.get_count() == 3
+
+
+@pytest.mark.django_db
+def test_does_rollback():
+
+    entity_test_0 = Entity(
+        id_persistent="test_id_0", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+    entity_test_1 = Entity(
+        id_persistent="test_id_1",
+        display_txt="foo",
+        time_edit=datetime(2022, 11, 14),
+        previous_version=entity_test_0,
+    )
+    entity_test_2 = Entity(
+        id_persistent="test_id_2",
+        display_txt="foo",
+        time_edit=datetime(2022, 11, 14),
+        previous_version=entity_test_0,
+    )
+
+    entities_test = [entity_test_0, entity_test_1, entity_test_2]
+
+    with pytest.raises(IntegrityError):
+        du.save_many_atomic(entities_test)
+    assert Entity.get_count() == 0

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0001_initial.py
@@ -1,0 +1,54 @@
+# Generated by Django 4.1.3 on 2022-11-28 12:19
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Entity",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("proxy_name", models.TextField()),
+                ("display_txt", models.TextField()),
+                ("time_edit", models.DateTimeField()),
+                ("id_persistent", models.TextField()),
+                ("names_personal", models.TextField(null=True)),
+                ("names_family", models.TextField(null=True)),
+                (
+                    "previous_version",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="cosmae.entity",
+                        unique=True,
+                    ),
+                ),
+            ],
+        ),
+        migrations.CreateModel(
+            name="Person",
+            fields=[],
+            options={
+                "proxy": True,
+                "indexes": [],
+                "constraints": [],
+            },
+            bases=("cosmae.entity",),
+        ),
+    ]

cosmae/person/api.py
@@ -1,0 +1,158 @@
+"""API for handling natural persons."""
+from datetime import datetime
+from typing import List, Optional
+from uuid import uuid4
+
+from django.db import IntegrityError
+from ninja import Router, Schema
+
+from cosmae.exception import (
+    ApiError,
+    EntityExistsException,
+    EntityUpdatedException,
+    ValidationException,
+)
+from cosmae.person.models_django import Person as PersonNaturalDb
+from cosmae.util.django import save_many_atomic
+
+router = Router()
+
+
+class PersonNatural(Schema):
+    # pylint: disable=too-few-public-methods
+    """API model for a natural person."""
+
+    names_personal: str
+    names_family: Optional[str]
+    display_txt: str
+    version: Optional[int]
+    """The version of the person that the change is made on.
+    If null on POST, a new person is created."""
+    id_persistent: Optional[str]
+
+
+class PersonNaturalList(Schema):
+    # pylint: disable=too-few-public-methods
+    """API Model for multiple natural persons."""
+
+    persons: List[PersonNatural]
+
+
+class PersonsGetRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    """Model for return type of posting persons."""
+
+    modified_ids: List[str]
+
+
+class PersonCountResponse(Schema):
+    # pylint: disable=too-few-public-methods
+    """Response for the count person request."""
+
+    count: int
+
+
+class ChunkRequest(Schema):
+    # pylint: disable=too-few-public-methods
+    """Request body for chunks of persons"""
+
+    offset: int
+    limit: int
+
+
+@router.post("", response={200: PersonNaturalList, 400: ApiError, 500: ApiError})
+def persons_post(_, persons: PersonNaturalList):
+    """Add a person to the DB.
+    Returns:
+        PersonNaturalList: The updated persons.
+    """
+    now = datetime.utcnow()
+    try:
+        person_dbs = [person_api_to_db(person, now) for person in persons.persons]
+    except ValidationException as valid_x:
+        return 400, ApiError(msg=str(valid_x))
+    except EntityExistsException as exists_x:
+        return 500, ApiError(
+            msg=(
+                "Could not generate an id for person "
+                f"with display_txt {exists_x.display_txt}."
+            )
+        )
+    except EntityUpdatedException as updated_x:
+        return 400, ApiError(
+            msg="There has been a concurrent modification "
+            f"to the person with id_persistent {updated_x.id_affected}."
+        )
+
+    try:
+        save_many_atomic(person for person, do_write in person_dbs if do_write)
+    except IntegrityError:
+        return 500, ApiError(msg="Provided data not consistent with database.")
+    return 200, PersonNaturalList(
+        persons=[person_db_to_api(person) for person, _ in person_dbs]
+    )
+
+
+@router.get("count", response={200: PersonCountResponse, 500: ApiError})
+def persons_count_get(_):
+    """Get the number of unique persons."""
+    try:
+        count = PersonNaturalDb.get_count()
+        return PersonCountResponse(count=count)
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not count persons.")
+
+
+@router.post("chunk", response={200: PersonNaturalList, 400: ApiError, 500: ApiError})
+def persons_chunks_post(_, req_data: ChunkRequest):
+    """Get a chunk of persons.
+    Note:
+        The persons are ordered by the order of initial creation."""
+    chunk_limit = 1000
+    if req_data.limit > chunk_limit:
+        return 400, ApiError(msg=f"Please specify limit smaller than {chunk_limit}.")
+    try:
+        person_dbs = PersonNaturalDb.get_most_recent_chunked(
+            req_data.offset, req_data.limit
+        )
+        person_apis = [person_db_to_api(person) for person in person_dbs]
+        return 200, PersonNaturalList(persons=person_apis)
+    except Exception:  # pylint: disable=broad-except
+        return 500, ApiError(msg="Could not get requested chunk.")
+
+
+def person_api_to_db(person: PersonNatural, time_edit: datetime) -> PersonNaturalDb:
+    """Transform an natural person from API to DB model."""
+    if person.id_persistent:
+        persistent_id = person.id_persistent
+        if person.version is None:
+            raise ValidationException(
+                f"person with persistent_id {person.id_persistent} "
+                "has no previous version."
+            )
+    else:
+        if person.version:
+            raise ValidationException(
+                f"Person with display_txt {person.display_txt} "
+                "has version but no persistent_id."
+            )
+        persistent_id = str(uuid4())
+    return PersonNaturalDb.change_or_create(
+        display_txt=person.display_txt,
+        time_edit=time_edit,
+        id_persistent=persistent_id,
+        version=person.version,
+        names_personal=person.names_personal,
+        names_family=person.names_family,
+    )
+
+
+def person_db_to_api(person: PersonNaturalDb) -> PersonNatural:
+    """Transform a natural person from DB to API representation."""
+    return PersonNatural(
+        names_personal=person.names_personal,
+        names_family=person.names_family,
+        display_txt=person.display_txt,
+        version=person.id,
+        id_persistent=person.id_persistent,
+    )

tests/person/api/integration/count_get_test.py
@@ -1,0 +1,37 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from unittest.mock import MagicMock, patch
+
+from django.db import IntegrityError
+
+from tests.person.api.integration.requests import get_count, post_person
+
+
+def test_get_count(live_server):
+    for i in range(10):
+        id_prev = None
+        version_prev = None
+        for j in range(3):
+            person = dict(
+                names_personal="test personal",
+                names_family=f"test family {i:02d}",
+                display_txt=f"display test {i:02d} {j}",
+            )
+            if id_prev and version_prev:
+                person["id_persistent"] = id_prev
+                person["version"] = version_prev
+            rsp = post_person(live_server.url, person)
+            assert rsp.status_code == 200
+            persons = rsp.json()
+            id_prev = persons["persons"][0]["id_persistent"]
+            version_prev = persons["persons"][0]["version"]
+    count = get_count(live_server.url)
+    assert count.json()["count"] == 10
+
+
+def test_bad_db(live_server):
+    mock = MagicMock()
+    mock.side_effect = IntegrityError()
+    with patch("cosmae.person.models_django.Person.get_count", mock):
+        rsp = get_count(live_server.url)
+    assert rsp.status_code == 500
+    assert rsp.json()["msg"] == "Could not count persons."

tests/person/django_models_test.py
@@ -1,0 +1,45 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+
+import pytest
+
+from tests.person import common as c
+from cosmae.entity.models_django import Entity
+from cosmae.person.models_django import Person
+
+
+@pytest.fixture
+def person():
+    return Person(
+        id_persistent=c.id_persistent_test,
+        names_family=c.names_family_test,
+        names_personal=c.names_personal_test,
+        display_txt=c.display_txt_test,
+        time_edit=c.time_edit_test,
+    )
+
+
+def test_create_person(person):
+    assert person.id_persistent == c.id_persistent_test
+    assert person.names_family == c.names_family_test
+    assert person.names_personal == c.names_personal_test
+    assert person.display_txt == c.display_txt_test
+    assert person.time_edit == c.time_edit_test
+
+
+@pytest.mark.django_db
+def test_store_and_retrieve_person(person):
+    person.save()
+    retrieved = Person.objects.get(names_family=c.names_family_test)
+    assert retrieved == person
+
+
+@pytest.mark.django_db
+def test_does_not_get_other_entities(person):
+    person.save()
+    entity = Entity(time_edit=c.time_edit_test)
+    entity.save()
+    persons = list(Person.objects.all())
+    assert len(persons) == 1
+    assert persons[0] == person
+    entities = list(Entity.objects.all())  # pylint: disable=no-member
+    assert len(entities) == 2

tests/util/django_test.py
@@ -1,0 +1,51 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+from datetime import datetime
+
+import pytest
+from django.db import IntegrityError
+
+from cosmae.entity.models_django import Entity
+from cosmae.util import django as du
+
+
+@pytest.mark.django_db
+def test_store_multiple():
+    entity_test_0 = Entity(
+        id_persistent="test_id_0", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+    entity_test_1 = Entity(
+        id_persistent="test_id_1", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+    entity_test_2 = Entity(
+        id_persistent="test_id_2", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+
+    entities_test = [entity_test_0, entity_test_1, entity_test_2]
+    du.save_many_atomic(entities_test)
+    assert Entity.get_count() == 3
+
+
+@pytest.mark.django_db
+def test_does_rollback():
+
+    entity_test_0 = Entity(
+        id_persistent="test_id_0", display_txt="foo", time_edit=datetime(2022, 11, 14)
+    )
+    entity_test_1 = Entity(
+        id_persistent="test_id_1",
+        display_txt="foo",
+        time_edit=datetime(2022, 11, 14),
+        previous_version=entity_test_0,
+    )
+    entity_test_2 = Entity(
+        id_persistent="test_id_2",
+        display_txt="foo",
+        time_edit=datetime(2022, 11, 14),
+        previous_version=entity_test_0,
+    )
+
+    entities_test = [entity_test_0, entity_test_1, entity_test_2]
+
+    with pytest.raises(IntegrityError):
+        du.save_many_atomic(entities_test)
+    assert Entity.get_count() == 0