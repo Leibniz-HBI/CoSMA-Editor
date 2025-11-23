Leibniz-HBI/DP-REX

Add view for most recent tag definition.

vran/contribution/tag_definition/queue/ingest.py
@@ -13,7 +13,11 @@ from vran.contribution.tag_definition.queue.util import read_csv_of_candidate
 from vran.entity.models_django import Entity
 from vran.exception import TagDefinitionExistsException
 from vran.merge_request.models_django import TagMergeRequest
-from vran.tag.models_django import TagDefinition, TagInstanceHistory
+from vran.tag.models_django import (
+    TagDefinition,
+    TagDefinitionHistory,
+    TagInstanceHistory,
+)
 
 
 def mk_display_txt_extractor(idx):
@@ -86,7 +90,10 @@ def ingest_values_from_csv(id_contribution_persistent):
                     id_tag_definition_origin_persistent = str(uuid4())
                     for idx in range(1, 10):
                         try:
-                            tag_definition_origin, _ = TagDefinition.change_or_create(
+                            (
+                                tag_definition_origin,
+                                _,
+                            ) = TagDefinitionHistory.change_or_create(
                                 id_persistent=id_tag_definition_origin_persistent,
                                 name=merge_request_name,
                                 id_parent_persistent=tag_definition_destination.id_persistent,

tests/conftest.py
@@ -14,7 +14,7 @@ from tests.user.api.integration.requests import post_login, post_register
 from vran.entity.models_django import Entity
 from vran.management.display_txt.util import DISPLAY_TXT_ORDER_CONFIG_KEY
 from vran.management.models_django import ConfigValue
-from vran.tag.models_django import TagDefinition
+from vran.tag.models_django import TagDefinition, TagDefinitionHistory
 from vran.util import VranUser
 
 
@@ -63,8 +63,8 @@ def entity2():
 
 @pytest.fixture()
 def tag_def(user):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
-        id_persistent=ct.id_tag_persistent_test,
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
+        id_persistent=ct.id_tag_def_persistent_test,
         name=ct.name_tag_def_test,
         time_edit=ct.time_edit_test,
         type=TagDefinition.STRING,
@@ -75,7 +75,7 @@ def tag_def(user):
 
 @pytest.fixture()
 def tag_def1(user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         id_persistent=ct.id_tag_def_persistent_test_user1,
         name=ct.name_tag_def_test1,
         time_edit=ct.time_edit_test1,
@@ -87,7 +87,7 @@ def tag_def1(user1):
 
 @pytest.fixture()
 def tag_def_curated():
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         id_persistent=ct.id_tag_def_curated_test,
         name=ct.name_tag_def_curated_test,
         time_edit=ct.time_edit_curated_test,

tests/contribution/entity/queue_test.py
@@ -10,7 +10,12 @@ import vran.contribution.entity.queue as q
 from vran.contribution.entity.models_django import EntityDuplicate
 from vran.contribution.models_django import ContributionCandidate
 from vran.entity.models_django import Entity
-from vran.tag.models_django import TagDefinition, TagInstance, TagInstanceHistory
+from vran.tag.models_django import (
+    TagDefinition,
+    TagDefinitionHistory,
+    TagInstance,
+    TagInstanceHistory,
+)
 
 
 @pytest.fixture()
@@ -66,7 +71,7 @@ def test_removes_contribution_candidate_from_others(entity_duplicate):
 
 @pytest.fixture
 def tag_def(user):
-    return TagDefinition.objects.create(  # pylint: disable = no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable = no-member
         id_persistent=c.id_tag_def_test,
         name=c.name_tag_def_test,
         type=TagDefinition.STRING,
@@ -77,7 +82,7 @@ def tag_def(user):
 
 @pytest.fixture
 def tag_def1(user):
-    return TagDefinition.objects.create(  # pylint: disable = no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable = no-member
         id_persistent=c.id_tag_def_test1,
         id_parent_persistent=c.id_tag_def_test,
         name=c.name_tag_def_test1,

tests/contribution/tag_definition/queue/ingest_test.py
@@ -12,7 +12,7 @@ from vran.contribution.tag_definition.models_django import TagDefinitionContribu
 from vran.contribution.tag_definition.queue.ingest import ingest_values_from_csv
 from vran.entity.models_django import Entity
 from vran.merge_request.models_django import TagMergeRequest
-from vran.tag.models_django import TagDefinition, TagInstance
+from vran.tag.models_django import TagDefinition, TagDefinitionHistory, TagInstance
 
 csv_cols = {
     "names": ["name_0", "name_1"],
@@ -47,7 +47,7 @@ def csv_mock_with_empty_lines():
 
 @pytest.fixture
 def verified_tag_def(db):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name="tag definition verified_test",
         id_parent_persistent=None,
         type=TagDefinition.INNER,
@@ -58,7 +58,7 @@ def verified_tag_def(db):
 
 @pytest.fixture
 def party_tag_def(db):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name="tag definition party test",
         id_parent_persistent=None,
         type=TagDefinition.STRING,

tests/merge_request/conftest.py
@@ -5,12 +5,16 @@ import tests.entity.common as ce
 import tests.merge_request.common as c
 from vran.contribution.models_django import ContributionCandidate
 from vran.merge_request.models_django import TagConflictResolution, TagMergeRequest
-from vran.tag.models_django import TagDefinition, TagInstanceHistory
+from vran.tag.models_django import (
+    TagDefinition,
+    TagDefinitionHistory,
+    TagInstanceHistory,
+)
 
 
 @pytest.fixture
 def destination_tag_def_for_mr(db, user):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_destination,
         id_persistent=c.id_persistent_tag_def_destination,
         type=TagDefinition.STRING,
@@ -21,7 +25,7 @@ def destination_tag_def_for_mr(db, user):
 
 @pytest.fixture
 def destination_tag_def_for_mr_changed(destination_tag_def_for_mr):
-    tag_def, _ = TagDefinition.change_or_create(
+    tag_def, _ = TagDefinitionHistory.change_or_create(
         id_persistent=destination_tag_def_for_mr.id_persistent,
         version=destination_tag_def_for_mr.id,
         name="changed tag definition test",
@@ -33,7 +37,7 @@ def destination_tag_def_for_mr_changed(destination_tag_def_for_mr):
 
 @pytest.fixture
 def destination_tag_def_for_mr_user1(db, user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_destination,
         id_persistent=c.id_persistent_tag_def_destination_fast_forward,
         type=TagDefinition.STRING,
@@ -44,7 +48,7 @@ def destination_tag_def_for_mr_user1(db, user1):
 
 @pytest.fixture
 def origin_tag_def_for_mr(db, user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_origin,
         id_persistent=c.id_persistent_tag_def_origin,
         type=TagDefinition.STRING,
@@ -55,7 +59,7 @@ def origin_tag_def_for_mr(db, user1):
 
 @pytest.fixture
 def origin_tag_def_for_mr_changed(origin_tag_def_for_mr):
-    tag_def, _ = TagDefinition.change_or_create(
+    tag_def, _ = TagDefinitionHistory.change_or_create(
         id_persistent=origin_tag_def_for_mr.id_persistent,
         version=origin_tag_def_for_mr.id,
         name="changed tag definition test",
@@ -109,10 +113,10 @@ def merge_request_user(
 
 @pytest.fixture
 def destination_tag_def_for_mr1(db, user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_destination1,
         id_persistent=c.id_persistent_tag_def_destination1,
-        type=TagDefinition.STRING,
+        type=TagDefinitionHistory.STRING,
         time_edit=c.time_tag_def_destination1,
         owner=user1,
     )
@@ -120,7 +124,7 @@ def destination_tag_def_for_mr1(db, user1):
 
 @pytest.fixture
 def origin_tag_def_for_mr1(db, user):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_origin1,
         id_persistent=c.id_persistent_tag_def_origin1,
         type=TagDefinition.STRING,

tests/merge_request/entity/models_django_test.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 from vran.merge_request.entity.models_django import EntityConflictResolution
-from vran.tag.models_django import TagDefinition
+from vran.tag.models_django import TagDefinition, TagDefinitionHistory
 
 
 def test_no_change_user(merge_request_user, conflict_resolution_replace, user):
@@ -345,7 +345,7 @@ def test_change_all(
     conflict_resolution_replace,
 ):
     old_tag_def = conflict_resolution_replace.tag_definition
-    TagDefinition.change_or_create(
+    TagDefinitionHistory.change_or_create(
         id_persistent=old_tag_def.id_persistent,
         time_edit=datetime(1912, 4, 7),
         name="edited tag definition",

tests/tag/models_django_instance_test.py
@@ -10,7 +10,7 @@ from vran.exception import (
     TagDefinitionMissingException,
     TagDefinitionPermissionException,
 )
-from vran.tag.models_django import TagDefinition, TagInstance, TagInstanceHistory
+from vran.tag.models_django import TagInstance, TagInstanceHistory
 
 
 @pytest.fixture
@@ -132,7 +132,6 @@ def test_tag_def_missing(entity0, user):
 @pytest.mark.django_db
 def test_tag_def_no_permission(entity0, tag_def_user, user1):
     entity0.save()
-    tag_def_user.save()
     with pytest.raises(TagDefinitionPermissionException) as exc:
         TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
@@ -142,42 +141,12 @@ def test_tag_def_no_permission(entity0, tag_def_user, user1):
             id_entity_persistent=entity0.id_persistent,
             id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
         )
-    assert exc.value.args[0] == tag_def_user
+    assert exc.value.args[0] == tag_def_user.id_persistent
 
 
 @pytest.mark.django_db
 def test_add_tag_root(entity0, tag_def_user):
     entity0.save()
-    tag_def_user.save()
-    ret, _ = TagInstanceHistory.change_or_create(
-        id_persistent=c.id_tag_persistent_test,
-        time_edit=c.time_edit_test,
-        user=tag_def_user.owner,
-        id_entity_persistent=entity0.id_persistent,
-        id_tag_definition_persistent=tag_def_user.id_persistent,
-        value="2.0",
-    )
-    assert ret.value == "2.0"
-    assert ret.id_persistent == c.id_tag_persistent_test
-    assert ret.id_entity_persistent == entity0.id_persistent
-    assert ret.id_tag_definition_persistent == tag_def_user.id_persistent
-    assert ret.time_edit == c.time_edit_test
-    assert ret.previous_version is None
-
-
-@pytest.mark.django_db
-def test_add_tag_child(entity0, tag_def_user):
-    parent_tag, _ = TagDefinition.change_or_create(
-        id_persistent=c.id_tag_def_parent_persistent_test,
-        id_parent_persistent=None,
-        time_edit=c.time_edit_test,
-        name=c.name_tag_def_test,
-        type=TagDefinition.INNER,
-    )
-    parent_tag.save()
-    entity0.save()
-    tag_def_user.id_parent_persistent = c.id_tag_def_parent_persistent_test
-    tag_def_user.save()
     ret, _ = TagInstanceHistory.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
@@ -196,14 +165,12 @@ def test_add_tag_child(entity0, tag_def_user):
 
 @pytest.mark.django_db
 def test_empty_chunk(tag_def):
-    tag_def.save()
     ret = TagInstance.by_tag_chunked(tag_def.id_persistent, 2, 3)
     assert ret == []
 
 
 @pytest.mark.django_db
 def test_chunk_versions(tag_def):
-    tag_def.save()
     last_values = []
     last_ids = []
     for i in range(10):
@@ -229,7 +196,6 @@ def test_chunk_versions(tag_def):
 
 @pytest.mark.django_db
 def test_chunk_filter_tag_instance(tag_def):
-    tag_def.save()
     for i in range(10):
         tag = TagInstanceHistory(
             id_persistent=f"id_tag_test{i}",

Leibniz-HBI/CoSMA-Editor

cosmae/contribution/tag_definition/queue/ingest.py
@@ -14,7 +14,11 @@ from cosmae.contribution.tag_definition.queue.util import read_csv_of_candidate
 from cosmae.entity.models_django import Entity
 from cosmae.exception import TagDefinitionExistsException
 from cosmae.merge_request.models_django import TagMergeRequest
-from cosmae.tag.models_django import TagDefinition, TagInstanceHistory
+from cosmae.tag.models_django import (
+    TagDefinition,
+    TagDefinitionHistory,
+    TagInstanceHistory,
+)
 
 
 def mk_display_txt_extractor(idx):
@@ -87,7 +91,10 @@ def ingest_values_from_csv(id_contribution_persistent):
                     id_tag_definition_origin_persistent = str(uuid4())
                     for idx in range(1, 10):
                         try:
-                            tag_definition_origin, _ = TagDefinition.change_or_create(
+                            (
+                                tag_definition_origin,
+                                _,
+                            ) = TagDefinitionHistory.change_or_create(
                                 id_persistent=id_tag_definition_origin_persistent,
                                 name=merge_request_name,
                                 id_parent_persistent=tag_definition_destination.id_persistent,

tests/conftest.py
@@ -14,7 +14,7 @@ from tests.user.api.integration.requests import post_login, post_register
 from cosmae.entity.models_django import Entity
 from cosmae.management.display_txt.util import DISPLAY_TXT_ORDER_CONFIG_KEY
 from cosmae.management.models_django import ConfigValue
-from cosmae.tag.models_django import TagDefinition
+from cosmae.tag.models_django import TagDefinition, TagDefinitionHistory
 from cosmae.util import CosmaeUser
 
 
@@ -63,8 +63,8 @@ def entity2():
 
 @pytest.fixture()
 def tag_def(user):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
-        id_persistent=ct.id_tag_persistent_test,
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
+        id_persistent=ct.id_tag_def_persistent_test,
         name=ct.name_tag_def_test,
         time_edit=ct.time_edit_test,
         type=TagDefinition.STRING,
@@ -75,7 +75,7 @@ def tag_def(user):
 
 @pytest.fixture()
 def tag_def1(user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         id_persistent=ct.id_tag_def_persistent_test_user1,
         name=ct.name_tag_def_test1,
         time_edit=ct.time_edit_test1,
@@ -87,7 +87,7 @@ def tag_def1(user1):
 
 @pytest.fixture()
 def tag_def_curated():
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         id_persistent=ct.id_tag_def_curated_test,
         name=ct.name_tag_def_curated_test,
         time_edit=ct.time_edit_curated_test,

tests/contribution/entity/queue_test.py
@@ -10,7 +10,12 @@ import cosmae.contribution.entity.queue as q
 from cosmae.contribution.entity.models_django import EntityDuplicate
 from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.entity.models_django import Entity
-from cosmae.tag.models_django import TagDefinition, TagInstance, TagInstanceHistory
+from cosmae.tag.models_django import (
+    TagDefinition,
+    TagDefinitionHistory,
+    TagInstance,
+    TagInstanceHistory,
+)
 
 
 @pytest.fixture()
@@ -66,7 +71,7 @@ def test_removes_contribution_candidate_from_others(entity_duplicate):
 
 @pytest.fixture
 def tag_def(user):
-    return TagDefinition.objects.create(  # pylint: disable = no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable = no-member
         id_persistent=c.id_tag_def_test,
         name=c.name_tag_def_test,
         type=TagDefinition.STRING,
@@ -77,7 +82,7 @@ def tag_def(user):
 
 @pytest.fixture
 def tag_def1(user):
-    return TagDefinition.objects.create(  # pylint: disable = no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable = no-member
         id_persistent=c.id_tag_def_test1,
         id_parent_persistent=c.id_tag_def_test,
         name=c.name_tag_def_test1,

tests/contribution/tag_definition/queue/ingest_test.py
@@ -12,7 +12,7 @@ from cosmae.contribution.tag_definition.models_django import TagDefinitionContri
 from cosmae.contribution.tag_definition.queue.ingest import ingest_values_from_csv
 from cosmae.entity.models_django import Entity
 from cosmae.merge_request.models_django import TagMergeRequest
-from cosmae.tag.models_django import TagDefinition, TagInstance
+from cosmae.tag.models_django import TagDefinition, TagDefinitionHistory, TagInstance
 
 csv_cols = {
     "names": ["name_0", "name_1"],
@@ -47,7 +47,7 @@ def csv_mock_with_empty_lines():
 
 @pytest.fixture
 def verified_tag_def(db):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name="tag definition verified_test",
         id_parent_persistent=None,
         type=TagDefinition.INNER,
@@ -58,7 +58,7 @@ def verified_tag_def(db):
 
 @pytest.fixture
 def party_tag_def(db):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name="tag definition party test",
         id_parent_persistent=None,
         type=TagDefinition.STRING,

tests/merge_request/conftest.py
@@ -5,12 +5,16 @@ import tests.entity.common as ce
 import tests.merge_request.common as c
 from cosmae.contribution.models_django import ContributionCandidate
 from cosmae.merge_request.models_django import TagConflictResolution, TagMergeRequest
-from cosmae.tag.models_django import TagDefinition, TagInstanceHistory
+from cosmae.tag.models_django import (
+    TagDefinition,
+    TagDefinitionHistory,
+    TagInstanceHistory,
+)
 
 
 @pytest.fixture
 def destination_tag_def_for_mr(db, user):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_destination,
         id_persistent=c.id_persistent_tag_def_destination,
         type=TagDefinition.STRING,
@@ -21,7 +25,7 @@ def destination_tag_def_for_mr(db, user):
 
 @pytest.fixture
 def destination_tag_def_for_mr_changed(destination_tag_def_for_mr):
-    tag_def, _ = TagDefinition.change_or_create(
+    tag_def, _ = TagDefinitionHistory.change_or_create(
         id_persistent=destination_tag_def_for_mr.id_persistent,
         version=destination_tag_def_for_mr.id,
         name="changed tag definition test",
@@ -33,7 +37,7 @@ def destination_tag_def_for_mr_changed(destination_tag_def_for_mr):
 
 @pytest.fixture
 def destination_tag_def_for_mr_user1(db, user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_destination,
         id_persistent=c.id_persistent_tag_def_destination_fast_forward,
         type=TagDefinition.STRING,
@@ -44,7 +48,7 @@ def destination_tag_def_for_mr_user1(db, user1):
 
 @pytest.fixture
 def origin_tag_def_for_mr(db, user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_origin,
         id_persistent=c.id_persistent_tag_def_origin,
         type=TagDefinition.STRING,
@@ -55,7 +59,7 @@ def origin_tag_def_for_mr(db, user1):
 
 @pytest.fixture
 def origin_tag_def_for_mr_changed(origin_tag_def_for_mr):
-    tag_def, _ = TagDefinition.change_or_create(
+    tag_def, _ = TagDefinitionHistory.change_or_create(
         id_persistent=origin_tag_def_for_mr.id_persistent,
         version=origin_tag_def_for_mr.id,
         name="changed tag definition test",
@@ -109,10 +113,10 @@ def merge_request_user(
 
 @pytest.fixture
 def destination_tag_def_for_mr1(db, user1):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_destination1,
         id_persistent=c.id_persistent_tag_def_destination1,
-        type=TagDefinition.STRING,
+        type=TagDefinitionHistory.STRING,
         time_edit=c.time_tag_def_destination1,
         owner=user1,
     )
@@ -120,7 +124,7 @@ def destination_tag_def_for_mr1(db, user1):
 
 @pytest.fixture
 def origin_tag_def_for_mr1(db, user):
-    return TagDefinition.objects.create(  # pylint: disable=no-member
+    return TagDefinitionHistory.objects.create(  # pylint: disable=no-member
         name=c.name_tag_def_origin1,
         id_persistent=c.id_persistent_tag_def_origin1,
         type=TagDefinition.STRING,

tests/merge_request/entity/models_django_test.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 from cosmae.merge_request.entity.models_django import EntityConflictResolution
-from cosmae.tag.models_django import TagDefinition
+from cosmae.tag.models_django import TagDefinition, TagDefinitionHistory
 
 
 def test_no_change_user(merge_request_user, conflict_resolution_replace, user):
@@ -345,7 +345,7 @@ def test_change_all(
     conflict_resolution_replace,
 ):
     old_tag_def = conflict_resolution_replace.tag_definition
-    TagDefinition.change_or_create(
+    TagDefinitionHistory.change_or_create(
         id_persistent=old_tag_def.id_persistent,
         time_edit=datetime(1912, 4, 7),
         name="edited tag definition",

tests/tag/models_django_instance_test.py
@@ -10,7 +10,7 @@ from cosmae.exception import (
     TagDefinitionMissingException,
     TagDefinitionPermissionException,
 )
-from cosmae.tag.models_django import TagDefinition, TagInstance, TagInstanceHistory
+from cosmae.tag.models_django import TagInstance, TagInstanceHistory
 
 
 @pytest.fixture
@@ -132,7 +132,6 @@ def test_tag_def_missing(entity0, user):
 @pytest.mark.django_db
 def test_tag_def_no_permission(entity0, tag_def_user, user1):
     entity0.save()
-    tag_def_user.save()
     with pytest.raises(TagDefinitionPermissionException) as exc:
         TagInstanceHistory.change_or_create(
             id_persistent=c.id_tag_persistent_test,
@@ -142,42 +141,12 @@ def test_tag_def_no_permission(entity0, tag_def_user, user1):
             id_entity_persistent=entity0.id_persistent,
             id_tag_definition_persistent=c.id_tag_def_persistent_test_user,
         )
-    assert exc.value.args[0] == tag_def_user
+    assert exc.value.args[0] == tag_def_user.id_persistent
 
 
 @pytest.mark.django_db
 def test_add_tag_root(entity0, tag_def_user):
     entity0.save()
-    tag_def_user.save()
-    ret, _ = TagInstanceHistory.change_or_create(
-        id_persistent=c.id_tag_persistent_test,
-        time_edit=c.time_edit_test,
-        user=tag_def_user.owner,
-        id_entity_persistent=entity0.id_persistent,
-        id_tag_definition_persistent=tag_def_user.id_persistent,
-        value="2.0",
-    )
-    assert ret.value == "2.0"
-    assert ret.id_persistent == c.id_tag_persistent_test
-    assert ret.id_entity_persistent == entity0.id_persistent
-    assert ret.id_tag_definition_persistent == tag_def_user.id_persistent
-    assert ret.time_edit == c.time_edit_test
-    assert ret.previous_version is None
-
-
-@pytest.mark.django_db
-def test_add_tag_child(entity0, tag_def_user):
-    parent_tag, _ = TagDefinition.change_or_create(
-        id_persistent=c.id_tag_def_parent_persistent_test,
-        id_parent_persistent=None,
-        time_edit=c.time_edit_test,
-        name=c.name_tag_def_test,
-        type=TagDefinition.INNER,
-    )
-    parent_tag.save()
-    entity0.save()
-    tag_def_user.id_parent_persistent = c.id_tag_def_parent_persistent_test
-    tag_def_user.save()
     ret, _ = TagInstanceHistory.change_or_create(
         id_persistent=c.id_tag_persistent_test,
         time_edit=c.time_edit_test,
@@ -196,14 +165,12 @@ def test_add_tag_child(entity0, tag_def_user):
 
 @pytest.mark.django_db
 def test_empty_chunk(tag_def):
-    tag_def.save()
     ret = TagInstance.by_tag_chunked(tag_def.id_persistent, 2, 3)
     assert ret == []
 
 
 @pytest.mark.django_db
 def test_chunk_versions(tag_def):
-    tag_def.save()
     last_values = []
     last_ids = []
     for i in range(10):
@@ -229,7 +196,6 @@ def test_chunk_versions(tag_def):
 
 @pytest.mark.django_db
 def test_chunk_filter_tag_instance(tag_def):
-    tag_def.save()
     for i in range(10):
         tag = TagInstanceHistory(
             id_persistent=f"id_tag_test{i}",