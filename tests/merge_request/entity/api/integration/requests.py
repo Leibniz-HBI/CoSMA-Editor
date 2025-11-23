Leibniz-HBI/DP-REX

move endpoint for getting entity merge requests.

tests/merge_request/entity/api/integration/requests.py
@@ -34,7 +34,7 @@ def post_reverse_origin_destination(url, id_merge_request_persistent, cookies=No
 
 def get_merge_requests(url, cookies=None):
     return requests.get(
-        url + "/vran/api/merge_requests/entities", cookies=cookies, timeout=900
+        url + "/vran/api/merge_requests/entities/all", cookies=cookies, timeout=900
     )
 
 

ui/src/merge_request/entity/__tests__/get_entity_merge_requests_integration.test.tsx
@@ -169,7 +169,7 @@ test('success', async () => {
     })
     expect(fetchMock.mock.calls).toEqual([
         [
-            'http://127.0.0.1:8000/vran/api/merge_requests/entities',
+            'http://127.0.0.1:8000/vran/api/merge_requests/entities/all',
             { credentials: 'include' }
         ]
     ])
@@ -204,7 +204,7 @@ test('error', async () => {
     })
     expect(fetchMock.mock.calls).toEqual([
         [
-            'http://127.0.0.1:8000/vran/api/merge_requests/entities',
+            'http://127.0.0.1:8000/vran/api/merge_requests/entities/all',
             { credentials: 'include' }
         ]
     ])

Leibniz-HBI/CoSMA-Editor

tests/merge_request/entity/api/integration/requests.py
@@ -34,7 +34,7 @@ def post_reverse_origin_destination(url, id_merge_request_persistent, cookies=No
 
 def get_merge_requests(url, cookies=None):
     return requests.get(
-        url + "/cosmae/api/merge_requests/entities", cookies=cookies, timeout=900
+        url + "/cosmae/api/merge_requests/entities/all", cookies=cookies, timeout=900
     )
 
 
@@ -86,7 +86,8 @@ def post_resolution(  # pylint: disable=too-many-arguments
 
 def post_start_merge(url, id_merge_request_persistent, cookies=None):
     return requests.post(
-        url + f"/cosmae/api/merge_requests/entities/{id_merge_request_persistent}/merge",
+        url
+        + f"/cosmae/api/merge_requests/entities/{id_merge_request_persistent}/merge",
         cookies=cookies,
         timeout=900,
     )

ui/src/merge_request/entity/__tests__/get_entity_merge_requests_integration.test.tsx
@@ -169,7 +169,7 @@ test('success', async () => {
     })
     expect(fetchMock.mock.calls).toEqual([
         [
-            'http://127.0.0.1:8000/cosmae/api/merge_requests/entities',
+            'http://127.0.0.1:8000/cosmae/api/merge_requests/entities/all',
             { credentials: 'include' }
         ]
     ])
@@ -204,7 +204,7 @@ test('error', async () => {
     })
     expect(fetchMock.mock.calls).toEqual([
         [
-            'http://127.0.0.1:8000/cosmae/api/merge_requests/entities',
+            'http://127.0.0.1:8000/cosmae/api/merge_requests/entities/all',
             { credentials: 'include' }
         ]
     ])