Leibniz-HBI/DP-REX

Hide tag definitions for merge requests in backend.

vran/migrations/0038_tagdefinitionhistory_disabled_and_more.py
@@ -1,0 +1,44 @@
+# Generated by Django 4.2.8 on 2024-02-15 10:55
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("vran", "0037_tagdefinition_and_more"),
+    ]
+
+    create_table_query = """
+            create view "vran_tagdefinition" as (
+                select *
+                from (
+                        select max("id") max_id
+                        from vran_tagdefinitionhistory vt
+                        group by id_persistent
+                    ) with_version
+                    left join  (
+                        select *
+                        from vran_tagdefinitionhistory vt1
+                    ) without_version
+                    on "with_version"."max_id"="without_version"."id"
+            )"""
+
+    drop_table_query = "drop view vran_tagdefinition"
+    operations = [
+        migrations.RunSQL(drop_table_query, reverse_sql=create_table_query),
+        migrations.AddField(
+            model_name="tagdefinitionhistory",
+            name="disabled",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name="tagmergerequest",
+            name="disable_origin_on_merge",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.RunSQL(
+            create_table_query,
+            reverse_sql=drop_table_query,
+        ),
+    ]

tests/merge_request/queue_test.py
@@ -2,7 +2,7 @@
 
 import vran.merge_request.queue as q
 from vran.merge_request.models_django import TagMergeRequest
-from vran.tag.models_django import TagInstance
+from vran.tag.models_django import TagDefinition, TagInstance
 
 
 def test_fast_forward_destination_empty(
@@ -14,6 +14,25 @@ def test_fast_forward_destination_empty(
         merge_request_user_fast_forward.created_by,
     )
     assert merge_request_after.state == TagMergeRequest.MERGED
+    assert not TagDefinition.most_recent_by_id(
+        merge_request_user_fast_forward.id_origin_persistent
+    ).disabled
+
+
+def test_fast_forward_destination_empty_with_disable(
+    merge_request_user_fast_forward_disable_origin, instances_merge_request_origin_user
+):
+    q.merge_request_fast_forward(
+        merge_request_user_fast_forward_disable_origin.id_persistent
+    )
+    merge_request_after = TagMergeRequest.by_id_persistent(
+        merge_request_user_fast_forward_disable_origin.id_persistent,
+        merge_request_user_fast_forward_disable_origin.created_by,
+    )
+    assert merge_request_after.state == TagMergeRequest.MERGED
+    assert TagDefinition.most_recent_by_id(
+        merge_request_user_fast_forward_disable_origin.id_origin_persistent
+    ).disabled
 
 
 def test_fast_forward_origin_empty(
@@ -82,6 +101,24 @@ def test_applies_resolutions(
     assert len(instances) == 1
     instance = instances[0]
     assert instance.value == "value origin 1"
+
+
+def test_applies_resolutions_disable_origin(merge_request_user_disable_origin):
+    q.merge_request_resolve_conflicts(merge_request_user_disable_origin.id_persistent)
+    merge_request = TagMergeRequest.by_id_persistent(
+        merge_request_user_disable_origin.id_persistent,
+        merge_request_user_disable_origin.assigned_to,
+    )
+    assert merge_request.state == TagMergeRequest.MERGED
+    instances = list(
+        TagInstance.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=merge_request_user_disable_origin.id_destination_persistent
+        )
+    )
+    assert len(instances) == 0
+    assert TagDefinition.most_recent_by_id(
+        merge_request_user_disable_origin.id_origin_persistent
+    ).disabled
 
 
 def test_incomplete_resolution_stays_open_keep(

tests/tag/api/integration/post_tag_def_children_test.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
 from unittest.mock import MagicMock, patch
 
 from django.db import DatabaseError
@@ -82,7 +82,14 @@ def test_multi_child(auth_server, root_tag_def, child_tag_def):
     assert sort_versioned(tag_definitions) == sort_versioned(child_tag_def_rsps)
 
 
-def test_multi_child_no_disabled(auth_server, root_tag_def, child_tag_def):
+def test_does_not_include_disabled(auth_server, tag_def_disabled):
+    live_server, cookies = auth_server
+    rsp = r.post_tag_def_children(live_server.url, None, cookies=cookies)
+    assert rsp.status_code == 200
+    assert rsp.json() == {"tag_definitions": []}
+
+
+def test_multi_child_include_hidden_for_owner(auth_server, root_tag_def, child_tag_def):
     live_server, cookies = auth_server
     req = r.post_tag_def(live_server.url, root_tag_def, cookies=cookies)
     assert req.status_code == 200
@@ -95,6 +102,7 @@ def test_multi_child_no_disabled(auth_server, root_tag_def, child_tag_def):
     )
     assert req.status_code == 200
     child_tag_def_rsps = req.json()["tag_definitions"]
+    assert len(child_tag_def_rsps) == 2
     req = r.post_tag_def_children(
         live_server.url, root_tag_def_rsp_id_persistent, cookies=cookies
     )
@@ -105,11 +113,35 @@ def test_multi_child_no_disabled(auth_server, root_tag_def, child_tag_def):
     )
 
 
+def test_multi_child_exclude_hidden_for_non_owner(
+    auth_server1, root_tag_def, child_tag_def
+):
+    live_server, cookies, cookies1 = auth_server1
+    req = r.post_tag_def(live_server.url, root_tag_def, cookies=cookies)
+    assert req.status_code == 200
+    root_tag_def_rsp_id_persistent = req.json()["tag_definitions"][0]["id_persistent"]
+    child_tag_def["id_parent_persistent"] = root_tag_def_rsp_id_persistent
+    child_tag_def1 = child_tag_def.copy()
+    child_tag_def1["hidden"] = True
+    req = r.post_tag_defs(
+        live_server.url, [child_tag_def, child_tag_def1], cookies=cookies
+    )
+    assert req.status_code == 200
+    child_tag_def_rsps = req.json()["tag_definitions"]
+    assert len(child_tag_def_rsps) == 2
+    req = r.post_tag_def_children(
+        live_server.url, root_tag_def_rsp_id_persistent, cookies=cookies1
+    )
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert_versioned(tag_definitions, [child_tag_def_rsps[0]])
+
+
 def test_bad_db(auth_server):
     live_server, cookies = auth_server
     mock = MagicMock()
     mock.side_effect = DatabaseError()
-    with patch("vran.tag.models_django.TagDefinition.most_recent_children", mock):
+    with patch("vran.tag.models_django.TagDefinition.children_query_set", mock):
         req = r.post_tag_def_children(live_server.url, None, cookies=cookies)
     assert req.status_code == 500
     assert req.json()["msg"] == "Database Error."

Leibniz-HBI/CoSMA-Editor

cosmae/migrations/0038_tagdefinitionhistory_disabled_and_more.py
@@ -1,0 +1,44 @@
+# Generated by Django 4.2.8 on 2024-02-15 10:55
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cosmae", "0037_tagdefinition_and_more"),
+    ]
+
+    create_table_query = """
+            create view "cosmae_tagdefinition" as (
+                select *
+                from (
+                        select max("id") max_id
+                        from cosmae_tagdefinitionhistory vt
+                        group by id_persistent
+                    ) with_version
+                    left join  (
+                        select *
+                        from cosmae_tagdefinitionhistory vt1
+                    ) without_version
+                    on "with_version"."max_id"="without_version"."id"
+            )"""
+
+    drop_table_query = "drop view cosmae_tagdefinition"
+    operations = [
+        migrations.RunSQL(drop_table_query, reverse_sql=create_table_query),
+        migrations.AddField(
+            model_name="tagdefinitionhistory",
+            name="disabled",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name="tagmergerequest",
+            name="disable_origin_on_merge",
+            field=models.BooleanField(default=False),
+        ),
+        migrations.RunSQL(
+            create_table_query,
+            reverse_sql=drop_table_query,
+        ),
+    ]

tests/merge_request/queue_test.py
@@ -2,7 +2,7 @@
 
 import cosmae.merge_request.queue as q
 from cosmae.merge_request.models_django import TagMergeRequest
-from cosmae.tag.models_django import TagInstance
+from cosmae.tag.models_django import TagDefinition, TagInstance
 
 
 def test_fast_forward_destination_empty(
@@ -14,6 +14,25 @@ def test_fast_forward_destination_empty(
         merge_request_user_fast_forward.created_by,
     )
     assert merge_request_after.state == TagMergeRequest.MERGED
+    assert not TagDefinition.most_recent_by_id(
+        merge_request_user_fast_forward.id_origin_persistent
+    ).disabled
+
+
+def test_fast_forward_destination_empty_with_disable(
+    merge_request_user_fast_forward_disable_origin, instances_merge_request_origin_user
+):
+    q.merge_request_fast_forward(
+        merge_request_user_fast_forward_disable_origin.id_persistent
+    )
+    merge_request_after = TagMergeRequest.by_id_persistent(
+        merge_request_user_fast_forward_disable_origin.id_persistent,
+        merge_request_user_fast_forward_disable_origin.created_by,
+    )
+    assert merge_request_after.state == TagMergeRequest.MERGED
+    assert TagDefinition.most_recent_by_id(
+        merge_request_user_fast_forward_disable_origin.id_origin_persistent
+    ).disabled
 
 
 def test_fast_forward_origin_empty(
@@ -82,6 +101,24 @@ def test_applies_resolutions(
     assert len(instances) == 1
     instance = instances[0]
     assert instance.value == "value origin 1"
+
+
+def test_applies_resolutions_disable_origin(merge_request_user_disable_origin):
+    q.merge_request_resolve_conflicts(merge_request_user_disable_origin.id_persistent)
+    merge_request = TagMergeRequest.by_id_persistent(
+        merge_request_user_disable_origin.id_persistent,
+        merge_request_user_disable_origin.assigned_to,
+    )
+    assert merge_request.state == TagMergeRequest.MERGED
+    instances = list(
+        TagInstance.objects.filter(  # pylint: disable=no-member
+            id_tag_definition_persistent=merge_request_user_disable_origin.id_destination_persistent
+        )
+    )
+    assert len(instances) == 0
+    assert TagDefinition.most_recent_by_id(
+        merge_request_user_disable_origin.id_origin_persistent
+    ).disabled
 
 
 def test_incomplete_resolution_stays_open_keep(

tests/tag/api/integration/post_tag_def_children_test.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name
+# pylint: disable=missing-module-docstring, missing-function-docstring,redefined-outer-name,invalid-name,unused-argument
 from unittest.mock import MagicMock, patch
 
 from django.db import DatabaseError
@@ -82,7 +82,14 @@ def test_multi_child(auth_server, root_tag_def, child_tag_def):
     assert sort_versioned(tag_definitions) == sort_versioned(child_tag_def_rsps)
 
 
-def test_multi_child_no_disabled(auth_server, root_tag_def, child_tag_def):
+def test_does_not_include_disabled(auth_server, tag_def_disabled):
+    live_server, cookies = auth_server
+    rsp = r.post_tag_def_children(live_server.url, None, cookies=cookies)
+    assert rsp.status_code == 200
+    assert rsp.json() == {"tag_definitions": []}
+
+
+def test_multi_child_include_hidden_for_owner(auth_server, root_tag_def, child_tag_def):
     live_server, cookies = auth_server
     req = r.post_tag_def(live_server.url, root_tag_def, cookies=cookies)
     assert req.status_code == 200
@@ -95,6 +102,7 @@ def test_multi_child_no_disabled(auth_server, root_tag_def, child_tag_def):
     )
     assert req.status_code == 200
     child_tag_def_rsps = req.json()["tag_definitions"]
+    assert len(child_tag_def_rsps) == 2
     req = r.post_tag_def_children(
         live_server.url, root_tag_def_rsp_id_persistent, cookies=cookies
     )
@@ -105,11 +113,35 @@ def test_multi_child_no_disabled(auth_server, root_tag_def, child_tag_def):
     )
 
 
+def test_multi_child_exclude_hidden_for_non_owner(
+    auth_server1, root_tag_def, child_tag_def
+):
+    live_server, cookies, cookies1 = auth_server1
+    req = r.post_tag_def(live_server.url, root_tag_def, cookies=cookies)
+    assert req.status_code == 200
+    root_tag_def_rsp_id_persistent = req.json()["tag_definitions"][0]["id_persistent"]
+    child_tag_def["id_parent_persistent"] = root_tag_def_rsp_id_persistent
+    child_tag_def1 = child_tag_def.copy()
+    child_tag_def1["hidden"] = True
+    req = r.post_tag_defs(
+        live_server.url, [child_tag_def, child_tag_def1], cookies=cookies
+    )
+    assert req.status_code == 200
+    child_tag_def_rsps = req.json()["tag_definitions"]
+    assert len(child_tag_def_rsps) == 2
+    req = r.post_tag_def_children(
+        live_server.url, root_tag_def_rsp_id_persistent, cookies=cookies1
+    )
+    assert req.status_code == 200
+    tag_definitions = req.json()["tag_definitions"]
+    assert_versioned(tag_definitions, [child_tag_def_rsps[0]])
+
+
 def test_bad_db(auth_server):
     live_server, cookies = auth_server
     mock = MagicMock()
     mock.side_effect = DatabaseError()
-    with patch("cosmae.tag.models_django.TagDefinition.most_recent_children", mock):
+    with patch("cosmae.tag.models_django.TagDefinition.children_query_set", mock):
         req = r.post_tag_def_children(live_server.url, None, cookies=cookies)
     assert req.status_code == 500
     assert req.json()["msg"] == "Database Error."