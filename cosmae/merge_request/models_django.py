Leibniz-HBI/DP-REX

Change tag merge requests when ownership changes.

vran/merge_request/models_django.py
@@ -80,6 +80,16 @@ class TagMergeRequest(AbstractMergeRequest):
         )
 
     @classmethod
+    def change_owner_for_tag_def(
+        cls, id_tag_definition_persistent: str, user: VranUser
+    ):
+        """Change the owner for all merge requests that have
+        a specific tag definition as destination."""
+        cls.objects.filter(  # pylint: disable=no-member
+            id_destination_persistent=id_tag_definition_persistent
+        ).update(assigned_to=user)
+
+    @classmethod
     def get_for_contribution_query_set(cls, id_contribution_persistent):
         "Get all tag merge requests for a contribution candidate."
         return cls.objects.filter(  # pylint: disable=no-member

vran/tag/api/permissions.py
@@ -7,6 +7,7 @@ from django.http import HttpRequest
 from ninja import Router, Schema
 
 from vran.exception import ApiError, NotAuthenticatedException
+from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.api.definitions import (
     tag_definition_db_dict_to_api,
     tag_definition_db_to_api,
@@ -169,6 +170,9 @@ def post_accept_ownership_request(
         if do_save:
             with transaction.atomic():
                 tag_definition_new.save()
+                TagMergeRequest.change_owner_for_tag_def(
+                    tag_definition_new.id_persistent, ownership_request.receiver
+                )
                 ownership_request.delete()
         return 200, tag_definition_db_to_api(tag_definition_new)
 

tests/tag/api/integration/post_accept_request_test.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 import tests.tag.api.integration.requests as req
 import tests.tag.common as c
 from vran.exception import NotAuthenticatedException
+from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.models_django import TagDefinition
 
 
@@ -55,3 +56,32 @@ def test_correct_user_curated(auth_server, ownership_request_curated):
     )
     assert tag_definition.owner == ownership_request_curated.receiver
     assert not tag_definition.curated
+
+
+def test_changes_owner_of_mrs(auth_server1, ownership_request_user, user_editor):
+    "Make sure that for existing merge requests the owner is changed"
+    server, _, cookies = auth_server1
+    id_mr_persistent = "ccc5e4bd-2db1-4c52-b21b-cd9d138c21ea"
+    TagMergeRequest.objects.create(  # pylint: disable=no-member
+        assigned_to=ownership_request_user.petitioner,
+        created_by=user_editor,
+        state=TagMergeRequest.OPEN,
+        id_origin_persistent="origin_for_test",
+        id_destination_persistent=ownership_request_user.id_tag_definition_persistent,
+        created_at=c.time_edit_test,
+        id_persistent=id_mr_persistent,
+    )
+    rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 200
+    tag_definition = TagDefinition.most_recent_by_id(
+        ownership_request_user.id_tag_definition_persistent
+    )
+    assert tag_definition.owner == ownership_request_user.receiver
+    assert (
+        TagMergeRequest.objects.filter(  # pylint: disable=no-member
+            id_persistent=id_mr_persistent
+        )
+        .get()
+        .assigned_to
+        == ownership_request_user.receiver
+    )

Leibniz-HBI/CoSMA-Editor

cosmae/merge_request/models_django.py
@@ -81,6 +81,16 @@ class TagMergeRequest(AbstractMergeRequest):
         )
 
     @classmethod
+    def change_owner_for_tag_def(
+        cls, id_tag_definition_persistent: str, user: CosmaeUser
+    ):
+        """Change the owner for all merge requests that have
+        a specific tag definition as destination."""
+        cls.objects.filter(  # pylint: disable=no-member
+            id_destination_persistent=id_tag_definition_persistent
+        ).update(assigned_to=user)
+
+    @classmethod
     def get_for_contribution_query_set(cls, id_contribution_persistent):
         "Get all tag merge requests for a contribution candidate."
         return cls.objects.filter(  # pylint: disable=no-member

cosmae/tag/api/permissions.py
@@ -1,4 +1,5 @@
 "API methods for changing tag definition permissions."
+
 from typing import List, Union
 from uuid import uuid4
 
@@ -7,6 +8,7 @@ from django.http import HttpRequest
 from ninja import Router, Schema
 
 from cosmae.exception import ApiError, NotAuthenticatedException
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.api.definitions import (
     tag_definition_db_dict_to_api,
     tag_definition_db_to_api,
@@ -169,6 +171,9 @@ def post_accept_ownership_request(
         if do_save:
             with transaction.atomic():
                 tag_definition_new.save()
+                TagMergeRequest.change_owner_for_tag_def(
+                    tag_definition_new.id_persistent, ownership_request.receiver
+                )
                 ownership_request.delete()
         return 200, tag_definition_db_to_api(tag_definition_new)
 

tests/tag/api/integration/post_accept_request_test.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 import tests.tag.api.integration.requests as req
 import tests.tag.common as c
 from cosmae.exception import NotAuthenticatedException
+from cosmae.merge_request.models_django import TagMergeRequest
 from cosmae.tag.models_django import TagDefinition
 
 
@@ -55,3 +56,32 @@ def test_correct_user_curated(auth_server, ownership_request_curated):
     )
     assert tag_definition.owner == ownership_request_curated.receiver
     assert not tag_definition.curated
+
+
+def test_changes_owner_of_mrs(auth_server1, ownership_request_user, user_editor):
+    "Make sure that for existing merge requests the owner is changed"
+    server, _, cookies = auth_server1
+    id_mr_persistent = "ccc5e4bd-2db1-4c52-b21b-cd9d138c21ea"
+    TagMergeRequest.objects.create(  # pylint: disable=no-member
+        assigned_to=ownership_request_user.petitioner,
+        created_by=user_editor,
+        state=TagMergeRequest.OPEN,
+        id_origin_persistent="origin_for_test",
+        id_destination_persistent=ownership_request_user.id_tag_definition_persistent,
+        created_at=c.time_edit_test,
+        id_persistent=id_mr_persistent,
+    )
+    rsp = req.post_accept(server.url, c.id_ownership_request_test, cookies=cookies)
+    assert rsp.status_code == 200
+    tag_definition = TagDefinition.most_recent_by_id(
+        ownership_request_user.id_tag_definition_persistent
+    )
+    assert tag_definition.owner == ownership_request_user.receiver
+    assert (
+        TagMergeRequest.objects.filter(  # pylint: disable=no-member
+            id_persistent=id_mr_persistent
+        )
+        .get()
+        .assigned_to
+        == ownership_request_user.receiver
+    )