Leibniz-HBI/DP-REX

Permission check for changing tag definitions in backend.

vran/tag/api/definitions.py
@@ -12,34 +12,24 @@ from vran.exception import (
     DbObjectExistsException,
     EntityUpdatedException,
     NoParentTagException,
+    NoSelfParentTagException,
     NotAuthenticatedException,
+    PermissionException,
     TagDefinitionExistsException,
     ValidationException,
 )
 from vran.tag.api.models_api import TagDefinitionResponse
+from vran.tag.api.models_conversion import (
+    _tag_type_mapping_api_to_db,
+    tag_definition_db_to_api,
+)
 from vran.tag.models_django import TagDefinition as TagDefinitionDb
 from vran.tag.models_django import TagDefinitionHistory as TagDefinitionHistoryDb
-from vran.tag.queue import (
-    get_tag_definition_name_path,
-    get_tag_definition_name_path_from_parts,
-)
+from vran.user.models_api.public import PublicUserInfo
 from vran.util import VranUser, timestamp
 from vran.util.auth import check_user
 
 router = Router()
-
-
-_tag_type_mapping_api_to_db = {
-    "INNER": TagDefinitionDb.INNER,
-    "FLOAT": TagDefinitionDb.FLOAT,
-    "STRING": TagDefinitionDb.STRING,
-}
-
-_tag_type_mapping_db_to_api = {
-    TagDefinitionDb.INNER: "INNER",
-    TagDefinitionDb.FLOAT: "FLOAT",
-    TagDefinitionDb.STRING: "STRING",
-}
 
 
 class TagDefinitionRequest(Schema):
@@ -50,7 +40,7 @@ class TagDefinitionRequest(Schema):
     name: str
     version: Optional[int]
     type: str
-    owner: Optional[str]
+    owner: Optional[PublicUserInfo]
     hidden: Optional[bool]
     disabled: Optional[bool]
 
@@ -81,7 +71,13 @@ class CurationPostRequest(Schema):
 
 
 @router.post(
-    "", response={200: TagDefinitionResponseList, 400: ApiError, 500: ApiError}
+    "",
+    response={
+        200: TagDefinitionResponseList,
+        400: ApiError,
+        403: ApiError,
+        500: ApiError,
+    },
 )
 def post_tag_definitions(
     request: HttpRequest, tag_definition_list: TagDefinitionRequestList
@@ -118,6 +114,10 @@ def post_tag_definitions(
             msg="There has been a concurrent modification to the tag definition "
             f"with id_persistent {exc.new_value.id_persistent}."
         )
+    except PermissionException:
+        return 403, ApiError(msg="Insufficient permissions")
+    except NoSelfParentTagException:
+        return 400, ApiError(msg="Can not set a tag definition as its own parent.")
     except KeyError as exc:
         return 400, ApiError(msg=f"Type {exc.args[0]} is not known.")
 
@@ -160,9 +160,10 @@ def post_get_tag_definition_children(
 
 
 def tag_definition_api_to_db(
-    tag_definition: TagDefinitionRequest, owner: VranUser, time_edit: datetime
+    tag_definition: TagDefinitionRequest, requester: VranUser, time_edit: datetime
 ) -> TagDefinitionDb:
     "Convert a tag definition from API to database model."
+    additional_values = {}
     if tag_definition.id_persistent:
         persistent_id = tag_definition.id_persistent
         if tag_definition.version is None:
@@ -176,6 +177,8 @@ def tag_definition_api_to_db(
                 f"Tag definition with name {tag_definition.name} "
                 "has version but no id_persistent."
             )
+        # new tag definition.
+        additional_values["owner_id"] = requester.id
         persistent_id = str(uuid4())
     return TagDefinitionHistoryDb.change_or_create(
         id_persistent=persistent_id,
@@ -184,52 +187,8 @@ def tag_definition_api_to_db(
         time_edit=time_edit,
         name=tag_definition.name,
         type=_tag_type_mapping_api_to_db[tag_definition.type],
-        owner=owner,
+        requester=requester,
         hidden=tag_definition.hidden or False,
         disabled=tag_definition.disabled or False,
-    )
-
-
-def tag_definition_db_to_api(tag_definition: TagDefinitionDb) -> TagDefinitionResponse:
-    "Convert a tag definition from database to API model."
-    owner = tag_definition.owner
-    if owner is None:
-        username = None
-    else:
-        username = owner.get_username()
-    return TagDefinitionResponse(
-        id_persistent=tag_definition.id_persistent,
-        id_parent_persistent=tag_definition.id_parent_persistent,
-        name=tag_definition.name,
-        name_path=get_tag_definition_name_path(tag_definition),
-        version=tag_definition.id,
-        type=_tag_type_mapping_db_to_api[tag_definition.type],
-        owner=username,
-        curated=tag_definition.curated,
-        hidden=tag_definition.hidden,
-        disabled=tag_definition.disabled,
-    )
-
-
-def tag_definition_db_dict_to_api(
-    tag_definition: TagDefinitionDb,
-) -> TagDefinitionResponse:
-    "Convert a tag definition from database to API model."
-    id_persistent = tag_definition["id_persistent"]
-    name = tag_definition["name"]
-    if tag_definition["owner"] is not None:
-        username = tag_definition["owner"]["username"]
-    else:
-        username = None
-    return TagDefinitionResponse(
-        id_persistent=id_persistent,
-        id_parent_persistent=tag_definition["id_parent_persistent"],
-        name=name,
-        name_path=get_tag_definition_name_path_from_parts(id_persistent, name),
-        version=tag_definition["id"],
-        type=_tag_type_mapping_db_to_api[tag_definition["type"]],
-        owner=username,
-        curated=tag_definition["curated"],
-        hidden=tag_definition["hidden"],
-        disabled=tag_definition["disabled"],
+        **additional_values,
     )

vran/tag/api/models_api.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 
 from ninja import Schema
 
+from vran.user.models_api.public import PublicUserInfo
+
 
 class TagDefinitionResponse(Schema):
     "API model for a tag definition as a response object."
@@ -13,7 +15,7 @@ class TagDefinitionResponse(Schema):
     name_path: List[str]
     version: int
     type: str
-    owner: Optional[str]
+    owner: Optional[PublicUserInfo]
     curated: bool
     hidden: bool
     disabled: bool

vran/user/models_api/login.py
@@ -1,10 +1,11 @@
-"Models for user API"
+"API models for login and registration."
 from typing import List, Union
 
 from ninja import Schema
 from pydantic import Field
 
 from vran.tag.api.models_api import TagDefinitionResponse
+from vran.user.models_api.public import PublicUserInfo
 
 
 class LoginRequest(Schema):
@@ -20,7 +21,7 @@ class LoginRequest(Schema):
 class LoginResponse(Schema):
     # pylint: disable=too-few-public-methods
     "API model for response to login requests"
-    user_name: str
+    username: str
     id_persistent: str
     names_personal: str
     names_family: str
@@ -36,17 +37,9 @@ class LoginResponseList(Schema):
     next_offset: int
 
 
-class PublicUserInfo(Schema):
-    # pylint: disable=too-few-public-methods
-    "API model for public user information."
-    user_name: str
-    id_persistent: str
-    permission_group: str
-
-
 class RegisterRequest(Schema):
     "API model for register requests."
-    user_name: str = Field(None, min_length=2, max_length=150)
+    username: str = Field(None, min_length=2, max_length=150)
     names_personal: str = Field(None, min_length=2, max_length=150)
     names_family: str = Field(None, min_length=2, max_length=150)
     email: str = Field(None, min_length=2, max_length=150)

vran/util/django.py
@@ -1,4 +1,5 @@
 """Utils for Django"""
+
 from functools import lru_cache
 from typing import Iterable, Optional, Type
 
@@ -7,7 +8,12 @@ from django.contrib.postgres.aggregates import JSONBAgg
 from django.db.models import Aggregate, JSONField, Model
 from django.db.transaction import atomic
 
-from vran.exception import DbObjectExistsException, EntityUpdatedException
+from vran.exception import (
+    DbObjectExistsException,
+    EntityUpdatedException,
+    PermissionException,
+)
+from vran.util import VranUser
 
 
 def save_many_atomic(models: Iterable[Model]):
@@ -20,7 +26,9 @@ def save_many_atomic(models: Iterable[Model]):
 def change_or_create_versioned(
     cls,
     id_persistent: str,
+    requester: VranUser,
     version: Optional[int] = None,
+    skip_write_check: bool = False,
     **kwargs,
 ):
     """Changes a versioned model instance in the database by adding a new version.
@@ -41,6 +49,11 @@ def change_or_create_versioned(
     if most_recent is None:
         new_values = {}
     else:
+        if not skip_write_check:
+            if hasattr(most_recent, "has_write_access"):
+                can_write = most_recent.has_write_access(requester)
+                if not can_write:
+                    raise PermissionException(id_persistent)
         new_values = {
             field.attname: getattr(most_recent, field.attname)
             for field in cls._meta.get_fields()  # pylint: disable=protected-access

Leibniz-HBI/CoSMA-Editor

cosmae/tag/api/definitions.py
@@ -13,34 +13,24 @@ from cosmae.exception import (
     DbObjectExistsException,
     EntityUpdatedException,
     NoParentTagException,
+    NoSelfParentTagException,
     NotAuthenticatedException,
+    PermissionException,
     TagDefinitionExistsException,
     ValidationException,
 )
 from cosmae.tag.api.models_api import TagDefinitionResponse
+from cosmae.tag.api.models_conversion import (
+    _tag_type_mapping_api_to_db,
+    tag_definition_db_to_api,
+)
 from cosmae.tag.models_django import TagDefinition as TagDefinitionDb
 from cosmae.tag.models_django import TagDefinitionHistory as TagDefinitionHistoryDb
-from cosmae.tag.queue import (
-    get_tag_definition_name_path,
-    get_tag_definition_name_path_from_parts,
-)
+from cosmae.user.models_api.public import PublicUserInfo
 from cosmae.util import CosmaeUser, timestamp
 from cosmae.util.auth import check_user
 
 router = Router()
-
-
-_tag_type_mapping_api_to_db = {
-    "INNER": TagDefinitionDb.INNER,
-    "FLOAT": TagDefinitionDb.FLOAT,
-    "STRING": TagDefinitionDb.STRING,
-}
-
-_tag_type_mapping_db_to_api = {
-    TagDefinitionDb.INNER: "INNER",
-    TagDefinitionDb.FLOAT: "FLOAT",
-    TagDefinitionDb.STRING: "STRING",
-}
 
 
 class TagDefinitionRequest(Schema):
@@ -51,7 +41,7 @@ class TagDefinitionRequest(Schema):
     name: str
     version: Optional[int]
     type: str
-    owner: Optional[str]
+    owner: Optional[PublicUserInfo]
     hidden: Optional[bool]
     disabled: Optional[bool]
 
@@ -86,7 +76,13 @@ class CurationPostRequest(Schema):
 
 
 @router.post(
-    "", response={200: TagDefinitionResponseList, 400: ApiError, 500: ApiError}
+    "",
+    response={
+        200: TagDefinitionResponseList,
+        400: ApiError,
+        403: ApiError,
+        500: ApiError,
+    },
 )
 def post_tag_definitions(
     request: HttpRequest, tag_definition_list: TagDefinitionRequestList
@@ -123,6 +119,10 @@ def post_tag_definitions(
             msg="There has been a concurrent modification to the tag definition "
             f"with id_persistent {exc.new_value.id_persistent}."
         )
+    except PermissionException:
+        return 403, ApiError(msg="Insufficient permissions")
+    except NoSelfParentTagException:
+        return 400, ApiError(msg="Can not set a tag definition as its own parent.")
     except KeyError as exc:
         return 400, ApiError(msg=f"Type {exc.args[0]} is not known.")
 
@@ -165,9 +165,10 @@ def post_get_tag_definition_children(
 
 
 def tag_definition_api_to_db(
-    tag_definition: TagDefinitionRequest, owner: CosmaeUser, time_edit: datetime
+    tag_definition: TagDefinitionRequest, requester: CosmaeUser, time_edit: datetime
 ) -> TagDefinitionDb:
     "Convert a tag definition from API to database model."
+    additional_values = {}
     if tag_definition.id_persistent:
         persistent_id = tag_definition.id_persistent
         if tag_definition.version is None:
@@ -181,6 +182,8 @@ def tag_definition_api_to_db(
                 f"Tag definition with name {tag_definition.name} "
                 "has version but no id_persistent."
             )
+        # new tag definition.
+        additional_values["owner_id"] = requester.id
         persistent_id = str(uuid4())
     return TagDefinitionHistoryDb.change_or_create(
         id_persistent=persistent_id,
@@ -189,52 +192,8 @@ def tag_definition_api_to_db(
         time_edit=time_edit,
         name=tag_definition.name,
         type=_tag_type_mapping_api_to_db[tag_definition.type],
-        owner=owner,
+        requester=requester,
         hidden=tag_definition.hidden or False,
         disabled=tag_definition.disabled or False,
-    )
-
-
-def tag_definition_db_to_api(tag_definition: TagDefinitionDb) -> TagDefinitionResponse:
-    "Convert a tag definition from database to API model."
-    owner = tag_definition.owner
-    if owner is None:
-        username = None
-    else:
-        username = owner.get_username()
-    return TagDefinitionResponse(
-        id_persistent=tag_definition.id_persistent,
-        id_parent_persistent=tag_definition.id_parent_persistent,
-        name=tag_definition.name,
-        name_path=get_tag_definition_name_path(tag_definition),
-        version=tag_definition.id,
-        type=_tag_type_mapping_db_to_api[tag_definition.type],
-        owner=username,
-        curated=tag_definition.curated,
-        hidden=tag_definition.hidden,
-        disabled=tag_definition.disabled,
-    )
-
-
-def tag_definition_db_dict_to_api(
-    tag_definition: TagDefinitionDb,
-) -> TagDefinitionResponse:
-    "Convert a tag definition from database to API model."
-    id_persistent = tag_definition["id_persistent"]
-    name = tag_definition["name"]
-    if tag_definition["owner"] is not None:
-        username = tag_definition["owner"]["username"]
-    else:
-        username = None
-    return TagDefinitionResponse(
-        id_persistent=id_persistent,
-        id_parent_persistent=tag_definition["id_parent_persistent"],
-        name=name,
-        name_path=get_tag_definition_name_path_from_parts(id_persistent, name),
-        version=tag_definition["id"],
-        type=_tag_type_mapping_db_to_api[tag_definition["type"]],
-        owner=username,
-        curated=tag_definition["curated"],
-        hidden=tag_definition["hidden"],
-        disabled=tag_definition["disabled"],
+        **additional_values,
     )

cosmae/tag/api/models_api.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 
 from ninja import Schema
 
+from cosmae.user.models_api.public import PublicUserInfo
+
 
 class TagDefinitionResponse(Schema):
     "API model for a tag definition as a response object."
@@ -13,7 +15,7 @@ class TagDefinitionResponse(Schema):
     name_path: List[str]
     version: int
     type: str
-    owner: Optional[str]
+    owner: Optional[PublicUserInfo]
     curated: bool
     hidden: bool
     disabled: bool

cosmae/user/models_api/login.py
@@ -1,10 +1,11 @@
-"Models for user API"
+"API models for login and registration."
 from typing import List, Union
 
 from ninja import Schema
 from pydantic import Field
 
 from cosmae.tag.api.models_api import TagDefinitionResponse
+from cosmae.user.models_api.public import PublicUserInfo
 
 
 class LoginRequest(Schema):
@@ -20,7 +21,7 @@ class LoginRequest(Schema):
 class LoginResponse(Schema):
     # pylint: disable=too-few-public-methods
     "API model for response to login requests"
-    user_name: str
+    username: str
     id_persistent: str
     names_personal: str
     names_family: str
@@ -36,17 +37,9 @@ class LoginResponseList(Schema):
     next_offset: int
 
 
-class PublicUserInfo(Schema):
-    # pylint: disable=too-few-public-methods
-    "API model for public user information."
-    user_name: str
-    id_persistent: str
-    permission_group: str
-
-
 class RegisterRequest(Schema):
     "API model for register requests."
-    user_name: str = Field(None, min_length=2, max_length=150)
+    username: str = Field(None, min_length=2, max_length=150)
     names_personal: str = Field(None, min_length=2, max_length=150)
     names_family: str = Field(None, min_length=2, max_length=150)
     email: str = Field(None, min_length=2, max_length=150)

cosmae/util/django.py
@@ -1,4 +1,5 @@
 """Utils for Django"""
+
 from functools import lru_cache
 from typing import Iterable, Optional, Type
 
@@ -7,7 +8,12 @@ from django.contrib.postgres.aggregates import JSONBAgg
 from django.db.models import Aggregate, JSONField, Model
 from django.db.transaction import atomic
 
-from cosmae.exception import DbObjectExistsException, EntityUpdatedException
+from cosmae.exception import (
+    DbObjectExistsException,
+    EntityUpdatedException,
+    PermissionException,
+)
+from cosmae.util import CosmaeUser
 
 
 def save_many_atomic(models: Iterable[Model]):
@@ -20,7 +26,9 @@ def save_many_atomic(models: Iterable[Model]):
 def change_or_create_versioned(
     cls,
     id_persistent: str,
+    requester: CosmaeUser,
     version: Optional[int] = None,
+    skip_write_check: bool = False,
     **kwargs,
 ):
     """Changes a versioned model instance in the database by adding a new version.
@@ -41,6 +49,11 @@ def change_or_create_versioned(
     if most_recent is None:
         new_values = {}
     else:
+        if not skip_write_check:
+            if hasattr(most_recent, "has_write_access"):
+                can_write = most_recent.has_write_access(requester)
+                if not can_write:
+                    raise PermissionException(id_persistent)
         new_values = {
             field.attname: getattr(most_recent, field.attname)
             for field in cls._meta.get_fields()  # pylint: disable=protected-access
@@ -72,6 +85,7 @@ def patch_from_dict(object_db, **kwargs):
 
 class JsonGroupArray(Aggregate):  # pylint: disable=abstract-method
     "Django JSON array aggregation for SQLite"
+
     function = "JSON_GROUP_ARRAY"
     output_field = JSONField()
     template = "%(function)s(%(distinct)s%(expressions)s)"