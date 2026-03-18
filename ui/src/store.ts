5801b8ae3a2359cc0ae8ca9b07dcd51ca9b7b897
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2026-03-18 11:50:31 +0100

    Send CSRF token for delete requests.

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -67,7 +67,7 @@ export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = ReturnType<typeof setupStore>
 export type AppDispatch = AppStore['dispatch']
 
-const modifyMethods = new Set(['POST', 'PUT', 'PATCH', 'DELETE '])
+const modifyMethods = new Set(['POST', 'PUT', 'PATCH', 'DELETE'])
 function csrfRequestInterceptor(request: Request) {
     if (modifyMethods.has(request.method)) {
         const csrfToken = getCookie('csrftoken')
